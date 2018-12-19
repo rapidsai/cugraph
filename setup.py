9719b118f2a2835973d3121a3380b43084ef666b
Dante Gama Dessavre <danteg@nvidia.com>
2018-12-19 14:23:23 -0600

    FIX Added cudf header folder path

--- setup.py
+++ setup.py
@@ -1,24 +1,9 @@
-#
-# Copyright (c) 2018, NVIDIA CORPORATION.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-
 from setuptools import setup
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy
+import os
+import sys
 
 from distutils.sysconfig import get_python_lib
 
@@ -33,20 +18,23 @@ try:
 except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
+# temporary fix. cudf 0.5 will have a cudf.get_include()
+cudf_include = os.path.normpath(sys.prefix) + '/include'
+
 cython_files = ['python/pagerank/pagerank_wrapper.pyx']
 
 extensions = [
     Extension("cugraph",
               sources=cython_files,
               include_dirs=[numpy_include,
-                            '/usr/local/include/gdf',
+                            cudf_include,
                             'src',
                             'include',
                             '../gunrock',
                             '../gunrock/externals/moderngpu/include',
                             '../gunrock/externals/cub'],
               library_dirs=[get_python_lib()],
-              libraries=['cugraph'],
+              libraries=['cugraph', 'cudf'],
               language='c++',
               extra_compile_args=['-std=c++11'])
 ]
