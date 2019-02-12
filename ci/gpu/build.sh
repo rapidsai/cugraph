2b1e0ca51ba8ee1cec26700bcc949f59efbf7f3c
Raymond Douglass <ray@raydouglass.com>
2019-02-12 17:03:49 -0500

    BLD Initial gpuCI scripts

---
+++ ci/gpu/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018, NVIDIA CORPORATION.
+##########################################
+# cuGraph GPU build & testscript for CI  #
+##########################################
+set -e
+
+exit 1
