97421273058c2edbebc2206d35e76d5b78478211
Seunghwa Kang <seunghwak@nvidia.com>
2019-02-28 23:21:15 -0800

    temporarily suppress deprecation warning (python 3.7) in importing networkx

--- python/cugraph/louvain/test_louvain.py
+++ python/cugraph/louvain/test_louvain.py
@@ -13,13 +13,22 @@
 
 import time
 
-import community
-import networkx as nx
 import pytest
 from scipy.io import mmread
 
 import cudf
 import cugraph
+
+# Temporarily suppress warnings till networkX fixes deprecation warnings
+# (Using or importing the ABCs from 'collections' instead of from
+# 'collections.abc' is deprecated, and in 3.8 it will stop working) for
+# python 3.7.  Also, these import community and import networkx need to be
+# relocated in the third-party group once this gets fixed.
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    import community
+    import networkx as nx
 
 
 print('Networkx version : {} '.format(nx.__version__))
