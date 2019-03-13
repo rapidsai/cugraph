7c1467581706e8fd44c50d05b2f383341c409703
Kumar Aatish <kaatish@nvidia.com>
2019-03-13 16:44:43 -0400

    rmat documentation

--- cpp/include/functions.h
+++ cpp/include/functions.h
@@ -150,6 +150,38 @@ gdf_error gdf_delete_transpose(gdf_graph *graph);
 /* ----------------------------------------------------------------------------*/
 gdf_error gdf_pagerank(gdf_graph *graph, gdf_column *pagerank, float alpha, float tolerance, int max_iter, bool has_guess);
 
+/**
+ * @Synopsis   Creates source, destination and value columns based on rmat input
+ *
+ * @Param[in] *argv                  String that accepts the following arguments
+ *                                   rmat (default: rmat_scale = 10, a = 0.57, b = c = 0.19)
+ *                                               Generate R-MAT graph as input
+ *                                               --rmat_scale=<vertex-scale>
+ *                                               --rmat_nodes=<number-nodes>
+ *                                               --rmat_edgefactor=<edge-factor>
+ *                                               --rmat_edges=<number-edges>
+ *                                               --rmat_a=<factor> --rmat_b=<factor> --rmat_c=<factor>
+ *                                               --rmat_self_loops If this option is supplied, then self loops will be retained
+ *                                               --rmat_undirected If this option is not mentioned, then the graps will be undirected
+ *                                       Optional arguments:
+ *                                       [--device=<device_index>] Set GPU(s) for testing (Default: 0).
+ *                                       [--quiet]                 No output (unless --json is specified).
+ *                                       [--random_seed]           This will enable usage of random seed, else it will use same seed
+ *                                       [--normalized]\n
+ *
+ * @Param[out] &vertices             Number of vertices in the generated edge list
+ *
+ * @Param[out] &edges                Number of edges in the generated edge list
+ *
+ * @Param[out] *src                  Columns containing the sources
+ *
+ * @Param[out] *dst                  Columns containing the destinations
+ *
+ * @Param[out] *val                  Columns containing the edge weights
+ *
+ * @Returns                          GDF_SUCCESS upon successful completion.
+ */
+/* ----------------------------------------------------------------------------*/
 gdf_error gdf_grmat_gen (const char* argv, size_t &vertices, size_t &edges, gdf_column* src, gdf_column* dest, gdf_column* val);
 
 /**
