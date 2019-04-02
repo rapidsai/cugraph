18174c4dd680f143d9d10b637c866469c9312e75
afender <afender@nvidia.com>
2019-04-02 11:54:32 -0500

    Added cusp as submodule and removed 645 files. Closes #198

--- cpp/nvgraph/external/cusp/testing/lcov.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Generate filtered LCOV docs
-lcov --capture --directory . --output-file coverage.info &&\
-lcov --extract coverage.info "*/cusp/*" -o filtered_coverage.info &&\
-coveralls-lcov --repo-token $REPO_TOKEN filtered_coverage.info
-# genhtml filtered_coverage.info --output-directory coverage_html
