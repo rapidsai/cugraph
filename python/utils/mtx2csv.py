f3a16b435dad4fa0693c6d68e591ff1846c503aa
afender <afender@nvidia.com>
2019-04-08 11:21:17 -0500

    improvements after review

--- python/utils/mtx2csv.py
+++ python/utils/mtx2csv.py
@@ -8,17 +8,18 @@ parser = argparse.ArgumentParser(description='Convert the sparsity pattern \
                                  of a MatrixMarket file into a CSV file. \
                                  Each directed edge is explicitely stored, \
                                  edges are unsorted, IDs are 0-based.')
-parser.add_argument('file', type=str,
+parser.add_argument('file', type=argparse.FileType(),
                     help='Path to the MatrixMarket file')
 parser.add_argument('--csv_separator_name', type=str, default="space",
+                    choices=["space","tab","comma"],
                     help='csv separator can be : \
                     space, tab or comma. Default is space')
 args = parser.parse_args()
 
 # Read
-print('Reading ' + str(args.file) + '...')
+print('Reading ' + str(args.file.name) + '...')
 t1 = time.time()
-M = mmread(args.file).asfptype()
+M = mmread(args.file.name).asfptype()
 read_time = time.time()-t1
 print('Time (s) : ' + str(round(read_time, 3)))
 
@@ -35,10 +36,10 @@ else:
 
 # Write
 print('Writing CSV file: '
-      + os.path.splitext(os.path.basename(args.file))[0] + '.csv ...')
+      + os.path.splitext(os.path.basename(args.file.name))[0] + '.csv ...')
 t1 = time.time()
-os.path.splitext(os.path.basename(args.file))[0] + '.csv'
-csv_file = open(os.path.splitext(os.path.basename(args.file))[0] + '.csv', "w")
+os.path.splitext(os.path.basename(args.file.name))[0] + '.csv'
+csv_file = open(os.path.splitext(os.path.basename(args.file.name))[0] + '.csv', "w")
 for item in range(M.getnnz()):
     csv_file.write("{}{}{}\n".format(M.row[item], separator, M.col[item]))
 csv_file.close()
