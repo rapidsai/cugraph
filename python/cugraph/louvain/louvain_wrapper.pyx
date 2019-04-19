dea90dc820f7d99d115e0f324e64cf96d6cbc23c
Seunghwa Kang <seunghwak@nvidia.com>
2019-04-18 23:51:05 -0700

    fixed a warning in louvain_wrapper.pyx (dereferencing type-punned pointer will break strict-aliasing rules [-Wstrict-aliasing])

--- python/cugraph/louvain/louvain_wrapper.pyx
+++ python/cugraph/louvain/louvain_wrapper.pyx
@@ -25,9 +25,9 @@ cpdef nvLouvain(input_graph):
 
     Parameters
     ----------
-    graph : cuGraph.Graph                 
+    input_graph : cuGraph.Graph
       cuGraph graph descriptor, should contain the connectivity information as an edge list (edge weights are not used for this algorithm).
-      The adjacency list will be computed if not already present.   
+      The adjacency list will be computed if not already present.
 
     Returns
     -------
@@ -43,7 +43,7 @@ cpdef nvLouvain(input_graph):
     >>> destinations = cudf.Series(M.col)
     >>> G = cuGraph.Graph()
     >>> G.add_edge_list(sources,destinations,None)
-    >>> louvain_parts = cuGraph.louvain(G)
+    >>> louvain_parts, modularity_score = cuGraph.louvain(G)
     """
 
     cdef uintptr_t graph = input_graph.graph_ptr
@@ -62,17 +62,37 @@ cpdef nvLouvain(input_graph):
     
     df['partition'] = cudf.Series(np.zeros(n,dtype=np.int32))
     cdef gdf_column c_louvain_parts_col = get_gdf_column_view(df['partition'])
-    cdef double final_modularity = 1.0
-    cdef int num_level
 
-    err = gdf_louvain(<gdf_graph*>g, <void*>&final_modularity, <void*>&num_level, &c_louvain_parts_col)
-    cudf.bindings.cudf_cpp.check_gdf_error(err)
-
-    cdef double fm = final_modularity
-    cdef float tmp = (<float*>(<void*>&final_modularity))[0]
+    cdef bool single_precision = False
+    # this implementation is tied to cugraph.cu line 503
+    # cudaDataType_t val_type = graph->adjList->edge_data?
+    #     gdf_to_cudadtype(graph->adjList->edge_data): CUDA_R_32F;
+    # this is tied to the low-level implementation detail of the lower level
+    # function, and very vulnerable to low level changes. Better be
+    # reimplemented, but we are planning to eventually remove nvgraph, so I may
+    # leave as is right at this moment.
     if g.adjList.edge_data:
         if g.adjList.edge_data.dtype == GDF_FLOAT32:
-            fm = tmp
+            single_precision = True;
     else:
-        fm = tmp
-    return df, fm
+        single_precision = True;
+
+    cdef float final_modularity_single_precision = 1.0
+    cdef double final_modularity_double_precision = 1.0
+    cdef int num_level = 0
+    cdef gdf_error error
+
+    if single_precision:
+        err = gdf_louvain(<gdf_graph*>g,
+                          <void*>&final_modularity_single_precision,
+                          <void*>&num_level, &c_louvain_parts_col)
+    else:
+        err = gdf_louvain(<gdf_graph*>g,
+                          <void*>&final_modularity_double_precision,
+                          <void*>&num_level, &c_louvain_parts_col)
+    cudf.bindings.cudf_cpp.check_gdf_error(err)
+
+    if single_precision:
+        return df, <double>final_modularity_single_precision
+    else:
+        return df, final_modularity_double_precision
