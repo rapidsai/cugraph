fb0217acca21c28a67f852082547f0ff1db5ecab
afender <afender@nvidia.com>
2019-04-25 17:26:22 -0500

    improved the s3 script to set the dataset path

--- datasets/get_test_data.sh
+++ datasets/get_test_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo Downloading ...
 mkdir tmp
 cd tmp
 wget https://s3.us-east-2.amazonaws.com/gpuci/cugraph/test/datasets.tgz
@@ -10,8 +11,12 @@ cd ..
 mkdir test
 mkdir test/ref
 
+echo Decompressing ...
 tar xvzf tmp/datasets.tar.gz -C test
 tar xvzf tmp/pagerank.tar.gz -C test/ref
 tar xvzf tmp/sssp.tar.gz -C test/ref
 
 rm -rf tmp
+
+export RAPIDS_DATASET_ROOT_DIR=$PWD
+echo RAPIDS_DATASET_ROOT_DIR was set to $PWD
