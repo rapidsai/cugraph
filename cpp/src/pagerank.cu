f1afc14d186565fc6e4c3c255e6cff737b07627e
Seunghwa Kang <seunghwak@nvidia.com>
2019-05-03 17:39:12 -0700

    fixed inconsistent indentations

--- cpp/src/pagerank.cu
+++ cpp/src/pagerank.cu
@@ -13,7 +13,7 @@
 // Author: Alex Fender afender@nvidia.com
 
 #include <stdio.h>
-#include <stdlib.h>  
+#include <stdlib.h>
 #include <stddef.h>
 #include <string>
  #include <sstream>
@@ -29,21 +29,20 @@
 
 namespace cugraph
 {
-  
+
 #ifdef DEBUG
   #define PR_VERBOSE
 #endif
 template <typename IndexType, typename ValueType>
-bool  pagerankIteration( IndexType n, IndexType e, IndexType *cscPtr, IndexType *cscInd,ValueType *cscVal,
-                                     ValueType alpha, ValueType *a, ValueType *b, float tolerance, int iter, int max_iter, 
-                                     ValueType * &tmp,  void* cub_d_temp_storage, size_t  cub_temp_storage_bytes, 
-                                     ValueType * &pr, ValueType *residual) {
-    
+bool pagerankIteration(IndexType n, IndexType e, IndexType *cscPtr, IndexType *cscInd,ValueType *cscVal,
+                       ValueType alpha, ValueType *a, ValueType *b, float tolerance, int iter, int max_iter,
+                       ValueType * &tmp,  void* cub_d_temp_storage, size_t  cub_temp_storage_bytes,
+                       ValueType * &pr, ValueType *residual) {
     ValueType  dot_res;
     cub::DeviceSpmv::CsrMV(cub_d_temp_storage, cub_temp_storage_bytes, cscVal,
         cscPtr, cscInd, tmp, pr,
         n, n, e);
-   
+
     scal(n, alpha, pr);
     dot_res = dot( n, a, tmp);
     axpy(n, dot_res,  b,  pr);
@@ -70,18 +69,18 @@ bool  pagerankIteration( IndexType n, IndexType e, IndexType *cscPtr, IndexType 
 }
 
 template <typename IndexType, typename ValueType>
-int pagerank (  IndexType n, IndexType e, IndexType *cscPtr, IndexType *cscInd, ValueType *cscVal,
-                       ValueType alpha, ValueType *a, bool has_guess, float tolerance, int max_iter, 
-                       ValueType * &pagerank_vector, ValueType * &residual) {
+int pagerank(IndexType n, IndexType e, IndexType *cscPtr, IndexType *cscInd, ValueType *cscVal,
+             ValueType alpha, ValueType *a, bool has_guess, float tolerance, int max_iter,
+             ValueType * &pagerank_vector, ValueType * &residual) {
   int max_it, i = 0 ;
   float tol;
   bool converged = false;
-  ValueType randomProbability =  static_cast<ValueType>( 1.0/n);
+  ValueType randomProbability = static_cast<ValueType>( 1.0/n);
   ValueType *b=0, *tmp=0;
-  void*    cub_d_temp_storage = NULL;
-  size_t   cub_temp_storage_bytes = 0;
+  void* cub_d_temp_storage = NULL;
+  size_t cub_temp_storage_bytes = 0;
 
-  if (max_iter > 0 )
+  if (max_iter > 0)
       max_it = max_iter;
   else
       max_it =  500;
@@ -94,15 +93,15 @@ int pagerank (  IndexType n, IndexType e, IndexType *cscPtr, IndexType *cscInd, 
       return -1;
 
   if (alpha <= 0.0f || alpha >= 1.0f)
-          return -1;
+      return -1;
 
   cudaStream_t stream{nullptr};
-	
-  ALLOC_MANAGED_TRY ((void**)&b,    sizeof(ValueType) * n, stream);
-  ALLOC_MANAGED_TRY ((void**)&tmp,    sizeof(ValueType) * n, stream);
+
+  ALLOC_MANAGED_TRY((void**)&b, sizeof(ValueType) * n, stream);
+  ALLOC_MANAGED_TRY((void**)&tmp, sizeof(ValueType) * n, stream);
   cudaCheckError();
 
-  if (!has_guess)  {
+  if (!has_guess) {
        fill(n, pagerank_vector, randomProbability);
        fill(n, tmp, randomProbability);
   }
@@ -110,49 +109,48 @@ int pagerank (  IndexType n, IndexType e, IndexType *cscPtr, IndexType *cscInd, 
     copy(n, pagerank_vector, tmp);
   }
 
-
   fill(n, b, randomProbability);
   update_dangling_nodes(n, a, alpha);
 
   cub::DeviceSpmv::CsrMV(cub_d_temp_storage, cub_temp_storage_bytes, cscVal,
-                                             cscPtr, cscInd, tmp, pagerank_vector, n, n, e);
+                         cscPtr, cscInd, tmp, pagerank_vector, n, n, e);
    // Allocate temporary storage
- ALLOC_MANAGED_TRY ((void**)&cub_d_temp_storage, cub_temp_storage_bytes, stream);
- cudaCheckError()
-  #ifdef PR_VERBOSE
-      std::stringstream ss;
-      ss.str(std::string());
-      ss <<" ------------------PageRank------------------"<< std::endl;
-      ss <<" --------------------------------------------"<< std::endl;
-      ss << std::setw(10) << "Iteration" << std::setw(15) << "Residual" << std::endl;
-      ss <<" --------------------------------------------"<< std::endl;
-      std::cout<<ss.str();
-  #endif
+  ALLOC_MANAGED_TRY ((void**)&cub_d_temp_storage, cub_temp_storage_bytes, stream);
+  cudaCheckError()
+#ifdef PR_VERBOSE
+  std::stringstream ss;
+  ss.str(std::string());
+  ss <<" ------------------PageRank------------------"<< std::endl;
+  ss <<" --------------------------------------------"<< std::endl;
+  ss << std::setw(10) << "Iteration" << std::setw(15) << "Residual" << std::endl;
+  ss <<" --------------------------------------------"<< std::endl;
+  std::cout<<ss.str();
+#endif
 
   while (!converged && i < max_it)
-  { 
+  {
       i++;
       converged = pagerankIteration(n, e, cscPtr, cscInd, cscVal,
-                                           alpha, a, b, tol, i, max_it, tmp, 
-                                           cub_d_temp_storage, cub_temp_storage_bytes, 
-                                           pagerank_vector, residual);
-       #ifdef PR_VERBOSE
-          ss.str(std::string());
-          ss << std::setw(10) << i ;
-          ss.precision(3);
-          ss << std::setw(15) << std::scientific << *residual  << std::endl;
-          std::cout<<ss.str();
-      #endif
+                                    alpha, a, b, tol, i, max_it, tmp,
+                                    cub_d_temp_storage, cub_temp_storage_bytes,
+                                    pagerank_vector, residual);
+#ifdef PR_VERBOSE
+      ss.str(std::string());
+      ss << std::setw(10) << i ;
+      ss.precision(3);
+      ss << std::setw(15) << std::scientific << *residual  << std::endl;
+      std::cout<<ss.str();
+#endif
   }
   #ifdef PR_VERBOSE
-      std::cout <<" --------------------------------------------"<< std::endl;
+  std::cout <<" --------------------------------------------"<< std::endl;
   #endif
   //printv(n,pagerank_vector,0);
 
-  ALLOC_FREE_TRY(b, stream);  
+  ALLOC_FREE_TRY(b, stream);
   ALLOC_FREE_TRY(tmp, stream);
-  ALLOC_FREE_TRY(cub_d_temp_storage, stream);    
-  
+  ALLOC_FREE_TRY(cub_d_temp_storage, stream);
+
   return converged ? 0 : 1;
 }
 
