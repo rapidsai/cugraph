4213a93bd6db43c35040839b5a5ea43ff17e1e60
Kumar Aatish <kaatish@nvidia.com>
2019-05-08 10:38:59 -0700

    Moved structure functions

--- cpp/src/cugraph.cu
+++ cpp/src/cugraph.cu
@@ -64,60 +64,6 @@ void gdf_col_release(gdf_column* col) {
   delete col;
 }
 
-void cpy_column_view(const gdf_column *in, gdf_column *out) {
-  if (in != nullptr && out !=nullptr) {
-    gdf_column_view(out, in->data, in->valid, in->size, in->dtype);
-  }
-}
-
-gdf_error gdf_adj_list_view(gdf_graph *graph, const gdf_column *offsets,
-                                 const gdf_column *indices, const gdf_column *edge_data) {
-  //This function returns an error if this graph object has at least one graph
-  //representation to prevent a single object storing two different graphs.
-  GDF_REQUIRE( ((graph->edgeList == nullptr) && (graph->adjList == nullptr) &&
-    (graph->transposedAdjList == nullptr)), GDF_INVALID_API_CALL);
-  GDF_REQUIRE( offsets->null_count == 0 , GDF_VALIDITY_UNSUPPORTED );
-  GDF_REQUIRE( indices->null_count == 0 , GDF_VALIDITY_UNSUPPORTED );
-  GDF_REQUIRE( (offsets->dtype == indices->dtype), GDF_UNSUPPORTED_DTYPE );
-  GDF_REQUIRE( ((offsets->dtype == GDF_INT32) || (offsets->dtype == GDF_INT64)), GDF_UNSUPPORTED_DTYPE );
-  GDF_REQUIRE( (offsets->size > 0), GDF_DATASET_EMPTY );
-
-  graph->adjList = new gdf_adj_list;
-  graph->adjList->offsets = new gdf_column;
-  graph->adjList->indices = new gdf_column;
-  graph->adjList->ownership = 0;
-
-  cpy_column_view(offsets, graph->adjList->offsets);
-  cpy_column_view(indices, graph->adjList->indices);
-  if (edge_data) {
-      GDF_REQUIRE( indices->size == edge_data->size, GDF_COLUMN_SIZE_MISMATCH );
-      graph->adjList->edge_data = new gdf_column;
-      cpy_column_view(edge_data, graph->adjList->edge_data);
-  }
-  else {
-    graph->adjList->edge_data = nullptr;
-  }
-  return GDF_SUCCESS;
-}
-
-gdf_error gdf_adj_list::get_vertex_identifiers(gdf_column *identifiers) {
-  GDF_REQUIRE( offsets != nullptr , GDF_INVALID_API_CALL);
-  GDF_REQUIRE( offsets->data != nullptr , GDF_INVALID_API_CALL);
-  cugraph::sequence<int>((int)offsets->size-1, (int*)identifiers->data);
-  return GDF_SUCCESS;
-}
-
-gdf_error gdf_adj_list::get_source_indices (gdf_column *src_indices) {
-  GDF_REQUIRE( offsets != nullptr , GDF_INVALID_API_CALL);
-  GDF_REQUIRE( offsets->data != nullptr , GDF_INVALID_API_CALL);
-  GDF_REQUIRE( src_indices->size == indices->size, GDF_COLUMN_SIZE_MISMATCH );
-  GDF_REQUIRE( src_indices->dtype == indices->dtype, GDF_UNSUPPORTED_DTYPE );
-  GDF_REQUIRE( src_indices->size > 0, GDF_DATASET_EMPTY );
-  cugraph::offsets_to_indices<int>((int*)offsets->data, offsets->size-1, (int*)src_indices->data);
-
-  return GDF_SUCCESS;
-}
-
 gdf_error gdf_renumber_vertices(const gdf_column *src, const gdf_column *dst,
 				gdf_column *src_renumbered, gdf_column *dst_renumbered,
 				gdf_column *numbering_map) {
@@ -125,7 +71,7 @@ gdf_error gdf_renumber_vertices(const gdf_column *src, const gdf_column *dst,
   GDF_REQUIRE( src->size == dst->size, GDF_COLUMN_SIZE_MISMATCH );
   GDF_REQUIRE( src->dtype == dst->dtype, GDF_UNSUPPORTED_DTYPE );
   GDF_REQUIRE( ((src->dtype == GDF_INT32) || (src->dtype == GDF_INT64)), GDF_UNSUPPORTED_DTYPE );
-  GDF_REQUIRE( src->size > 0, GDF_DATASET_EMPTY ); 
+  GDF_REQUIRE( src->size > 0, GDF_DATASET_EMPTY );
 
   //
   //  TODO: we're currently renumbering without using valid.  We need to
@@ -153,7 +99,7 @@ gdf_error gdf_renumber_vertices(const gdf_column *src, const gdf_column *dst,
 
     ALLOC_MANAGED_TRY((void**) &tmp, sizeof(int32_t) * src->size, stream);
     gdf_column_view(src_renumbered, tmp, src->valid, src->size, src->dtype);
-  
+
     ALLOC_MANAGED_TRY((void**) &tmp, sizeof(int32_t) * src->size, stream);
     gdf_column_view(dst_renumbered, tmp, dst->valid, dst->size, dst->dtype);
 
@@ -182,7 +128,7 @@ gdf_error gdf_renumber_vertices(const gdf_column *src, const gdf_column *dst,
     int64_t *tmp;
     ALLOC_MANAGED_TRY((void**) &tmp, sizeof(int32_t) * src->size, stream);
     gdf_column_view(src_renumbered, tmp, src->valid, src->size, GDF_INT32);
-  
+
     ALLOC_MANAGED_TRY((void**) &tmp, sizeof(int32_t) * src->size, stream);
     gdf_column_view(dst_renumbered, tmp, dst->valid, dst->size, GDF_INT32);
 
@@ -211,143 +157,6 @@ gdf_error gdf_renumber_vertices(const gdf_column *src, const gdf_column *dst,
   }
 
   return GDF_SUCCESS;
-}
-
-gdf_error gdf_edge_list_view(gdf_graph *graph, const gdf_column *src_indices,
-                                 const gdf_column *dest_indices, const gdf_column *edge_data) {
-  //This function returns an error if this graph object has at least one graph
-  //representation to prevent a single object storing two different graphs.
-  GDF_REQUIRE( ((graph->edgeList == nullptr) && (graph->adjList == nullptr) &&
-    (graph->transposedAdjList == nullptr)), GDF_INVALID_API_CALL);
-  GDF_REQUIRE( src_indices->size == dest_indices->size, GDF_COLUMN_SIZE_MISMATCH );
-  GDF_REQUIRE( src_indices->dtype == dest_indices->dtype, GDF_UNSUPPORTED_DTYPE );
-  GDF_REQUIRE( ((src_indices->dtype == GDF_INT32) || (src_indices->dtype == GDF_INT64)), GDF_UNSUPPORTED_DTYPE );
-  GDF_REQUIRE( src_indices->size > 0, GDF_DATASET_EMPTY );
-  GDF_REQUIRE( src_indices->null_count == 0 , GDF_VALIDITY_UNSUPPORTED );
-  GDF_REQUIRE( dest_indices->null_count == 0 , GDF_VALIDITY_UNSUPPORTED );
-
-  graph->edgeList = new gdf_edge_list;
-  graph->edgeList->src_indices = new gdf_column;
-  graph->edgeList->dest_indices = new gdf_column;
-  graph->edgeList->ownership = 0;
-
-  cpy_column_view(src_indices, graph->edgeList->src_indices);
-  cpy_column_view(dest_indices, graph->edgeList->dest_indices);
-  if (edge_data) {
-      GDF_REQUIRE( src_indices->size == edge_data->size, GDF_COLUMN_SIZE_MISMATCH );
-      graph->edgeList->edge_data = new gdf_column;
-      cpy_column_view(edge_data, graph->edgeList->edge_data);
-  }
-  else {
-    graph->edgeList->edge_data = nullptr;
-  }
-
-  return GDF_SUCCESS;
-}
-
-template <typename T, typename WT>
-gdf_error gdf_add_adj_list_impl (gdf_graph *graph) {
-    if (graph->adjList == nullptr) {
-      GDF_REQUIRE( graph->edgeList != nullptr , GDF_INVALID_API_CALL);
-      int nnz = graph->edgeList->src_indices->size, status = 0;
-      graph->adjList = new gdf_adj_list;
-      graph->adjList->offsets = new gdf_column;
-      graph->adjList->indices = new gdf_column;
-      graph->adjList->ownership = 1;
-
-    if (graph->edgeList->edge_data!= nullptr) {
-      graph->adjList->edge_data = new gdf_column;
-
-      CSR_Result_Weighted<int32_t,WT> adj_list;
-      status = ConvertCOOtoCSR_weighted((int*)graph->edgeList->src_indices->data, (int*)graph->edgeList->dest_indices->data, (WT*)graph->edgeList->edge_data->data, nnz, adj_list);
-
-      gdf_column_view(graph->adjList->offsets, adj_list.rowOffsets,
-                            nullptr, adj_list.size+1, graph->edgeList->src_indices->dtype);
-      gdf_column_view(graph->adjList->indices, adj_list.colIndices,
-                            nullptr, adj_list.nnz, graph->edgeList->src_indices->dtype);
-      gdf_column_view(graph->adjList->edge_data, adj_list.edgeWeights,
-                          nullptr, adj_list.nnz, graph->edgeList->edge_data->dtype);
-    }
-    else {
-      CSR_Result<int> adj_list;
-      status = ConvertCOOtoCSR((int*)graph->edgeList->src_indices->data,(int*)graph->edgeList->dest_indices->data, nnz, adj_list);
-      gdf_column_view(graph->adjList->offsets, adj_list.rowOffsets,
-                            nullptr, adj_list.size+1, graph->edgeList->src_indices->dtype);
-      gdf_column_view(graph->adjList->indices, adj_list.colIndices,
-                            nullptr, adj_list.nnz, graph->edgeList->src_indices->dtype);
-    }
-    if (status !=0) {
-      std::cerr << "Could not generate the adj_list" << std::endl;
-      return GDF_CUDA_ERROR;
-    }
-  }
-  return GDF_SUCCESS;
-}
-
-gdf_error gdf_add_edge_list (gdf_graph *graph) {
-    if (graph->edgeList == nullptr) {
-      GDF_REQUIRE( graph->adjList != nullptr , GDF_INVALID_API_CALL);
-      int *d_src;
-      graph->edgeList = new gdf_edge_list;
-      graph->edgeList->src_indices = new gdf_column;
-      graph->edgeList->dest_indices = new gdf_column;
-      graph->edgeList->ownership = 2;
-
-      CUDA_TRY(cudaMallocManaged ((void**)&d_src, sizeof(int) * graph->adjList->indices->size));
-
-      cugraph::offsets_to_indices<int>((int*)graph->adjList->offsets->data,
-                                  graph->adjList->offsets->size-1,
-                                  (int*)d_src);
-
-      gdf_column_view(graph->edgeList->src_indices, d_src,
-                      nullptr, graph->adjList->indices->size, graph->adjList->indices->dtype);
-      cpy_column_view(graph->adjList->indices, graph->edgeList->dest_indices);
-
-      if (graph->adjList->edge_data != nullptr) {
-        graph->edgeList->edge_data = new gdf_column;
-        cpy_column_view(graph->adjList->edge_data, graph->edgeList->edge_data);
-      }
-  }
-  return GDF_SUCCESS;
-}
-
-
-template <typename WT>
-gdf_error gdf_add_transposed_adj_list_impl (gdf_graph *graph) {
-    if (graph->transposedAdjList == nullptr ) {
-      GDF_REQUIRE( graph->edgeList != nullptr , GDF_INVALID_API_CALL);
-      int nnz = graph->edgeList->src_indices->size, status = 0;
-      graph->transposedAdjList = new gdf_adj_list;
-      graph->transposedAdjList->offsets = new gdf_column;
-      graph->transposedAdjList->indices = new gdf_column;
-      graph->transposedAdjList->ownership = 1;
-
-      if (graph->edgeList->edge_data) {
-        graph->transposedAdjList->edge_data = new gdf_column;
-        CSR_Result_Weighted<int32_t,WT> adj_list;
-        status = ConvertCOOtoCSR_weighted( (int*)graph->edgeList->dest_indices->data, (int*)graph->edgeList->src_indices->data, (WT*)graph->edgeList->edge_data->data, nnz, adj_list);
-        gdf_column_view(graph->transposedAdjList->offsets, adj_list.rowOffsets,
-                              nullptr, adj_list.size+1, graph->edgeList->src_indices->dtype);
-        gdf_column_view(graph->transposedAdjList->indices, adj_list.colIndices,
-                              nullptr, adj_list.nnz, graph->edgeList->src_indices->dtype);
-        gdf_column_view(graph->transposedAdjList->edge_data, adj_list.edgeWeights,
-                            nullptr, adj_list.nnz, graph->edgeList->edge_data->dtype);
-      }
-      else {
-
-        CSR_Result<int> adj_list;
-        status = ConvertCOOtoCSR((int*)graph->edgeList->dest_indices->data, (int*)graph->edgeList->src_indices->data, nnz, adj_list);
-        gdf_column_view(graph->transposedAdjList->offsets, adj_list.rowOffsets,
-                              nullptr, adj_list.size+1, graph->edgeList->src_indices->dtype);
-        gdf_column_view(graph->transposedAdjList->indices, adj_list.colIndices,
-                              nullptr, adj_list.nnz, graph->edgeList->src_indices->dtype);
-      }
-      if (status !=0) {
-        std::cerr << "Could not generate the adj_list" << std::endl;
-        return GDF_CUDA_ERROR;
-      }
-    }
-    return GDF_SUCCESS;
 }
 
 gdf_error gdf_degree_impl(int n, int e, gdf_column* col_ptr, gdf_column* degree, bool offsets) {
@@ -475,68 +284,6 @@ gdf_error gdf_pagerank_impl (gdf_graph *graph,
   return GDF_SUCCESS;
 }
 
-gdf_error gdf_add_adj_list(gdf_graph *graph) {
-  if (graph->adjList != nullptr)
-    return GDF_SUCCESS;
-
-  GDF_REQUIRE( graph->edgeList != nullptr , GDF_INVALID_API_CALL);
-  GDF_REQUIRE( graph->edgeList->src_indices->dtype == GDF_INT32, GDF_UNSUPPORTED_DTYPE );
-
-  if (graph->edgeList->edge_data != nullptr) {
-    switch (graph->edgeList->edge_data->dtype) {
-      case GDF_FLOAT32:   return gdf_add_adj_list_impl<int32_t, float>(graph);
-      case GDF_FLOAT64:   return gdf_add_adj_list_impl<int32_t, double>(graph);
-      default: return GDF_UNSUPPORTED_DTYPE;
-    }
-  }
-  else {
-    return gdf_add_adj_list_impl<int32_t, float>(graph);
-  }
-}
-
-gdf_error gdf_add_transposed_adj_list(gdf_graph *graph) {
-  if (graph->edgeList == nullptr)
-    gdf_add_edge_list(graph);
-
-  GDF_REQUIRE(graph->edgeList->src_indices->dtype == GDF_INT32, GDF_UNSUPPORTED_DTYPE);
-  GDF_REQUIRE(graph->edgeList->dest_indices->dtype == GDF_INT32, GDF_UNSUPPORTED_DTYPE);
-
-  if (graph->edgeList->edge_data != nullptr) {
-    switch (graph->edgeList->edge_data->dtype) {
-      case GDF_FLOAT32:   return gdf_add_transposed_adj_list_impl<float>(graph);
-      case GDF_FLOAT64:   return gdf_add_transposed_adj_list_impl<double>(graph);
-      default: return GDF_UNSUPPORTED_DTYPE;
-    }
-  }
-  else {
-    return gdf_add_transposed_adj_list_impl<float>(graph);
-  }
-}
-
-gdf_error gdf_delete_adj_list(gdf_graph *graph) {
-  if (graph->adjList) {
-    delete graph->adjList;
-  }
-  graph->adjList = nullptr;
-  return GDF_SUCCESS;
-}
-
-gdf_error gdf_delete_edge_list(gdf_graph *graph) {
-  if (graph->edgeList) {
-    delete graph->edgeList;
-  }
-  graph->edgeList = nullptr;
-  return GDF_SUCCESS;
-}
-
-gdf_error gdf_delete_transposed_adj_list(gdf_graph *graph) {
-  if (graph->transposedAdjList) {
-    delete graph->transposedAdjList;
-  }
-  graph->transposedAdjList = nullptr;
-  return GDF_SUCCESS;
-}
-
 gdf_error gdf_pagerank(gdf_graph *graph, gdf_column *pagerank, float alpha, float tolerance, int max_iter, bool has_guess) {
   //
   //  page rank operates on CSR and can't currently support 64-bit integers.
@@ -631,7 +378,7 @@ gdf_error gdf_louvain(gdf_graph *graph, void *final_modularity, void *num_level,
 
 template <typename idx_t,typename val_t>
 gdf_error gdf_snmg_csrmv_impl (size_t * part_offsets, gdf_column * off, gdf_column * ind, gdf_column * val, gdf_column ** x_cols){
-  
+
   GDF_REQUIRE( part_offsets != nullptr, GDF_INVALID_API_CALL );
   GDF_REQUIRE( off != nullptr, GDF_INVALID_API_CALL );
   GDF_REQUIRE( ind != nullptr, GDF_INVALID_API_CALL );
@@ -640,9 +387,9 @@ gdf_error gdf_snmg_csrmv_impl (size_t * part_offsets, gdf_column * off, gdf_colu
   GDF_REQUIRE( off->size > 0, GDF_INVALID_API_CALL );
   GDF_REQUIRE( ind->size > 0, GDF_INVALID_API_CALL );
   GDF_REQUIRE( val->size > 0, GDF_INVALID_API_CALL );
-  GDF_REQUIRE( ind->size == val->size, GDF_COLUMN_SIZE_MISMATCH ); 
-  GDF_REQUIRE( off->dtype == ind->dtype, GDF_UNSUPPORTED_DTYPE );  
-  GDF_REQUIRE( off->null_count + ind->null_count + val->null_count == 0 , GDF_VALIDITY_UNSUPPORTED );                 
+  GDF_REQUIRE( ind->size == val->size, GDF_COLUMN_SIZE_MISMATCH );
+  GDF_REQUIRE( off->dtype == ind->dtype, GDF_UNSUPPORTED_DTYPE );
+  GDF_REQUIRE( off->null_count + ind->null_count + val->null_count == 0 , GDF_VALIDITY_UNSUPPORTED );
 
   gdf_error status;
   auto p = omp_get_num_threads();
@@ -655,9 +402,9 @@ gdf_error gdf_snmg_csrmv_impl (size_t * part_offsets, gdf_column * off, gdf_colu
     x[i]= static_cast<val_t*>(x_cols[i]->data);
   }
   status = cugraph::snmg_csrmv<idx_t,val_t>(part_offsets,
-                                      static_cast<idx_t*>(off->data), 
-                                      static_cast<idx_t*>(ind->data), 
-                                      static_cast<val_t*>(val->data), 
+                                      static_cast<idx_t*>(off->data),
+                                      static_cast<idx_t*>(ind->data),
+                                      static_cast<val_t*>(val->data),
                                       x);
   return status;
 }
