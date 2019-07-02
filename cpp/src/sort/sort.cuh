b9cc722ad7bbd5330cf8adeec1fecfb8c5e3987c
Chuck Hastings <charlesh@nvidia.com>
2019-07-02 10:19:53 -0400

    updates to comments based on PR feedback

--- cpp/src/sort/sort.cuh
+++ cpp/src/sort/sort.cuh
@@ -13,19 +13,36 @@ namespace cusort {
    *   around multiple GPUs, redistributes them so that GPU 0 contains
    *   the smallest elements, GPU 1 the next smallest elements, etc.
    *
-   * @param[in]   d_input_keys     The unsorted keys, stored in device arrays.
-   *                               input_keys_d[i] is the array of keys on GPU i
-   * @param[in]   d_input_values   The unsorted values, stored in device arrays.
-   *                               input_values_d[i] is the array of values on GPU i
-   * @param[in]   h_input_length   Host array containing the number of elements on
-   *                               each GPU in the input key/value arrays.
-   * @param[out]  d_output_keys    The sorted keys, stored in device arrays.
-   *                               output_keys_d[i] is the array of keys on GPU i
-   * @param[out]  d_output_values  The sorted values, stored in device arrays.
-   *                               output_values_d[i] is the array of values on GPU i
-   * @param[out]  h_output_length  Host array containing the number of elements on
-   *                               each GPU in the output key/value arrays.
-   * @param[in]   num_gpus         The number of GPUs
+   *   The sort function should be called from a serial region of code.
+   *   it executes multiple openmp parallel regions to execute functions
+   *   on each GPU.
+   *
+   *   This function will be more efficient if each GPU has been configured
+   *   to allow peer access to every other GPU.
+   *
+   *   The device arrays in d_output_keys and d_output_values are
+   *   allocated by this function - since the ultimate partitioning of
+   *   the output cannot be known a priori.
+   *
+   * @param[in]   d_input_keys               The unsorted keys, stored in
+   *                                         device arrays. input_keys_d[i]
+   *                                         is the array of keys on GPU i
+   * @param[in]   d_input_values             The unsorted values, stored in
+   *                                         device arrays. input_values_d[i]
+   *                                         is the array of values on GPU i
+   * @param[in]   h_input_partition_offset   Host array containing the number
+   *                                         of elements on each GPU in the
+   *                                         input key/value arrays.
+   * @param[out]  d_output_keys              The sorted keys, stored in device
+   *                                         arrays. output_keys_d[i] is the
+   *                                         array of keys on GPU i
+   * @param[out]  d_output_values            The sorted values, stored in
+   *                                         device arrays. output_values_d[i]
+   *                                         is the array of values on GPU i
+   * @param[out]  h_output_partition_offset  Host array containing the number
+   *                                         of elements on each GPU in the
+   *                                         output key/value arrays.
+   * @param[in]   num_gpus                   The number of GPUs
    *
    * @return   GDF_SUCCESS upon successful completion
    */
@@ -48,15 +65,30 @@ namespace cusort {
    *   around multiple GPUs, redistributes them so that GPU 0 contains
    *   the smallest elements, GPU 1 the next smallest elements, etc.
    *
-   * @param[in]   d_input_keys     The unsorted keys, stored in device arrays.
-   *                               input_keys_d[i] is the array of keys on GPU i
-   * @param[in]   h_input_length   Host array containing the number of elements on
-   *                               each GPU in the input key/value arrays.
-   * @param[out]  d_output_keys    The sorted keys, stored in device arrays.
-   *                               output_keys_d[i] is the array of keys on GPU i
-   * @param[out]  h_output_length  Host array containing the number of elements on
-   *                               each GPU in the output key/value arrays.
-   * @param[in]   num_gpus         The number of GPUs
+   *   The sort function should be called from a serial region of code.
+   *   it executes multiple openmp parallel regions to execute functions
+   *   on each GPU.
+   *
+   *   This function will be more efficient if each GPU has been configured
+   *   to allow peer access to every other GPU.
+   *
+   *   The device arrays in d_output_keys and d_output_values are
+   *   allocated by this function - since the ultimate partitioning of
+   *   the output cannot be known a priori.
+   *
+   * @param[in]   d_input_keys               The unsorted keys, stored in
+   *                                         device arrays. input_keys_d[i]
+   *                                         is the array of keys on GPU i
+   * @param[in]   h_input_partition_offset   Host array containing the number
+   *                                         of elements on each GPU in the
+   *                                         input key/value arrays.
+   * @param[out]  d_output_keys              The sorted keys, stored in device
+   *                                         arrays. output_keys_d[i] is the
+   *                                         array of keys on GPU i
+   * @param[out]  h_output_partition_offset  Host array containing the number
+   *                                         of elements on each GPU in the
+   *                                         output key/value arrays.
+   * @param[in]   num_gpus                   The number of GPUs
    *
    * @return   GDF_SUCCESS upon successful completion
    */
