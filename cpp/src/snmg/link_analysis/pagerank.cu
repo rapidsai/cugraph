b306fb512a9645c7c978197d8ffd8c042189e830
Ishika Roy <iroy@exp01.aselab.nvidia.com>
2019-08-05 12:50:37 -0700

    add device sync

--- cpp/src/snmg/link_analysis/pagerank.cu
+++ cpp/src/snmg/link_analysis/pagerank.cu
@@ -126,6 +126,7 @@ void SNMGpagerank<IndexType,ValueType>::solve (int max_iter, ValueType ** pagera
     ValueType one = 1.0;
     ValueType *pr = pagerank[id];
     fill(v_glob, pagerank[id], one/v_glob);
+    cudaDeviceSynchronize();
     dot_res = dot( v_glob, bookmark, pr);
     SNMGcsrmv<IndexType,ValueType> spmv_solver(env, part_off, off, ind, val, pagerank);
     for (auto i = 0; i < max_iter; ++i) {
