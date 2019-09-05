03f065614e8d15d873b319eda14e06977b16f847
Charles Hastings <charlesh@nvidia.com>
2019-09-05 07:22:06 -0700

    Address issues from PR review
     - add testing for values
     - add configuration information so documentation will be generated
     - can't use drop_duplicates with weights and get a consistent answer,
       so change implementation to use groupby

--- python/cugraph/structure/symmetrize.py
+++ python/cugraph/structure/symmetrize.py
@@ -17,10 +17,10 @@ import cudf
 
 def symmetrize_df(df, src_name, dst_name):
     """
-    Take a COO stored in a dataframe, along with the column names of
-    the source and destination columns assumed to represent a directed graph
-    and create a new data frame using the same column names that
-    symmetrize the graph so that all edges appear in both directions.
+    Take a COO stored in a DataFrame, along with the column names of
+    the source and destination columns and create a new data frame
+    using the same column names that symmetrize the graph so that all
+    edges appear in both directions.
 
     Note that if other columns exist in the data frame (e.g. edge weights)
     the other columns will also be replicated.  That is, if (u,v,data)
@@ -36,7 +36,7 @@ def symmetrize_df(df, src_name, dst_name):
 
     Parameters
     ----------
-    df : cdf.DataFrame
+    df : cudf.DataFrame
         Input data frame containing COO.  Columns should contain source
         ids, destination ids and any properties associated with the
         edges.
@@ -51,9 +51,22 @@ def symmetrize_df(df, src_name, dst_name):
     >>>                   dtype=['int32', 'int32', 'float32'], header=None)
     >>> sym_df = cugraph.symmetrize(M, '0', '1')
     >>> G = cugraph.Graph()
-    >>> G.add_edge_list(M['0]', M['1'], M['2'])
+    >>> G.add_edge_list(sym_df['0]', sym_df['1'], sym_df['2'])
     """
     gdf = cudf.DataFrame()
+
+    #
+    #  NOTE: if there are values then we can't use drop_duplicates - in
+    #        case the values are different in different directions.  To
+    #        address this, we will use groupby if there are values.  If
+    #        there are no values then groupby won't eliminate the duplicate
+    #        keys.  Believe this is a bug, see
+    #        https://github.com/rapidsai/cudf/issues/2730.  Once this
+    #        is resolved we should be able to just use groupby.
+    #
+    #  We will use drop_duplicates if there are no non-key fields
+    #
+    use_groupby = False
 
     #
     #  Now append the columns.  We add sources to the end of destinations,
@@ -69,19 +82,24 @@ def symmetrize_df(df, src_name, dst_name):
                                                 ignore_index=True)
         else:
             gdf[name] = df[name].append(df[name], ignore_index=True)
+            use_groupby = True
 
-    return gdf.drop_duplicates(subset=[src_name, dst_name], keep='first')
+    if use_groupby:
+        return gdf.groupby(by=[src_name, dst_name], as_index=False).min()
+    else:
+        return gdf.drop_duplicates(subset=[src_name, dst_name], keep='first')
 
 
-def symmetrize(source_col, dest_col, weights=None):
+def symmetrize(source_col, dest_col, value_col=None):
     """
-    Take a COO set of src/dest pairs assumed to represent a directed graph
-    along with associated weights and create a new COO set of src/dest
-    pairs along with weights where all edges exist in both directions.
+    Take a COO set of source destination pairs along with associated values and
+    create a new COO set of source destination pairs along with values where
+    all edges exist in both directions.
 
     Return from this call will be a COO stored as two cudf Series - the
     symmetrized source column and the symmetrized dest column, along with
-    a new cudf Series containing the associated weights.
+    an optional cudf Series containing the associated values (only if the
+    values are passed in).
 
     Parameters
     ----------
@@ -93,10 +111,10 @@ def symmetrize(source_col, dest_col, weights=None):
         This cudf.Series wraps a gdf_column of size E (E: number of edges).
         The gdf column contains the destination index for each edge.
         Destination indices must be an integer type.
-    weights : cudf.Series (optional)
+    value_col : cudf.Series (optional)
         This cudf.Series wraps a gdf_column of size E (E: number of edges).
-        The gdf column contains weights associated with this edge.
-        For this function the weights can be any type, they are not
+        The gdf column contains values associated with this edge.
+        For this function the values can be any type, they are not
         examined, just copied.
 
     Examples
@@ -105,23 +123,26 @@ def symmetrize(source_col, dest_col, weights=None):
     >>>                   dtype=['int32', 'int32', 'float32'], header=None)
     >>> sources = cudf.Series(M['0'])
     >>> destinations = cudf.Series(M['1'])
-    >>> weights = cudf.Series(M['2'])
-    >>> src, dst, wt = cugraph.symmetrize(sources, destinations, weights)
+    >>> values = cudf.Series(M['2'])
+    >>> src, dst, val = cugraph.symmetrize(sources, destinations, values)
     >>> G = cugraph.Graph()
-    >>> G.add_edge_list(src, dst, wt)
+    >>> G.add_edge_list(src, dst, val)
     """
     null_check(source_col)
     null_check(dest_col)
 
-    input_df = cudf.DataFrame([('src', source_col), ('dst', dest_col)])
+    input_df = cudf.DataFrame([('source', source_col),
+                               ('destination', dest_col)])
 
-    if weights is not None:
-        null_check(weights)
-        input_df.add_column('weight', weights)
+    if value_col is not None:
+        null_check(value_col)
+        input_df.add_column('value', value_col)
 
-    output_df = symmetrize_df(input_df, 'src', 'dst')
+    output_df = symmetrize_df(input_df, 'source', 'destination')
 
-    if weights is not None:
-        return output_df['src'], output_df['dst'], output_df['weight']
+    if value_col is not None:
+        return (output_df['source'],
+                output_df['destination'],
+                output_df['value'])
 
-    return output_df['src'], output_df['dst']
+    return output_df['source'], output_df['destination']
