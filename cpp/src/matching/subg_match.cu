09ce3cc33b0e4527798155bb3e586d7cc6f93aab
Andrei Schaffer <aschaffer@nvidia.com>
2019-09-25 14:41:45 -0500

    Gunrock submodule + SM. Removed GRMAT dependency from tests. Submodule issue fixed. Corrected (empty) SM API (pending Gunrock exposure). Milestone 3.

--- cpp/src/matching/subg_match.cu
+++ cpp/src/matching/subg_match.cu
@@ -1,4 +1,7 @@
-///#include <gunrock/app/sm/sm_app.cuh>
+//#include <gunrock/app/sm/sm_app.cuh>
+//TODO: have Gunrock fix the issue of including above gunrock header that triggers _linker_ errors
+//(probably they still have function calls to non-templates defined in implementation files):
+//
 #include <thrust/sequence.h>
 
 #include "utilities/graph_utils.cuh"
@@ -8,17 +11,8 @@
 
 #include <iostream>
 #include <type_traits>
+#include <array>
 #include <cstdint>
-
-extern double sm(const int            num_nodes,
-                 const int            num_edges,
-                 const int           *row_offsets,
-                 const int           *col_indices,
-                 const unsigned long *edge_values,
-                 const int            num_runs,
-                 int           *subgraphs);
-
-
 
 //#define _DEBUG_SM_
 
@@ -30,14 +24,16 @@ extern double sm(const int            num_nodes,
  * @tparam VertexT the indexing type for vertices
  * @tparam SizeT the type for sizes/dimensions
  * @tparam GValueT the type for edge weights
- * @param  graph input graph; assumed undirected [in]
+ * @param  graph_src input source graph (to search into); assumed undirected [in]
+ * @param  graph_query input query graph (to search for); assumed undirected [in]
  * @param  subgraphs   Return number of subgraphs [out]
  * @param  stream the cuda stream [in / optional]
  */
 template <typename VertexT,
           typename SizeT,
           typename GValueT>
-gdf_error gdf_subgraph_matching_impl(gdf_graph *graph,
+gdf_error gdf_subgraph_matching_impl(gdf_graph *graph_src,
+                                     gdf_graph *graph_query,
                                      VertexT* subgraphs,
                                      cudaStream_t stream = nullptr)
 {
@@ -61,38 +57,47 @@ gdf_error gdf_subgraph_matching_impl(gdf_graph *graph,
   static auto nnz_ = [](const gdf_graph* G){
     return static_cast<SizeT>(G->adjList->indices->size);
   };
-  
-  GDF_REQUIRE(graph != nullptr, GDF_INVALID_API_CALL);
-    
-  GDF_REQUIRE(graph->adjList != nullptr, GDF_INVALID_API_CALL);
-    
-  GDF_REQUIRE(row_offsets_(graph) != nullptr, GDF_INVALID_API_CALL);
+  std::array<gdf_graph*, 2> arr_graph = {graph_src, graph_query};
 
-  GDF_REQUIRE(col_indices_(graph) != nullptr, GDF_INVALID_API_CALL);
-    
-  auto type_id = graph->adjList->offsets->dtype;
-  GDF_REQUIRE( type_id == GDF_INT32 || type_id == GDF_INT64, GDF_UNSUPPORTED_DTYPE);
-  
-  GDF_REQUIRE( type_id == graph->adjList->indices->dtype, GDF_UNSUPPORTED_DTYPE);
-  
-  const SizeT* p_d_row_offsets = row_offsets_(graph);
-  const VertexT* p_d_col_ind = col_indices_(graph);
-  const GValueT* p_d_values = values_(graph);
-
-  assert( p_d_values );
-
-  SizeT nnz = nnz_(graph);
-  SizeT nrows = nrows_(graph);
-
-  //return unused (for now)...
+  //check consistency of both graphs:
   //
-  auto t_elapsed = sm(nrows,
-                      nnz,
-                      p_d_row_offsets,
-                      p_d_col_ind,
-                      p_d_values,
-                      1,
-                      subgraphs);
+  for(auto&& graph: arr_graph)
+    {
+      GDF_REQUIRE(graph != nullptr, GDF_INVALID_API_CALL);
+    
+      GDF_REQUIRE(graph->adjList != nullptr, GDF_INVALID_API_CALL);
+    
+      GDF_REQUIRE(row_offsets_(graph) != nullptr, GDF_INVALID_API_CALL);
+
+      GDF_REQUIRE(col_indices_(graph) != nullptr, GDF_INVALID_API_CALL);
+    
+      auto type_id = graph->adjList->offsets->dtype;
+      GDF_REQUIRE( type_id == GDF_INT32 || type_id == GDF_INT64, GDF_UNSUPPORTED_DTYPE);
+  
+      GDF_REQUIRE( type_id == graph->adjList->indices->dtype, GDF_UNSUPPORTED_DTYPE);
+  
+      const SizeT* p_d_row_offsets = row_offsets_(graph);
+      const VertexT* p_d_col_ind = col_indices_(graph);
+      const GValueT* p_d_values = values_(graph);
+
+      assert( p_d_values );
+
+      SizeT nnz = nnz_(graph);
+      SizeT nrows = nrows_(graph);
+    }
+
+  //TODO: call into proper Gunrock API (non-existent, yet)
+  //
+  //below is the wrong API to call;
+  //Gunrock has yet to properly expose one...
+  //
+  // auto t_elapsed = sm(nrows,
+  //                     nnz,
+  //                     p_d_row_offsets,
+  //                     p_d_col_ind,
+  //                     p_d_values,
+  //                     1,
+  //                     subgraphs);
     
   return GDF_SUCCESS;
 }
@@ -101,10 +106,12 @@ gdf_error gdf_subgraph_matching_impl(gdf_graph *graph,
  * @brief Subgraph matching. 
  * API for gunrock implementation.
  *
- * @param  graph input graph; assumed undirected [in]
+ * @param  graph_src input source graph (to search into); assumed undirected [in]
+ * @param  graph_query input query graph (to search for); assumed undirected [in]
  * @param  subgraphs   Return number of matched subgraphs [out]
  */
-gdf_error gdf_subgraph_matching(gdf_graph *graph,
+gdf_error gdf_subgraph_matching(gdf_graph *graph_src,
+                                gdf_graph *graph_query,
                                 gdf_column* subgraphs)
 
 {
@@ -122,38 +129,18 @@ gdf_error gdf_subgraph_matching(gdf_graph *graph,
 
   
   auto subg_dtype = subgraphs->dtype;
-  auto ro_dtype   = row_offsets_t_(graph);
-  auto ci_dtype   = col_indices_t_(graph);
-  auto v_dtype    = values_t_(graph);
-  
-  GDF_REQUIRE( subg_dtype == ci_dtype, GDF_INVALID_API_CALL);//currently Gunrock's API requires that graph's col indices and subgraphs must be same type;
+  //auto ro_dtype   = row_offsets_t_(graph_src);//not yet necessary...possibly later, when smoke clears out
+  auto ci_src_dtype   = col_indices_t_(graph_src);
+  auto ci_qry_dtype   = col_indices_t_(graph_query);
+  //auto v_dtype    = values_t_(graph_src);//not yet necessary...possibly later, when smoke clears out
 
+  //currently Gunrock's API requires that graph's col indices and subgraphs must be same type:
+  //
+  GDF_REQUIRE( subg_dtype == ci_src_dtype, GDF_INVALID_API_CALL);
+  GDF_REQUIRE( subg_dtype == ci_qry_dtype, GDF_INVALID_API_CALL);
+
+  //TODO: hopefully multi-type-dispatch on various combos of types:
+  //
   int* p_d_subg = static_cast<int*>(subgraphs->data);
-  return gdf_subgraph_matching_impl<int, int, unsigned long>(graph, p_d_subg);
-  
-  /*
-  switch( subg_dtype )
-    {
-    case GDF_INT32:
-      {
-        using VertexT = int32_t;
-        VertexT* p_d_subg = static_cast<VertexT*>(subgraphs->data);
-        switch( v_dtype )
-          {
-          case GDF_INT64:
-            {
-              using GValueT = unsigned long;
-              return gdf_subgraph_matching_impl<VertexT, VertexT, GValueT>(graph, p_d_subg);
-            }
-          //more...
-          default:
-            break;//warning eater
-          }
-      }
-    //more...
-    default:
-      break;//warning eater
-    }
-  */
-  return GDF_UNSUPPORTED_DTYPE;
+  return gdf_subgraph_matching_impl<int, int, unsigned long>(graph_src, graph_query, p_d_subg);
 }
