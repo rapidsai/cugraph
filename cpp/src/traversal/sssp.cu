2de4defa11580492512d6606cbdb03c6015a8d34
Ishika Roy <iroy@nvidia.com>
2019-10-06 21:53:02 -0500

    remove adjacency list conversions

--- cpp/src/traversal/sssp.cu
+++ cpp/src/traversal/sssp.cu
@@ -288,9 +288,11 @@ gdf_error gdf_sssp(gdf_graph* gdf_G,
                    gdf_column* sssp_distances,
                    gdf_column* predecessors,
                    const int source_vert) {
-  GDF_REQUIRE(gdf_G, GDF_INVALID_API_CALL);
+
+  /*GDF_REQUIRE(gdf_G, GDF_INVALID_API_CALL);
   GDF_REQUIRE(gdf_G->adjList || gdf_G->edgeList, GDF_INVALID_API_CALL);
   GDF_REQUIRE(source_vert >= 0, GDF_INVALID_API_CALL);
+  */
 
   void *sssp_dist_ptr, *pred_ptr;
   // NOTE: gdf_column struct doesn't have a default constructor. So we can get
@@ -314,7 +316,7 @@ gdf_error gdf_sssp(gdf_graph* gdf_G,
                 GDF_INVALID_API_CALL);
   }
 
-  gdf_error err = gdf_add_adj_list(gdf_G);
+  /*gdf_error err = gdf_add_adj_list(gdf_G);
   if (err != GDF_SUCCESS)
     return err;
 
@@ -324,6 +326,7 @@ gdf_error gdf_sssp(gdf_graph* gdf_G,
               GDF_UNSUPPORTED_DTYPE);
   GDF_REQUIRE(source_vert < gdf_G->adjList->offsets->size - 1,
               GDF_INVALID_API_CALL);
+  */
 
   if (pred_ptr)
     GDF_REQUIRE(predecessors->dtype == gdf_G->adjList->indices->dtype,
