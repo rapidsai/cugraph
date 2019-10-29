21f7656964832bce7fa2928eb013a89931777a7c
Rick Ratzel <rratzel@nvidia.com>
2019-10-29 13:44:27 -0500

    Added updated benchmark files that use asvdb.

--- python/utils/run_benchmarks.py
+++ python/utils/run_benchmarks.py
@@ -1,7 +1,7 @@
 import argparse
 import os
+from os import path
 import sys
-import time
 from collections import OrderedDict
 
 from scipy.io import mmread
@@ -9,74 +9,77 @@ from scipy.io import mmread
 import cugraph
 import cudf
 
+from benchmark import (
+    Benchmark, logExeTime, printLastResult, noStdoutWrapper, nop
+)
+
+from asv_report import (
+    cugraph_update_asv
+)
 
 ########################################
 # Update this function to add new algos
 ########################################
-def getAlgoData(G, edgelist_gdf, args):
-    """
-    keyname = algo method/function name
-    args = args to pass the method/function (default is no args)
-    obj = object containing the method/function (default is the cugraph module)
-    extraWrappers = list of functions that return a callable, used for
-                    "wrapping" the algo to modify its environment, add timers,
-                    log calls, etc.
-    """
-    algoData = {"pagerank":
-                {"args": (G, args.damping_factor, None, args.max_iter,
-                          args.tolerance),
-                 },
-                "bfs":
-                {"args": (G, args.source, True),
-                 },
-                "sssp":
-                {"args": (G, args.source),
-                 "extraWrappers": [noStdoutWrapper],
-                 },
-                "jaccard":
-                {"args": (G,),
-                 },
-                "louvain":
-                {"args": (G,),
-                 },
-                "weakly_connected_components":
-                {"args": (G,),
-                 },
-                "overlap":
-                {"args": (G,),
-                 },
-                "triangles":
-                {"args": (G,),
-                 },
-                "spectralBalancedCutClustering":
-                {"args": (G, 2),
-                 },
-                "spectralModularityMaximizationClustering":
-                {"args": (G, 2),
-                 },
-                "renumber":
-                {"args": (edgelist_gdf["src"], edgelist_gdf["dst"]),
-                 },
-                "view_adj_list":
-                {"obj": G,
-                 },
-                "degree":
-                {"obj": G,
-                 },
-                "degrees":
-                {"obj": G,
-                 },
-                }
-    return algoData
+def getBenchmarks(G, edgelist_gdf, args):
+    benches = [
+        Benchmark(name="pagerank",
+                  func=cugraph.pagerank,
+                  args=(G, args.damping_factor, None, args.max_iter,
+                        args.tolerance)),
+        Benchmark(name="bfs",
+                  func=cugraph.bfs,
+                  args=(G, args.source, True)),
+        Benchmark(name="sssp",
+                  func=cugraph.sssp,
+                  args=(G, args.source)),
+                  #extraRunWrappers=[noStdoutWrapper]),
+        Benchmark(name="jaccard",
+                  func=cugraph.jaccard,
+                  args=(G,)),
+        Benchmark(name="louvain",
+                  func=cugraph.louvain,
+                  args=(G,)),
+        Benchmark(name="weakly_connected_components",
+                  func=cugraph.weakly_connected_components,
+                  args=(G,)),
+        Benchmark(name="overlap",
+                  func=cugraph.overlap,
+                  args=(G,)),
+        Benchmark(name="triangles",
+                  func=cugraph.triangles,
+                  args=(G,)),
+        Benchmark(name="spectralBalancedCutClustering",
+                  func=cugraph.spectralBalancedCutClustering,
+                  args=(G, 2)),
+        Benchmark(name="spectralModularityMaximizationClustering",
+                  func=cugraph.spectralModularityMaximizationClustering,
+                  args=(G, 2)),
+        Benchmark(name="renumber",
+                  func=cugraph.renumber,
+                  args=(edgelist_gdf["src"], edgelist_gdf["dst"])),
+        Benchmark(name="view_adj_list",
+                  func=G.view_adj_list),
+        Benchmark(name="degree",
+                  func=G.degree),
+        Benchmark(name="degrees",
+                  func=G.degrees),
+    ]
+    # Return a dictionary of Benchmark name to Benchmark obj mappings
+    return dict([(b.name, b) for b in benches])
 
 
-def loadDataFile(file_name, file_type, delimiter=' '):
+########################################
+# cugraph benchmarking utilities
+def loadDataFile(file_name, csv_delimiter=' '):
+    file_type = file_name.split(".")[-1]
+
     if file_type == "mtx":
         edgelist_gdf = read_mtx(file_name)
     elif file_type == "csv":
-        edgelist_gdf = read_csv(file_name, delimiter)
+        edgelist_gdf = read_csv(file_name, csv_delimiter)
     else:
-        raise ValueError("bad file type: '%s'" % file_type)
+        raise ValueError("bad file type: '%s', %s " % (file_type, file_name) +
+                         "must have a .csv or .mtx extension")
     return edgelist_gdf
 
 
@@ -122,47 +125,12 @@ def read_csv(csv_file, delimiter):
     return gdf
 
 
-# wrappers for running and observing algorithms
-def noStdoutWrapper(algoFunction):
-    def wrapper(*algoArgs):
-        prev = sys.stdout
-        sys.stdout = open(os.devnull, 'w')
-        try:
-            retVal = algoFunction(*algoArgs)
-            sys.stdout = prev
-            return retVal
-        except Exception:
-            sys.stdout = prev
-            raise
-    return wrapper
-
-
-def logExeTime(algoFunction, perfData):
-    def wrapper(*algoArgs):
-        retVal = None
-        try:
-            st = time.time()
-            retVal = algoFunction(*algoArgs)
-        except Exception as e:
-            perfData.append((algoFunction.__name__, "ERROR: %s" % e))
-            return
-        perfData.append((algoFunction.__name__, (time.time()-st)))
-        return retVal
-    return wrapper
-
-
 def parseCLI(argv):
     parser = argparse.ArgumentParser(description='CuGraph benchmark script.')
     parser.add_argument('file', type=str,
                         help='Path to the input file')
-    parser.add_argument('--file_type', type=str, default="mtx",
-                        choices=["mtx", "csv"],
-                        help='Input file type: csv or mtx. If csv, cuDF '
-                        'reader is used (set for  [src dest] pairs '
-                        'separated by a tab). If mtx, Scipy reder is used '
-                        '(slow but supports weights). Default is mtx.')
     parser.add_argument('--algo', type=str, action="append",
-                        help='Algorithm to run, must be one of %s, or "all"'
+                        help='Algorithm to run, must be one of %s, or "ALL"'
                         % ", ".join(['"%s"' % k
                                      for k in getAllPossibleAlgos()]))
     parser.add_argument('--damping_factor', type=float, default=0.85,
@@ -180,71 +148,84 @@ def parseCLI(argv):
                         help='Automatically do the csr and transposed '
                         'transformations. Default is 0, switch to another '
                         'value to enable')
-    parser.add_argument('--times_only', action="store_true",
-                        help='Only output the times, no table')
     parser.add_argument('--delimiter', type=str, choices=["tab", "space"],
                         default="space",
                         help='Delimiter for csv files (default is space)')
+    parser.add_argument('--update_results_dir', type=str,
+                        help='Add (and compare) results to the dir specified')
+    parser.add_argument('--update_asv_dir', type=str,
+                        help='Add results to the specified ASV dir in ASV '
+                        'format')
+    parser.add_argument('--report_cuda_ver', type=str, default="",
+                        help='The CUDA version to include in reports')
+    parser.add_argument('--report_python_ver', type=str, default="",
+                        help='The Python version to include in reports')
+    parser.add_argument('--report_os_type', type=str, default="",
+                        help='The OS type to include in reports')
+    parser.add_argument('--report_machine_name', type=str, default="",
+                        help='The machine name to include in reports')
+
     return parser.parse_args(argv)
 
 
 def getAllPossibleAlgos():
-    class fakeArgs:
-        def __getattr__(self, a): return None
-    return list(getAlgoData(None, {"src": 0, "dst": 0}, fakeArgs()).keys())
-
+    return list(getBenchmarks(nop, nop, nop).keys())
 
 ###############################################################################
 if __name__ == "__main__":
     perfData = []
     args = parseCLI(sys.argv[1:])
-    delimiter = {"space": ' ', "tab": '\t'}[args.delimiter]
 
+    # set algosToRun based on the command line args
     allPossibleAlgos = getAllPossibleAlgos()
-    if args.algo:
-        if (set(args.algo) - set(allPossibleAlgos)) != set():
+    if args.algo and ("ALL" not in args.algo):
+        allowedAlgoNames = allPossibleAlgos + ["ALL"]
+        if (set(args.algo) - set(allowedAlgoNames)) != set():
             raise ValueError(
                 "bad algo(s): '%s', must be in set of %s" %
-                (args.algo, ", ".join(['"%s"' % a for a in allPossibleAlgos])))
+                (args.algo, ", ".join(['"%s"' % a for a in allowedAlgoNames])))
         algosToRun = args.algo
     else:
         algosToRun = allPossibleAlgos
 
-    # Load the data file and create a Graph, include exe time in perfData
-    edgelist_gdf = logExeTime(loadDataFile, perfData)(args.file,
-                                                      args.file_type,
-                                                      delimiter)
-    G = logExeTime(createGraph, perfData)(edgelist_gdf, args.auto_csr)
+    # Update the various wrappers with a list to log to and formatting settings
+    # (name width must be >= 15)
+    logExeTime.perfData = perfData
+    printLastResult.perfData = perfData
+    printLastResult.nameCellWidth = max(*[len(a) for a in algosToRun], 15)
+    printLastResult.valueCellWidth = 30
+
+    # Load the data file and create a Graph, treat these as benchmarks too
+    csvDelim = {"space": ' ', "tab": '\t'}[args.delimiter]
+    edgelist_gdf = Benchmark(loadDataFile, args=(args.file, csvDelim)).run()
+    G = Benchmark(createGraph, args=(edgelist_gdf, args.auto_csr)).run()
 
     if G is None:
         raise RuntimeError("could not create graph!")
 
-    # Get the data on the algorithms present and how to run them
-    algoData = getAlgoData(G, edgelist_gdf, args)
+    print("-" * (printLastResult.nameCellWidth +
+                 printLastResult.valueCellWidth))
 
-    # For each algo to run, look up the object it belongs to (the cugraph
-    # module by default), the args it needs passed (none by default), and any
-    # extra function wrappers that should be applied (none by default).
+    benches = getBenchmarks(G, edgelist_gdf, args)
+
     for algo in algosToRun:
-        obj = algoData[algo].get("obj", cugraph)
-        algoArgs = algoData[algo].get("args", ())
-        extraWrappers = algoData[algo].get("extraWrappers", [])
+        benches[algo].run()
 
-        # get the callable, wrap it in any wrappers (which results in a wrapped
-        # callable), wrap it in the logger, then finally call it with algoArgs.
-        callable = getattr(obj, algo)
-        for wrapper in extraWrappers:
-            callable = wrapper(callable)
-        callable = logExeTime(callable, perfData)
-        callable(*algoArgs)
+    #### reports ########################
+    if args.update_results_dir:
+        raise NotImplementedError
 
-    print()
-    if args.times_only:
-        print(",".join([str(exeTime) for (name, exeTime) in perfData]))
-    else:
-        # Print a formatted table of the perfData
-        nameCellWidth = max([len(name) for (name, _) in perfData])
-        exeTimeCellWith = max([len(str(exeTime)) for (_, exeTime) in perfData])
-        for (name, exeTime) in perfData:
-            print("%s | %s" % (name.ljust(nameCellWidth),
-                               str(exeTime).ljust(exeTimeCellWith)))
+    if args.update_asv_dir:
+        # Convert Exception strings in results to None for ASV
+        asvPerfData = [(name, value if not isinstance(value, str) else None) for (name, value) in perfData]
+        # special case: do not include the full path to the datasetName, since
+        # the leading parts are redundant and take up UI space.
+        datasetName = "/".join(args.file.split("/")[-3:])
+
+        cugraph_update_asv(asvDir=args.update_asv_dir,
+                           datasetName=datasetName,
+                           algoRunResults=asvPerfData,
+                           cudaVer=args.report_cuda_ver,
+                           pythonVer=args.report_python_ver,
+                           osType=args.report_os_type,
+                           machineName=args.report_machine_name)
