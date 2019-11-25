422d49627f049339b0a2778383a64c66556702e4
Brad Rees <brees@nvidia.com>
2019-11-25 09:41:20 -0500

    updated for better performance

--- python/cugraph/pregel/traversal/bfs_bsp.py
+++ python/cugraph/pregel/traversal/bfs_bsp.py
@@ -17,41 +17,6 @@ import numpy as np
 from collections import OrderedDict
 
 
-def pregel_bfs(G, start) :
-    """
-    This function executes an unwieghted Breadth-First-Search (BFS) traversal to find the distances and predecessors 
-    from a sepcified starting vertex in the graph. 
-    
-    Parameters
-    ----------
-     G : cugraph.Graph
-        cuGraph graph descriptor, should contain the connectivity information
-        as an edge list (edge weights are not used for this algorithm).
-        
-    start
-        The index of the graph vertex from which the traversal begins
-
-    Returns
-    -------
-    df : cudf.DataFrame
-        df['vertex'][i] gives the vertex id of the i'th vertex
-        df['distance'][i] gives the path distance for the i'th vertex from the
-        starting vertex
-        df['predecessor'][i] gives for the i'th vertex the vertex it was
-        reached from in the traversal
-    
-    Examples
-    --------
-    >>> data_df = cudf.read_csv('datasets/karate.csv', delimiter=' ', dtype=['int32', 'int32', 'float32'], header=None)
-    >>> df = cugraph.pregel_bfs(data_df, 1)    
-    
-    """    
-
-    ###  TBD
-    
-    
-    
-
 def bfs_df(df, start, src_col='src', dst_col='dst', copy_data=True) :
     """
     This function executes an unwieghted Breadth-First-Search (BFS) traversal to find the distances and predecessors 
@@ -62,7 +27,7 @@ def bfs_df(df, start, src_col='src', dst_col='dst', copy_data=True) :
     df : cudf.dataframe
         a dataframe containing the source and destination edge list
 
-    start : Integer
+    start : same type as 'src' and 'dst' 
         The index of the graph vertex from which the traversal begins
         
     src : string
@@ -91,19 +56,19 @@ def bfs_df(df, start, src_col='src', dst_col='dst', copy_data=True) :
     >>> df = cugraph.pregel_bfs(data_df, 1, '0', '1')    
     
     """
-    # extract the source and destination columns into a new dataframe that can be modified
+ 
+     # extract the source and destination columns into a new dataframe that can be modified
     if copy_data : 
-        coo_data = cudf.DataFrame()
-        coo_data['src'] = df[src_col]
-        coo_data['dst'] = df[dst_col]
+        coo_data = df[[src_col, dst_col]]
     else :
         coo_data = df
-        coo_data.rename(columns={src_col:'src', dst_col:'dst'}, inplace=True)
+    
+    coo_data.rename(columns={src_col:'src', dst_col:'dst'}, inplace=True)
        
     
     # convert the "start" vertex into a series
-    start_v = cudf.Series(start)
-    ddf = start_v.to_frame('dst')
+    frontier = cudf.Series(start).to_frame('dst')
+    #frontier = start_v.to_frame('dst')
     
     # create the answer DF
     answer = cudf.DataFrame()
@@ -116,37 +81,41 @@ def bfs_df(df, start, src_col='src', dst_col='dst', copy_data=True) :
     done = False
 
     while not done :
+        
         #---------------------------------      
         # update the distance and add it to the dataframe
         distance = distance + 1
-        ddf['distance'] = distance
+        frontier['distance'] = distance
 
         #-----------------------------------
-        # Removed all instances of the vertices in 'ddf' from 'dst' side of coo
+        # Removed all instances of the frontier vertices from 'dst' side of coo_data
         # we do not want to hop to a vertex that has already been seen 
-        coo_data = coo_data.merge(ddf, on=['dst'], how='left') 
+        coo_data = coo_data.merge(frontier, on=['dst'], how='left') 
         coo_data = coo_data[coo_data.distance.isnull()]
         coo_data.drop_column('distance')  
 
         # now update column names for finding source vertices
-        ddf.rename(columns={'dst':'src'}, inplace=True)
+        frontier.rename(columns={'dst':'src'}, inplace=True)
 
         #---------------------------------      
         # merge the list of vertices and distances with the COO list
-        hop_df = coo_data.merge(ddf, on=['src'], how='left')
-
+        # there are two sets of results that we get from the "hop_df" nerge
+        # (A) the set of edges that start with a vertice in the frontier set 
+        #     - this goes into the answer set
+        #     - this also forms the next frontier set
+        # (B) the set of edges that did not start with a frontier vertex - this form the new set of coo_data        
+        hop_df = coo_data.merge(frontier, on=['src'], how='left')
+        
         #---------------------------------  
-        # get the data that was one hop out 
+        # (A) get the data where the 'src' was in the frontier list (e.g. there is a distance score) 
+        # create a new dataframe of vertices to hop out from (the 'dst')
         one_hop = hop_df.query("distance == @distance")    
+        frontier = one_hop['dst'].to_frame('dst')
 
         #---------------------------------    
-        # get all the edges that where not touched - also drop all reverse edges 
+        # (B) get all the edges that where not touched 
         coo_data = hop_df[hop_df.distance.isnull()]
         coo_data.drop_column('distance')    
-
-        #---------------------------------  
-        # create a new dataframe of vertices to hop out from
-        ddf = one_hop['dst'].to_frame('dst')
 
         #---------------------------------      
         # update the answer  
@@ -156,7 +125,7 @@ def bfs_df(df, start, src_col='src', dst_col='dst', copy_data=True) :
         #aggsOut = OrderedDict()
         #aggsOut['predecessor'] = 'min'   
         #aggsOut['distance'] = 'min'   
- 
+
         #_a = one_hop.groupby(['vertex'], as_index=False).agg(aggsOut)      
 
         answer = cudf.concat([answer,one_hop])     
@@ -164,13 +133,15 @@ def bfs_df(df, start, src_col='src', dst_col='dst', copy_data=True) :
         if len(coo_data) == 0 :
             done = True    
     
-        if not done and len(ddf) == 0:
+        if not done and len(frontier) == 0:
             # remaining vertices are unreachable
-            ddf.rename(columns={'dst':'vertex'}, inplace=True)
-            ddf['predecessor'] = -1
-            ddf['distance'] = np.iinfo(np.int32).max
-            answer = cudf.concat([answer, ddf])  
+            frontier.rename(columns={'dst':'vertex'}, inplace=True)
+            frontier['predecessor'] = -1
+            frontier['distance'] = np.iinfo(np.int32).max
+            answer = cudf.concat([answer, frontier])  
             done = True
     
     # all done, return the answer
     return answer
+ 
+ 
