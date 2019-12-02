82c8293e20e789ae7b12436ee8c83f8bc792901d
brees <brees@nvidia.com>
2019-12-02 16:19:23 -0500

    renamed to bsp

--- python/cugraph/bsp/__init__.py
+++ python/cugraph/bsp/__init__.py
@@ -11,5 +11,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cugraph.pregel.traversal import bfs_df_pregel
+from cugraph.bsp.traversal import bfs_df_pregel
 
--- python/cugraph/bsp/traversal/__init__.py
+++ python/cugraph/bsp/traversal/__init__.py
@@ -11,5 +11,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cugraph.pregel.traversal.bfs_bsp import bfs_df_pregel
+from cugraph.bsp.traversal.bfs_bsp import bfs_df_pregel
 
