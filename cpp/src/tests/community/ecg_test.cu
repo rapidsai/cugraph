778f34bd68a1aeabfaddfa707a0961c1f8a1c3db
James Wyles <jwyles@nvidia.com>
2019-12-16 17:16:28 -0700

    Added python bindings, Added C test case to aid debugging.

---
+++ cpp/src/tests/community/ecg_test.cu
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * NVIDIA CORPORATION and its licensors retain all intellectual property
+ * and proprietary rights in and to this software, related documentation
+ * and any modifications thereto.  Any use, reproduction, disclosure or
+ * distribution of this software and related documentation without an express
+ * license agreement from NVIDIA CORPORATION is strictly prohibited.
+ *
+ */
+#include <gtest/gtest.h>
+#include <nvgraph/nvgraph.h>
+#include <cugraph.h>
+#include <algorithm>
+#include "test_utils.h"
+
+#include <rmm_utils.h>
+
+TEST(nvgraph_louvain, success)
+{
+  cugraph::Graph G;
+
+  std::vector<int> off_h = {0, 16, 25, 35, 41, 44, 48, 52, 56, 61, 63, 66, 67, 69, 74, 76, 78, 80, 82, 84, 87, 89, 91, 93, 98, 101, 104, 106, 110, 113, 117, 121, 127, 139, 156};
+  std::vector<int> ind_h = {1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 17, 19, 21, 31, 0, 2, 3, 7, 13, 17, 19, 21, 30, 0, 1, 3, 7, 8, 9, 13, 27, 28, 32, 0, 1, 2, 7, 12, 13, 0, 6, 10, 0,
+      6, 10, 16, 0, 4, 5, 16, 0, 1, 2, 3, 0, 2, 30, 32, 33, 2, 33, 0, 4, 5, 0, 0, 3, 0, 1, 2, 3, 33, 32, 33, 32, 33, 5, 6, 0, 1, 32, 33, 0, 1, 33, 32, 33, 0, 1, 32, 33, 25, 27, 29, 32, 33,
+      25, 27, 31, 23, 24, 31, 29, 33, 2, 23, 24, 33, 2, 31, 33, 23, 26, 32, 33, 1, 8, 32, 33, 0, 24, 25, 28, 32, 33, 2, 8, 14, 15, 18, 20, 22, 23, 29, 30, 31, 33, 8, 9, 13, 14, 15,
+      18, 19, 20, 22, 23, 26, 27, 28, 29, 30, 31, 32};
+  std::vector<float> w_h = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+      1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+      1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+      1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+      1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+
+  gdf_column col_off, col_ind, col_w;
+
+
+  create_gdf_column(off_h,&col_off);
+  create_gdf_column(ind_h,&col_ind);
+  create_gdf_column(w_h  ,&col_w);
+
+  cugraph::adj_list_view(&G, &col_off, &col_ind, &col_w);
+
+  int no_vertex = off_h.size()-1;
+  int* best_cluster_vec = NULL;
+
+  cudaStream_t stream{nullptr};
+  ALLOC_TRY((void**)&best_cluster_vec, sizeof(int) * no_vertex, stream);
+  gdf_column result_col;
+  gdf_column_view(&result_col, best_cluster_vec, nullptr, no_vertex, GDF_INT32);
+
+  ASSERT_NO_THROW(cugraph::ecg(&G, .05, 16, &result_col));
+
+  ALLOC_FREE_TRY (best_cluster_vec, stream);
+}
+
+int main( int argc, char** argv )
+{
+    rmmInitialize(nullptr);
+    testing::InitGoogleTest(&argc,argv);
+    int rc = RUN_ALL_TESTS();
+    rmmFinalize();
+    return rc;
+}
---
+++ python/cugraph/community/ecg.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2019, NVIDIA CORPORATION.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cugraph.community import ecg_wrapper
+
+
+def ecg(input_graph, min_weight=.05, ensemble_size=16):
+    """
+    Compute the ensemble clustering for graphs partition of the input graph
+
+    Parameters
+    ----------
+    input_graph : cugraph.graph
+        cuGraph graph descriptor, should contain the connectivity information and weights.
+        The adjacency list will be computed if not already present. The graph should be 
+        undirected where an undirected edge is represented by an a directed edge in both
+        directions.
+        
+    min_weight : floating point
+        The minimum value to assign as an edgeweight in the ECG algorithm
+        
+    ensemble_size : integer
+        The number of graph permutations to use for the ensemble.
+        
+    Returns
+    -------
+    parts : cudf.DataFrame
+        GPU data frame of size V containing two columns, the vertex id and the partition
+        id it is assigned to.
+        
+    Examples
+    --------
+    >>> M = cudf.read_csv('datasets/karate.csv', delimiter = ' ', dtype=['int32', 'int32', 'float32'], header=None)
+    >>> sources = cudf.Series(M['0'])
+    >>> destinations = cudf.Series(M['1'])
+    >>> values = cudf.Series(M['2'])
+    >>> G = cugraph.Graph()
+    >>> G.add_edge_list(sources, destinations, values)
+    >>> parts = cugraph.ecg(G)
+    """
+
+    parts = ecg_wrapper.ecg(input_graph, min_weight, ensemble_size)
+    
+    return parts
