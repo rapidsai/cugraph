19e18238be8b753515766bda64cfd8f326a82bb1
James Wyles <jwyles@nvidia.com>
2019-12-18 10:47:44 -0700

    Fixing style issues

--- python/cugraph/community/ecg.py
+++ python/cugraph/community/ecg.py
@@ -21,26 +21,29 @@ def ecg(input_graph, min_weight=.05, ensemble_size=16):
     Parameters
     ----------
     input_graph : cugraph.graph
-        cuGraph graph descriptor, should contain the connectivity information and weights.
-        The adjacency list will be computed if not already present. The graph should be 
-        undirected where an undirected edge is represented by an a directed edge in both
-        directions.
-        
+        cuGraph graph descriptor, should contain the connectivity information
+        and weights.The adjacency list will be computed if not already 
+        present. The graph should be undirected where an undirected edge is 
+        represented by an a directed edge in both directions.
+
     min_weight : floating point
         The minimum value to assign as an edgeweight in the ECG algorithm
-        
+
     ensemble_size : integer
         The number of graph permutations to use for the ensemble.
-        
+
     Returns
     -------
     parts : cudf.DataFrame
-        GPU data frame of size V containing two columns, the vertex id and the partition
-        id it is assigned to.
-        
+        GPU data frame of size V containing two columns, the vertex id and
+        the partition id it is assigned to.
+
     Examples
     --------
-    >>> M = cudf.read_csv('datasets/karate.csv', delimiter = ' ', dtype=['int32', 'int32', 'float32'], header=None)
+    >>> M = cudf.read_csv('datasets/karate.csv', 
+                          delimiter = ' ', 
+                          dtype=['int32', 'int32', 'float32'], 
+                          header=None)
     >>> sources = cudf.Series(M['0'])
     >>> destinations = cudf.Series(M['1'])
     >>> values = cudf.Series(M['2'])
@@ -50,5 +53,5 @@ def ecg(input_graph, min_weight=.05, ensemble_size=16):
     """
 
     parts = ecg_wrapper.ecg(input_graph, min_weight, ensemble_size)
-    
+
     return parts
