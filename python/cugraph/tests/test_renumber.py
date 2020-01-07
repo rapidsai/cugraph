e705c2c6f6bcf619ef48fe0d9b7dc0db6b1c0bff
Brad Rees <brees@nvidia.com>
2020-01-07 11:05:05 -0500

    adding more tests

--- python/cugraph/tests/test_renumber.py
+++ python/cugraph/tests/test_renumber.py
@@ -80,11 +80,37 @@ def test_renumber_ips_cols():
     gdf['dest_as_int'] = gdf['dest_list'].str.ip2int()
 
     src, dst, number_df = cugraph.renumber_from_cudf(
-        gdf, ['source_list'], ['dest_list'])
+        gdf, ['source_as_int'], ['dest_as_int'])
 
     for i in range(len(gdf)):
         assert number_df['0'][src[i]] == gdf['source_as_int'][i]
         assert number_df['0'][dst[i]] == gdf['dest_as_int'][i]
+
+
+def test_renumber_ips_str_cols():
+
+    source_list = ['192.168.1.1',
+                   '172.217.5.238',
+                   '216.228.121.209',
+                   '192.16.31.23']
+    dest_list = ['172.217.5.238',
+                 '216.228.121.209',
+                 '192.16.31.23',
+                 '192.168.1.1']
+
+    pdf = pd.DataFrame({
+            'source_list': source_list,
+            'dest_list': dest_list
+            })
+
+    gdf = cudf.from_pandas(pdf)
+
+    src, dst, number_df = cugraph.renumber_from_cudf(
+        gdf, ['source_list'], ['dest_list'])
+
+    for i in range(len(gdf)):
+        assert number_df['0'][src[i]] == gdf['source_list'][i]
+        assert number_df['0'][dst[i]] == gdf['dest_list'][i]
 
 
 def test_renumber_negative():
@@ -181,8 +207,8 @@ def test_renumber_files_col(managed, pool, graph_file):
     gdf['src'] = cudf.Series([x + translate for x in sources])
     gdf['dst'] = cudf.Series([x + translate for x in destinations])
 
-    src, dst, numbering = cugraph.renumber_from_cudf(gdf, 'src', 'dst')
+    src, dst, numbering = cugraph.renumber_from_cudf(gdf, ['src'], ['dst'])
 
-    for i in range(len(sources)):
+    for i in range(len(gdf)):
         assert sources[i] == (numbering['0'][src[i]] - translate)
         assert destinations[i] == (numbering['0'][dst[i]] - translate)
