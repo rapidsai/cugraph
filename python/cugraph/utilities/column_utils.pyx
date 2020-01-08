8edd455b7151f45d762bf046476cad6a959b0825
Brad Rees <brees@nvidia.com>
2020-01-08 16:59:08 -0500

    updated

--- python/cugraph/utilities/column_utils.pyx
+++ python/cugraph/utilities/column_utils.pyx
@@ -80,7 +80,7 @@ cdef gdf_column* get_gdf_column_ptr(ipc_data_ptr, col_len):
                                     <void*> data_ptr,
                                     <valid_type*> valid_ptr,
                                     <size_type> col_len,
-                                    gdf_dtype_from_dtype(None, np.int32),
+                                    gdf_dtype_from_dtype(np.int32),
                                     <size_type> 0,
                                     c_extra_dtype_info)
     libcudf.cudf.check_gdf_error(err)
