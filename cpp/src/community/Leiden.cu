26b9a1cc7e3de514edba5e9ee2fda89daa4a3ed4
James Wyles <jwyles@nvidia.com>
2020-01-16 12:19:35 -0700

    WIP

--- cpp/src/community/Leiden.cu
+++ cpp/src/community/Leiden.cu
@@ -26,15 +26,57 @@ void leiden(Graph* graph,
             double gamma,
             IdxT* leiden_parts,
             int max_iter = 100){
+  // Check for error conditions
+  CUGRAPH_EXPECTS(graph != nullptr, "Invalid API parameter");
+  CUGRAPH_EXPECTS(leiden_parts != nullptr, "Invalid API parameter");
+  CUGRAPH_EXPECTS(graph->adjList != nullptr, "Graph must have adjacency list");
+  CUGRAPH_EXPECTS(graph->adjList->edge_data != nullptr, "Graph must have weights");
+
+  // Get info about the graph
+  IdxT n = graph->adjList->offsets->size - 1;
+  IdxT nnz = graph->adjList->indices->size;
+
   // Assign initial singleton partition
+  thrust::sequence(rmm::exec_policy(nullptr)->on(nullptr), leiden_parts, leiden_parts + n, 0);
+
   // Compute metric
+
   // Compute delta metric
+
   // Reassign nodes with positive delta metric
+
   // Repeat until no swaps are made
+
   // Refine the partition
+
   // Aggregate the graph according to the refined partition
+
   // Set initial partitioning according to unrefined partition
+
   //
 
 }
+
+// Explicit template instantiations
+template void leiden<int32_t, float>(Graph* graph,
+                                     int metric,
+                                     double gamma,
+                                     int32_t* leiden_parts,
+                                     int max_iter);
+template void leiden<int32_t, double>(Graph* graph,
+                                      int metric,
+                                      double gamma,
+                                      int32_t* leiden_parts,
+                                      int max_iter);
+template void leiden<int64_t, float>(Graph* graph,
+                                     int metric,
+                                     double gamma,
+                                     int64_t* leiden_parts,
+                                     int max_iter);
+template void leiden<int64_t, double>(Graph* graph,
+                                      int metric,
+                                      double gamma,
+                                      int64_t* leiden_parts,
+                                      int max_iter);
+
 } // cugraph namespace
