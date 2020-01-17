a8deb6a9406090fe9f5e15e58a235a523210310e
Ishika Roy <iroy@nvidia.com>
2020-01-17 13:21:18 -0600

    nstart renumbering edit

--- python/cugraph/link_analysis/pagerank_wrapper.pyx
+++ python/cugraph/link_analysis/pagerank_wrapper.pyx
@@ -66,10 +66,11 @@ def pagerank(input_graph, alpha=0.85, personalization=None, max_iter=100, tol=1.
         if len(nstart) != num_verts:
             raise ValueError('nstart must have initial guess for all vertices')
         if input_graph.renumbered is True:
-            renumber_series = cudf.Series(input_graph.edgelist.renumber_map.index,
-                                          index=input_graph.edgelist.renumber_map, dtype=np.int32)        
-            vertex_renumbered = renumber_series.loc[nstart['vertex']]
-            df['pagerank'][vertex_renumbered] = nstart['values']
+            renumber_df = cudf.DataFrame()
+            renumber_df['map'] = input_graph.edgelist.renumber_map
+            renumber_df['id'] = input_graph.edgelist.renumber_map.index.astype(np.int32)
+            guess = nstart.merge(renumber_df, left_on='vertex', right_on='map', how='left').drop('map')
+            df['pagerank'][guess['id']] = guess['values']
         else:
             df['pagerank'][nstart['vertex']] = nstart['values']
         has_guess = <bool> 1
