cfceb0dd9d7020583ff38dea855f1cd5a769a6b7
afender <afender@nvidia.com>
2020-01-17 15:28:11 -0600

    limited import to graph class

--- python/cugraph/__init__.py
+++ python/cugraph/__init__.py
@@ -11,17 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cugraph.community import (
-    louvain,
-    spectralBalancedCutClustering,
-    spectralModularityMaximizationClustering,
-    analyzeClustering_modularity,
-    analyzeClustering_edge_cut,
-    analyzeClustering_ratio_cut,
-    subgraph,
-    triangles
-)
-
 from cugraph.structure import (
     Graph, 
     DiGraph, 
@@ -31,23 +20,6 @@ from cugraph.structure import (
     symmetrize, 
     symmetrize_df
 )
-
-from cugraph.centrality import katz_centrality
-from cugraph.cores import core_number, k_core
-from cugraph.components import weakly_connected_components, strongly_connected_components
-from cugraph.link_analysis import pagerank
-
-from cugraph.link_prediction import jaccard, overlap, jaccard_w, overlap_w
-from cugraph.traversal import bfs, sssp, filter_unreachable
-# from cugraph.utilities import grmat_gen
-from cugraph.utilities import device_of_gpu_pointer
-
-from cugraph.snmg.link_analysis.mg_pagerank import mg_pagerank
-
-from cugraph.bsp.traversal import bfs_df_pregel
-
-from cugraph.proto.components import strong_connected_component
-from cugraph.proto.structure import find_bicliques
 
 # Versioneer
 from ._version import get_versions
