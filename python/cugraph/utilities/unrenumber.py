296718a949318f6168f9e59cb8a0fd0a7608a801
Iroy30 <41401566+Iroy30@users.noreply.github.com>
2020-03-06 11:07:10 -0600

    Update unrenumber.py

--- python/cugraph/utilities/unrenumber.py
+++ python/cugraph/utilities/unrenumber.py
@@ -3,11 +3,11 @@ import cudf
 
 def unrenumber(renumber_map, df, col):
     if isinstance(renumber_map, cudf.DataFrame):
-        unrenumered_df = df.merge(renumber_map, left_on=col,
-                                  right_on='id',
-                                  how='left').drop(['id', col])
-        cols = unrenumered_df.columns.to_list()
-        df = unrenumered_df[cols[1:] + [cols[0]]]
+        unrenumbered_df = df.merge(renumber_map, left_on=col,
+                                   right_on='id',
+                                   how='left').drop(['id', col])
+        cols = unrenumbered_df.columns.to_list()
+        df = unrenumbered_df[cols[1:] + [cols[0]]]
     else:
         df[col] = renumber_map[df[col]]
     return df
