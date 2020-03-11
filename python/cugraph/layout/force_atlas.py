0a7a2c115a6d8d86ff0ae9b17580941edd0986ca
Hugo Linsenmaier <hlinsenmaier@gmail.com>
2020-03-10 19:32:33 -0500

    Add interface for force atlas 2

---
+++ python/cugraph/layout/force_atlas.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2019, NVIDIA CORPORATION.
+#aLicensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from cugraph.layout import force_atlas_wrapper
+from cugraph.structure.graph import Graph
+
+
+def force_atlas(input_graph,
+                max_iter=1000,
+                gravity=1.0,
+		scaling_ratio=1.0,
+                barnes_hut_theta=0.5,
+                edge_weight_influence=1.0,
+                lin_log_mode=False,
+                prevent_overlapping=False,
+                ):
+
+	"""
+        ForceAtlas2 is a continuous graph layout algorithm for handy network
+        visualization.
+
+	Parameters
+	----------
+	input_graph : cugraph.Graph
+	cuGraph graph descriptor, should contain the connectivity information
+	as an edge list.
+	The adjacency list will be computed if not already present. The graph
+	should be undirected where an undirected edge is represented by a
+	directed edge in both direction.
+
+	max_iter : integer
+	This controls the maximum number of levels/iterations of the Force Atlas
+	algorithm. When specified the algorithm will terminate after no more
+	than the specified number of iterations. No error occurs when the
+	algorithm terminates early in this manner.
+
+	gravity:
+        scaling_ratio:
+	edge_weight_influence:
+	lin_log_mode:
+        prevent_overlapping:
+
+	Returns
+	-------
+	parts : cudf.DataFrame
+	GPU data frame of size V containing two columns the vertex id and the
+	partition id it is assigned to.
+
+	"""
+
+	parts = force_atlas_wrapper.force_atlas(input_graph,
+						max_iter=max_iter,
+						gravity=gravity
+						scaling_ratio=scaling_ratio,
+                                                barnes_hut_theta=barnes_hut_theta,
+						edge_weight_influence=edge_weight_influence,
+                                                lin_log_mode=lin_log_mode,
+                                                prevent_overlapping=prevent_overlapping)
+	return parts
