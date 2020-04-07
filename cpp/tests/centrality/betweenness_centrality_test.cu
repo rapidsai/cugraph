d7f96f5fe833af364e6ce3a2096f211dde4892c4
Xavier Cadet <xcadet@nvidia.com>
2020-04-07 17:40:44 -0500

    add c++ ref algorithm

--- cpp/tests/centrality/betweenness_centrality_test.cu
+++ cpp/tests/centrality/betweenness_centrality_test.cu
@@ -24,6 +24,120 @@
 #include <graph.hpp>
 #include <algorithms.hpp>
 
+#include <queue>
+#include <stack>
+
+// =============================================================================
+// C++ Reference Implementation
+// =============================================================================
+template<typename VT, typename ET>
+void populate_neighbors(VT *indices, ET *offsets,
+                        VT w, std::vector<VT> &neighbors) {
+  ET edge_start = offsets[w];
+  ET edge_end = offsets[w + 1];
+  ET edge_count = edge_end - edge_start;
+
+  neighbors.clear(); // Reset neighbors vector's size
+  for (ET edge_idx = 0; edge_idx < edge_count; ++edge_idx) {
+    VT dst = indices[edge_start + edge_idx];
+    neighbors.push_back(dst);
+  }
+}
+// Algorithm 1: Shortest-path vertex betweenness, (Brandes, 2001)
+template <typename VT, typename ET, typename WT, typename result_t>
+void reference_betweenness_centrality_impl(VT *indices, ET *offsets,
+                                           VT const number_of_vertices,
+                                           result_t *result) {
+  std::queue<VT> Q;
+  std::stack<VT> S;
+  // NOTE: dist is of type VT not WT
+  std::vector<VT> dist(number_of_vertices);
+  std::vector<std::vector<VT>> pred(number_of_vertices);
+  std::vector<result_t> sigmas(number_of_vertices);
+  std::vector<result_t> deltas(number_of_vertices);
+
+  std::vector<VT> neighbors;
+
+  for (VT s = 0; s < number_of_vertices; ++s) { 
+    // Step 1: Single-source shortest-paths problem
+    //   a. Initialization
+    for (VT w = 0 ; w < number_of_vertices; ++w) {
+      pred[w].clear();
+      dist[w] = std::numeric_limits<VT>::max();
+      sigmas[w] = 0;
+    }
+    dist[s] = 0;
+    sigmas[s] = 1;
+    Q.push(s);
+    //   b. Traversal
+    while (!Q.empty()) {
+      VT v = Q.front();
+      Q.pop();
+      S.push(v);
+      populate_neighbors<VT, ET>(indices, offsets, v, neighbors);
+      for (VT w : neighbors) {
+        // Path Discovery:
+        // Found for the first time?
+        if (dist[w] == std::numeric_limits<VT>::max()) {
+          dist[w] = dist[v] + 1;
+          Q.push(w);
+        }
+        // Path counting
+        // Edge(v, w) on  a shortest path?
+        if (dist[w] == dist[v] + 1) {
+          sigmas[w] +=  sigmas[v];
+          pred[w].push_back(v);
+        }
+      }
+    }
+    //  Step 2: Accumulation
+    //          Back propagation of dependencies
+    for (VT v = 0; v < number_of_vertices; ++v) {
+      deltas[v] = 0;
+    }
+    while (!S.empty()) {
+      VT w = S.top();
+      S.pop();
+      for (VT v : pred[w]) {
+        deltas[v] +=  (sigmas[v] / sigmas[w]) * (1 + deltas[w]);
+      }
+      if (w != s) {
+        result[w] += deltas[w];
+      }
+    }
+  }
+}
+
+template <typename VT, typename ET, typename WT, typename result_t>
+void reference_betweenness_centrality(cugraph::experimental::GraphCSR<VT, ET, WT> &graph,
+                                      result_t *result, bool normalize) {
+
+  VT number_of_vertices = graph.number_of_vertices;
+  ET number_of_edges = graph.number_of_edges;
+  std::vector<VT> indices(number_of_edges);
+  std::vector<ET> offsets(number_of_vertices + 1);
+
+  cudaMemcpy(indices.data(), graph.indices,
+             sizeof(VT) * indices.size(), cudaMemcpyDeviceToHost);
+  cudaMemcpy(offsets.data(), graph.offsets,
+             sizeof(ET) * offsets.size(), cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
+  reference_betweenness_centrality_impl<VT, ET, WT, result_t>(indices.data(), offsets.data(),
+                                        number_of_vertices, result);
+  if (normalize && number_of_vertices > 2) {
+    result_t factor = static_cast<result_t>(number_of_vertices - 1) * static_cast<result_t>(number_of_vertices - 2);
+    for (VT v = 0; v < number_of_vertices; ++v) {
+      result[v] /= factor;
+    }
+  }
+}
+// Explicit declaration
+template void reference_betweenness_centrality<int, int, float, float>(cugraph::experimental::GraphCSR<int, int, float> &,
+                                                                  float *, bool);
+
+// =============================================================================
+// Test Suite
+// =============================================================================
 struct BetweennessCentralityTest : public ::testing::Test
 {
 };
@@ -56,6 +170,12 @@ TEST_F(BetweennessCentralityTest, SimpleGraph)
 
   for (int i = 0 ; i < num_verts ; ++i)
     EXPECT_FLOAT_EQ(result[i], expected[i]);
+
+  // TODO(xcadet) Remove this part, it is for testing the reference
+  std::vector<float> ref_result(num_verts);
+  reference_betweenness_centrality(G, ref_result.data(), true);
+  for (int i = 0 ; i < num_verts ; ++i)
+    EXPECT_FLOAT_EQ(ref_result[i], expected[i]);
 }
 
 int main( int argc, char** argv )
