df148924efa019eace65f275ee8c86b357da0a32
Xavier Cadet <xcadet@nvidia.com>
2020-04-28 12:34:34 -0500

    bc: reorganized python betweenness_centrality tests

--- python/cugraph/tests/test_betweenness_centrality.py
+++ python/cugraph/tests/test_betweenness_centrality.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, NVIDIA CORPORATION.
+# Copyright (c) 2019, NVIDIA CORPORATION.:
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -20,7 +20,8 @@ import cugraph
 from cugraph.tests import utils
 import rmm
 import random
-import time # To add call timer
+import time
+import numpy as np
 
 # Temporarily suppress warnings till networkX fixes deprecation warnings
 # (Using or importing the ABCs from 'collections' instead of from
@@ -32,10 +33,100 @@ with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
     import networkx as nx
 
-
 print('Networkx version : {} '.format(nx.__version__))
+#===============================================================================
+# Parameters
+#===============================================================================
+RMM_MANAGED_MEMORY_OPTIONS = [False, True]
+RMM_POOL_ALLOCATOR_OPTIONS = [False, True]
+DEFAULT_EPSILON = 0.0001
 
-def compare_close_scores(scores, idx, epsilon):
+TINY_DATASETS = ['../datasets/karate.csv',
+                 '../datasets/dolphins.csv',
+                '../datasets/polbooks.csv']
+
+SMALL_DATASETS = ['../datasets/netscience.csv']
+
+LARGE_DATASETS = ['../datasets/road_central.csv']
+
+SUBSET_SIZE_OPTIONS = [4]
+SUBSET_SEED_OPTIONS = [42]
+
+#===============================================================================
+# Comparison functions
+#===============================================================================
+def build_graphs(graph_file, directed=True):
+    # cugraph
+    cu_M = utils.read_csv_file(graph_file)
+    G = cugraph.DiGraph() if directed else cugraph.Graph()
+    G.from_cudf_edgelist(cu_M, source='0', destination='1')
+    G.view_adj_list() # Enforce generation before computation
+
+    # networkx
+    M = utils.read_csv_for_nx(graph_file)
+    Gnx = nx.from_pandas_edgelist(M, create_using=(nx.DiGraph() if directed
+                                                   else nx.Graph()),
+                                  source='0', target='1')
+    return G, Gnx
+
+def calc_betweenness_centrality(graph_file, normalized, k=None, seed=None):
+    """ Generate both cugraph and networkx betweenness centrality
+
+    Parameters
+    ----------
+    graph_file : string
+        Path to COO Graph representation in .csv format
+
+    normalized : bool
+        True: Normalize Betweenness Centrality scores
+        False: Scores are left unormalized
+
+    Returns
+    -------
+        df : cudf.DataFrame
+            Contains 'vertex', 'cu' and 'nx' columns
+            'vertex': Indices of the vertices
+            'cu': Betweenness Centrality scores obtained with cugraph
+            'nx': Betweenness Centrality scores obtained with networkx
+    """
+    G, Gnx = build_graphs(graph_file, directed=True)
+    if k is not None and seed is not None:
+        df, nb = _calc_betweenness_centrality_subset(G, Gnx, normalized, k, seed)
+    else:
+        df, nb = _calc_betweenness_centrality_full(G, Gnx, normalized)
+
+    pdf = [nb[k] for k in sorted(nb.keys())]
+    df['nx'] = pdf
+    df = df.rename({'betweenness_centrality': 'cu'})
+    return df
+
+def _calc_betweenness_centrality_subset(G, Gnx, normalized, k, seed):
+    # NOTE: Networkx API does not allow passing a list of vertices
+    # And the sampling is operated on Gnx.nodes() directly
+    # We first mimic acquisition of the nodes to compare with same sources
+    random.seed(seed) # It will be called again on nx call
+    sources = random.sample(Gnx.nodes(), k)
+    df = cugraph.betweenness_centrality(G, normalized=normalized, k=sources)
+    nb = nx.betweenness_centrality(Gnx, normalized=normalized, k=k, seed=seed)
+    return df, nb
+
+def _calc_betweenness_centrality_full(G, Gnx, normalized):
+    df = cugraph.betweenness_centrality(G, normalized=normalized)
+    nb = nx.betweenness_centrality(Gnx, normalized=normalized)
+    return df, nb
+
+#===============================================================================
+# Utils
+#===============================================================================
+def prepare_rmm(managed_memory, pool_allocator):
+    gc.collect()
+    rmm.reinitialize(
+        managed_memory=managed_memory,
+        pool_allocator=pool_allocator,
+    )
+    assert(rmm.is_initialized)
+
+def compare_single_score(result, expected, epsilon):
     """
     Compare value in score at given index with relative error
 
@@ -50,171 +141,65 @@ def compare_close_scores(scores, idx, epsilon):
 
     Returns
     -------
-    err : int
-        1: If there is a mismatch
-        0: Otherwise
+    err : bool
+        True: Result and expected are close to each oter
+        False: Ohterwise
     """
-    err = 0
-    if (scores['cu'][idx] < (scores['nx'][idx] * (1 - epsilon)) or
-        scores['cu'][idx] > (scores['nx'][idx] * (1 + epsilon))):
-        err = err + 1
-        print('ERROR: vid = {}, cu = {}, nx = {}'.format(scores['vertex'][idx],
-                                                         scores['cu'][idx],
-                                                         scores['nx'][idx]))
-    #print("Abs diff:", abs(scores["cu"][idx] - scores["nx"][idx]))
+    err = np.isclose(result, expected, rtol=epsilon)
     return err
 
-def calc_betweenness_centrality(graph_file, normalized=True):
-    cu_M = utils.read_csv_file(graph_file)
-    G = cugraph.DiGraph()
-    G.from_cudf_edgelist(cu_M, source='0', destination='1')
+def compare_scores(scores, epsilon=DEFAULT_EPSILON):
+    err = 0
+    for idx in range(len(scores)):
+        score_cu =  scores['cu'][idx]
+        score_nx = scores['nx'][idx]
+        if not compare_single_score(score_cu, score_nx, epsilon=epsilon):
+            err += 1
+            print('ERROR: vid = {}, cu = {}, nx = {}'.format(scores['vertex'][idx],
+                                                            score_cu,
+                                                            score_nx))
+    assert err == 0, "Some scores were not close enough"
 
-    df = cugraph.betweenness_centrality(G, normalized=normalized)
-
-    NM = utils.read_csv_for_nx(graph_file)
-    Gnx = nx.from_pandas_edgelist(NM, create_using=nx.DiGraph(),
-                                  source='0', target='1')
-
-    nb = nx.betweenness_centrality(Gnx, normalized=normalized)
-
-    pdf = [nb[k] for k in sorted(nb.keys())]
-    df['nx'] = pdf
-    df = df.rename({'betweenness_centrality': 'cu'})
-    return df
-
-# TODO(xcadet)  Fix the following part with the number of sources
-# TODO(xcadet) Clean this part
-def calc_betweenness_centrality_k(graph_file, normalized=True):
-    # For this case we need to swap Gnx and G generation,
-    # In order to ensure comparability of the resultS with a subsample
-    NM = utils.read_csv_for_nx(graph_file)
-    Gnx = nx.from_pandas_edgelist(NM, create_using=nx.DiGraph(),
-                                  source='0', target='1')
-    number_of_sources = int(len(Gnx) * 0.05)
-    number_of_sources = 4 # For GAP equivalence
-    seed = 42
-    random.seed(seed)
-    vertices = random.sample(Gnx.nodes(), number_of_sources)
-    print("[DBG]Processing vertices:", vertices)
-    print("[DBG]Normalized:", normalized)
-    random.seed(seed)
-    second_vertices = random.sample(Gnx.nodes(), number_of_sources)
-    print("[DBG]Processing second vertices:", second_vertices)
-    start = time.perf_counter()
-    nb = nx.betweenness_centrality(Gnx, normalized=normalized, k=number_of_sources, seed=seed)
-    end = time.perf_counter()
-    print("[DBG]nx: {}".format(end - start))
-
-    cu_M = utils.read_csv_file(graph_file)
-    G = cugraph.DiGraph()
-    G.from_cudf_edgelist(cu_M, source='0', destination='1')
-    G.view_adj_list() # Enforce Adjacency
-
-    print("[DBG] Is Renumbered ?", G.renumbered)
-    start = time.perf_counter()
-    df = cugraph.betweenness_centrality(G, normalized=normalized, k=vertices)
-    end = time.perf_counter()
-    print("[DBG]cu: {}".format(end - start))
-
-    pdf = [nb[k] for k in sorted(nb.keys())]
-    df['nx'] = pdf
-    df = df.rename({'betweenness_centrality': 'cu'})
-    return df
-
-TINY_DATASETS = ['../datasets/karate.csv',
-                 '../datasets/dolphins.csv',
-                '../datasets/polbooks.csv']
-SMALL_DATASETS = ['../datasets/netscience.csv']
-
-
+#===============================================================================
+# Tests
+#===============================================================================
 @pytest.mark.parametrize('managed, pool',
-                         list(product([False, True], [False, True])))
+                         list(product(RMM_MANAGED_MEMORY_OPTIONS,
+                                      RMM_POOL_ALLOCATOR_OPTIONS)))
 @pytest.mark.parametrize('graph_file', TINY_DATASETS)
 def test_betweenness_centrality(managed, pool, graph_file):
-    gc.collect()
-
-    rmm.reinitialize(
-        managed_memory=managed,
-        pool_allocator=pool
-    )
-
-    assert(rmm.is_initialized())
-
-    scores = calc_betweenness_centrality(graph_file)
-
-    err = 0
-    epsilon = 0.0001
-    for idx in range(len(scores)):
-        err += compare_close_scores(scores, idx, epsilon)
-    assert err == 0
+    """Test Normalized Betweenness Centrality  on Directed Graph"""
+    prepare_rmm(managed, pool)
+    scores = calc_betweenness_centrality(graph_file, normalized=True)
+    compare_scores(scores)
 
 @pytest.mark.parametrize('managed, pool',
-                         list(product([False, True], [False, True])))
+                         list(product(RMM_MANAGED_MEMORY_OPTIONS,
+                                      RMM_POOL_ALLOCATOR_OPTIONS)))
 @pytest.mark.parametrize('graph_file', TINY_DATASETS)
 def test_betweenness_centrality_unnormalized(managed, pool, graph_file):
-    gc.collect()
-
-    rmm.reinitialize(
-        managed_memory=managed,
-        pool_allocator=pool
-    )
-
-    assert(rmm.is_initialized())
-
-    scores = calc_betweenness_centrality(graph_file, False)
-
-    err = 0
-    epsilon = 0.0001
-
-
-    for idx in range(len(scores)):
-        err += compare_close_scores(scores, idx, epsilon)
-    assert err == 0
+    """Test Unnormalized Betweenness Centrality on Directed Graph"""
+    prepare_rmm(managed, pool)
+    scores = calc_betweenness_centrality(graph_file, normalized=False)
+    compare_scores(scores)
 
 @pytest.mark.small
 @pytest.mark.parametrize('managed, pool',
-                         list(product([False, True], [False, True])))
+                         list(product(RMM_MANAGED_MEMORY_OPTIONS,
+                                      RMM_POOL_ALLOCATOR_OPTIONS)))
 @pytest.mark.parametrize('graph_file', SMALL_DATASETS)
-def test_betweenness_centrality_unnormalized_5percent(managed, pool, graph_file):
-    gc.collect()
+@pytest.mark.parametrize('subset_size', SUBSET_SIZE_OPTIONS)
+@pytest.mark.parametrize('subset_seed', SUBSET_SEED_OPTIONS)
+def test_betweenness_centrality_unnormalized_subset(managed, pool, graph_file,
+                                                    subset_size,
+                                                    subset_seed):
+    """Test Unnormalized Betweenness Centrality on Directed Graph on subset
 
-    rmm.reinitialize(
-        managed_memory=managed,
-        pool_allocator=pool
-    )
-
-    assert(rmm.is_initialized())
-
-    scores = calc_betweenness_centrality_k(graph_file, False)
-
-    err = 0
-    epsilon = 0.0001
-
-    for idx in range(len(scores)):
-        err += compare_close_scores(scores, idx, epsilon)
-    assert err == 0
-
-#LARGE_DATASETS = ['/datasets/GAP/GAP-road.csv']
-LARGE_DATASETS = ['../datasets/road_central.csv']
-@pytest.mark.large
-@pytest.mark.parametrize('managed, pool',
-                         list(product([False, True], [False, True])))
-@pytest.mark.parametrize('graph_file', LARGE_DATASETS)
-def test_betweenness_centrality_unnormalized_5percent(managed, pool, graph_file):
-    gc.collect()
-
-    rmm.reinitialize(
-        managed_memory=managed,
-        pool_allocator=pool
-    )
-
-    assert(rmm.is_initialized())
-
-    scores = calc_betweenness_centrality_k(graph_file, False)
-
-    err = 0
-    epsilon = 0.0001
-
-    for idx in range(len(scores)):
-        err += compare_close_scores(scores, idx, epsilon)
-    assert err == 0
+    Only k sources are considered for an approximate Betweenness Centrality
+    """
+    prepare_rmm(managed, pool)
+    scores = calc_betweenness_centrality(graph_file,
+                                         normalized=False,
+                                         k=subset_size,
+                                         seed=subset_seed)
+    compare_scores(scores)
