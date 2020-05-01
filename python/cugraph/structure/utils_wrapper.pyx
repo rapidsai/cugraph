039681c28be51b2f4c3431795b41949ad3c67e0b
Kumar Aatish <kaatish@nvidia.com>
2020-05-01 13:57:02 -0400

    Fixed cyclic import

--- python/cugraph/structure/utils_wrapper.pyx
+++ python/cugraph/structure/utils_wrapper.pyx
@@ -19,7 +19,6 @@
 from libc.stdint cimport uintptr_t
 from cugraph.structure cimport utils as c_utils
 from cugraph.structure.graph_new cimport *
-from cugraph.structure import graph_new_wrapper
 from libc.stdint cimport uintptr_t
 
 import cudf
