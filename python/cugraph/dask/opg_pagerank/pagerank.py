ff32e47a6a33297213144a646e7578204b7fa627
Ishika Roy <iroy@nvidia.com>
2020-06-11 22:01:14 -0500

    add test

--- python/cugraph/dask/opg_pagerank/pagerank.py
+++ python/cugraph/dask/opg_pagerank/pagerank.py
@@ -1,82 +1,82 @@
 # Copyright (c) 2019-2020, NVIDIA CORPORATION.
- #
- # Licensed under the Apache License, Version 2.0 (the "License");
- # you may not use this file except in compliance with the License.
- # You may obtain a copy of the License at
- #
- #     http://www.apache.org/licenses/LICENSE-2.0
- #
- # Unless required by applicable law or agreed to in writing, software
- # distributed under the License is distributed on an "AS IS" BASIS,
- # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- # See the License for the specific language governing permissions and
- # limitations under the License.
- #
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
- from cugraph.dask.common.comms import CommsContext
- from cugraph.dask.common.input_utils import DistributedDataHandler
- from dask.distributed import wait, default_client
- from cugraph.dask.common.comms import worker_state
- from cugraph.opg.link_analysis import mg_pagerank_wrapper as mg_pagerank
+from cugraph.raft.dask.common.comms import Comms
+from cugraph.dask.common.input_utils import DistributedDataHandler
+from dask.distributed import wait, default_client
+from cugraph.raft.dask.common.comms import worker_state
+from cugraph.opg.link_analysis import mg_pagerank_wrapper as mg_pagerank
 
-
- def common_func(sID, data):
-     print(data)
-     sessionstate = worker_state(sID)
-     print("nworkers: ", sessionstate['nworkers'],"  id: ", sessionstate['wid'])
-     mg_pagerank.mg_pagerank(data[0], sessionstate['comm'])
-     return 1
-
- def pagerank(input_graph):
-     print("INSIDE DASK PAGERANK")
-     client = default_client()
-     _ddf = input_graph.edgelist.edgelist_df
-     ddf = _ddf.sort_values(by='dst', ignore_index=True)
-     data = DistributedDataHandler.create(data=ddf)
-
-     comms = CommsContext(comms_p2p=False)
-     comms.init(data.gpu_futures)
-     data.calculate_parts_to_sizes(comms)
-     #self.ranks = data.ranks
-
-     print("Calling function")
-     result = dict([(data.worker_info[wf[0]]["rank"],
-                             client.submit(
-             common_func,
-             comms.sessionId,
-             wf[1],
-             workers=[wf[0]]))
-             for idx, wf in enumerate(data.worker_to_parts.items())])
-     wait(result)
-
- def get_n_gpus():
-     import os
-     try:
-         return len(os.environ["CUDA_VISIBLE_DEVICES"].split(","))
-     except KeyError:
-         return len(os.popen("nvidia-smi -L").read().strip().split("\n"))
+def common_func(sID, data):
+    print(data)
+    sessionstate = worker_state(sID)
+    print("nworkers: ", sessionstate['nworkers'],"  id: ", sessionstate['wid'])
+    mg_pagerank.mg_pagerank(data[0], sessionstate['handle'])
+    return 1
 
 
- def get_chunksize(input_path):
-     """
-     Calculate the appropriate chunksize for dask_cudf.read_csv
-     to get a number of partitions equal to the number of GPUs
+def pagerank(input_graph):
+    print("INSIDE DASK PAGERANK")
+    client = default_client()
+    _ddf = input_graph.edgelist.edgelist_df
+    ddf = _ddf.sort_values(by='dst', ignore_index=True)
+    data = DistributedDataHandler.create(data=ddf)
 
-     Examples
-     --------
-     >>> import dask_cugraph.pagerank as dcg
-     >>> chunksize = dcg.get_chunksize(edge_list.csv)
-     """
+    comms = Comms(comms_p2p=False)
+    comms.init(data.workers)
+    data.calculate_parts_to_sizes(comms)
+    print("Calling function")
+    result = dict([(data.worker_info[wf[0]]["rank"],
+                            client.submit(
+            common_func,
+            comms.sessionId,
+            wf[1],
+            workers=[wf[0]]))
+            for idx, wf in enumerate(data.worker_to_parts.items())])
+    wait(result)
 
-     import os
-     from glob import glob
-     import math
 
-     input_files = sorted(glob(str(input_path)))
-     if len(input_files) == 1:
-         size = os.path.getsize(input_files[0])
-         chunksize = math.ceil(size/get_n_gpus())
-     else:
-         size = [os.path.getsize(_file) for _file in input_files]
-         chunksize = max(size)
-     return chunksize
+def get_n_gpus():
+    import os
+    try:
+        return len(os.environ["CUDA_VISIBLE_DEVICES"].split(","))
+    except KeyError:
+        return len(os.popen("nvidia-smi -L").read().strip().split("\n"))
+
+
+def get_chunksize(input_path):
+    """
+    Calculate the appropriate chunksize for dask_cudf.read_csv
+    to get a number of partitions equal to the number of GPUs
+
+    Examples
+    --------
+    >>> import dask_cugraph.pagerank as dcg
+    >>> chunksize = dcg.get_chunksize(edge_list.csv)
+    """
+
+    import os
+    from glob import glob
+    import math
+
+    input_files = sorted(glob(str(input_path)))
+    if len(input_files) == 1:
+        size = os.path.getsize(input_files[0])
+        chunksize = math.ceil(size/get_n_gpus())
+    else:
+        size = [os.path.getsize(_file) for _file in input_files]
+        chunksize = max(size)
+    return chunksize
+
---
+++ python/cugraph/tests/dask/opg_pagerank_testing.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2020, NVIDIA CORPORATION.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import cugraph.dask.opg_pagerank as dcg
+from dask.distributed import Client
+import gc
+import cudf
+
+import cugraph
+import dask_cudf
+
+## Move to conftest
+from dask_cuda import LocalCUDACluster
+#cluster = LocalCUDACluster(protocol="tcp", scheduler_port=0)
+##
+
+def test_dask_pagerank():
+
+    gc.collect()
+    cluster = LocalCUDACluster(protocol="tcp", scheduler_port=0)
+    client = Client(cluster)
+
+    input_data_path = r"../datasets/karate.csv"
+
+    chunksize = dcg.get_chunksize(input_data_path)
+
+    ddf = dask_cudf.read_csv(input_data_path, chunksize=chunksize,
+                             delimiter=' ',
+                             names=['src', 'dst', 'value'],
+                             dtype=['int32', 'int32', 'float32'])
+
+
+
+    g = cugraph.DiGraph()
+    g.from_dask_cudf_edgelist(ddf)
+
+    dcg.pagerank(g)
+
+    client.close()
+    cluster.close()
