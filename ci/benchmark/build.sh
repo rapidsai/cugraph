e22c1b71f5d57e6b78cb592d02542a7b9be8a650
dillon-cullinan <dcullinan92@gmail.com>
2020-06-24 08:52:54 -0700

    Add benchmark build file

---
+++ ci/benchmark/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018, NVIDIA CORPORATION.
+##########################################
+# cuGraph Benchmark test script for CI   #
+##########################################
+
