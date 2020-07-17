46ec295ebdfb2a1d4c796549a14907f41c29dcf5
AJ Schmidt <aschmidt@nvidia.com>
2020-07-17 12:00:09 -0400

    add tooltip requirements to benchmarks/build.sh

--- ci/benchmark/build.sh
+++ ci/benchmark/build.sh
@@ -63,10 +63,28 @@ nvidia-smi
 logger "Activate conda env..."
 source activate rapids
 
+
+# Enter dependencies to be shown in ASV tooltips.
+# These dependencies will also be installed by conda.
+# A dependency can exist in both arrays without causing issues.
+CUGRAPH_DEPS=(cudf rmm)
+LIBCUGRAPH_DEPS=(cudf rmm)
+
+# Concatenate dependency arrays, convert to JSON array,
+# and remove duplicates.
+X=("${CUGRAPH_DEPS[@]}" "${LIBCUGRAPH_DEPS[@]}")
+DEPS=$(printf '%s\n' "${X[@]}" | jq -R . | jq -s 'unique')
+
+# Create install args for conda (i.e. "cudf=0.15" "rmm=0.15")
+CONDA_INSTALL=
+for DEP in $(echo "${DEPS}" | jq -r '.[]'); do
+  CONDA_INSTALL+="${DEP}=${MINOR_VERSION}"
+  CONDA_INSTALL+=" "
+done
+
 logger "conda install required packages"
 conda install -c nvidia -c rapidsai -c rapidsai-nightly -c conda-forge -c defaults \
-      "cudf=${MINOR_VERSION}" \
-      "rmm=${MINOR_VERSION}" \
+      ${CONDA_INSTALL} \
       "cudatoolkit=$CUDA_REL" \
       "dask-cudf=${MINOR_VERSION}" \
       "dask-cuda=${MINOR_VERSION}" \
@@ -107,17 +125,42 @@ if (( ${ERRORCODE} != 0 )); then
     exit ${ERRORCODE}
 fi
 
+# Build object with k/v pairs of "dependency:version"
+DEP_VER_DICT=$(jq -n '{}')
+for DEP in $(echo "${DEPS}" | jq -r '.[]'); do
+  VER=$(conda list | grep "^${DEP}" | awk '{print $2"-"$3}')
+  DEP_VER_DICT=$(echo "${DEP_VER_DICT}" | jq -c --arg DEP "${DEP}" --arg VER "${VER}" '. + { ($DEP): $VER }')
+done
+
+# Pass in an array of dependencies to get a dict of "dependency:version"
+function getReqs() {
+  local DEPS_ARR=("$@")
+  local REQS="{}"
+  for DEP in "${DEPS_ARR[@]}"; do
+    VER=$(echo "${DEP_VER_DICT}" | jq -r --arg DEP "${DEP}" '.[$DEP]')
+    REQS=$(echo "${REQS}" | jq -c --arg DEP "${DEP}" --arg VER "${VER}" '. + { ($DEP): $VER }')
+  done
+
+  echo "${REQS}"
+}
+
+REQS=$(getReqs "${CUGRAPH_DEPS[@]}")
+
 logger "Running Benchmarks..."
 cd $BENCHMARKS_DIR
 set +e
 time pytest -v -m "small and managedmem_on and poolallocator_on" \
     --benchmark-gpu-device=0 \
     --benchmark-gpu-max-rounds=3 \
-    --benchmark-asv-metadata="machineName=${NODE_NAME}, commitBranch=branch-${MINOR_VERSION}" \
+    --benchmark-asv-metadata="machineName=${NODE_NAME}, commitBranch=branch-${MINOR_VERSION}, requirements=${REQS}" \
     --benchmark-asv-output-dir=${ASVRESULTS_DIR}
+
+
 
 EXITCODE=$?
 
+# libcugraph reqs
+# REQS=$(getReqs "${LIBCUGRAPH_DEPS[@]}")
+
 set -e
 JOBEXITCODE=0
-
