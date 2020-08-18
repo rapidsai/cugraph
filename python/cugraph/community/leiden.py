baa68be3ad24ee207ebeae144ba45048c48241ce
Chuck Hastings <charlesh@nvidia.com>
2020-08-18 10:06:30 -0400

    Update documentation per comments on PR 1063

--- python/cugraph/community/leiden.py
+++ python/cugraph/community/leiden.py
@@ -18,26 +18,32 @@ from cugraph.structure.graph import Graph
 def leiden(input_graph, max_iter=100, resolution=1.):
     """
     Compute the modularity optimizing partition of the input graph using the
-    Louvain heuristic
+    Leiden algorithm
+
+    It uses the Louvain method described in:
+
+    Traag, V. A., Waltman, L., & van Eck, N. J. (2019). From Louvain to Leiden:
+    guaranteeing well-connected communities. Scientific reports, 9(1), 5233.
+    doi: 10.1038/s41598-019-41695-z
 
     Parameters
     ----------
     input_graph : cugraph.Graph
         cuGraph graph descriptor of type Graph
 
-        The adjacency list will be computed if not already present. The graph
-        should be undirected where an undirected edge is represented by a
-        directed edge in both direction.
+        The adjacency list will be computed if not already present.
 
     max_iter : integer
-        This controls the maximum number of levels/iterations of the Louvain
+        This controls the maximum number of levels/iterations of the Leiden
         algorithm. When specified the algorithm will terminate after no more
         than the specified number of iterations. No error occurs when the
         algorithm terminates early in this manner.
 
     resolution: float/double, optional
         Called gamma in the modularity formula, this changes the size
-        of the communities.  Defaults to 1.
+        of the communities.  Higher resolutions lead to more smaller
+        communities, lower resolutions lead to fewer larger communities.
+        Defaults to 1.
 
     Returns
     -------
@@ -62,7 +68,7 @@ def leiden(input_graph, max_iter=100, resolution=1.):
                           header=None)
     >>> G = cugraph.Graph()
     >>> G.from_cudf_edgelist(M, source='0', destination='1')
-    >>> parts, modularity_score = cugraph.louvain(G)
+    >>> parts, modularity_score = cugraph.leiden(G)
     """
 
     if type(input_graph) is not Graph:
