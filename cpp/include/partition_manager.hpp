553d02ef80db7873516f66a98c16ce8663ade807
Andrei Schaffer <aschaffer@nvidia.com>
2020-08-31 17:54:36 -0500

    Created per-worker subcomm_factory_t to fix erroneous implementation.

--- cpp/include/partition_manager.hpp
+++ cpp/include/partition_manager.hpp
@@ -36,6 +36,64 @@ std::string to_string(from_t const& value)
   return ss.str();
 }
 
+// class responsible for creating 2D partition of workers:
+// responsible with finding appropriate P_ROW x P_COL
+// 2D partition and initializing the raft::handle_t communicator
+//
+// (this might be removed; or, it might exist already)
+//
+template <typename size_type = int>
+class partition_manager_t {
+ public:
+  partition_manager_t(raft::handle_t& handle, size_type p_row_size, size_type p_col_size)
+    : handle_(handle), p_row_size_(p_row_size), p_col_size_(p_col_size)
+  {
+    init_communicator();
+  }
+
+  partition_manager_t(raft::handle_t const& handle, size_type p_size) : handle_(handle)
+  {
+    partition2d(p_size);
+    init_communicator();
+  }
+
+  virtual ~partition_manager_t(void) {}
+
+ protected:
+  virtual void partition2d(size_type p_size)
+  {
+    auto sqr = static_cast<size_type>(std::sqrt(p_size));
+
+    // find divisor of p_size
+    // nearest to sqr;
+    //
+    p_row_size_ = nearest_divisor(sqr, p_size);
+    p_col_size_ = p_size / p_row_size_;
+
+    assert(p_row_size_ > 1 && p_col_size_ > 1);
+  }
+
+  virtual void init_communicator(void)
+  {
+    // TODO: init's handle's communicator (singleton?)
+  }
+
+ private:
+  raft::handle_t& handle_;
+  size_type p_row_size_;
+  size_type p_col_size_;
+
+  static decltype(auto) nearest_divisor(size_type sqr, size_type p_size)
+  {
+    assert(sqr > 0);
+
+    for (size_type div = sqr; div > 0; --div) {
+      auto p_div = p_size % div;
+      if (p_div == 0) return div;
+    }
+  }
+};
+
 // default key-naming mechanism:
 //
 struct key_naming_t {
@@ -62,99 +120,52 @@ struct key_naming_t {
   std::string name_;
 };
 
-template <typename value_t>
-using matrix_t = std::vector<std::vector<value_t>>;
-
 using pair_comms_t =
   std::pair<std::shared_ptr<raft::comms::comms_t>, std::shared_ptr<raft::comms::comms_t>>;
 
-enum class colors_2d_t : int { ROW = 0, COL = 1 };
+enum class key_2d_t : int { ROW = 0, COL = 1 };
 
 // class responsible for creating 2D partition sub-comms:
+// this is instantiated by each worker (processing element, PE)
+// for the row/column it belongs to;
 //
-template <typename key_name_policy_t = key_naming_t, typename size_type = int>
-class partition_manager_t {
+template <typename name_policy_t = key_naming_t, typename size_type = int>
+class subcomm_factory_t {
  public:
-  partition_manager_t(raft::handle_t& handle, size_type p_row_size, size_type p_col_size)
-    : handle_(handle), p_row_size_(p_row_size), p_col_size_(p_col_size)
+  subcomm_factory_t(raft::handle_t& handle, size_type p_row_index, size_type p_col_index)
+    : handle_(handle), row_index_(p_row_index), col_index_(p_col_index)
   {
-    init_comms();
+    init_row_col_comms();
   }
-
-  partition_manager_t(raft::handle_t const& handle, size_type p_size) : handle_(handle)
-  {
-    partition2d(p_size);
-    init_comms();
-  }
-
-  virtual ~partition_manager_t(void) {}
-
-  matrix_t<pair_comms_t> const& comms_matrix(void) const { return comms_set_; }
+  virtual ~subcomm_factory_t(void) {}
 
  protected:
-  virtual void partition2d(size_type p_size)
+  virtual void init_row_col_comms(void)
   {
-    auto sqr = static_cast<size_type>(std::sqrt(p_size));
-
-    // find divisor of p_size
-    // nearest to sqr;
-    //
-    p_row_size_ = nearest_divisor(sqr, p_size);
-    p_col_size_ = p_size / p_row_size_;
-
-    assert(p_row_size_ > 1 && p_col_size_ > 1);
-  }
-
-  void init_comms(void)
-  {
-    std::vector<pair_comms_t> empty_row(p_col_size_, std::make_pair(nullptr, nullptr));
-    comms_set_.assign(p_row_size_, empty_row);
-
+    name_policy_t key{row_index_, col_index_};
     raft::comms::comms_t const& communicator = handle_.get_comms();
 
-    for (size_type row = 0; row < p_row_size_; ++row)
-      for (size_type col = 0; col < p_col_size_; ++col) {
-        key_name_policy_t key{row, col};
+    int const rank = communicator.get_rank();
+    int row_color  = rank / row_index_;
+    int col_color  = rank % row_index_;
 
-        // comm_slpit() on same key=linear_key,
-        // but different colors for row and column
-        //
-        // TODO: check if this assummed
-        // functionality is correct
-        //
-        size_type linear_key{p_col_size_ * row + col};
+    auto row_comm = std::make_shared<raft::comms::comms_t>(
+      communicator.comm_split(row_color, static_cast<int>(key_2d_t::ROW)));
+    handle_.set_subcomm(key.row_name(), row_comm);
 
-        auto shared_row_comm = std::make_shared<raft::comms::comms_t>(
-          communicator.comm_split(static_cast<int>(colors_2d_t::ROW), linear_key));
-        handle_.set_subcomm(key.row_name(), shared_row_comm);
+    auto col_comm = std::make_shared<raft::comms::comms_t>(
+      communicator.comm_split(col_color, static_cast<int>(key_2d_t::COL)));
+    handle_.set_subcomm(key.col_name(), col_comm);
 
-        auto shared_col_comm = std::make_shared<raft::comms::comms_t>(
-          communicator.comm_split(static_cast<int>(colors_2d_t::COL), linear_key));
-        handle_.set_subcomm(key.col_name(), shared_col_comm);
-
-        // Also store in a matrix of comms_t;
-        // this may be redundant, but useful;
-        // TODO: check if this is okay...
-        //
-        comms_set_[row][col] = std::make_pair(shared_row_comm, shared_col_comm);
-      }
+    row_col_subcomms_.first  = row_comm;
+    row_col_subcomms_.second = col_comm;
   }
 
  private:
   raft::handle_t& handle_;
-  size_type p_row_size_;
-  size_type p_col_size_;
-  matrix_t<pair_comms_t> comms_set_;
-
-  static decltype(auto) nearest_divisor(size_type sqr, size_type p_size)
-  {
-    assert(sqr > 0);
-
-    for (size_type div = sqr; div > 0; --div) {
-      auto p_div = p_size % div;
-      if (p_div == 0) return div;
-    }
-  }
+  size_type row_index_;
+  size_type col_index_;
+  pair_comms_t row_col_subcomms_;
 };
 }  // namespace partition_2d
 }  // namespace cugraph
