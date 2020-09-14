39f2830cdae3609717506f84dc0deff6f1386f35
BradReesWork <BradReesWork@users.noreply.github.com>
2020-09-14 14:48:41 -0400

    jaccard_coefficient

--- python/cugraph/__init__.py
+++ python/cugraph/__init__.py
@@ -47,7 +47,13 @@ from cugraph.components import (
 )
 from cugraph.link_analysis import pagerank, hits
 
-from cugraph.link_prediction import jaccard, overlap, jaccard_w, overlap_w
+from cugraph.link_prediction import {
+    jaccard,
+    jaccard_coefficient,
+    overlap,
+    jaccard_w,
+    overlap_w,
+}
 from cugraph.traversal import bfs, sssp, filter_unreachable
 
 from cugraph.utilities import utils
