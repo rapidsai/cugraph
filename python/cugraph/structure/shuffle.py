8f14babaa32bd40d23c4b8b8053c2f135a2e981c
Iroy30 <41401566+Iroy30@users.noreply.github.com>
2020-09-30 12:19:50 -0500

    Update shuffle.py

--- python/cugraph/structure/shuffle.py
+++ python/cugraph/structure/shuffle.py
@@ -17,7 +17,7 @@ def get_2D_div(ngpus):
 
 
 def _set_partitions_pre(df, vertex_row_partitions, vertex_col_partitions,
-                        prows, pcols, transposed):
+                        prows, pcols, transposed, partition_type):
     if transposed:
         r = df['dst']
         c = df['src']
@@ -26,11 +26,15 @@ def _set_partitions_pre(df, vertex_row_partitions, vertex_col_partitions,
         c = df['dst']
     r_div = vertex_row_partitions.searchsorted(r, side='right')-1
     c_div = vertex_col_partitions.searchsorted(c, side='right')-1
-    partitions = r_div % prows + c_div * prows
+
+    if partition_type == 1:
+        partitions = r_div * pcols + c_div
+    else:
+        partitions = r_div % prows + c_div * prows
     return partitions
 
 
-def shuffle(dg, transposed=False, prows=None, pcols=None):
+def shuffle(dg, transposed=False, prows=None, pcols=None, partition_type=1):
     """
     Shuffles the renumbered input distributed graph edgelist into ngpu
     partitions. The number of processes/gpus P = prows*pcols. The 2D
@@ -42,7 +46,10 @@ def shuffle(dg, transposed=False, prows=None, pcols=None):
     ddf = dg.edgelist.edgelist_df
     ngpus = get_n_workers()
     if prows is None and pcols is None:
-        prows, pcols = get_2D_div(ngpus)
+        if partition_type == 1:
+            pcols, prows = get_2D_div(ngpus)
+        else:
+            prows, pcols = get_2D_div(ngpus)
     else:
         if prows is not None and pcols is not None:
             if ngpus != prows*pcols:
@@ -62,24 +69,39 @@ def shuffle(dg, transposed=False, prows=None, pcols=None):
     renumber_vertex_count = dg.renumber_map.implementation.\
         ddf.map_partitions(len).compute()
     renumber_vertex_cumsum = renumber_vertex_count.cumsum()
-    src_dtype = ddf['src'].dtype
-    dst_dtype = ddf['dst'].dtype
 
-    vertex_row_partitions = cudf.Series([0], dtype=src_dtype)
-    vertex_row_partitions = vertex_row_partitions.append(cudf.Series(
-        renumber_vertex_cumsum, dtype=src_dtype))
-    num_verts = vertex_row_partitions.iloc[-1]
+    if transposed:
+        row_dtype = ddf['dst'].dtype
+        col_dtype = ddf['src'].dtype
+    else:
+        row_dtype = ddf['src'].dtype
+        col_dtype = ddf['dst'].dtype
+
+    vertex_partition_offsets = cudf.Series([0], dtype=row_dtype)
+    vertex_partition_offsets = vertex_partition_offsets.append(cudf.Series(
+        renumber_vertex_cumsum, dtype=row_dtype))
+    num_verts = vertex_partition_offsets.iloc[-1]
+    if partition_type == 1:
+        vertex_row_partitions = []
+        for i in range(prows + 1):
+            vertex_row_partitions.append(
+                vertex_partition_offsets.iloc[i*pcols])
+        vertex_row_partitions = cudf.Series(
+            vertex_row_partitions, dtype=row_dtype)
+    else:
+        vertex_row_partitions = vertex_partition_offsets
     vertex_col_partitions = []
     for i in range(pcols + 1):
-        vertex_col_partitions.append(vertex_row_partitions.iloc[i*prows])
-    vertex_col_partitions = cudf.Series(vertex_col_partitions, dtype=dst_dtype)
+        vertex_col_partitions.append(vertex_partition_offsets.iloc[i*prows])
+    vertex_col_partitions = cudf.Series(vertex_col_partitions, dtype=col_dtype)
 
     meta = ddf._meta._constructor_sliced([0])
     partitions = ddf.map_partitions(
         _set_partitions_pre,
         vertex_row_partitions=vertex_row_partitions,
         vertex_col_partitions=vertex_col_partitions, prows=prows,
-        pcols=pcols, transposed=transposed, meta=meta)
+        pcols=pcols, transposed=transposed, partition_type=partition_type,
+        meta=meta)
     ddf2 = ddf.assign(_partitions=partitions)
     ddf3 = rearrange_by_column(
         ddf2,
@@ -90,6 +112,4 @@ def shuffle(dg, transposed=False, prows=None, pcols=None):
         ignore_index=True,
     ).drop(columns=["_partitions"])
 
-    partition_row_size = pcols
-    partition_col_size = prows
-    return ddf3, num_verts, partition_row_size, partition_col_size, vertex_row_partitions
+    return ddf3, num_verts, vertex_partition_offsets
