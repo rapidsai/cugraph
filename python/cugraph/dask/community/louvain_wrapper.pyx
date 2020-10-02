60b9b85d79fd677ff7b11befc5d179c8bb050407
Rick Ratzel <rlratzel@ratzel.com>
2020-10-02 15:14:47 -0500

    ENH Refactored cython graph factory code to scale to additional data types (#1178)
    * Minor update to comment to describe array sizes.
    
    * Changed graph container to use smart pointers, added arg for instantiating legacy types and switch statements for it to factory function.
    
    * Added PR 1152 to CHANGELOG.md
    
    * Removing unnecessary .get() call on unique_ptr instance
    
    * Using make_unique() instead of new
    
    * Updated to call drop() correctly after cudf API update.
    
    * Added args to support calling get_vertex_identifiers().
    
    * Style fixes, removed commented out code meant for a future change.
    
    * Updated comment with description of new 'identifiers' arg.
    
    * Safety commit, still WIP, does not compile - updates for 2D graph support and upcoming 2D shuffle support
    
    * safety commit, does not pass tests: updated enough to be able to run the MG Louvain test.
    
    * Updated call_louvain() to use the new graph_t types. Still WIP, needs louvain updates to compile.
    
    * WIP: updates for incorporating new 2D shuffle data, still does not pass test.
    
    * Adding updates from iroy30 for calling shuffle from louvain.py
    
    * Updated to extract and pass the partition_t info and call the graph_t ctor. Now having a problem finding the right subcommunicator.
    
    * Updates to set up subcomms - having a problem with something needed by subcomms not being initialized: "address not mapped to object at address (nil)"
    
    * Added p2p flag to comms initialize() to enable initialization of UCX endpoints needed for MG test.
    
    * some proposed cleanup
    
    * safety commit: committing with debug prints to allow other team members to debug in parallel.
    
    * new technique for factory
    
    * safety commit: more updates to address problems instantiating graph_t (using num edges for partition instead of global for edgelist) and for debugging (print statments).
    
    * Changing how row and col rank are obtained, added debug prints for edge lists info
    
    * Fixes to partition_t get_matrix_partition_major/minor methods based on feedback.
    
    * Update shuffle.py
    
    * Integrating changes from iroy30 to produce "option 1" shuffle output by default, with an option to enable "option 2", temporarily enabled graph expensive checks for debugging.
    
    * Addressed review feedback: made var names consistent, fixed weights=None bug in cython code, added copyright to shuffle.py, changed how ranks are retrieved from the raft handle.
    
    * Removed debug prints.
    
    * Added PR 1163 to CHANGELOG.md
    
    * Removed extra newlines accidentally added to clean up diff in the PR, updated comment in cython code.
    
    * Added specific newlines back so file does not differ unnecessarily.
    
    * Disabled graph_t expensive check that was left enabled for debugging.
    
    * Added code path in call_louvain to support legacy graph types, to be removed when migration to graph_t types is complete.
    
    * Updates based on feedback from PR 1163: code cleanup/removed unused union members, consolidated legacy enum types, updated comments, initial support added for 64-bit vertex types (untested)
    
    * plumbed bool set based on running renumbering to set sorted_by_degree flag in graph container.
    
    * Added PR 1178 to CHANGELOG.md, C++ style fixes.
    
    * Addressed PR review feedback: added support for proper edge_t in cython wrapper and removed unnecessary vertex_t/edge_t int64,int32 combinations.
    
    Co-authored-by: Rick Ratzel <rratzel@nvidia.com>
    Co-authored-by: Chuck Hastings <charlesh@nvidia.com>
    Co-authored-by: Iroy30 <41401566+Iroy30@users.noreply.github.com>

--- python/cugraph/dask/community/louvain_wrapper.pyx
+++ python/cugraph/dask/community/louvain_wrapper.pyx
@@ -25,6 +25,13 @@ import cudf
 import numpy as np
 
 
+# FIXME: move this to a more reusable location
+numberTypeMap = {np.dtype("int32") : <int>numberTypeEnum.int32Type,
+                 np.dtype("int64") : <int>numberTypeEnum.int64Type,
+                 np.dtype("float32") : <int>numberTypeEnum.floatType,
+                 np.dtype("double") : <int>numberTypeEnum.doubleType}
+
+
 def louvain(input_df,
             num_global_verts,
             num_global_edges,
@@ -33,6 +40,7 @@ def louvain(input_df,
             vertex_partition_offsets,
             rank,
             handle,
+            sorted_by_degree,
             max_level,
             resolution):
     """
@@ -51,30 +59,28 @@ def louvain(input_df,
 
     src = input_df['src']
     dst = input_df['dst']
+    num_partition_edges = len(src)
+
     if "value" in input_df.columns:
         weights = input_df['value']
     else:
-        weights = None
+        weights = cudf.Series(np.full(num_partition_edges, 1.0, dtype=np.float32))
 
-    # FIXME: needs to be edge_t type not int
-    cdef int num_partition_edges = len(src)
+    vertex_t = src.dtype
+    if num_global_edges > (2**31 - 1):
+        edge_t = np.dtype("int64")
+    else:
+        edge_t = np.dtype("int32")
+    weight_t = weights.dtype
 
     # COO
     cdef uintptr_t c_src_vertices = src.__cuda_array_interface__['data'][0]
     cdef uintptr_t c_dst_vertices = dst.__cuda_array_interface__['data'][0]
-    cdef uintptr_t c_edge_weights = <uintptr_t>NULL
-    if weights is not None:
-        c_edge_weights = weights.__cuda_array_interface__['data'][0]
+    cdef uintptr_t c_edge_weights = weights.__cuda_array_interface__['data'][0]
 
     # data is on device, move to host (.values_host) since graph_t in
     # graph_container needs a host array
     cdef uintptr_t c_vertex_partition_offsets = vertex_partition_offsets.values_host.__array_interface__['data'][0]
-
-    # FIXME: Offsets and indices are currently hardcoded to int, but this may
-    #        not be acceptable in the future.
-    weightTypeMap = {np.dtype("float32") : <int>numberTypeEnum.floatType,
-                     np.dtype("double") : <int>numberTypeEnum.doubleType}
-    weightType = weightTypeMap[weights.dtype] if weights is not None else <int>numberTypeEnum.floatType
 
     cdef graph_container_t graph_container
 
@@ -85,30 +91,35 @@ def louvain(input_df,
                              handle_[0],
                              <void*>c_src_vertices, <void*>c_dst_vertices, <void*>c_edge_weights,
                              <void*>c_vertex_partition_offsets,
-                             <numberTypeEnum>(<int>(numberTypeEnum.intType)),
-                             <numberTypeEnum>(<int>(numberTypeEnum.intType)),
-                             <numberTypeEnum>(<int>(weightType)),
+                             <numberTypeEnum>(<int>(numberTypeMap[vertex_t])),
+                             <numberTypeEnum>(<int>(numberTypeMap[edge_t])),
+                             <numberTypeEnum>(<int>(numberTypeMap[weight_t])),
                              num_partition_edges,
                              num_global_verts, num_global_edges,
                              partition_row_size, partition_col_size,
+                             sorted_by_degree,
                              False, True)  # store_transposed, multi_gpu
 
     # Create the output dataframe
     df = cudf.DataFrame()
-    df['vertex'] = cudf.Series(np.zeros(num_global_verts, dtype=np.int32))
-    df['partition'] = cudf.Series(np.zeros(num_global_verts, dtype=np.int32))
+    df['vertex'] = cudf.Series(np.zeros(num_global_verts, dtype=vertex_t))
+    df['partition'] = cudf.Series(np.zeros(num_global_verts, dtype=vertex_t))
 
     cdef uintptr_t c_identifiers = df['vertex'].__cuda_array_interface__['data'][0]
     cdef uintptr_t c_partition = df['partition'].__cuda_array_interface__['data'][0]
 
-    if weightType == <int>numberTypeEnum.floatType:
+    if weight_t == np.float32:
         num_level, final_modularity_float = c_louvain.call_louvain[float](
-            handle_[0], graph_container, <void*>c_identifiers, <void*>c_partition, max_level, resolution)
+            handle_[0], graph_container,
+            <void*>c_identifiers, <void*>c_partition,
+            max_level, resolution)
         final_modularity = final_modularity_float
 
     else:
         num_level, final_modularity_double = c_louvain.call_louvain[double](
-            handle_[0], graph_container, <void*>c_identifiers, <void*>c_partition, max_level, resolution)
+            handle_[0], graph_container,
+            <void*>c_identifiers, <void*>c_partition,
+            max_level, resolution)
         final_modularity = final_modularity_double
 
     return df, final_modularity
