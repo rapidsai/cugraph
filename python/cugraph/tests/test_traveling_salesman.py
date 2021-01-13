f1730ed96e537a68ee68d85000d2e01781e6a829
Hugo Linsenmaier <hlinsenmaier@gmail.com>
2021-01-13 12:12:22 -0600

    Add python tests

---
+++ python/cugraph/tests/test_traveling_salesman.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2019-2020, NVIDIA CORPORATION.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import gc
+
+import pytest
+
+import cugraph
+from cugraph.tests import utils
+import rmm
+import cudf
+import time
+import numpy as np
+
+TSPLIB_REF = "../datasets/ref/tsp/tsplib.csv"
+
+# Temporarily suppress warnings till networkX fixes deprecation warnings
+# (Using or importing the ABCs from 'collections' instead of from
+# 'collections.abc' is deprecated, and in 3.8 it will stop working) for
+# python 3.7.  Also, this import networkx needs to be relocated in the
+# third-party group once this gets fixed.
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    import networkx as nx
+
+print("Networkx version : {} ".format(nx.__version__))
+
+
+def load_tsp(filename=None):
+    gdf = cudf.read_csv(filename,
+                        delim_whitespace=True,
+                        skiprows=6,
+                        names=["vertex", "x", "y"])
+    gdf = gdf.dropna()
+    return gdf
+
+
+@pytest.mark.parametrize("tsplib_file", utils.DATASETS_TSPLIB)
+def test_traveling_salesman(tsplib_file):
+    gc.collect()
+    pos_list = load_tsp(tsplib_file)
+    ref_gdf = cudf.read_csv(TSPLIB_REF, sep=",", names=["file", "cost"])
+    # cugraph
+    t1 = time.time()
+    cu_route, cu_cost = cugraph.traveling_salesman(pos_list,
+                                                   restarts=10000,
+                                                   verbose=False)
+    t2 = time.time() - t1
+    print("CuGraph time : " + str(t2))
+
+    tsp_instance = tsplib_file.split('/')[-1].split('.')[0]
+    ref_cost = int(ref_gdf.loc[ref_gdf["file"] == tsp_instance, 'cost'].values[0][0])
+    print("Cugraph cost: ", cu_cost)
+    print("Ref cost: ", ref_cost)
+
+    error = np.abs(cu_cost - ref_cost) / ref_cost
+    print("Approximation error is: {:.2f}%".format(error * 100))
+    # Check we are within 5% of TSPLIB
+    assert(error * 100 < 5.)
