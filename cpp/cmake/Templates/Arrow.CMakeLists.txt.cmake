8dc88ec4560708ab19d18296f3e530e066931b3a
Rick Ratzel <rratzel@nvidia.com>
2021-02-01 00:15:22 -0600

    Added CUDA_ARCHITECTURES=OFF setting for test targets, added min cmake requirement back to thirdparty modules to remove warning.

--- cpp/cmake/Templates/Arrow.CMakeLists.txt.cmake
+++ cpp/cmake/Templates/Arrow.CMakeLists.txt.cmake
@@ -14,6 +14,8 @@
 # limitations under the License.
 #=============================================================================
 
+cmake_minimum_required(VERSION 3.14)
+
 project(cugraph-Arrow)
 
 include(ExternalProject)
