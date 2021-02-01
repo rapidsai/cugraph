00add04090599096d7e7f0da883f3d132906b272
Rick Ratzel <rratzel@nvidia.com>
2021-01-31 23:48:29 -0600

    Updated cmake min version to 3.14 for consistency with rest of RAPIDS, replaced usage of CMAKE_SOURCE_DIR with CMAKE_CURRENT_SOURCE_DIR and added project() lines to thirdparty packages to better support users including cugraph as a 3rd party project in their applications (and for consistency with RAPIDS).

--- cpp/cmake/Templates/GoogleTest.CMakeLists.txt.cmake
+++ cpp/cmake/Templates/GoogleTest.CMakeLists.txt.cmake
@@ -1,19 +1,28 @@
-cmake_minimum_required(VERSION 3.12)
+#=============================================================================
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#=============================================================================
+
+project(cugraph-GoogleTest)
 
 include(ExternalProject)
 
 ExternalProject_Add(GoogleTest
                     GIT_REPOSITORY    https://github.com/google/googletest.git
                     GIT_TAG           release-1.8.0
+                    GIT_SHALLOW       true
                     SOURCE_DIR        "${GTEST_ROOT}/googletest"
                     BINARY_DIR        "${GTEST_ROOT}/build"
-                    INSTALL_DIR		  "${GTEST_ROOT}/install"
+                    INSTALL_DIR       "${GTEST_ROOT}/install"
                     CMAKE_ARGS        ${GTEST_CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${GTEST_ROOT}/install)
-
-
-
-
-
-
-
-
