d8d35e44150c9ea9aaabab7a547f2063e4bd73c3
Iroy30 <41401566+Iroy30@users.noreply.github.com>
2021-02-05 09:57:50 -0600

    update subgraph tests and remove legacy pagerank (#1378)
    Authors:
      - @Iroy30
    
    Approvers:
      - Brad Rees (@BradReesWork)
      - Alex Fender (@afender)
    
    URL: https://github.com/rapidsai/cugraph/pull/1378

--- python/cugraph/tests/test_ecg.py
+++ python/cugraph/tests/test_ecg.py
@@ -83,4 +83,6 @@ def test_ecg_clustering_nx(graph_file, min_weight, ensemble_size):
     )
 
     # Get the modularity score for partitioning versus random assignment
-    _ = cugraph.ecg(G, min_weight, ensemble_size, "weight")
+    df_dict = cugraph.ecg(G, min_weight, ensemble_size, "weight")
+
+    assert isinstance(df_dict, dict)
