f8fe57f377564a277aa6adaf9d7c18a6dfb58a75
root <jnke2016@gmail.com>
2021-03-07 21:20:03 +0000

    add datasets to test self-loops, string vertex IDs and isolated vertices

--- python/cugraph/tests/utils.py
+++ python/cugraph/tests/utils.py
@@ -61,6 +61,18 @@ DATASETS = [PurePath(RAPIDS_DATASET_ROOT_DIR)/f for f in [
     "netscience.csv"]
 ]
 
+DATASETS_SELF_LOOPS = [PurePath(RAPIDS_DATASET_ROOT_DIR)/f for f in [
+    "ca-HepTh.csv",
+    "ca-AstroPh.csv",
+    "ca-CondMat.csv",
+    "ca-GrQc.csv"]
+]
+
+DATASETS_STR_ISLD_V = [PurePath(RAPIDS_DATASET_ROOT_DIR)/f for f in [
+    "karate_mod.mtx",
+    "karate_str.mtx"]
+]
+
 
 #            '../datasets/email-Eu-core.csv']
 
