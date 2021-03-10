14713783fcc3d68fdc0c5f03b66b5157a07ae490
Ishika Roy <iroy@dgx07.aselab.nvidia.com>
2021-03-10 09:33:42 -0800

    new graph structure

---
+++ python/cugraph/structure/graph_implementation/npartiteGraph.py
@@ -0,0 +1,8 @@
+class npartiteGraphImpl:
+    self.edgelist = None
+    self.adjlist = None
+    self.transposedadjlist = None
+    self.renumber_map = None
+    self.partition_map = None
+
+
---
+++ python/cugraph/structure/graph_implementation/simpleDistributedGraph.py
@@ -0,0 +1,9 @@
+class simpleDistributedGraphImpl:
+    def __init__(self):
+        #Structure
+        self.edgelist = None
+        self.adjlist = None
+        self.transposedadjlist = None
+        self.renumber_map = None
+
+    #Functions
