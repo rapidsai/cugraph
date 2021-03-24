91d97103acb523724e9cc295e84cc948598262dd
afender <afender@nvidia.com>
2021-03-24 10:58:40 -0500

    doc

--- cpp/include/experimental/graph_generator.hpp
+++ cpp/include/experimental/graph_generator.hpp
@@ -47,7 +47,7 @@ namespace experimental {
  * handles to various CUDA libraries) to run graph algorithms.
  * @param scale Scale factor to set the number of verties in the graph. Vertex IDs have values in
  * [0, V), where V = 1 << @p scale.
- * @param num_edges Number of edges to generate.
+ * @param edge_factor Average number of edges per vertex to generate.
  * @param a a, b, c, d (= 1.0 - (a + b + c)) in the R-mat graph generator (vist https://graph500.org
  * for additional details). a, b, c, d should be non-negative and a + b + c should be no larger
  * than 1.0.
@@ -103,11 +103,14 @@ enum class generator_distribution_t { POWER_LAW = 0, UNIFORM };
  * @tparam vertex_t Type of vertex identifiers. Needs to be an integral type.
  * @param handle RAFT handle object to encapsulate resources (e.g. CUDA stream, communicator, and
  * handles to various CUDA libraries) to run graph algorithms.
- * @param scale Scale factor to set the number of verties in the graph. Vertex IDs have values in
- * [0, V), where V = 1 << @p scale.
- * @param num_edges Number of edges to generate.
- * @param a
- * @param b
+ * @param n_edgelists Number of edge lists (graphs) to generate
+ * @param min_scale Scale factor to set the minimum number of verties in the graph.
+ * @param max_scale Scale factor to set the maximum number of verties in the graph.
+ * @param edge_factor Average number of edges per vertex to generate.
+ * @param size_distribution Distribution of the graph sizes, impacts the scale parameter of the
+ * R-MAT generator
+ * @param edge_distribution Edges distribution for each graph, impacts how R-MAT parameters a,b,c,d,
+ * are set.
  * @param seed Seed value for the random number generator.
  * @param clip_and_flip Flag controlling whether to generate edges only in the lower triangular part
  * (including the diagonal) of the graph adjacency matrix (if set to `true`) or not (if set to
@@ -116,8 +119,9 @@ enum class generator_distribution_t { POWER_LAW = 0, UNIFORM };
  * or not (if set to `false`); scrambling vertx ID bits breaks correlation between vertex ID values
  * and vertex degrees. The scramble code here follows the algorithm in the Graph 500 reference
  * implementation version 3.0.0.
- * @return std::tuple<rmm::device_uvector<vertex_t>, rmm::device_uvector<vertex_t>> A tuple of
- * rmm::device_uvector objects for edge source vertex IDs and edge destination vertex IDs.
+ * @return A vector of std::tuple<rmm::device_uvector<vertex_t>, rmm::device_uvector<vertex_t>> of
+ *size @p n_edgelists, each vector element being a tuple of rmm::device_uvector objects for edge
+ *source vertex IDs and edge destination vertex IDs.
  */
 template <typename vertex_t>
 std::vector<std::tuple<rmm::device_uvector<vertex_t>, rmm::device_uvector<vertex_t>>>
@@ -126,12 +130,12 @@ generate_rmat_edgelists(
   size_t n_edgelists,
   size_t min_scale,
   size_t max_scale,
-  size_t edge_factor                              = 16,
-  generator_distribution_t component_distribution = generator_distribution_t::POWER_LAW,
-  generator_distribution_t edge_distribution      = generator_distribution_t::POWER_LAW,
-  uint64_t seed                                   = 0,
-  bool clip_and_flip                              = false,
-  bool scramble_vertex_ids                        = false);
+  size_t edge_factor                         = 16,
+  generator_distribution_t size_distribution = generator_distribution_t::POWER_LAW,
+  generator_distribution_t edge_distribution = generator_distribution_t::POWER_LAW,
+  uint64_t seed                              = 0,
+  bool clip_and_flip                         = false,
+  bool scramble_vertex_ids                   = false);
 
 }  // namespace experimental
 }  // namespace cugraph
