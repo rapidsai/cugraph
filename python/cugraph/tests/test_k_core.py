924f6782762d105cd5ab0f79111975d5e93916b5
Iroy30 <41401566+Iroy30@users.noreply.github.com>
2021-05-06 09:11:33 -0500

    add multi-column support in algorithms - part 1 (#1559)
    Enable multi-column support in algorithms.
    This PR updates:
    
    - Katz_centrality
    - Egonet
    - SSSP
    - Subgraph_extraction
    - K_core
    - Lap
    - RandonWalk - added test, waiting for Joseph's wrapper updates
    
    Authors:
      - https://github.com/Iroy30
    
    Approvers:
      - Brad Rees (https://github.com/BradReesWork)
    
    URL: https://github.com/rapidsai/cugraph/pull/1559

--- python/cugraph/tests/test_k_core.py
+++ python/cugraph/tests/test_k_core.py
@@ -57,7 +57,6 @@ def calc_k_cores(graph_file, directed=True):
 def compare_edges(cg, nxg):
     edgelist_df = cg.view_edge_list()
     src, dest = edgelist_df["src"], edgelist_df["dst"]
-
     assert cg.edgelist.weights is False
     assert len(src) == nxg.size()
     for i in range(len(src)):
@@ -66,7 +65,7 @@ def compare_edges(cg, nxg):
 
 
 @pytest.mark.parametrize("graph_file", utils.DATASETS_UNDIRECTED)
-def test_core_number_Graph(graph_file):
+def test_k_core_Graph(graph_file):
     gc.collect()
 
     cu_kcore, nx_kcore = calc_k_cores(graph_file, False)
@@ -75,7 +74,7 @@ def test_core_number_Graph(graph_file):
 
 
 @pytest.mark.parametrize("graph_file", utils.DATASETS_UNDIRECTED)
-def test_core_number_Graph_nx(graph_file):
+def test_k_core_Graph_nx(graph_file):
     gc.collect()
 
     NM = utils.read_csv_for_nx(graph_file)
@@ -86,3 +85,35 @@ def test_core_number_Graph_nx(graph_file):
     cc = cugraph.k_core(Gnx)
 
     assert nx.is_isomorphic(nc, cc)
+
+
+@pytest.mark.parametrize("graph_file", utils.DATASETS_UNDIRECTED)
+def test_k_core_corenumber_multicolumn(graph_file):
+    gc.collect()
+
+    cu_M = utils.read_csv_file(graph_file)
+    cu_M.rename(columns={'0': 'src_0', '1': 'dst_0'}, inplace=True)
+    cu_M['src_1'] = cu_M['src_0'] + 1000
+    cu_M['dst_1'] = cu_M['dst_0'] + 1000
+
+    G1 = cugraph.Graph()
+    G1.from_cudf_edgelist(cu_M, source=["src_0", "src_1"],
+                          destination=["dst_0", "dst_1"])
+
+    corenumber_G1 = cugraph.core_number(G1)
+    corenumber_G1.rename(columns={'core_number': 'values'}, inplace=True)
+    corenumber_G1 = corenumber_G1[['0_vertex', '1_vertex', 'values']]
+
+    ck_res = cugraph.k_core(G1, core_number=corenumber_G1)
+    G2 = cugraph.Graph()
+    G2.from_cudf_edgelist(cu_M, source="src_0",
+                          destination="dst_0")
+    ck_exp = cugraph.k_core(G2)
+
+    # FIXME: Replace with multi-column view_edge_list()
+    edgelist_df = ck_res.edgelist.edgelist_df
+    edgelist_df_res = ck_res.unrenumber(edgelist_df, "src")
+    edgelist_df_res = ck_res.unrenumber(edgelist_df_res, "dst")
+    for i in range(len(edgelist_df_res)):
+        assert ck_exp.has_edge(edgelist_df_res["0_src"].iloc[i],
+                               edgelist_df_res["0_dst"].iloc[i])
