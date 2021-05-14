2871281b37ce29ffb9ad3b48e5379332ab2a0202
Seunghwa Kang <45857425+seunghwak@users.noreply.github.com>
2021-05-14 09:37:15 -0400

    Update graph primitives to support vertex-tagging in frontier expansion (#1551)
    To get early feedback and to create a baseline weakly-connected-component PR dependent on this PR.
    This PR requires C++17 and will not compile till https://github.com/rapidsai/cugraph/issues/1528 gets resolved.
    
    Tagging vertex IDs with root IDs is necessary in 
    
    multi-source BFS, k-hop neighbors, and the newly designed weakly-connected-component algorithm based on multi-root collaborative frontier expansion.
    
    To fully support multi-source BFS & k-hop neighbors, we also need to store/query properties for (vertex, tag) pairs, and this update will be added in a future PR.
    
    Authors:
      - Seunghwa Kang (https://github.com/seunghwak)
    
    Approvers:
      - Chuck Hastings (https://github.com/ChuckHastings)
    
    URL: https://github.com/rapidsai/cugraph/pull/1551

--- cpp/include/cugraph/patterns/copy_v_transform_reduce_in_out_nbr.cuh
+++ cpp/include/cugraph/patterns/copy_v_transform_reduce_in_out_nbr.cuh
@@ -101,6 +101,7 @@ __global__ void for_all_major_for_all_nbr_low_degree(
                           ? static_cast<vertex_t>(major_offset)
                           : minor_offset;
       return evaluate_edge_op<GraphViewType,
+                              vertex_t,
                               AdjMatrixRowValueInputIterator,
                               AdjMatrixColValueInputIterator,
                               EdgeOp>()
@@ -189,6 +190,7 @@ __global__ void for_all_major_for_all_nbr_mid_degree(
                           ? static_cast<vertex_t>(major_offset)
                           : minor_offset;
       auto e_op_result = evaluate_edge_op<GraphViewType,
+                                          vertex_t,
                                           AdjMatrixRowValueInputIterator,
                                           AdjMatrixColValueInputIterator,
                                           EdgeOp>()
@@ -263,6 +265,7 @@ __global__ void for_all_major_for_all_nbr_high_degree(
                           ? static_cast<vertex_t>(major_offset)
                           : minor_offset;
       auto e_op_result = evaluate_edge_op<GraphViewType,
+                                          vertex_t,
                                           AdjMatrixRowValueInputIterator,
                                           AdjMatrixColValueInputIterator,
                                           EdgeOp>()
