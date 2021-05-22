4cb7e30a1aba7d1796fca09093a0d3347f07e6e4
Andrei Schaffer <aschaffer@nvidia.com>
2021-05-22 16:04:46 -0500

    Graph compare utility for testing / debugging.

--- cpp/tests/serialization/un_serialize_test.cpp
+++ cpp/tests/serialization/un_serialize_test.cpp
@@ -44,20 +44,6 @@ TEST(SerializationTest, GraphSerUnser)
 
   auto graph = cugraph::test::make_graph(handle, v_src, v_dst, v_w, num_vertices, num_edges, true);
 
-  auto graph_view = graph.view();
-
-  edge_t const* offsets   = graph_view.offsets();
-  vertex_t const* indices = graph_view.indices();
-  weight_t const* values  = graph_view.weights();
-
-  std::vector<edge_t> v_ro(num_vertices + 1);
-  std::vector<vertex_t> v_ci(num_edges);
-  std::vector<weight_t> v_vs(num_edges);
-
-  raft::update_host(v_ro.data(), offsets, num_vertices + 1, handle.get_stream());
-  raft::update_host(v_ci.data(), indices, num_edges, handle.get_stream());
-  raft::update_host(v_vs.data(), values, num_edges, handle.get_stream());
-
   auto pair_sz      = serializer_t::get_device_graph_sz_bytes(graph);
   auto total_ser_sz = pair_sz.first + pair_sz.second;
 
@@ -72,30 +58,10 @@ TEST(SerializationTest, GraphSerUnser)
 
   auto graph_copy = ser.unserialize<decltype(graph)>(pair_sz.first, pair_sz.second);
 
-  auto graph_copy_view   = graph_copy.view();
-  auto num_vertices_copy = graph_copy_view.get_number_of_vertices();
-  auto num_edges_copy    = graph_copy_view.get_number_of_edges();
+  auto pair = cugraph::test::compare_graphs(handle, graph, graph_copy);
+  if (pair.first == false) std::cerr << "Test failed with " << pair.second << ".\n";
 
-  EXPECT_EQ(num_vertices, num_vertices_copy);
-  EXPECT_EQ(num_edges, num_edges_copy);
-  EXPECT_EQ(graph.is_symmetric(), graph_copy.is_symmetric());
-  EXPECT_EQ(graph.is_multigraph(), graph_copy.is_multigraph());
-  EXPECT_EQ(graph.is_weighted(), graph_copy.is_weighted());
-
-  std::vector<edge_t> v_ro_copy(num_vertices + 1);
-  std::vector<vertex_t> v_ci_copy(num_edges);
-  std::vector<weight_t> v_vs_copy(num_edges);
-
-  raft::update_host(
-    v_ro_copy.data(), graph_copy_view.offsets(), num_vertices + 1, handle.get_stream());
-  raft::update_host(v_ci_copy.data(), graph_copy_view.indices(), num_edges, handle.get_stream());
-  raft::update_host(v_vs_copy.data(), graph_copy_view.weights(), num_edges, handle.get_stream());
-
-  EXPECT_EQ(v_ro, v_ro_copy);
-  EXPECT_EQ(v_ci, v_ci_copy);
-  EXPECT_EQ(v_vs, v_vs_copy);
-  EXPECT_EQ(graph_view.get_local_adj_matrix_partition_segment_offsets(0),
-            graph_copy_view.get_local_adj_matrix_partition_segment_offsets(0));
+  ASSERT_TRUE(pair.first);
 }
 
 TEST(SerializationTest, GraphDecoupledSerUnser)
@@ -117,20 +83,6 @@ TEST(SerializationTest, GraphDecoupledSerUnser)
   std::vector<weight_t> v_w{0.1, 1.1, 2.1, 3.1, 4.1, 5.1, 6.1, 7.1};
 
   auto graph = cugraph::test::make_graph(handle, v_src, v_dst, v_w, num_vertices, num_edges, true);
-
-  auto graph_view = graph.view();
-
-  edge_t const* offsets   = graph_view.offsets();
-  vertex_t const* indices = graph_view.indices();
-  weight_t const* values  = graph_view.weights();
-
-  std::vector<edge_t> v_ro(num_vertices + 1);
-  std::vector<vertex_t> v_ci(num_edges);
-  std::vector<weight_t> v_vs(num_edges);
-
-  raft::update_host(v_ro.data(), offsets, num_vertices + 1, handle.get_stream());
-  raft::update_host(v_ci.data(), indices, num_edges, handle.get_stream());
-  raft::update_host(v_vs.data(), values, num_edges, handle.get_stream());
 
   auto pair_sz      = serializer_t::get_device_graph_sz_bytes(graph);
   auto total_ser_sz = pair_sz.first + pair_sz.second;
@@ -159,30 +111,10 @@ TEST(SerializationTest, GraphDecoupledSerUnser)
 
     auto graph_copy = ser.unserialize<decltype(graph)>(pair_sz.first, pair_sz.second);
 
-    auto graph_copy_view   = graph_copy.view();
-    auto num_vertices_copy = graph_copy_view.get_number_of_vertices();
-    auto num_edges_copy    = graph_copy_view.get_number_of_edges();
+    auto pair = cugraph::test::compare_graphs(handle, graph, graph_copy);
+    if (pair.first == false) std::cerr << "Test failed with " << pair.second << ".\n";
 
-    EXPECT_EQ(num_vertices, num_vertices_copy);
-    EXPECT_EQ(num_edges, num_edges_copy);
-    EXPECT_EQ(graph.is_symmetric(), graph_copy.is_symmetric());
-    EXPECT_EQ(graph.is_multigraph(), graph_copy.is_multigraph());
-    EXPECT_EQ(graph.is_weighted(), graph_copy.is_weighted());
-
-    std::vector<edge_t> v_ro_copy(num_vertices + 1);
-    std::vector<vertex_t> v_ci_copy(num_edges);
-    std::vector<weight_t> v_vs_copy(num_edges);
-
-    raft::update_host(
-      v_ro_copy.data(), graph_copy_view.offsets(), num_vertices + 1, handle.get_stream());
-    raft::update_host(v_ci_copy.data(), graph_copy_view.indices(), num_edges, handle.get_stream());
-    raft::update_host(v_vs_copy.data(), graph_copy_view.weights(), num_edges, handle.get_stream());
-
-    EXPECT_EQ(v_ro, v_ro_copy);
-    EXPECT_EQ(v_ci, v_ci_copy);
-    EXPECT_EQ(v_vs, v_vs_copy);
-    EXPECT_EQ(graph_view.get_local_adj_matrix_partition_segment_offsets(0),
-              graph_copy_view.get_local_adj_matrix_partition_segment_offsets(0));
+    ASSERT_TRUE(pair.first);
   }
 }
 
