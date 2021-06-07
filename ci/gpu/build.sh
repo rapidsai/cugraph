0f4fa068f77f324098e1ebefc1cecf25b956624a
Peter Andreas Entschev <peter@entschev.com>
2021-06-07 07:29:14 -0700

    Update UCX-Py version to 0.21

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -62,7 +62,7 @@ gpuci_conda_retry install -y \
       "cudatoolkit=$CUDA_REL" \
       "dask-cudf=${MINOR_VERSION}" \
       "dask-cuda=${MINOR_VERSION}" \
-      "ucx-py=${MINOR_VERSION}" \
+      "ucx-py=0.21.*" \
       "ucx-proc=*=gpu" \
       "rapids-build-env=$MINOR_VERSION.*" \
       "rapids-notebook-env=$MINOR_VERSION.*" \
