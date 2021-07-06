ae9df4ed054b54821e4de5908e626fe476bd17b4
Conor Hoekstra <codereport@outlook.com>
2021-07-06 16:19:17 -0400

    Update copyright headers

--- cpp/src/traversal/bfs.cuh
+++ cpp/src/traversal/bfs.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2020, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2019-2021, NVIDIA CORPORATION.  All rights reserved.
  *
  * NVIDIA CORPORATION and its licensors retain all intellectual property
  * and proprietary rights in and to this software, related documentation
