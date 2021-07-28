7c603dd18d0ac69cb8a39404626010cb95e10501
Rick Ratzel <3039903+rlratzel@users.noreply.github.com>
2021-07-28 14:58:05 -0500

    Pinning cuco to a specific commit hash for release (#1741)
    Using the latest `cuco` commit hash as the `GIT_TAG` for release purposes.  See also RAFT PR https://github.com/rapidsai/raft/pull/304 .
    Also updated a debug message.
    
    NOTE: using `skip-ci` until https://github.com/rapidsai/raft/pull/304 is merged.
    NOTE: The commit hash will likely be reverted to `dev` in 21.10 during development, then a new hash will be used for 21.10 release, and so on.
    
    Authors:
      - Rick Ratzel (https://github.com/rlratzel)
    
    Approvers:
      - Chuck Hastings (https://github.com/ChuckHastings)
    
    URL: https://github.com/rapidsai/cugraph/pull/1741

--- cpp/cmake/thirdparty/get_cuco.cmake
+++ cpp/cmake/thirdparty/get_cuco.cmake
@@ -20,7 +20,7 @@ function(find_and_configure_cuco VERSION)
       GLOBAL_TARGETS cuco cuco::cuco
       CPM_ARGS
         GIT_REPOSITORY https://github.com/NVIDIA/cuCollections.git
-        GIT_TAG        dev
+        GIT_TAG        b1fea0cbe4c384160740af00f7c8760846539abb
         OPTIONS        "BUILD_TESTS OFF"
                        "BUILD_BENCHMARKS OFF"
                        "BUILD_EXAMPLES OFF"
