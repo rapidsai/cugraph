ba702615192ddae293446e28c8200f2e54a5bc86
Joseph Nke <jnke@dgx18.aselab.nvidia.com>
2021-08-04 15:11:06 -0700

    consolidate these tests to use the fixture dask_client

--- python/cugraph/tests/dask/test_mg_batch_betweenness_centrality.py
+++ python/cugraph/tests/dask/test_mg_batch_betweenness_centrality.py
@@ -14,7 +14,6 @@
 import pytest
 import numpy as np
 
-from cugraph.tests.dask.mg_context import MGContext, skip_if_not_enough_devices
 from cugraph.dask.common.mg_utils import is_single_gpu
 from cugraph.tests import utils
 
@@ -46,11 +45,6 @@ DATASETS = [utils.DATASETS_UNDIRECTED[0]]
 # scripts can filter these tests out. Consider a different mechanism that's
 # compatible with testing scripts that need to control GPU count and cluster
 # setup/teardown.
-MG_DEVICE_COUNT_OPTIONS = [pytest.param(1, marks=pytest.mark.preset_gpu_count),
-                           pytest.param(2, marks=pytest.mark.preset_gpu_count),
-                           pytest.param(3, marks=pytest.mark.preset_gpu_count),
-                           pytest.param(4, marks=pytest.mark.preset_gpu_count),
-                           None]
 RESULT_DTYPE_OPTIONS = [np.float64]
 
 
@@ -66,7 +60,6 @@ RESULT_DTYPE_OPTIONS = [np.float64]
 @pytest.mark.parametrize("endpoints", ENDPOINTS_OPTIONS)
 @pytest.mark.parametrize("subset_seed", SUBSET_SEED_OPTIONS)
 @pytest.mark.parametrize("result_dtype", RESULT_DTYPE_OPTIONS)
-@pytest.mark.parametrize("mg_device_count", MG_DEVICE_COUNT_OPTIONS)
 def test_mg_betweenness_centrality(
     graph_file,
     directed,
@@ -76,23 +69,20 @@ def test_mg_betweenness_centrality(
     endpoints,
     subset_seed,
     result_dtype,
-    mg_device_count,
+    dask_client,
 ):
     prepare_test()
-    skip_if_not_enough_devices(mg_device_count)
-    with MGContext(number_of_devices=mg_device_count,
-                   p2p=True):
-        sorted_df = calc_betweenness_centrality(
-            graph_file,
-            directed=directed,
-            normalized=normalized,
-            k=subset_size,
-            weight=weight,
-            endpoints=endpoints,
-            seed=subset_seed,
-            result_dtype=result_dtype,
-            multi_gpu_batch=True,
-        )
+    sorted_df = calc_betweenness_centrality(
+        graph_file,
+        directed=directed,
+        normalized=normalized,
+        k=subset_size,
+        weight=weight,
+        endpoints=endpoints,
+        seed=subset_seed,
+        result_dtype=result_dtype,
+        multi_gpu_batch=True,
+    )
     compare_scores(
         sorted_df,
         first_key="cu_bc",
--- python/cugraph/tests/dask/test_mg_batch_edge_betweenness_centrality.py
+++ python/cugraph/tests/dask/test_mg_batch_edge_betweenness_centrality.py
@@ -16,7 +16,6 @@ import numpy as np
 
 from cugraph.dask.common.mg_utils import is_single_gpu
 
-from cugraph.tests.dask.mg_context import MGContext, skip_if_not_enough_devices
 from cugraph.tests.utils import RAPIDS_DATASET_ROOT_DIR_PATH
 
 # Get parameters from standard betwenness_centrality_test
@@ -47,11 +46,6 @@ DATASETS = [(RAPIDS_DATASET_ROOT_DIR_PATH / "karate.csv").as_posix()]
 # scripts can filter these tests out. Consider a different mechanism that's
 # compatible with testing scripts that need to control GPU count and cluster
 # setup/teardown.
-MG_DEVICE_COUNT_OPTIONS = [pytest.param(1, marks=pytest.mark.preset_gpu_count),
-                           pytest.param(2, marks=pytest.mark.preset_gpu_count),
-                           pytest.param(3, marks=pytest.mark.preset_gpu_count),
-                           pytest.param(4, marks=pytest.mark.preset_gpu_count),
-                           None]
 RESULT_DTYPE_OPTIONS = [np.float64]
 
 
@@ -66,7 +60,6 @@ RESULT_DTYPE_OPTIONS = [np.float64]
 @pytest.mark.parametrize("weight", [None])
 @pytest.mark.parametrize("subset_seed", SUBSET_SEED_OPTIONS)
 @pytest.mark.parametrize("result_dtype", RESULT_DTYPE_OPTIONS)
-@pytest.mark.parametrize("mg_device_count", MG_DEVICE_COUNT_OPTIONS)
 def test_mg_edge_betweenness_centrality(
     graph_file,
     directed,
@@ -75,22 +68,19 @@ def test_mg_edge_betweenness_centrality(
     weight,
     subset_seed,
     result_dtype,
-    mg_device_count,
+    dask_client,
 ):
     prepare_test()
-    skip_if_not_enough_devices(mg_device_count)
-    with MGContext(number_of_devices=mg_device_count,
-                   p2p=True):
-        sorted_df = calc_edge_betweenness_centrality(
-            graph_file,
-            directed=directed,
-            normalized=normalized,
-            k=subset_size,
-            weight=weight,
-            seed=subset_seed,
-            result_dtype=result_dtype,
-            multi_gpu_batch=True,
-        )
+    sorted_df = calc_edge_betweenness_centrality(
+        graph_file,
+        directed=directed,
+        normalized=normalized,
+        k=subset_size,
+        weight=weight,
+        seed=subset_seed,
+        result_dtype=result_dtype,
+        multi_gpu_batch=True,
+    )
     compare_scores(
         sorted_df,
         first_key="cu_bc",
