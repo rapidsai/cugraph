45b6eeb8d9a3d8be8284d4ae2b66314b11277b29
Kumar Aatish <kaatish@nvidia.com>
2021-08-10 11:10:29 -0400

    Transform Reduce V test
    Fixed comparator in reduce_v test.

--- cpp/tests/prims/mg_transform_reduce_v.cu
+++ cpp/tests/prims/mg_transform_reduce_v.cu
@@ -26,7 +26,7 @@
 
 #include <cuco/detail/hash_functions.cuh>
 #include <cugraph/experimental/graph_view.hpp>
-#include <cugraph/prims/reduce_v.cuh>
+#include <cugraph/prims/transform_reduce_v.cuh>
 
 #include <thrust/count.h>
 #include <raft/comms/comms.hpp>
@@ -44,8 +44,21 @@
 //
 static int PERF = 0;
 
+template <typename vertex_t, typename T>
+struct property_transform : public thrust::unary_function<vertex_t, T> {
+  int mod{};
+  property_transform(int mod_count) : mod(mod_count) {}
+  __device__ auto operator()(const vertex_t& val)
+  {
+    cuco::detail::MurmurHash3_32<vertex_t> hash_func{};
+    auto value = hash_func(val) % mod;
+    return static_cast<T>(value);
+  }
+};
+
 template <typename vertex_t, typename... T>
-struct property_transform : public thrust::unary_function<vertex_t, thrust::tuple<T...>> {
+struct property_transform<vertex_t, std::tuple<T...>>
+  : public thrust::unary_function<vertex_t, thrust::tuple<T...>> {
   int mod{};
   property_transform(int mod_count) : mod(mod_count) {}
   __device__ auto operator()(const vertex_t& val)
@@ -53,72 +66,6 @@ struct property_transform : public thrust::unary_function<vertex_t, thrust::tupl
     cuco::detail::MurmurHash3_32<vertex_t> hash_func{};
     auto value = hash_func(val) % mod;
     return thrust::make_tuple(static_cast<T>(value)...);
-  }
-};
-template <typename vertex_t, template <typename...> typename Tuple, typename... T>
-struct property_transform<vertex_t, Tuple<T...>> : public property_transform<vertex_t, T...> {
-};
-
-template <typename Tuple, std::size_t... I>
-auto make_iterator_tuple(Tuple& data, std::index_sequence<I...>)
-{
-  return thrust::make_tuple((std::get<I>(data).begin())...);
-}
-
-template <typename... T>
-auto get_zip_iterator(std::tuple<T...>& data)
-{
-  return thrust::make_zip_iterator(make_iterator_tuple(
-    data, std::make_index_sequence<std::tuple_size<std::tuple<T...>>::value>()));
-}
-
-template <typename T>
-auto get_property_iterator(std::tuple<T>& data)
-{
-  return (std::get<0>(data)).begin();
-}
-
-template <typename T0, typename... T>
-auto get_property_iterator(std::tuple<T0, T...>& data)
-{
-  return get_zip_iterator(data);
-}
-
-template <typename... T>
-struct generate_impl {
-  static thrust::tuple<T...> initial_value(int init)
-  {
-    return thrust::make_tuple(static_cast<T>(init)...);
-  }
-  template <typename label_t>
-  static std::tuple<rmm::device_uvector<T>...> property(rmm::device_uvector<label_t>& labels,
-                                                        int hash_bin_count,
-                                                        raft::handle_t const& handle)
-  {
-    auto data = std::make_tuple(rmm::device_uvector<T>(labels.size(), handle.get_stream())...);
-    auto zip  = get_zip_iterator(data);
-    thrust::transform(rmm::exec_policy(handle.get_stream())->on(handle.get_stream()),
-                      labels.begin(),
-                      labels.end(),
-                      zip,
-                      property_transform<label_t, T...>(hash_bin_count));
-    return data;
-  }
-  template <typename label_t>
-  static std::tuple<rmm::device_uvector<T>...> property(thrust::counting_iterator<label_t> begin,
-                                                        thrust::counting_iterator<label_t> end,
-                                                        int hash_bin_count,
-                                                        raft::handle_t const& handle)
-  {
-    auto length = thrust::distance(begin, end);
-    auto data   = std::make_tuple(rmm::device_uvector<T>(length, handle.get_stream())...);
-    auto zip    = get_zip_iterator(data);
-    thrust::transform(rmm::exec_policy(handle.get_stream())->on(handle.get_stream()),
-                      begin,
-                      end,
-                      zip,
-                      property_transform<label_t, T...>(hash_bin_count));
-    return data;
   }
 };
 
@@ -162,11 +109,15 @@ struct result_compare<thrust::tuple<Args...>> {
 };
 
 template <typename T>
-struct generate : public generate_impl<T> {
+struct generate {
   static T initial_value(int init) { return static_cast<T>(init); }
 };
 template <typename... T>
-struct generate<std::tuple<T...>> : public generate_impl<T...> {
+struct generate<std::tuple<T...>> {
+  static thrust::tuple<T...> initial_value(int init)
+  {
+    return thrust::make_tuple(static_cast<T>(init)...);
+  }
 };
 
 struct Prims_Usecase {
@@ -174,10 +125,10 @@ struct Prims_Usecase {
 };
 
 template <typename input_usecase_t>
-class Tests_MG_ReduceV
+class Tests_MG_TransformReduceV
   : public ::testing::TestWithParam<std::tuple<Prims_Usecase, input_usecase_t>> {
  public:
-  Tests_MG_ReduceV() {}
+  Tests_MG_TransformReduceV() {}
   static void SetupTestCase() {}
   static void TearDownTestCase() {}
 
@@ -230,15 +181,13 @@ class Tests_MG_ReduceV
 
     auto mg_graph_view = mg_graph.view();
 
-    // 3. run MG reduce v
+    // 3. run MG transform reduce
 
     const int hash_bin_count = 5;
     const int initial_value  = 10;
 
+    property_transform<vertex_t, result_t> prop(hash_bin_count);
     auto property_initial_value = generate<result_t>::initial_value(initial_value);
-    auto property_data =
-      generate<result_t>::property((*d_mg_renumber_map_labels), hash_bin_count, handle);
-    auto property_iter = get_property_iterator(property_data);
 
     if (PERF) {
       CUDA_TRY(cudaDeviceSynchronize());  // for consistent performance measurement
@@ -246,18 +195,15 @@ class Tests_MG_ReduceV
       hr_clock.start();
     }
 
-    auto result = reduce_v(handle,
-                           mg_graph_view,
-                           property_iter,
-                           property_iter + (*d_mg_renumber_map_labels).size(),
-                           property_initial_value);
+    auto result = transform_reduce_v(
+      handle, mg_graph_view, d_mg_renumber_map_labels->begin(), prop, property_initial_value);
 
     if (PERF) {
       CUDA_TRY(cudaDeviceSynchronize());  // for consistent performance measurement
       handle.get_comms().barrier();
       double elapsed_time{0.0};
       hr_clock.stop(&elapsed_time);
-      std::cout << "MG reduce_v took " << elapsed_time * 1e-6 << " s.\n";
+      std::cout << "MG transform reduce took " << elapsed_time * 1e-6 << " s.\n";
     }
 
     //// 4. compare SG & MG results
@@ -269,77 +215,71 @@ class Tests_MG_ReduceV
         input_usecase.template construct_graph<vertex_t, edge_t, weight_t, store_transposed, false>(
           handle, true, false);
       auto sg_graph_view = sg_graph.view();
+      using property_t   = decltype(property_initial_value);
 
-      auto sg_property_data = generate<result_t>::property(
+      auto expected_result = thrust::transform_reduce(
+        rmm::exec_policy(handle.get_stream())->on(handle.get_stream()),
         thrust::make_counting_iterator(sg_graph_view.get_local_vertex_first()),
         thrust::make_counting_iterator(sg_graph_view.get_local_vertex_last()),
-        hash_bin_count,
-        handle);
-      auto sg_property_iter = get_property_iterator(sg_property_data);
-      using property_t      = decltype(property_initial_value);
-
-      auto expected_result =
-        thrust::reduce(rmm::exec_policy(handle.get_stream())->on(handle.get_stream()),
-                       sg_property_iter,
-                       sg_property_iter + sg_graph_view.get_number_of_local_vertices(),
-                       property_initial_value,
-                       cugraph::experimental::property_add<property_t>());
+        prop,
+        property_initial_value,
+        cugraph::experimental::property_add<property_t>());
       result_compare<property_t> compare;
       ASSERT_TRUE(compare(expected_result, result));
     }
   }
 };
 
-using Tests_MG_ReduceV_File = Tests_MG_ReduceV<cugraph::test::File_Usecase>;
-using Tests_MG_ReduceV_Rmat = Tests_MG_ReduceV<cugraph::test::Rmat_Usecase>;
+using Tests_MG_TransformReduceV_File = Tests_MG_TransformReduceV<cugraph::test::File_Usecase>;
+using Tests_MG_TransformReduceV_Rmat = Tests_MG_TransformReduceV<cugraph::test::Rmat_Usecase>;
 
-TEST_P(Tests_MG_ReduceV_File, CheckInt32Int32FloatTupleIntFloatTransposeFalse)
+TEST_P(Tests_MG_TransformReduceV_File, CheckInt32Int32FloatTupleIntFloatTransposeFalse)
 {
   auto param = GetParam();
   run_current_test<int32_t, int32_t, float, std::tuple<int, float>, false>(std::get<0>(param),
                                                                            std::get<1>(param));
 }
 
-TEST_P(Tests_MG_ReduceV_Rmat, CheckInt32Int32FloatTupleIntFloatTransposeFalse)
+TEST_P(Tests_MG_TransformReduceV_Rmat, CheckInt32Int32FloatTupleIntFloatTransposeFalse)
 {
   auto param = GetParam();
   run_current_test<int32_t, int32_t, float, std::tuple<int, float>, false>(std::get<0>(param),
                                                                            std::get<1>(param));
 }
 
-TEST_P(Tests_MG_ReduceV_File, CheckInt32Int32FloatTupleIntFloatTransposeTrue)
+TEST_P(Tests_MG_TransformReduceV_File, CheckInt32Int32FloatTupleIntFloatTransposeTrue)
 {
   auto param = GetParam();
   run_current_test<int32_t, int32_t, float, std::tuple<int, float>, true>(std::get<0>(param),
                                                                           std::get<1>(param));
 }
 
-TEST_P(Tests_MG_ReduceV_Rmat, CheckInt32Int32FloatTupleIntFloatTransposeTrue)
+TEST_P(Tests_MG_TransformReduceV_Rmat, CheckInt32Int32FloatTupleIntFloatTransposeTrue)
 {
   auto param = GetParam();
   run_current_test<int32_t, int32_t, float, std::tuple<int, float>, true>(std::get<0>(param),
                                                                           std::get<1>(param));
 }
 
-TEST_P(Tests_MG_ReduceV_File, CheckInt32Int32FloatTransposeFalse)
+TEST_P(Tests_MG_TransformReduceV_File, CheckInt32Int32FloatTransposeFalse)
 {
   auto param = GetParam();
   run_current_test<int32_t, int32_t, float, int, false>(std::get<0>(param), std::get<1>(param));
 }
 
-TEST_P(Tests_MG_ReduceV_Rmat, CheckInt32Int32FloatTransposeFalse)
+TEST_P(Tests_MG_TransformReduceV_Rmat, CheckInt32Int32FloatTransposeFalse)
 {
   auto param = GetParam();
   run_current_test<int32_t, int32_t, float, int, false>(std::get<0>(param), std::get<1>(param));
 }
 
-TEST_P(Tests_MG_ReduceV_File, CheckInt32Int32FloatTransposeTrue)
+TEST_P(Tests_MG_TransformReduceV_File, CheckInt32Int32FloatTransposeTrue)
 {
   auto param = GetParam();
   run_current_test<int32_t, int32_t, float, int, true>(std::get<0>(param), std::get<1>(param));
 }
 
-TEST_P(Tests_MG_ReduceV_Rmat, CheckInt32Int32FloatTransposeTrue)
+TEST_P(Tests_MG_TransformReduceV_Rmat, CheckInt32Int32FloatTransposeTrue)
 {
   auto param = GetParam();
   run_current_test<int32_t, int32_t, float, int, true>(std::get<0>(param), std::get<1>(param));
@@ -347,7 +287,7 @@ TEST_P(Tests_MG_ReduceV_Rmat, CheckInt32Int32FloatTransposeTrue)
 
 INSTANTIATE_TEST_SUITE_P(
   file_test,
-  Tests_MG_ReduceV_File,
+  Tests_MG_TransformReduceV_File,
   ::testing::Combine(
     ::testing::Values(Prims_Usecase{true}),
     ::testing::Values(cugraph::test::File_Usecase("test/datasets/karate.mtx"),
@@ -357,14 +297,14 @@ INSTANTIATE_TEST_SUITE_P(
 
 INSTANTIATE_TEST_SUITE_P(
   rmat_small_test,
-  Tests_MG_ReduceV_Rmat,
+  Tests_MG_TransformReduceV_Rmat,
   ::testing::Combine(::testing::Values(Prims_Usecase{true}),
                      ::testing::Values(cugraph::test::Rmat_Usecase(
                        10, 16, 0.57, 0.19, 0.19, 0, false, false, 0, true))));
 
 INSTANTIATE_TEST_SUITE_P(
   rmat_large_test,
-  Tests_MG_ReduceV_Rmat,
+  Tests_MG_TransformReduceV_Rmat,
   ::testing::Combine(::testing::Values(Prims_Usecase{false}),
                      ::testing::Values(cugraph::test::Rmat_Usecase(
                        20, 32, 0.57, 0.19, 0.19, 0, false, false, 0, true))));
