da1bef6da9747ca7845ae9e183e70140e5f4f24f
root <iroy@nvidia.com>
2021-08-31 21:26:56 +0000

    connected components

--- python/pylibcugraph/pylibcugraph/components/_connectivity.pyx
+++ python/pylibcugraph/pylibcugraph/components/_connectivity.pyx
@@ -11,10 +11,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-def strongly_connected_components(src, dst, weights, num_verts, num_edges, labels):
-    raise NotImplementedError()
+from pylibcugraph.components.connectivity cimport *
+from libc.stdint cimport uintptr_t
+from libcpp.memory cimport unique_ptr
 
 
 def weakly_connected_components(src, dst, weights, num_verts, num_edges, labels):
-    raise NotImplementedError()
+
+    cdef unique_ptr[handle_t] handle_ptr
+    handle_ptr.reset(new handle_t())
+    handle_ = handle_ptr.get()
+
+    cdef uintptr_t c_src_vertices    = src['data'][0]
+    cdef uintptr_t c_dst_vertices    = dst['data'][0]
+    cdef uintptr_t c_edge_weights = <uintptr_t>NULL
+    cdef uintptr_t c_labels_val = labels['data'][0]
+
+    cdef graph_container_t graph_container
+    populate_graph_container(graph_container,
+                             handle_[0],
+                             <void*>c_src_vertices, <void*>c_dst_vertices, <void*>c_edge_weights,
+                             <void*>NULL,
+                             <void*>NULL,
+                             0,
+                             <numberTypeEnum>(<int>(numberTypeEnum.int32Type)),
+                             <numberTypeEnum>(<int>(numberTypeEnum.int32Type)),
+                             <numberTypeEnum>(<int>(numberTypeEnum.floatType)),
+                             num_edges,
+                             num_verts, num_edges,
+                             False,
+                             True,
+                             False,
+                             False)
+
+    call_wcc[int, float](handle_ptr.get()[0],
+                         graph_container,
+                         <int*> c_labels_val)
+
+
+def strongly_connected_components(offsets, indices, weights, num_verts, num_edges, labels):
+    cdef unique_ptr[handle_t] handle_ptr
+    handle_ptr.reset(new handle_t())
+    handle_ = handle_ptr.get()
+
+    cdef uintptr_t c_offsets    = offsets['data'][0]
+    cdef uintptr_t c_indices    = indices['data'][0]
+    cdef uintptr_t c_edge_weights = <uintptr_t>NULL
+    cdef uintptr_t c_labels_val = labels['data'][0]
+
+    cdef GraphCSRView[int,int,float] g
+
+    g = GraphCSRView[int,int,float](<int*>c_offsets, <int*>c_indices, <float*>NULL, num_verts, num_edges)
+
+    cdef cugraph_cc_t connect_type=CUGRAPH_STRONG
+    connected_components(g, <cugraph_cc_t>connect_type, <int *>c_labels_val)
--- python/pylibcugraph/pylibcugraph/tests/test_connected_components.py
+++ python/pylibcugraph/pylibcugraph/tests/test_connected_components.py
@@ -12,34 +12,61 @@
 # limitations under the License.
 
 import pytest
+from pylibcugraph.tests import utils
+import cupy
+import cugraph
+import pylibcugraph
+import numpy as np
 
 
-@pytest.fixture
-def plc():
-    import pylibcugraph
-    return pylibcugraph
-
-
-###############################################################################
-def test_import():
-    """
-    Ensure pylibcugraph is importable.
-    """
-    # suppress F401 (imported but never used) in flake8
-    import pylibcugraph  # noqa: F401
-
-
-def test_scc(plc):
+@pytest.mark.parametrize("graph_file", utils.DATASETS)
+def test_scc(graph_file):
     """
     FIXME: rewrite once SCC is implemented.
     """
-    with pytest.raises(NotImplementedError):
-        plc.strongly_connected_components(None, None, None, None, None, None)
+    cu_M = utils.read_csv_file(graph_file)
+    G = cugraph.DiGraph()
+    G.from_cudf_edgelist(cu_M, source="0", destination="1", edge_attr="2")
 
+    offsets, indices, weights = G.view_adj_list()
+    cupy_off = cupy.array(offsets)
+    cudf_ind = indices
 
-def test_wcc(plc):
+    cupy_labels = cupy.array(np.zeros(G.number_of_vertices()))
+    pylibcugraph.strongly_connected_components(cupy_off.__cuda_array_interface__,
+                                               cudf_ind.__cuda_array_interface__,
+                                               None,
+                                               G.number_of_vertices(),
+                                               G.number_of_edges(directed_edges=True),
+                                               cupy_labels.__cuda_array_interface__
+                                               )
+    print(cupy_labels)
+    df = cugraph.strongly_connected_components(G)
+    print(df)
+
+@pytest.mark.parametrize("graph_file", utils.DATASETS)
+def test_wcc(graph_file):
     """
     FIXME: rewrite once WCC is implemented.
     """
-    with pytest.raises(NotImplementedError):
-        plc.weakly_connected_components(None, None, None, None, None, None)
+    cu_M = utils.read_csv_file(graph_file)
+    G = cugraph.DiGraph()
+    G.from_cudf_edgelist(cu_M, source="0", destination="1", edge_attr="2")
+
+    
+    cupy_src = cupy.array(cu_M["0"])
+    cudf_dst = cu_M["1"]
+
+    cupy_labels = cupy.array(np.zeros(G.number_of_vertices()), dtype='int32')
+    pylibcugraph.weakly_connected_components(cupy_src.__cuda_array_interface__,
+                                             cudf_dst.__cuda_array_interface__,
+                                             None,
+                                             G.number_of_vertices(),
+                                             G.number_of_edges(directed_edges=True),
+                                             cupy_labels.__cuda_array_interface__
+                                             )
+
+
+    print(cupy_labels)
+    df = cugraph.weakly_connected_components(G)
+    print(df)
