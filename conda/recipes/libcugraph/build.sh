ffc813278b763cfe6f0a5e31ac01c573c0acaa7b
Rick Ratzel <rratzel@nvidia.com>
2021-09-02 16:05:23 -0500

    Added copyright statement.

--- conda/recipes/libcugraph/build.sh
+++ conda/recipes/libcugraph/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2019-2021, NVIDIA CORPORATION.
+
 # This assumes the script is executed from the root of the repo directory
 
 # NOTE: the "libcugraph" target also builds all single-gpu gtest binaries, and
