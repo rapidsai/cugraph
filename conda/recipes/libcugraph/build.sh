1a0753fad21a9128cd499eb03ef0e46bc87e57d0
Rick Ratzel <rratzel@nvidia.com>
2021-09-02 18:54:18 -0500

    Updated comment to reflect that the openmpi dependencies are required.

--- conda/recipes/libcugraph/build.sh
+++ conda/recipes/libcugraph/build.sh
@@ -5,8 +5,8 @@
 # This assumes the script is executed from the root of the repo directory
 
 # NOTE: the "libcugraph" target also builds all single-gpu gtest binaries, and
-# the "cpp-mgtests" target builds the multi-gpu gtest binaries (and installs
-# some MG dependencies via cmake). The conda package does NOT include these test
+# the "cpp-mgtests" target builds the multi-gpu gtest binaries (and requires the
+# openmpi build dependencies). The conda package does NOT include these test
 # binaries or extra dependencies, but these are built here for use in CI runs.
 
 ./build.sh libcugraph cpp-mgtests -v --allgpuarch
