c3d798f42f46d6a4184da648ef93a1a0398002c1
Chuck Hastings <45364586+ChuckHastings@users.noreply.github.com>
2021-09-08 13:38:05 -0400

    Split many files to separate SG from MG template instantiations (#1803)
    Partially addresses #1770
    
    This is preliminary work to separate the SG and MG shared object libraries. This PR splits many of the source files that instantiate SG and MG variants of algorithms/objects/functions.
    
    There will be subsequent PRs that will refactor the CMake structure and probably move some more files around.
    
    Authors:
      - Chuck Hastings (https://github.com/ChuckHastings)
    
    Approvers:
      - Seunghwa Kang (https://github.com/seunghwak)
    
    URL: https://github.com/rapidsai/cugraph/pull/1803

---
+++ cpp/src/structure/relabel_mg.cu
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2021, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <structure/relabel_impl.cuh>
+
+namespace cugraph {
+
+// MG instantiation
+
+template void relabel<int32_t, true>(raft::handle_t const& handle,
+                                     std::tuple<int32_t const*, int32_t const*> old_new_label_pairs,
+                                     int32_t num_label_pairs,
+                                     int32_t* labels,
+                                     int32_t num_labels,
+                                     bool skip_missing_labels,
+                                     bool do_expensive_check);
+
+template void relabel<int64_t, true>(raft::handle_t const& handle,
+                                     std::tuple<int64_t const*, int64_t const*> old_new_label_pairs,
+                                     int64_t num_label_pairs,
+                                     int64_t* labels,
+                                     int64_t num_labels,
+                                     bool skip_missing_labels,
+                                     bool do_expensive_check);
+
+}  // namespace cugraph
--- cpp/src/structure/renumber_utils_impl.cuh
+++ cpp/src/structure/renumber_utils_impl.cuh
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#pragma once
 
 #include <cugraph/detail/graph_utils.cuh>
 #include <cugraph/graph.hpp>
@@ -316,95 +317,5 @@ void unrenumber_int_vertices(raft::handle_t const& handle,
                                   do_expensive_check);
   }
 }
-
-// explicit instantiation
-
-template void renumber_ext_vertices<int32_t, false>(raft::handle_t const& handle,
-                                                    int32_t* vertices,
-                                                    size_t num_vertices,
-                                                    int32_t const* renumber_map_labels,
-                                                    int32_t local_int_vertex_first,
-                                                    int32_t local_int_vertex_last,
-                                                    bool do_expensive_check);
-
-template void renumber_ext_vertices<int32_t, true>(raft::handle_t const& handle,
-                                                   int32_t* vertices,
-                                                   size_t num_vertices,
-                                                   int32_t const* renumber_map_labels,
-                                                   int32_t local_int_vertex_first,
-                                                   int32_t local_int_vertex_last,
-                                                   bool do_expensive_check);
-
-template void renumber_ext_vertices<int64_t, false>(raft::handle_t const& handle,
-                                                    int64_t* vertices,
-                                                    size_t num_vertices,
-                                                    int64_t const* renumber_map_labels,
-                                                    int64_t local_int_vertex_first,
-                                                    int64_t local_int_vertex_last,
-                                                    bool do_expensive_check);
-
-template void renumber_ext_vertices<int64_t, true>(raft::handle_t const& handle,
-                                                   int64_t* vertices,
-                                                   size_t num_vertices,
-                                                   int64_t const* renumber_map_labels,
-                                                   int64_t local_int_vertex_first,
-                                                   int64_t local_int_vertex_last,
-                                                   bool do_expensive_check);
-
-template void unrenumber_local_int_vertices<int32_t>(raft::handle_t const& handle,
-                                                     int32_t* vertices,
-                                                     size_t num_vertices,
-                                                     int32_t const* renumber_map_labels,
-                                                     int32_t local_int_vertex_first,
-                                                     int32_t local_int_vertex_last,
-                                                     bool do_expensive_check);
-
-template void unrenumber_local_int_vertices<int64_t>(raft::handle_t const& handle,
-                                                     int64_t* vertices,
-                                                     size_t num_vertices,
-                                                     int64_t const* renumber_map_labels,
-                                                     int64_t local_int_vertex_first,
-                                                     int64_t local_int_vertex_last,
-                                                     bool do_expensive_check);
-
-template void unrenumber_int_vertices<int32_t, false>(
-  raft::handle_t const& handle,
-  int32_t* vertices,
-  size_t num_vertices,
-  int32_t const* renumber_map_labels,
-  int32_t local_int_vertex_first,
-  int32_t local_int_vertex_last,
-  std::vector<int32_t> const& vertex_partition_lasts,
-  bool do_expensive_check);
-
-template void unrenumber_int_vertices<int32_t, true>(
-  raft::handle_t const& handle,
-  int32_t* vertices,
-  size_t num_vertices,
-  int32_t const* renumber_map_labels,
-  int32_t local_int_vertex_first,
-  int32_t local_int_vertex_last,
-  std::vector<int32_t> const& vertex_partition_lasts,
-  bool do_expensive_check);
-
-template void unrenumber_int_vertices<int64_t, false>(
-  raft::handle_t const& handle,
-  int64_t* vertices,
-  size_t num_vertices,
-  int64_t const* renumber_map_labels,
-  int64_t local_int_vertex_first,
-  int64_t local_int_vertex_last,
-  std::vector<int64_t> const& vertex_partition_lasts,
-  bool do_expensive_check);
-
-template void unrenumber_int_vertices<int64_t, true>(
-  raft::handle_t const& handle,
-  int64_t* vertices,
-  size_t num_vertices,
-  int64_t const* renumber_map_labels,
-  int64_t local_int_vertex_first,
-  int64_t local_int_vertex_last,
-  std::vector<int64_t> const& vertex_partition_lasts,
-  bool do_expensive_check);
 
 }  // namespace cugraph
