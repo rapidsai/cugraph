40f59d17e3b833fe21c5b11ad21bffc9adbcfe9d
Rick Ratzel <rratzel@nvidia.com>
2021-09-13 22:28:26 -0500

    Updated to enforce only supported dtypes, changed to use legacy connected_components API so consistent arg types are used (to be updated to new connected_componets APIs when available), updated tests to improve coverage.

--- python/pylibcugraph/pylibcugraph/tests/utils.py
+++ python/pylibcugraph/pylibcugraph/tests/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2021, NVIDIA CORPORATION.
+# Copyright (c) 2021, NVIDIA CORPORATION.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,6 +13,7 @@
 
 import os
 from pathlib import Path
+
 import cudf
 
 
@@ -25,19 +26,10 @@ DATASETS = [RAPIDS_DATASET_ROOT_DIR_PATH/f for f in [
     "dolphins.csv"]]
 
 
-def read_csv_file(csv_file, read_weights_in_sp=True):
-    print("Reading " + str(csv_file) + "...")
-    if read_weights_in_sp is True:
-        return cudf.read_csv(
-            csv_file,
-            delimiter=" ",
-            dtype=["int32", "int32", "float32"],
-            header=None,
-        )
-    else:
-        return cudf.read_csv(
-            csv_file,
-            delimiter=" ",
-            dtype=["int32", "int32", "float64"],
-            header=None,
-        )
+def read_csv_file(csv_file, weights_dtype="float32"):
+    return cudf.read_csv(
+        csv_file,
+        delimiter=" ",
+        dtype=["int32", "int32", weights_dtype],
+        header=None,
+    )
