947ef7beaf4f9c65b89e8bf7568b7dd196980702
Seunghwa Kang <seunghwak@nvidia.com>
2021-09-15 10:01:58 -0400

    bug fix

--- cpp/src/structure/create_graph_from_edgelist_impl.hpp
+++ cpp/src/structure/create_graph_from_edgelist_impl.hpp
@@ -131,11 +131,14 @@ create_graph_from_edgelist_impl(raft::handle_t const& handle,
     cugraph::graph_t<vertex_t, edge_t, weight_t, store_transposed, multi_gpu>(
       handle,
       edgelists,
-      cugraph::graph_meta_t<vertex_t, edge_t, multi_gpu>{meta.number_of_vertices,
-                                                         meta.number_of_edges,
-                                                         graph_properties,
-                                                         meta.partition,
-                                                         meta.segment_offsets}),
+      cugraph::graph_meta_t<vertex_t, edge_t, multi_gpu>{
+        meta.number_of_vertices,
+        meta.number_of_edges,
+        graph_properties,
+        meta.partition,
+        meta.segment_offsets,
+        store_transposed ? meta.num_local_unique_edge_minors : meta.num_local_unique_edge_majors,
+        store_transposed ? meta.num_local_unique_edge_majors : meta.num_local_unique_edge_minors}),
     std::optional<rmm::device_uvector<vertex_t>>{std::move(renumber_map_labels)});
 }
 
