f10ea068a6b2ce64c13f3f7a924a2e3a3725b77e
Andrei Schaffer <aschaffer@nvidia.com>
2021-09-27 16:40:00 -0500

    C-API RW test checking functionality.

--- cpp/tests/c_api/random_walks_test.c
+++ cpp/tests/c_api/random_walks_test.c
@@ -14,29 +14,76 @@
  * limitations under the License.
  */
 
-#include "c_test_utils.h"  /* RUN_TEST */
+#include "c_test_utils.h" /* RUN_TEST */
 
 #include <cugraph_c/cugraph_api.h>
 
+/* sample graph:
+    0 --(.1)--> 1 --(1.1)--> 4
+   /|\       /\ |            |
+    |       /   |            |
+   (5.1) (3.1)(2.1)        (3.2)
+    |   /       |            |
+    | /        \|/          \|/
+    2 --(4.1)-->3 --(7.2)--> 5
+*/
 
-int test_random_walks_1() {
-   return 0;
+/* array of compressed paths,
+   using 1-based indexing for vertices,
+   to avoid confusion between, for example,
+   `012` and `12`, which result in same number*/
+#define N_PATHS 30
+static int32_t c_ps_array[N_PATHS] = {1,    2,     3,     4,   5,   6,    12,   124, 125, 1246,
+                                      1256, 24,    25,    246, 256, 31,   32,   34,  312, 3124,
+                                      3125, 31246, 31256, 324, 325, 3246, 3256, 346, 46,  56};
+
+/* linear search of `value` inside `p_cmprsd_path[max_num_paths]`*/
+bool_t is_one_of(int32_t value, int32_t* p_cmprsd_path, int max_num_paths)
+{
+  int i = 0;
+  for (; i < max_num_paths; ++i)
+    if (value == p_cmprsd_path[i]) return 1;
+
+  return 0;
 }
 
-int test_random_walks_2() {
-   return 0;
-}
+/* check on host if all obtained paths are possible paths */
+bool_t host_check_paths(
+  int32_t* p_path_v, int32_t* p_path_sz, int num_paths, int32_t* p_cmprsd_path, int max_num_paths)
+{
+  int i            = 0;
+  int count_passed = 0;
 
-int test_random_walks_3() {
-   return 0;
+  for (; i < num_paths; ++i) {
+    int32_t crt_path_sz          = p_path_sz[i];
+    int path_it                  = 0;
+    int32_t crt_path_accumulator = 0;
+    bool_t flag_passed           = 0;
+
+    for (; path_it < crt_path_sz; ++path_it) {
+      crt_path_accumulator = *p_path_v + 10 * crt_path_accumulator;
+      ++p_path_v; /* iterate p_path_v*/
+    }
+
+    flag_passed = is_one_of(crt_path_accumulator, p_cmprsd_path, max_num_paths);
+    if (flag_passed) ++count_passed;
+  }
+
+  return (count_passed == num_paths);
 }
+int test_random_walks_1() { return 0; }
+
+int test_random_walks_2() { return 0; }
+
+int test_random_walks_3() { return 0; }
 
 /******************************************************************************/
 
-int main(int argc, char** argv) {
-   int result = 0;
-   result |= RUN_TEST(test_random_walks_1);
-   result |= RUN_TEST(test_random_walks_2);
-   result |= RUN_TEST(test_random_walks_3);
-   return result;
+int main(int argc, char** argv)
+{
+  int result = 0;
+  result |= RUN_TEST(test_random_walks_1);
+  result |= RUN_TEST(test_random_walks_2);
+  result |= RUN_TEST(test_random_walks_3);
+  return result;
 }
