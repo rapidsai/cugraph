583024c3aa4a4fe920e0180cc63d74c866238e12
ptaylor <paul.e.taylor@me.com>
2021-10-13 19:25:10 -0500

    switch to my fork to test in CI

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -42,6 +42,8 @@ set(CUGRAPH_BRANCH_VERSION_raft "${CUGRAPH_VERSION_MAJOR}.${CUGRAPH_VERSION_MINO
 # To use a different RAFT locally, set the CMake variable
 # RPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION    ${CUGRAPH_MIN_VERSION_raft}
-                        FORK       rapidsai
-                        PINNED_TAG branch-${CUGRAPH_BRANCH_VERSION_raft}
+                        # FORK       rapidsai
+                        # PINNED_TAG branch-${CUGRAPH_BRANCH_VERSION_raft}
+                        FORK       trxcllnt
+                        PINNED_TAG fix/build-shared-faiss
                         )
