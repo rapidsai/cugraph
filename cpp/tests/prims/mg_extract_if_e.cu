5daa32a211ec58655b30fb8cdf226c56da4f4500
Seunghwa Kang <seunghwak@nvidia.com>
2021-11-12 16:55:25 -0500

    added missing test for the else case

--- cpp/tests/prims/mg_extract_if_e.cu
+++ cpp/tests/prims/mg_extract_if_e.cu
@@ -318,6 +318,21 @@ class Tests_MG_ExtractIfE
                         compare_equal_t<
                           typename thrust::iterator_traits<decltype(mg_edge_first)>::value_type>{});
         } else {
+          auto mg_edge_first = thrust::make_zip_iterator(thrust::make_tuple(
+            mg_aggregate_edgelist_srcs.begin(), mg_aggregate_edgelist_dsts.begin()));
+          auto sg_edge_first = thrust::make_zip_iterator(
+            thrust::make_tuple(sg_edgelist_srcs.begin(), sg_edgelist_dsts.begin()));
+          thrust::sort(handle.get_thrust_policy(),
+                       mg_edge_first,
+                       mg_edge_first + mg_aggregate_edgelist_srcs.size());
+          thrust::sort(
+            handle.get_thrust_policy(), sg_edge_first, sg_edge_first + sg_edgelist_srcs.size());
+          thrust::equal(handle.get_thrust_policy(),
+                        mg_edge_first,
+                        mg_edge_first + mg_aggregate_edgelist_srcs.size(),
+                        sg_edge_first,
+                        compare_equal_t<
+                          typename thrust::iterator_traits<decltype(mg_edge_first)>::value_type>{});
         }
       }
     }
