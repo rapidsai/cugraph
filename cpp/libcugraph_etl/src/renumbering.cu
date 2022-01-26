8ed515be9df9ac4c64e4a64fdb8c9e4bdfa3e2fb
Chirayu <cgarg@nvidia.com>
2022-01-25 17:07:50 -0800

    remove unused headers

--- cpp/libcugraph_etl/src/renumbering.cu
+++ cpp/libcugraph_etl/src/renumbering.cu
@@ -20,10 +20,7 @@
 #include <raft/mr/host/allocator.hpp>
 #include <raft/mr/host/buffer.hpp>
 
-// #include <cudf/types.hpp>
-// #include <cudf/table/table.hpp>
 #include <cudf/table/table_view.hpp>
-//#include <cudf/column/column.hpp>
 #include <cudf/column/column_view.hpp>
 #include <cudf/column/column_factories.hpp>
 #include <cudf/strings/strings_column_view.hpp>
