8837403c78f72a9d44414d0eb7ff43ff59569315
Chuck Hastings <charlesh@nvidia.com>
2022-02-11 17:36:39 -0500

    add node2vec C API implementation

--- cpp/include/cugraph/api_helpers.hpp
+++ cpp/include/cugraph/api_helpers.hpp
@@ -28,6 +28,14 @@ enum class sampling_strategy_t : int { UNIFORM = 0, BIASED, NODE2VEC };
 struct sampling_params_t {
   sampling_params_t(void) {}
 
+  sampling_params_t(sampling_strategy_t sampling_type, double p = 1.0, double q = 1.0, bool use_alpha_cache = false)
+    : sampling_type_(sampling_type),
+      p_(p),
+      q_(q),
+      use_alpha_cache_(use_alpha_cache)
+  {
+  }
+
   sampling_params_t(int sampling_type, double p = 1.0, double q = 1.0, bool use_alpha_cache = false)
     : sampling_type_(static_cast<sampling_strategy_t>(sampling_type)),
       p_(p),
