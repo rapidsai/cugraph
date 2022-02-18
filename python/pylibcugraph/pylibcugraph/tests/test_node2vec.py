09e229857d9fd659d05182087a1cc085a59f93f5
betochimas <dylanc@nvidia.com>
2022-02-18 09:09:01 -0800

    Revert "Merge branch 'branch-22.04-node2vec-cugraph-layer' of https://github.com/betochimas/cugraph into branch-22.04-node2vec-cugraph-layer"
    This reverts commit 43b25b51f481a574097d01be733fafc95a1c4afd, reversing
    changes made to e123a6a5aec68d68dddd4736b4c163d348774b9f.

--- python/pylibcugraph/pylibcugraph/tests/test_node2vec.py
+++ python/pylibcugraph/pylibcugraph/tests/test_node2vec.py
@@ -25,19 +25,18 @@ _test_data = {"karate.csv": {
                   "seeds": cp.asarray([0, 0], dtype=np.int32),
                   "paths": cp.asarray([0, 8, 33, 29, 26, 0, 1, 3, 13, 33],
                                       dtype=np.int32),
-                  "weights": cp.asarray([1., 1., 1., 1., 1., 1., 1., 1.,
-                                         0., 0.],
+                  "weights": cp.asarray([1., 1., 1., 1., 1., 1., 0., 0.],
                                         dtype=np.float32),
-                  "path_sizes": cp.asarray([5, 5], dtype=np.int32),
+                  "offsets": cp.asarray([5, 5], dtype=np.int32),
                   "max_depth": 5
                   },
               "dolphins.csv": {
                   "seeds": cp.asarray([11], dtype=np.int32),
                   "paths": cp.asarray([11, 51, 11, 51],
                                       dtype=np.int32),
-                  "weights": cp.asarray([1., 1., 1.],
+                  "weights": cp.asarray([1., 1., 1., 1.],
                                         dtype=np.float32),
-                  "path_sizes": cp.asarray([4], dtype=np.int32),
+                  "offsets": cp.asarray([4], dtype=np.int32),
                   "max_depth": 4
                   },
               "Simple_1": {
@@ -46,16 +45,16 @@ _test_data = {"karate.csv": {
                                       dtype=np.int32),
                   "weights": cp.asarray([1., 1., 0.],
                                         dtype=np.float32),
-                  "path_sizes": cp.asarray([3, 1], dtype=np.int32),
+                  "offsets": cp.asarray([3, 1], dtype=np.int32),
                   "max_depth": 3
                   },
               "Simple_2": {
                   "seeds": cp.asarray([0, 3], dtype=np.int32),
                   "paths": cp.asarray([0, 1, 3, 5, 3, 5],
                                       dtype=np.int32),
-                  "weights": cp.asarray([0.1, 2.1, 7.2, 7.2, 7.2, 3.2],
+                  "weights": cp.asarray([0.1, 2.1, 7.2, 7.2],
                                         dtype=np.float32),
-                  "path_sizes": cp.asarray([4, 2], dtype=np.int32),
+                  "offsets": cp.asarray([4, 2], dtype=np.int32),
                   "max_depth": 4
                   },
               }
@@ -74,8 +73,11 @@ def test_node2vec(sg_graph_objs):
 
     (g, resource_handle, ds_name) = sg_graph_objs
 
-    (seeds, expected_paths, expected_weights, expected_path_sizes, max_depth) \
-        = _test_data[ds_name].values()
+    # if ds_name not in ("Simple_1", "Simple_2"):
+    #    return
+
+    (seeds, expected_paths, expected_weights, expected_offsets, max_depth) = \
+        _test_data[ds_name].values()
 
     compress_result = True
     p = 0.8
@@ -84,32 +86,33 @@ def test_node2vec(sg_graph_objs):
     result = node2vec(resource_handle, g, seeds, max_depth,
                       compress_result, p, q)
 
-    (actual_paths, actual_weights, actual_path_sizes) = result
+    (actual_paths, actual_weights, actual_offsets) = result
+    num_walks = len(actual_paths)
     num_paths = len(seeds)
 
-    # Do a simple check using the vertices as array indices.
-    assert len(actual_path_sizes) == num_paths
+    # breakpoint()
+    # Do a simple check using the vertices as array indices. First, ensure
+    # the test data vertices start from 0 with no gaps.
+    assert len(actual_offsets) == num_paths
 
     assert actual_paths.dtype == expected_paths.dtype
     assert actual_weights.dtype == expected_weights.dtype
-    assert actual_path_sizes.dtype == expected_path_sizes.dtype
+    assert actual_offsets.dtype == expected_offsets.dtype
 
     actual_paths = actual_paths.tolist()
     actual_weights = actual_weights.tolist()
-    actual_path_sizes = actual_path_sizes.tolist()
+    actual_offsets = actual_offsets.tolist()
     expected_paths = expected_paths.tolist()
     expected_weights = expected_weights.tolist()
-    expected_path_sizes = expected_path_sizes.tolist()
+    expected_offsets = expected_offsets.tolist()
 
     if ds_name not in ["karate.csv", "dolphins.csv", "Simple_2"]:
-        for i in range(len(expected_paths)):
+        for i in range(num_walks):
             assert pyt.approx(actual_paths[i], 1e-4) == expected_paths[i]
-        for i in range(len(expected_weights)):
             assert pyt.approx(actual_weights[i], 1e-4) == expected_weights[i]
 
     # Starting vertex of each path should be the seed
     path_start = 0
     for i in range(num_paths):
-        assert actual_path_sizes[i] == expected_path_sizes[i]
         assert actual_paths[path_start] == seeds[i]
-        path_start += actual_path_sizes[i]
+        path_start += actual_offsets[i]
