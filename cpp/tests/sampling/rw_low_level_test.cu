cef4924f5e7c30d00028fd85bdc3585c51fe31f2
Corey J. Nolet <cjnolet@users.noreply.github.com>
2022-03-01 09:06:06 -0500

    Updating a few headers that have been renamed in raft (#2090)
    Authors:
      - Corey J. Nolet (https://github.com/cjnolet)
      - Rick Ratzel (https://github.com/rlratzel)
    
    Approvers:
      - Chuck Hastings (https://github.com/ChuckHastings)
      - Jordan Jacobelli (https://github.com/Ethyling)
    
    URL: https://github.com/rapidsai/cugraph/pull/2090

--- cpp/tests/sampling/rw_low_level_test.cu
+++ cpp/tests/sampling/rw_low_level_test.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, NVIDIA CORPORATION.
+ * Copyright (c) 2021-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@
 #include <sampling/random_walks.cuh>
 
 #include <raft/handle.hpp>
-#include <raft/random/rng.hpp>
+#include <raft/random/rng.cuh>
 
 #include "random_walks_utils.cuh"
 
