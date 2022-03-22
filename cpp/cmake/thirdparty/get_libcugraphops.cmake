0f9830aee7aa57d3a5fbd83cd405da608b0769c2
ptaylor <paul.e.taylor@me.com>
2022-03-22 16:18:34 -0700

    use CPMFindPackage to get cugraph-ops

--- cpp/cmake/thirdparty/get_libcugraphops.cmake
+++ cpp/cmake/thirdparty/get_libcugraphops.cmake
@@ -14,26 +14,50 @@
 # limitations under the License.
 #=============================================================================
 
-function(find_and_configure_cugraphops)
+set(CUGRAPH_MIN_VERSION_cugraph_ops "${CUGRAPH_VERSION_MAJOR}.${CUGRAPH_VERSION_MINOR}.00")
+set(CUGRAPH_BRANCH_VERSION_cugraph_ops "${CUGRAPH_VERSION_MAJOR}.${CUGRAPH_VERSION_MINOR}")
 
-    if(TARGET cugraphops::cugraphops)
-        return()
+function(find_and_configure_cugraph_ops)
+
+    set(oneValueArgs VERSION FORK PINNED_TAG CLONE_ON_PIN)
+    cmake_parse_arguments(PKG "" "${oneValueArgs}" "" ${ARGN} )
+
+    if(PKG_CLONE_ON_PIN AND NOT PKG_PINNED_TAG STREQUAL "branch-${CUGRAPH_BRANCH_VERSION_cugraph_ops}")
+        message("Pinned tag found: ${PKG_PINNED_TAG}. Cloning cugraph-ops locally.")
+        set(CPM_DOWNLOAD_cugraph-ops ON)
     endif()
 
-    rapids_find_generate_module(cugraphops
-        HEADER_NAMES            graph/sampling.hpp
-        LIBRARY_NAMES           cugraph-ops++
-        INCLUDE_SUFFIXES        cugraph-ops
-        BUILD_EXPORT_SET    cugraph-exports
-        INSTALL_EXPORT_SET  cugraph-exports
-    )
-
-    rapids_find_package(cugraphops
-        REQUIRED
-        BUILD_EXPORT_SET    cugraph-exports
-        INSTALL_EXPORT_SET  cugraph-exports
+    rapids_cpm_find(cugraph-ops ${PKG_VERSION}
+      GLOBAL_TARGETS      cugraph-ops::cugraph-ops++
+      BUILD_EXPORT_SET    cugraph-exports
+      INSTALL_EXPORT_SET  cugraph-exports
+        CPM_ARGS
+            GIT_REPOSITORY https://github.com/${PKG_FORK}/cugraph-ops.git
+            GIT_TAG        ${PKG_PINNED_TAG}
+            OPTIONS
+                "BUILD_CUGRAPH_OPS_CPP_LIBRARY ON"
+                "BUILD_CUGRAPH_OPS_CPP_TESTS OFF"
+                "BUILD_CUGRAPH_OPS_PYTHON_LIBRARY ${PKG_BUILD_CUGRAPH_OPS_PYTHON_LIBRARY}"
     )
 
 endfunction()
 
-find_and_configure_cugraphops()
+###
+# Change pinned tag and fork here to test a commit in CI
+#
+# To use a locally-built cugraph-ops package, set the CMake variable
+# `-D cugraph-ops_ROOT=/path/to/cugraph-ops/build`
+#
+# To use a local clone of cugraph-ops source and allow CMake to build
+# cugraph-ops as part of building cugraph itself, set the CMake variable
+# `-D CPM_cugraph-ops_SOURCE=/path/to/cugraph-ops`
+###
+find_and_configure_cugraph_ops(VERSION    ${CUGRAPH_MIN_VERSION_cugraph_ops}
+                               FORK       rapidsai
+                               PINNED_TAG branch-${CUGRAPH_BRANCH_VERSION_cugraph_ops}
+                               # When PINNED_TAG above doesn't match cugraph,
+                               # force local cugraph-ops clone in build directory
+                               # even if it's already installed.
+                               CLONE_ON_PIN     ON
+                               BUILD_CUGRAPH_OPS_PYTHON_LIBRARY ${BUILD_CUGRAPH_OPS_PYTHON_LIBRARY}
+                               )
