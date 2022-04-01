b98c48aa1e537c68ad66b87100d237abe0e69ce5
Chuck Hastings <charlesh@nvidia.com>
2022-04-01 13:35:18 -0400

    C API changes for labeling algorithms
     1) Add C API definition/implementation/testing for WCC
     2) Add C API definition for SCC
     3) Update test utilities to support symmetric graph property

---
+++ cpp/include/cugraph_c/labeling_algorithms.h
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cugraph_c/error.h>
+#include <cugraph_c/graph.h>
+#include <cugraph_c/resource_handle.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief     Opaque labeling result type
+ */
+typedef struct {
+  int32_t align_;
+} cugraph_labeling_result_t;
+
+/**
+ * @brief     Get the vertex ids from the labeling result
+ *
+ * @param [in]   result   The result from a labeling algorithm
+ * @return type erased array of vertex ids
+ */
+cugraph_type_erased_device_array_view_t* cugraph_labeling_result_get_vertices(
+  cugraph_labeling_result_t* result);
+
+/**
+ * @brief     Get the label values from the labeling result
+ *
+ * @param [in]   result   The result from a labeling algorithm
+ * @return type erased array of label values
+ */
+cugraph_type_erased_device_array_view_t* cugraph_labeling_result_get_labels(
+  cugraph_labeling_result_t* result);
+
+/**
+ * @brief     Free labeling result
+ *
+ * @param [in]   result   The result from a labeling algorithm
+ */
+void cugraph_labeling_result_free(cugraph_labeling_result_t* result);
+
+/**
+ * @brief Labels each vertex in the input graph with its (weakly-connected-)component ID
+ *
+ * The input graph must be symmetric. Component IDs can be arbitrary integers (they can be
+ * non-consecutive and are not ordered by component size or any other criterion).
+ *
+ * @param [in]  handle      Handle for accessing resources
+ * @param [in]  graph       Pointer to graph
+ * @param [in]  do_expensive_check A flag to run expensive checks for input arguments (if set to
+ * `true`).
+ * @param [out] result      Opaque pointer to labeling results
+ * @param [out] error       Pointer to an error object storing details of any error.  Will
+ *                          be populated if error code is not CUGRAPH_SUCCESS
+ */
+cugraph_error_code_t cugraph_weakly_connected_components(
+  const cugraph_resource_handle_t* handle,
+  cugraph_graph_t* graph,
+  bool_t do_expensive_check,
+  cugraph_labeling_result_t** result,
+  cugraph_error_t** error);
+
+/**
+ * @brief Labels each vertex in the input graph with its (strongly-connected-)component ID
+ *
+ * The input graph may be asymmetric. Component IDs can be arbitrary integers (they can be
+ * non-consecutive and are not ordered by component size or any other criterion).
+ *
+ * @param [in]  handle      Handle for accessing resources
+ * @param [in]  graph       Pointer to graph
+ * @param [in]  do_expensive_check A flag to run expensive checks for input arguments (if set to
+ * `true`).
+ * @param [out] result      Opaque pointer to labeling results
+ * @param [out] error       Pointer to an error object storing details of any error.  Will
+ *                          be populated if error code is not CUGRAPH_SUCCESS
+ */
+cugraph_error_code_t cugraph_strongly_connected_components(
+  const cugraph_resource_handle_t* handle,
+  cugraph_graph_t* graph,
+  bool_t do_expensive_check,
+  cugraph_labeling_result_t** result,
+  cugraph_error_t** error);
+
+#ifdef __cplusplus
+}
+#endif
--- cpp/tests/c_api/mg_pagerank_test.c
+++ cpp/tests/c_api/mg_pagerank_test.c
@@ -46,7 +46,7 @@ int generic_pagerank_test(const cugraph_resource_handle_t* handle,
   cugraph_pagerank_result_t* p_result = NULL;
 
   ret_code = create_mg_test_graph(
-    handle, h_src, h_dst, h_wgt, num_edges, store_transposed, &p_graph, &ret_error);
+    handle, h_src, h_dst, h_wgt, num_edges, store_transposed, FALSE, &p_graph, &ret_error);
 
   TEST_ASSERT(test_ret_value, ret_code == CUGRAPH_SUCCESS, "create_mg_test_graph failed.");
 
@@ -64,8 +64,10 @@ int generic_pagerank_test(const cugraph_resource_handle_t* handle,
   vertices  = cugraph_pagerank_result_get_vertices(p_result);
   pageranks = cugraph_pagerank_result_get_pageranks(p_result);
 
-  vertex_t h_vertices[num_vertices];
-  weight_t h_pageranks[num_vertices];
+  size_t num_local_vertices = cugraph_type_erased_device_array_view_size(vertices);
+
+  vertex_t h_vertices[num_local_vertices];
+  weight_t h_pageranks[num_local_vertices];
 
   ret_code = cugraph_type_erased_device_array_view_copy_to_host(
     handle, (byte_t*)h_vertices, vertices, &ret_error);
@@ -74,8 +76,6 @@ int generic_pagerank_test(const cugraph_resource_handle_t* handle,
   ret_code = cugraph_type_erased_device_array_view_copy_to_host(
     handle, (byte_t*)h_pageranks, pageranks, &ret_error);
   TEST_ASSERT(test_ret_value, ret_code == CUGRAPH_SUCCESS, "copy_to_host failed.");
-
-  size_t num_local_vertices = cugraph_type_erased_device_array_view_size(vertices);
 
   for (int i = 0; (i < num_local_vertices) && (test_ret_value == 0); ++i) {
     TEST_ASSERT(test_ret_value,
