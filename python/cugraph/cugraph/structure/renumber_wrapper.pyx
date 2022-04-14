f406bec75d4687ee1dd74e421e01e602dc77f8b9
Chuck Hastings <charlesh@nvidia.com>
2022-04-14 12:07:34 -0400

    add else for two other blocks of code

--- python/cugraph/cugraph/structure/renumber_wrapper.pyx
+++ python/cugraph/cugraph/structure/renumber_wrapper.pyx
@@ -54,10 +54,12 @@ cdef renumber_helper(shuffled_vertices_t* ptr_maj_min_w, vertex_t, weights):
     # which is not supported by '__cuda_array_interface__'
     if shuffled_major_series is None:
         shuffled_major_series = cudf.Series(dtype=vertex_t)
+    else:
+        shuffled_df['major_vertices']= shuffled_major_series
     if shuffled_minor_series is None:
         shuffled_minor_series = cudf.Series(dtype=vertex_t)
-    shuffled_df['major_vertices']= shuffled_major_series
-    shuffled_df['minor_vertices']= shuffled_minor_series
+    else:
+        shuffled_df['minor_vertices']= shuffled_minor_series
 
     if weights is not None:
         weight_t = weights.dtype
