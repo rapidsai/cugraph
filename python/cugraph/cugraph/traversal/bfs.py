a9fd6a0b782612fd0b62976cab72608ecff50a58
Alexandria Barghi <abarghi@nvidia.com>
2022-05-13 18:53:39 +0000

    fix series conversion for notebook test

--- python/cugraph/cugraph/traversal/bfs.py
+++ python/cugraph/cugraph/traversal/bfs.py
@@ -215,15 +215,19 @@ def bfs(G,
 
     # The BFS C++ extension assumes the start vertex is a cudf.Series object,
     # and operates on internal vertex IDs if renumbered.
+    is_dataframe = isinstance(start, cudf.DataFrame) or \
+                   isinstance(start, dask_cudf.DataFrame)
     if G.renumbered is True:
-        is_dataframe = isinstance(start, cudf.DataFrame) or \
-                       isinstance(start, dask_cudf.DataFrame)
         if is_dataframe:
             start = G.lookup_internal_vertex_id(start, start.columns)
         else:
             start = G.lookup_internal_vertex_id(cudf.Series(start))
+    
     else:
-        start = cudf.Series(start)
+        if is_dataframe:
+            start = start[start.columns[0]]
+        else:
+            start = cudf.Series(start, name='starts')
 
     df = bfs_wrapper.bfs(G, start, depth_limit)
     if G.renumbered:
