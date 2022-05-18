98fd1b2aaa6ef2e5947f5fa108e00fc7b9c18f6c
Joseph Nke <jnke@dgx18.aselab.nvidia.com>
2022-05-18 13:27:04 -0700

    move the current implementation of mg neighborhood sampling to proto

--- python/cugraph/cugraph/proto/sampling/__init__.py
+++ python/cugraph/cugraph/proto/sampling/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2022, NVIDIA CORPORATION.
+# Copyright (c) 2022, NVIDIA CORPORATION.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -9,8 +9,5 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
 
-from cugraph.proto.components import strong_connected_component
-from cugraph.proto.structure  import find_bicliques
-from cugraph.proto.sampling import uniform_neighborhood
+from cugraph.proto.sampling.neighborhood_sampling import uniform_neighborhood
--- python/pylibcugraph/pylibcugraph/proto/__init__.py
+++ python/pylibcugraph/pylibcugraph/proto/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2022, NVIDIA CORPORATION.
+# Copyright (c) 2022, NVIDIA CORPORATION.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -11,6 +11,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cugraph.proto.components import strong_connected_component
-from cugraph.proto.structure  import find_bicliques
-from cugraph.proto.sampling import uniform_neighborhood
+# FIXME: The directory proto should be deleted once the the experimental
+# uniform neighborhood sampling is completed
+
+from .sampling.uniform_neighborhood_sampling import EXPERIMENTAL__uniform_neighborhood_sampling
+uniform_neighborhood_sampling = EXPERIMENTAL__uniform_neighborhood_sampling
