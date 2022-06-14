57e8a3ae43b1507119d6e4a6e656f0e61da95ec0
Ralph Liu <ralphl@dgx18.aselab.nvidia.com>
2022-06-14 12:26:37 -0700

    update yaml datatypes. prevent yaml sorting. reading config and storing default dowload dir

--- python/cugraph/cugraph/experimental/datasets/dataset.py
+++ python/cugraph/cugraph/experimental/datasets/dataset.py
@@ -25,6 +25,7 @@ class Dataset:
         self.dir_path = "python/cugraph/cugraph/experimental/datasets/"
         self.__meta_data_file_name = meta_data_file_name
         self.__read_meta_data_file(self.__meta_data_file_name)
+        self.__read_configs()
         self.__edgelist = None
         self.__graph = None
 
@@ -32,12 +33,26 @@ class Dataset:
     def __read_meta_data_file(self, meta_data_file):
         with open(self.dir_path + meta_data_file, 'r') as file:
             self.metadata = yaml.safe_load(file)
+            file.close()
+
+    
+    def __read_configs(self):
+        config_path = "python/cugraph/cugraph/experimental/datasets/datasets_config.yaml"
+        with open(config_path, 'r') as file:
+            cfg = yaml.safe_load(file)
+            self.download_dir = cfg['download_dir'] # should this be accessible by user?
+            file.close()
 
 
     # figure out throwing errors if fetch=False and file doesn't exist...
     def get_edgelist(self, fetch=False):
         """
         Return an Edgelist
+
+        Parameters
+        ----------
+        fetch : Boolean (default=False)
+        
         """
         if self.__edgelist is None:
             if not os.path.isfile(self.metadata['path']):
@@ -46,9 +61,7 @@ class Dataset:
                     # cudf failure to write
                     # self.__download_csv(self.metadata['url'], "python/experimental/datasets")
                 else:
-                    #make into exception
-                    print("The datafile does not exist. Try running with fetch=True to download the datafile")
-                    return
+                    raise RuntimeError("The datafile does not exist. Try get_edgelist(fetch=True) to download the datafile")
     
             self.__edgelist = cudf.read_csv(self.metadata['path'], delimiter=self.metadata['delimiter'], names=self.metadata['col_names'], dtype=self.metadata['col_types'])
 
@@ -58,21 +71,27 @@ class Dataset:
     def get_graph(self, fetch=False):
         """
         Return a Graph object.
+        Parameters
+        ----------
+        fetch : Boolean (default=False)
+        
         """
         if self.__edgelist is None:
             self.get_edgelist(fetch)
-        
+        # pdb.set_trace()
+        # FIXME: graph does not properly set itself as directed/undirected
+        self.__graph = cugraph.Graph(directed=self.metadata['is_directed'])
         self.__graph = cugraph.from_cudf_edgelist(self.__edgelist, source='src', destination='dst')
 
         return self.__graph
 
 
     def __download_csv(self, url, default_path):
-        # fetch from metadata.url
-        #pdb.set_trace()
         filename = self.metadata['url'].split('/')[-1]
         df = cudf.read_csv(self.metadata['url'])
         df.to_csv(default_path+filename, index=False)
         self.metadata['path'] = default_path + filename
+
         with open(self.dir_path + self.__meta_data_file_name, 'w') as file:
-            yaml.dump(self.metadata, file)
+            yaml.dump(self.metadata, file, sort_keys=False)
+            file.close()
