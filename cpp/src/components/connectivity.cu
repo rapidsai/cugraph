a356b0f6293f599fc5d6dcd5e34be8b62bbc4285
Chuck Hastings <charlesh@nvidia.com>
2022-06-15 19:06:04 -0400

    remove topology header, unused

--- cpp/src/components/connectivity.cu
+++ cpp/src/components/connectivity.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021, NVIDIA CORPORATION.
+ * Copyright (c) 2019-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,8 +26,6 @@
 #include <iostream>
 #include <type_traits>
 #include <utilities/graph_utils.cuh>
-
-#include "topology/topology.cuh"
 
 namespace cugraph {
 namespace detail {
