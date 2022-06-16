72c60580d2fce4a792289157709be60b3ea643c9
Alexandria Barghi <abarghi@nvidia.com>
2022-06-16 15:31:24 +0000

    fix imports

--- python/cugraph/cugraph/experimental/__init__.py
+++ python/cugraph/cugraph/experimental/__init__.py
@@ -12,6 +12,7 @@
 # limitations under the License.
 
 from cugraph.utilities.api_tools import experimental_warning_wrapper
+from cugraph.utilities.api_tools import deprecated_warning_wrapper
 
 from cugraph.structure.property_graph import EXPERIMENTAL__PropertyGraph
 PropertyGraph = experimental_warning_wrapper(EXPERIMENTAL__PropertyGraph)
@@ -35,4 +36,6 @@ strong_connected_component = \
     experimental_warning_wrapper(EXPERIMENTAL__strong_connected_component)
 
 from cugraph.experimental.structure.bicliques import EXPERIMENTAL__find_bicliques
-find_bicliques = experimental_warning_wrapper(EXPERIMENTAL__find_bicliques)
+find_bicliques = deprecated_warning_wrapper(
+    experimental_warning_wrapper(EXPERIMENTAL__find_bicliques)
+)
