ee897c4307e3ca679a99f3e4e202f244be36da01
Chuck Hastings <45364586+ChuckHastings@users.noreply.github.com>
2022-06-17 10:16:57 -0400

    Support raft updating to new version of cuco (#2360)
    RAFT is updating to the newest version of `cuco` in this PR: https://github.com/rapidsai/raft/pull/714
    
    This PR makes the necessary changes to cugraph to support the latest `cuco`.  It should be merged after the RAFT PR.
     * update version of cuco
     * update constructor calls to cuco::static_map
    
    Authors:
      - Chuck Hastings (https://github.com/ChuckHastings)
    
    Approvers:
      - Brad Rees (https://github.com/BradReesWork)
    
    URL: https://github.com/rapidsai/cugraph/pull/2360

--- cpp/src/structure/renumber_utils_impl.cuh
+++ cpp/src/structure/renumber_utils_impl.cuh
@@ -199,8 +199,8 @@ void unrenumber_local_int_edges(
                      std::max(static_cast<size_t>(
                                 static_cast<double>(edge_partition_major_range_size) / load_factor),
                               static_cast<size_t>(edge_partition_major_range_size) + 1),
-                     invalid_vertex_id<vertex_t>::value,
-                     invalid_vertex_id<vertex_t>::value,
+                     cuco::sentinel::empty_key<vertex_t>{invalid_vertex_id<vertex_t>::value},
+                     cuco::sentinel::empty_value<vertex_t>{0},
                      stream_adapter,
                      handle.get_stream()};
       auto pair_first = thrust::make_zip_iterator(
@@ -262,8 +262,8 @@ void unrenumber_local_int_edges(
         renumber_map{// cuco::static_map requires at least one empty slot
                      std::max(static_cast<size_t>(static_cast<double>(segment_size) / load_factor),
                               static_cast<size_t>(segment_size) + 1),
-                     invalid_vertex_id<vertex_t>::value,
-                     invalid_vertex_id<vertex_t>::value,
+                     cuco::sentinel::empty_key<vertex_t>{invalid_vertex_id<vertex_t>::value},
+                     cuco::sentinel::empty_value<vertex_t>{0},
                      stream_adapter,
                      handle.get_stream()};
       auto pair_first = thrust::make_zip_iterator(
@@ -315,8 +315,8 @@ void unrenumber_local_int_edges(
                    std::max(static_cast<size_t>(
                               static_cast<double>(renumber_map_minor_labels.size()) / load_factor),
                             renumber_map_minor_labels.size() + 1),
-                   invalid_vertex_id<vertex_t>::value,
-                   invalid_vertex_id<vertex_t>::value,
+                   cuco::sentinel::empty_key<vertex_t>{invalid_vertex_id<vertex_t>::value},
+                   cuco::sentinel::empty_value<vertex_t>{0},
                    stream_adapter,
                    handle.get_stream()};
     auto pair_first = thrust::make_zip_iterator(
@@ -369,8 +369,8 @@ void renumber_ext_vertices(raft::handle_t const& handle,
   auto renumber_map_ptr = std::make_unique<
     cuco::static_map<vertex_t, vertex_t, cuda::thread_scope_device, decltype(stream_adapter)>>(
     size_t{0},
-    invalid_vertex_id<vertex_t>::value,
-    invalid_vertex_id<vertex_t>::value,
+    cuco::sentinel::empty_key<vertex_t>{invalid_vertex_id<vertex_t>::value},
+    cuco::sentinel::empty_value<vertex_t>{0},
     stream_adapter,
     handle.get_stream());
   if (multi_gpu) {
@@ -417,8 +417,8 @@ void renumber_ext_vertices(raft::handle_t const& handle,
       std::max(
         static_cast<size_t>(static_cast<double>(sorted_unique_ext_vertices.size()) / load_factor),
         sorted_unique_ext_vertices.size() + 1),
-      invalid_vertex_id<vertex_t>::value,
-      invalid_vertex_id<vertex_t>::value,
+      cuco::sentinel::empty_key<vertex_t>{invalid_vertex_id<vertex_t>::value},
+      cuco::sentinel::empty_value<vertex_t>{0},
       stream_adapter,
       handle.get_stream());
 
@@ -438,8 +438,8 @@ void renumber_ext_vertices(raft::handle_t const& handle,
       std::max(static_cast<size_t>(
                  static_cast<double>(local_int_vertex_last - local_int_vertex_first) / load_factor),
                static_cast<size_t>(local_int_vertex_last - local_int_vertex_first) + 1),
-      invalid_vertex_id<vertex_t>::value,
-      invalid_vertex_id<vertex_t>::value,
+      cuco::sentinel::empty_key<vertex_t>{invalid_vertex_id<vertex_t>::value},
+      cuco::sentinel::empty_value<vertex_t>{0},
       stream_adapter,
       handle.get_stream());
 
@@ -504,8 +504,8 @@ void renumber_local_ext_vertices(raft::handle_t const& handle,
     std::max(static_cast<size_t>(
                static_cast<double>(local_int_vertex_last - local_int_vertex_first) / load_factor),
              static_cast<size_t>(local_int_vertex_last - local_int_vertex_first) + 1),
-    invalid_vertex_id<vertex_t>::value,
-    invalid_vertex_id<vertex_t>::value,
+    cuco::sentinel::empty_key<vertex_t>{invalid_vertex_id<vertex_t>::value},
+    cuco::sentinel::empty_value<vertex_t>{0},
     stream_adapter,
     handle.get_stream());
 
@@ -673,8 +673,8 @@ void unrenumber_int_vertices(raft::handle_t const& handle,
                    std::max(static_cast<size_t>(
                               static_cast<double>(sorted_unique_int_vertices.size()) / load_factor),
                             sorted_unique_int_vertices.size() + 1),
-                   invalid_vertex_id<vertex_t>::value,
-                   invalid_vertex_id<vertex_t>::value,
+                   cuco::sentinel::empty_key<vertex_t>{invalid_vertex_id<vertex_t>::value},
+                   cuco::sentinel::empty_value<vertex_t>{0},
                    stream_adapter,
                    handle.get_stream()};
 
