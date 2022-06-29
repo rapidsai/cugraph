5c0378c634bd3351dfeb8e4484919988f7a64b42
Joseph Nke <jnke@dgx18.aselab.nvidia.com>
2022-06-28 18:41:48 -0700

    install required dependencies in the .toml file, remove setuptools extension build and setup, rely on skbuild setup instead

--- python/cugraph/setup.py
+++ python/cugraph/setup.py
@@ -16,24 +16,10 @@ import sys
 import sysconfig
 import shutil
 
-# Must import in this order:
-#   setuptools -> Cython.Distutils.build_ext -> setuptools.command.build_ext
-# Otherwise, setuptools.command.build_ext ends up inheriting from
-# Cython.Distutils.old_build_ext which we do not want
-import setuptools
-
-try:
-    from Cython.Distutils.build_ext import new_build_ext as _build_ext
-except ImportError:
-    from setuptools.command.build_ext import build_ext as _build_ext
-
-from distutils.sysconfig import get_python_lib
-
-import setuptools.command.build_ext
 from setuptools import find_packages, setup, Command
-from setuptools.extension import Extension
+from skbuild import setup
 
-from setuputils import get_environment_option
+from setuputils import get_environment_option, get_cuda_version_from_header
 
 import versioneer
 
@@ -41,20 +27,26 @@ import versioneer
 INSTALL_REQUIRES = ['numba', 'cython']
 CYTHON_FILES = ['cugraph/**/*.pyx']
 
+
+
 UCX_HOME = get_environment_option("UCX_HOME")
 CUDA_HOME = get_environment_option('CUDA_HOME')
 CONDA_PREFIX = get_environment_option('CONDA_PREFIX')
 
+# FIXME: No need to include the path to the conda dir
+"""
 conda_lib_dir = os.path.normpath(sys.prefix) + '/lib'
 conda_include_dir = os.path.normpath(sys.prefix) + '/include'
 
 if CONDA_PREFIX:
     conda_include_dir = CONDA_PREFIX + '/include'
     conda_lib_dir = CONDA_PREFIX + '/lib'
+"""
 
 if not UCX_HOME:
     UCX_HOME = CONDA_PREFIX if CONDA_PREFIX else os.sys.prefix
 
+# FIXME: add ucx to INSTALL_REQUIRES
 ucx_include_dir = os.path.join(UCX_HOME, "include")
 ucx_lib_dir = os.path.join(UCX_HOME, "lib")
 
@@ -75,13 +67,10 @@ if not os.path.isdir(CUDA_HOME):
     )
 
 cuda_include_dir = os.path.join(CUDA_HOME, "include")
-cuda_lib_dir = os.path.join(CUDA_HOME, "lib64")
+INSTALL_REQUIRES.append(
+    "cupy-cuda" + get_cuda_version_from_header(cuda_include_dir)
+)
 
-# Optional location of C++ build folder that can be configured by the user
-libcugraph_path = get_environment_option('CUGRAPH_BUILD_PATH')
-
-if not libcugraph_path:
-    libcugraph_path = conda_lib_dir
 
 extensions = [
     Extension("*",
@@ -130,46 +119,6 @@ class CleanCommand(Command):
         os.system('find . -name "*.cpp" -type f -delete')
         os.system('find . -name "*.cpython*.so" -type f -delete')
 
-
-class build_ext_no_debug(_build_ext):
-
-    def build_extensions(self):
-        def remove_flags(compiler, *flags):
-            for flag in flags:
-                try:
-                    compiler.compiler_so = list(
-                        filter((flag).__ne__, compiler.compiler_so)
-                    )
-                except Exception:
-                    pass
-        # Full optimization
-        self.compiler.compiler_so.append("-O3")
-        # No debug symbols, full optimization, no '-Wstrict-prototypes' warning
-        remove_flags(
-            self.compiler, "-g", "-G", "-O1", "-O2", "-Wstrict-prototypes"
-        )
-        super().build_extensions()
-
-    def finalize_options(self):
-        if self.distribution.ext_modules:
-            # Delay import this to allow for Cython-less installs
-            from Cython.Build.Dependencies import cythonize
-
-            nthreads = getattr(self, "parallel", None)  # -j option in Py3.5+
-            nthreads = int(nthreads) if nthreads else None
-            self.distribution.ext_modules = cythonize(
-                self.distribution.ext_modules,
-                nthreads=nthreads,
-                force=self.force,
-                gdb_debug=False,
-                compiler_directives=dict(
-                    profile=False, language_level=3, embedsignature=True
-                ),
-            )
-        # Skip calling super() and jump straight to setuptools
-        setuptools.command.build_ext.build_ext.finalize_options(self)
-
-
 cmdclass = dict()
 cmdclass.update(versioneer.get_cmdclass())
 cmdclass["build_ext"] = build_ext_no_debug
@@ -183,13 +132,12 @@ setup(name='cugraph',
           "Intended Audience :: Developers",
           # "Operating System :: OS Independent",
           "Programming Language :: Python",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7"
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3."
       ],
       # Include the separately-compiled shared library
       author="NVIDIA Corporation",
       setup_requires=['Cython>=0.29,<0.30'],
-      ext_modules=extensions,
       packages=find_packages(include=['cugraph', 'cugraph.*']),
       install_requires=INSTALL_REQUIRES,
       license="Apache",
