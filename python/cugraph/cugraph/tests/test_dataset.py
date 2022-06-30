7207934c4bd7d16be48701a7a713594a3b100583
Ralph Liu <ralphl@dgx18.aselab.nvidia.com>
2022-06-29 20:57:37 -0700

    Adding updated unit test

--- python/cugraph/cugraph/tests/test_dataset.py
+++ python/cugraph/cugraph/tests/test_dataset.py
@@ -16,6 +16,7 @@ import gc
 import pytest
 import warnings
 import yaml
+import os
 from pathlib import Path
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 # from cugraph.testing import utils
@@ -37,51 +38,70 @@ with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
+# Helper function to restore the default cfg
 def default_config():
     set_config(Path(__file__).parent.parent /
                "experimental/datasets/datasets_config.yaml")
 
 
-@pytest.fixture
-def create_config():
-    config_yaml = """
-                  fetch: False
-                  force: False
-                  download_dir: custom_storage_location
-                  """
-    c = yaml.safe_load(config_yaml)
+# We use this to create tempfiles that act as config files when we call
+# set_config(). Arguments passed will be handled in _custom_config and
+# act as custom download directories
+@pytest.fixture()
+def create_config(**kwargs):
+    # FIXME: remove inner function def
+    def _custom_config(**kwargs):
+        custom_path = kwargs.pop("name", "custom_storage_location")
 
-    outfile = NamedTemporaryFile()
-    with open(outfile.name, 'w') as f:
-        yaml.dump(c, f, sort_keys=False)
+        config_yaml = """
+                        fetch: False
+                        force: False
+                        download_dir':
+                        """
+        c = yaml.safe_load(config_yaml)
+        c['download_dir'] = custom_path
+        print(c)
 
-    yield outfile
-    outfile.close()
-    default_config()
+        outfile = NamedTemporaryFile()
+        with open(outfile.name, 'w') as f:
+            yaml.dump(c, f, sort_keys=False)
+
+        return outfile
+
+    return _custom_config
 
 
 def test_set_config(create_config):
-    set_config(create_config.name)
+    cfg = create_config()
+    set_config(cfg.name)
 
     assert str(karate.view_config()) == "custom_storage_location"
     assert str(dolphins.view_config()) == "custom_storage_location"
     assert str(netscience.view_config()) == "custom_storage_location"
     assert str(polbooks.view_config()) == "custom_storage_location"
+    default_config()
+
+
+@pytest.skip(msg="wip")
+def test_get_path():
+    ...
 
 
 def test_load_all(create_config):
     tmpd = TemporaryDirectory()
+    cfg = create_config(name=tmpd.name)
+    set_config(cfg.name)
+    load_all()
 
-    # with open(create_config.name, 'rw') as f:
-    #     cfg = yaml.safe_load(f)
-    #     print(cfg)
-    #     cfg['download_dir'] = tmpd.name
-    #     yaml.dump(cfg, f, sort_keys=False)
-    #     print(yaml.safe_load(f))
+    for data in ALL_DATASETS:
+        file_path = Path(tmpd.name) / (data.metadata['name'] +
+                                       data.metadata['file_type'])
+        print(str(file_path))
+        assert os.path.isfile(file_path)
 
+    os.listdir(tmpd.name)
     tmpd.cleanup()
-
-    assert False
+    default_config()
 
 
 @pytest.mark.parametrize("dataset", ALL_DATASETS)
