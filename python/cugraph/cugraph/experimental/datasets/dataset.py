05cfa0db897f416dddf3da85c68f166945f5b023
Ralph Liu <ralphl@dgx18.aselab.nvidia.com>
2022-07-05 11:17:16 -0700

    Adding support for environment variables

--- python/cugraph/cugraph/experimental/datasets/dataset.py
+++ python/cugraph/cugraph/experimental/datasets/dataset.py
@@ -18,10 +18,7 @@ import os
 from pathlib import Path
 
 
-this_dir = Path(os.getenv("this_dir", "cugraph/cugraph/experimental/datasets"))
-datasets_dir = this_dir.parent / "datasets"
-
-download_dir = Path("datasets")
+download_dir = Path.home() / ".cugraph/datasets"
 
 
 class Dataset:
@@ -39,11 +36,14 @@ class Dataset:
     """
     def __init__(self, meta_data_file_name):
         self.dir_path = Path(__file__).parent.absolute()
-        self.__read_config()
-        self.__meta_data_file_name = meta_data_file_name
-        self.__read_meta_data_file(self.__meta_data_file_name)
-        self.__edgelist = None
-        self.__graph = None
+        global download_dir
+        download_dir = Path(os.environ.get("RAPIDS_DATASET_ROOT_DIR", download_dir))
+
+        self._meta_data_file_name = meta_data_file_name
+        self.__read_meta_data_file(self._meta_data_file_name)
+
+        self._edgelist = None
+        self._graph = None
         self.path = None
 
     def __read_meta_data_file(self, meta_data_file):
@@ -53,6 +53,7 @@ class Dataset:
             file.close()
 
     def __read_config(self):
+        # This is the default config file
         config_path = self.dir_path / "datasets_config.yaml"
         with open(config_path, 'r') as file:
             cfg = yaml.safe_load(file)
@@ -61,14 +62,14 @@ class Dataset:
             file.close()
 
     def __download_csv(self, url):
+        if not os.path.isdir(download_dir):
+            os.makedirs(download_dir)
+
         filename = self.metadata['name'] + self.metadata['file_type']
         if os.path.isdir(download_dir):
             df = cudf.read_csv(url)
-            try:
-                df.to_csv(download_dir / filename, index=False)
-            except RuntimeError:
-                print("Error: cannot write files here")
-            self.path = download_dir / filename
+            df.to_csv(download_dir / filename, index=False)
+
         else:
             raise RuntimeError("The directory " + str(download_dir.absolute())
                                + " does not exist")
@@ -84,7 +85,7 @@ class Dataset:
             the YAML file.
         """
 
-        if self.__edgelist is None:
+        if self._edgelist is None:
             full_path = download_dir / (self.metadata['name'] +
                                         self.metadata['file_type'])
 
@@ -96,13 +97,13 @@ class Dataset:
                                        " get_edgelist(fetch=True) to" +
                                        " download the datafile")
 
-            self.__edgelist = cudf.read_csv(full_path,
+            self._edgelist = cudf.read_csv(full_path,
                                             delimiter=self.metadata['delim'],
                                             names=self.metadata['col_names'],
                                             dtype=self.metadata['col_types'])
             self.path = full_path
 
-        return self.__edgelist
+        return self._edgelist
 
     def get_graph(self, fetch=False):
         """
@@ -114,23 +115,24 @@ class Dataset:
             Automatically fetch for the dataset from the 'url' location within
             the YAML file.
         """
-        if self.__edgelist is None:
+        if self._edgelist is None:
             self.get_edgelist(fetch)
 
-        self.__graph = cugraph.Graph(directed=self.metadata['is_directed'])
-        self.__graph.from_cudf_edgelist(self.__edgelist, source='src',
+        self._graph = cugraph.Graph(directed=self.metadata['is_directed'])
+        self._graph.from_cudf_edgelist(self._edgelist, source='src',
                                         destination='dst')
 
-        return self.__graph
+        return self._graph
 
     def get_path(self):
         """
             Returns the location of the stored dataset file
         """
-        return self.path
+        if self.path is None:
+            raise RuntimeError("Path to datafile has not been set." + 
+                               " Call get_edgelist or get_graph first")
 
-    def view_config(self):
-        return download_dir
+        return self.path.absolute()
 
 
 def load_all(force=False):
@@ -141,10 +143,8 @@ def load_all(force=False):
     Parameters
         ----------
     """
-    # FIXME: Remove prints
     if not os.path.isdir(download_dir):
-        # print("Creating directory: " + str(download_dir.absolute()))
-        os.mkdir(download_dir)
+        os.makedirs(download_dir)
 
     meta_path = Path(__file__).parent.absolute() / "metadata"
     for file in os.listdir(meta_path):
@@ -158,8 +158,6 @@ def load_all(force=False):
                 filename = meta['name'] + meta['file_type']
                 save_to = download_dir / filename
                 if not os.path.isfile(save_to) or force:
-                    # print("Downloading dataset from: " + meta['url'])
-                    # print("  Saving file to " + str(save_to.absolute()))
                     df = cudf.read_csv(meta['url'])
                     df.to_csv(save_to, index=False)
 
@@ -173,14 +171,26 @@ def set_config(cfgpath):
     cfgfile : String
         Read the custom config file given its path, and override the default
     """
-
-    global download_dir
-    if not os.path.isabs(cfgpath):
-        download_dir = Path(cfgpath).absolute()
-    else:
-        download_dir = this_dir / cfgpath
-
-    with open(download_dir, 'r') as file:
+    with open(Path(cfgpath), 'r') as file:
         cfg = yaml.safe_load(file)
+        global download_dir
         download_dir = Path(cfg['download_dir'])
         file.close()
+
+
+def set_download_dir(path):
+    """
+    Set the download directory for fetching datasets
+
+    Parameters
+    ----------
+    path : String
+        Use as the storage location
+    """
+
+    global download_dir
+    download_dir = Path(path)
+
+
+def get_download_dir():
+    return download_dir.absolute()
