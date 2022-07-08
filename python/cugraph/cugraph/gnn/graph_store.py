f8228400c4c9a43adbdc7d42f111a0cc9c609215
Vibhu Jawa <vibhujawa@gmail.com>
2022-07-08 14:57:52 -0700

    Fixed Style

--- python/cugraph/cugraph/gnn/graph_store.py
+++ python/cugraph/cugraph/gnn/graph_store.py
@@ -55,14 +55,20 @@ class CuGraphStore:
         self.backend_lib = backend_lib
 
     def add_node_data(self, df, node_col_name, node_key, ntype=None):
-        self.gdata.add_vertex_data(df, vertex_col_name=node_col_name, type_name=ntype)
+        self.gdata.add_vertex_data(
+            df, vertex_col_name=node_col_name, type_name=ntype
+        )
         col_names = list(df.columns)
         col_names.remove(node_col_name)
         self.ndata_key_col_d[node_key] += col_names
 
     def add_edge_data(self, df, vertex_col_names, edge_key, etype=None):
-        self.gdata.add_edge_data(df, vertex_col_names=vertex_col_names, type_name=etype)
-        col_names = [col for col in list(df.columns) if col not in vertex_col_names]
+        self.gdata.add_edge_data(
+            df, vertex_col_names=vertex_col_names, type_name=etype
+        )
+        col_names = [
+            col for col in list(df.columns) if col not in vertex_col_names
+        ]
         self.edata_key_col_d[edge_key] += col_names
 
     def get_node_storage(self, key, ntype=None):
@@ -210,7 +216,9 @@ class CuGraphStore:
         _g = self.__G.extract_subgraph(
             create_using=cugraph.Graph, allow_multi_edges=True
         )
-        ego_edge_list, seeds_offsets = batched_ego_graphs(_g, current_seeds, radius=1)
+        ego_edge_list, seeds_offsets = batched_ego_graphs(
+            _g, current_seeds, radius=1
+        )
 
         del _g
         # filter and get a certain size neighborhood
@@ -232,7 +240,9 @@ class CuGraphStore:
         # Step 2
         # Sample Fan Out
         # for each dst take maximum of fanout samples
-        filtered_list = sample_groups(filtered_list, by="dst", n_samples=fanout)
+        filtered_list = sample_groups(
+            filtered_list, by="dst", n_samples=fanout
+        )
 
         # TODO: Verify order of execution
         sample_df = cudf.DataFrame(
@@ -258,7 +268,9 @@ class CuGraphStore:
         Return type is
         cudf.Series, cudf.Series
         """
-        edge_df = self.gdata._edge_prop_dataframe[[src_n, dst_n, eid_n, type_n]]
+        edge_df = self.gdata._edge_prop_dataframe[
+            [src_n, dst_n, eid_n, type_n]
+        ]
         subset_df = get_subset_df(
             edge_df, PropertyGraph.edge_id_col_name, edge_ids, etype
         )
@@ -368,7 +380,9 @@ class CuGraphStore:
             create_using=cugraph.Graph, allow_multi_edges=True
         )
 
-        p, w, s = cugraph.random_walks(_g, nodes, max_depth=length, use_padding=True)
+        p, w, s = cugraph.random_walks(
+            _g, nodes, max_depth=length, use_padding=True
+        )
 
         return p, w, s
 
