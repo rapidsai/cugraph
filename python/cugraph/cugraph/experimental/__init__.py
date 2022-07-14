283bc97d4128fd2e25ebb6524a8d072ae2934ce1
Joseph Nke <jnke@dgx18.aselab.nvidia.com>
2022-07-14 13:49:49 -0700

    add core number to experimental

--- python/cugraph/cugraph/experimental/__init__.py
+++ python/cugraph/cugraph/experimental/__init__.py
@@ -39,3 +39,7 @@ from cugraph.experimental.structure.bicliques import EXPERIMENTAL__find_biclique
 find_bicliques = deprecated_warning_wrapper(
     experimental_warning_wrapper(EXPERIMENTAL__find_bicliques)
 )
+
+from cugraph.experimental.cores.core_number import \
+    EXPERIMENTAL__core_number
+core_number = experimental_warning_wrapper(EXPERIMENTAL__core_number)
