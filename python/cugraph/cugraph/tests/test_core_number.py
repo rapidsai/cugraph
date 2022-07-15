64071771e6f8093cb6eb04f13804e8b1050247ae
Joseph Nke <jnke@dgx18.aselab.nvidia.com>
2022-07-14 19:29:13 -0700

    update tests for sg core number by testing the experimental implementation

--- python/cugraph/cugraph/tests/test_core_number.py
+++ python/cugraph/cugraph/tests/test_core_number.py
@@ -18,21 +18,9 @@ import random
 
 import cudf
 import cugraph
+import networkx as nx
 from cugraph.testing import utils
 from cugraph.experimental import core_number as experimental_core_number
-
-
-# Temporarily suppress warnings till networkX fixes deprecation warnings
-# (Using or importing the ABCs from 'collections' instead of from
-# 'collections.abc' is deprecated, and in 3.8 it will stop working) for
-# python 3.7.  Also, this import networkx needs to be relocated in the
-# third-party group once this gets fixed.
-import warnings
-
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    import networkx as nx
-
 
 # =============================================================================
 # Pytest Setup / Teardown - called for each test function
@@ -45,8 +33,7 @@ def setup_function():
 # Pytest fixtures
 # =============================================================================
 datasets = utils.DATASETS_UNDIRECTED
-# FIXME: The `start_list` parameter is not supported yet therefore it has been
-# disabled in these tests. Enable it once it is supported
+
 fixture_params = utils.genFixtureParamsProduct((datasets, "graph_file"),
                                                ([0,1], "degree_type"),
                                                )
@@ -56,7 +43,7 @@ fixture_params = utils.genFixtureParamsProduct((datasets, "graph_file"),
 def input_combo(request):
     """
     This fixture returns a dictionary containing all input params required to
-    run a Triangle Count algo
+    run a Core number algo
     """
     parameters = dict(
         zip(("graph_file", "degree_type"), request.param))
@@ -93,32 +80,22 @@ def test_core_number(input_combo):
     nx_core_number_results = nx_core_number_results.sort_values(
         "vertex").reset_index(drop=True)
 
-    assert cugraph_legacy_core_number_results == \
-        nx_core_number_results["core_number"].sum()
-
     core_number_results = experimental_core_number(G, degree_type).sort_values(
         "vertex").reset_index(drop=True).rename(columns={
             "core_number": "exp_cugraph_core_number"})
 
-    cugraph_exp_core_number_results = \
-        core_number_results["exp_cugraph_core_number"].sum()
-    # Compare the total number of triangles with the experimental
-    # implementation
-    assert cugraph_exp_core_number_results == nx_core_number_results
-    # Compare the number of triangles per vertex with the
-    # experimental implementation
+    # Compare the nx core number results with cugraph (both the legacy and
+    # and experimental version)
     core_number_results["nx_core_number"] = nx_core_number_results["core_number"]
-    core_number_results["legacy_cugraph_core_number"] = cugraph_legacy_core_number_results["core_number"]
-    print("core number results is \n", core_number_results)
+    core_number_results["legacy_cugraph_core_number"] = \
+        cugraph_legacy_core_number_results["core_number"]
     counts_diff = core_number_results.query(
-        'nx_core_number != exp_cugraph_core_number or exp_cugraph_core_number != legacy_cugraph_core_number')
+        'nx_core_number != exp_cugraph_core_number or \
+            exp_cugraph_core_number != legacy_cugraph_core_number')
     assert len(counts_diff) == 0
     
-    #print("core number results is \n", core_number_results)
 
-
-"""
-def test_triangles_directed_graph():
+def test_core_number_invalid_input(input_combo):
     input_data_path = (utils.RAPIDS_DATASET_ROOT_DIR_PATH /
                        "karate-asymmetric.csv").as_posix()
     M = utils.read_csv_for_nx(input_data_path)
@@ -133,8 +110,13 @@ def test_triangles_directed_graph():
     )
 
     with pytest.raises(ValueError):
-        cugraph.triangles(G)
+        cugraph.core_number(G)
 
     with pytest.raises(ValueError):
-        experimental_triangles(G)
-"""
+        experimental_core_number(G)
+
+    invalid_degree_type = 3
+    G = input_combo["G"]
+    with pytest.raises(ValueError):
+        experimental_core_number(G, invalid_degree_type)
+
