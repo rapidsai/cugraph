aeb6a3e16ad9ef2a0f619afec306fefeb0a9ad55
galipremsagar <sagarprem75@gmail.com>
2022-07-29 08:34:20 -0700

    rename

--- docs/cugraph/source/conf.py
+++ docs/cugraph/source/conf.py
@@ -212,7 +212,7 @@ numpydoc_class_members_toctree = False
 
 
 def setup(app):
-    app.add_css_file('custom.css')
+    app.add_css_file('custom_styles.css')
     app.add_js_file('custom.js')
     app.add_css_file('references.css')
 
