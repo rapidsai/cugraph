c25b508b51fc33cf3f0aaddb096b9fe89769cb62
Ralph Liu <ralphl@dgx18.aselab.nvidia.com>
2022-07-29 12:47:44 -0700

    Update ALL docstrings

--- python/cugraph/cugraph/structure/convert_matrix.py
+++ python/cugraph/cugraph/structure/convert_matrix.py
@@ -121,9 +121,8 @@ def from_adjlist(offsets, indices, values=None, create_using=Graph):
 
     Examples
     --------
-    >>> pdf = pd.read_csv(datasets_path / 'karate.csv', delimiter=' ',
-    ...                   dtype={0:'int32', 1:'int32', 2:'float32'},
-    ...                   header=None)
+    >>> from cugraph.experimental.datasets import karate
+    >>> pdf = karate.get_edgelist(fetch=True)
     >>> M = scipy.sparse.coo_matrix((pdf[2],(pdf[0],pdf[1])))
     >>> M = M.tocsr()
     >>> offsets = pd.Series(M.indptr)
