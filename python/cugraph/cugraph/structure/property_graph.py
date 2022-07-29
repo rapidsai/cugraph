b2f38be81d103e0b8037d7ffbb91b5e57e4b7389
Erik Welch <erik.n.welch@gmail.com>
2022-07-28 22:50:30 -0500

    use inplace=True
    Co-authored-by: Vibhu Jawa <vibhujawa@gmail.com>

--- python/cugraph/cugraph/structure/property_graph.py
+++ python/cugraph/cugraph/structure/property_graph.py
@@ -455,7 +455,7 @@ class EXPERIMENTAL__PropertyGraph:
                                                    default_edge_columns)
         else:
             column_names_to_drop = {vertex_col_names[0], vertex_col_names[1]}
-        tmp_df = tmp_df.drop(labels=column_names_to_drop, axis=1)
+        tmp_df.drop(labels=column_names_to_drop, axis=1, inplace=True)
 
         # Save the original dtypes for each new column so they can be restored
         # prior to constructing subgraphs (since column dtypes may get altered
