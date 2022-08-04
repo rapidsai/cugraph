f8e4aaa267b447c53b086f9d9004dc3d3ab928f1
GALI PREM SAGAR <sagarprem75@gmail.com>
2022-08-04 14:39:43 -0500

    Defer loading of `custom.js` (#2506)
    This PR switches the loading of `custom.js` to defer because we will need the entire page to be loading until the methods in this script can even execute correctly.
    
    xref: https://github.com/rapidsai/cudf/pull/11465
    
    Authors:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    Approvers:
      - AJ Schmidt (https://github.com/ajschmidt8)
      - Rick Ratzel (https://github.com/rlratzel)
    
    URL: https://github.com/rapidsai/cugraph/pull/2506

--- docs/cugraph/source/conf.py
+++ docs/cugraph/source/conf.py
@@ -213,7 +213,7 @@ numpydoc_class_members_toctree = False
 
 def setup(app):
     app.add_css_file("https://docs.rapids.ai/assets/css/custom.css")
-    app.add_js_file("https://docs.rapids.ai/assets/js/custom.js")
+    app.add_js_file("https://docs.rapids.ai/assets/js/custom.js", loading_method="defer")
     app.add_css_file("references.css")
 
 
