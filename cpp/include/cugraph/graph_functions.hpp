278a6d4b65cbe18a4bddff0a69e4e2bcec6ded5d
Seunghwa Kang <seunghwak@nvidia.com>
2022-08-09 12:57:49 -0700

    define create_graph_from_edgelist function API that takes edge IDs in addition

--- cpp/include/cugraph/graph_functions.hpp
+++ cpp/include/cugraph/graph_functions.hpp
@@ -522,4 +522,60 @@ create_graph_from_edgelist(raft::handle_t const& handle,
                            bool renumber,
                            bool do_expensive_check = false);
 
+// FIXME: this code should be re-factored (there should be a header file for this function including
+// implementation) to support different types (arithmetic types or thrust tuple of arithmetic types)
+// of edge properties.
+/**
+ * @brief create a graph from (the optional vertex list and) the given edge list with edge IDs.
+ *
+ * @tparam vertex_t Type of vertex identifiers. Needs to be an integral type.
+ * @tparam edge_t Type of edge identifiers. Needs to be an integral type.
+ * @tparam weight_t Type of edge weights. Needs to be a floating point type.
+ * @tparam store_transposed Flag indicating whether to use sources (if false) or destinations (if
+ * true) as major indices in storing edges using a 2D sparse matrix. transposed.
+ * @tparam multi_gpu Flag indicating whether template instantiation should target single-GPU (false)
+ * or multi-GPU (true).
+ * @param handle RAFT handle object to encapsulate resources (e.g. CUDA stream, communicator, and
+ * handles to various CUDA libraries) to run graph algorithms.
+ * @param vertices  If valid, part of the entire set of vertices in the graph to be renumbered.
+ * This parameter can be used to include isolated vertices. If multi-GPU, applying the
+ * compute_gpu_id_from_vertex_t to every vertex should return the local GPU ID for this function to
+ * work (vertices should be pre-shuffled).
+ * @param edgelist_srcs Vector of edge source vertex IDs. If multi-GPU, applying the
+ * compute_gpu_id_from_edge_t to every edge should return the local GPU ID for this function to work
+ * (edges should be pre-shuffled).
+ * @param edgelist_dsts Vector of edge destination vertex IDs.
+ * @param edgelist_weights Vector of edge weights.
+ * @param edgelist_ids Vector of edge IDs.
+ * @param graph_properties Properties of the graph represented by the input (optional vertex list
+ * and) edge list.
+ * @param renumber Flag indicating whether to renumber vertices or not.
+ * @param do_expensive_check A flag to run expensive checks for input arguments (if set to `true`).
+ * @return std::tuple<cugraph::graph_t<vertex_t, edge_t, weight_t, store_transposed,
+ * multi_gpu>, cugraph::edge_property_t<cugraph::graph_view_t<vertex_t, edge_t, weight_t,
+ * store_transposed, multi_gpu>, edge_t>, rmm::device_uvector<vertex_t>> Tuple of the generated
+ * graph, edge_property_t object storing edge IDs, and renumber map (if @p renumber is true) or
+ * std::nullopt (if @p renumber is false).
+ *
+ */
+template <typename vertex_t,
+          typename edge_t,
+          typename weight_t,
+          bool store_transposed,
+          bool multi_gpu>
+std::tuple<
+  cugraph::graph_t<vertex_t, edge_t, weight_t, store_transposed, multi_gpu>,
+  edge_property_t<cugraph::graph_view_t<vertex_t, edge_t, weight_t, store_transposed, multi_gpu>,
+                  edge_t>,
+  std::optional<rmm::device_uvector<vertex_t>>>
+create_graph_from_edgelist(raft::handle_t const& handle,
+                           std::optional<rmm::device_uvector<vertex_t>>&& vertices,
+                           rmm::device_uvector<vertex_t>&& edgelist_srcs,
+                           rmm::device_uvector<vertex_t>&& edgelist_dsts,
+                           std::optional<rmm::device_uvector<weight_t>>&& edgelist_weights,
+                           rmm::device_uvector<edge_t>&& edgelist_ids,
+                           graph_properties_t graph_properties,
+                           bool renumber,
+                           bool do_expensive_check = false);
+
 }  // namespace cugraph
