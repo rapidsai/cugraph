0f38540b49a478edbad5117d55eca0a41e2788b0
Seunghwa Kang <seunghwak@nvidia.com>
2022-08-09 10:25:21 -0700

    initial edge property data structure definitions

---
+++ cpp/src/prims/edge_partition_edge_property_device_view.cuh
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2021-2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <prims/edge_src_dst_property.hpp>
+
+#include <cugraph/utilities/device_functors.cuh>
+
+#include <raft/span.hpp>
+
+#include <thrust/binary_search.h>
+#include <thrust/distance.h>
+#include <thrust/execution_policy.h>
+#include <thrust/fill.h>
+#include <thrust/iterator/iterator_traits.h>
+#include <thrust/optional.h>
+
+namespace cugraph {
+
+namespace detail {
+
+template <typename edge_t, typename ValueIterator>
+class edge_partition_edge_property_device_view_t {
+ public:
+  using value_type = typename thrust::iterator_traits<ValueIterator>::value_type;
+
+  edge_partition_edge_property_device_view_t() = default;
+
+  edge_partition_edge_property_device_view_t(
+    edge_property_view_t<edge_t, ValueIterator> const& view, size_t partition_idx)
+    : value_first_(view.value_firsts()[partition_idx])
+  {
+    value_first_ = view.value_firsts()[partition_idx];
+  }
+
+  __device__ ValueIterator get_iter(edge_t offset) const
+  {
+    return value_first_ + offset;
+  }
+
+  __device__ value_type get(edge_t offset) const { return *get_iter(offset); }
+
+ private:
+  ValueIterator value_first_{};
+};
+
+template <typename edge_t>
+class edge_partition_edge_dummy_property_device_view_t {
+ public:
+  using value_type = thrust::nullopt_t;
+
+  edge_partition_edge_dummy_property_device_view_t() = default;
+
+  edge_partition_edge_dummy_property_device_view_t(
+    edge_dummy_property_view_t<edge_t> const& view, size_t partition_idx)
+  {
+  }
+
+  __device__ auto get(edge_t offset) const { return thrust::nullopt; }
+};
+
+}  // namespace detail
+
+}  // namespace cugraph
