784f4114d7832e4886f232621e1d73d1803f8339
Md Naim <mnaim@dgx16.aselab.nvidia.com>
2022-08-12 06:07:49 -0700

    API for Leiden community detection

--- cpp/src/community/leiden_impl.cuh
+++ cpp/src/community/leiden_impl.cuh
@@ -22,6 +22,7 @@
 #include <prims/update_v_frontier.cuh>
 #include <prims/vertex_frontier.cuh>
 
+#include <community/flatten_dendrogram.cuh>
 #include <cugraph/algorithms.hpp>
 #include <cugraph/graph_view.hpp>
 #include <cugraph/utilities/error.hpp>
@@ -48,6 +49,14 @@
 namespace cugraph {
 
 namespace detail {
+
+template <typename GraphViewType>
+void check_clustering(GraphViewType const& graph_view, typename GraphViewType::vertex_t* clustering)
+{
+  if (graph_view.local_vertex_partition_range_size() > 0)
+    CUGRAPH_EXPECTS(clustering != nullptr, "Invalid input argument: clustering is null");
+}
+
 template <typename GraphViewType>
 std::pair<std::unique_ptr<Dendrogram<typename GraphViewType::vertex_type>>,
           typename GraphViewType::weight_type>
@@ -64,16 +73,80 @@ leiden(raft::handle_t const& handle,
   return std::make_pair(std::make_unique<Dendrogram<vertex_t>>(), (weight_t)0.0);
 }
 
+template <typename GraphViewType>
+std::pair<size_t, typename GraphViewType::weight_type> leiden(
+  raft::handle_t const& handle,
+  GraphViewType const& graph_view,
+  typename GraphViewType::vertex_type* clustering,
+  size_t max_level,
+  typename GraphViewType::weight_type resolution);
+
+template <typename GraphViewType>
+void flatten_dendrogram(raft::handle_t const& handle,
+                        GraphViewType const& graph_view,
+                        Dendrogram<typename GraphViewType::vertex_t> const& dendrogram,
+                        typename GraphViewType::vertex_t* clustering)
+{
+  using vertex_t = typename GraphViewType::vertex_type;
+  using weight_t = typename GraphViewType::weight_type;
+  bool multi_gpu = GraphViewType::multi_gpu;
+
+  rmm::device_uvector<vertex_t> vertex_ids_v(graph_view.number_of_vertices(), handle.get_stream());
+  thrust::sequence(handle.get_thrust_policy(),
+                   vertex_ids_v.begin(),
+                   vertex_ids_v.end(),
+                   graph_view.local_vertex_partition_range_first());
+  partition_at_level<vertex_t, multi_gpu>(
+    handle, dendrogram, vertex_ids_v.data(), clustering, dendrogram.num_levels());
+}
+
 }  // namespace detail
 
-template <typename graph_view_t>
-std::pair<std::unique_ptr<Dendrogram<typename graph_view_t::vertex_type>>,
-          typename graph_view_t::weight_type>
+// Keep the API similar to Louvain
+
+template <typename GraphViewType>
+void flatten_dendrogram(raft::handle_t const& handle,
+                        GraphViewType const& graph_view,
+                        Dendrogram<typename GraphViewType::vertex_type> const& dendrogram,
+                        typename GraphViewType::vertex_type* clustering)
+{
+  detail::flatten_dendrogram(handle, graph_view, dendrogram, clustering);
+}
+
+template <typename GraphViewType>
+std::pair<std::unique_ptr<Dendrogram<typename GraphViewType::vertex_type>>,
+          typename GraphViewType::weight_type>
 leiden(raft::handle_t const& handle,
-       graph_view_t const& graph_view,
+       GraphViewType const& graph_view,
        size_t max_level,
-       typename graph_view_t::weight_type resolution)
+       typename GraphViewType::weight_type resolution)
 {
   return detail::leiden(handle, graph_view, max_level, resolution);
 }
+
+template <typename GraphViewType>
+std::pair<size_t, typename GraphViewType::weight_type> leiden(
+  raft::handle_t const& handle,
+  GraphViewType const& graph_view,
+  typename GraphViewType::vertex_type* clustering,
+  size_t max_level,
+  typename GraphViewType::weight_type resolution)
+{
+  using vertex_t = typename GraphViewType::vertex_type;
+  using weight_t = typename GraphViewType::weight_type;
+
+  CUGRAPH_EXPECTS(graph_view.is_weighted(), "Graph must be weighted");
+
+  detail::check_clustering(graph_view, clustering);
+
+  std::unique_ptr<Dendrogram<vertex_t>> dendrogram;
+  weight_t modularity;
+
+  std::tie(dendrogram, modularity) = leiden(handle, graph_view, max_level, resolution);
+
+  flatten_dendrogram(handle, graph_view, *dendrogram, clustering);
+
+  return std::make_pair(dendrogram->num_levels(), modularity);
+}
+
 }  // namespace cugraph
