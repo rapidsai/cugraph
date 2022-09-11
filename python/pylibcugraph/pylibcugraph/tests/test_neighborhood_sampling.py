6041ac580addc6f63808a1a6d13e1db579f29dc2
Rick Ratzel <rratzel@nvidia.com>
2022-09-11 14:17:31 -0500

    Updated uniform_neighbor_sample() to use new SamplingResult class, updated SG uniform_neighbor_sample test to add benchmark and check for leaks.

--- python/pylibcugraph/pylibcugraph/tests/test_neighborhood_sampling.py
+++ python/pylibcugraph/pylibcugraph/tests/test_neighborhood_sampling.py
@@ -130,21 +130,21 @@ def test_neighborhood_sampling_cudf():
     check_edges(result, device_srcs, device_dsts, device_weights, 6, 8, 2)
 
 
-def test_neighborhood_sampling_no_copy():
+def test_neighborhood_sampling_large_sg_graph(gpubenchmark):
     """
+    Use a large SG graph and set input args accordingly to test/benchmark
+    returning a large result.
     """
     resource_handle = ResourceHandle()
     graph_props = GraphProperties(is_symmetric=False, is_multigraph=False)
 
-    #device_srcs = cp.asarray([0, 1, 1, 2, 2, 2, 3, 4], dtype=np.int32)
-    #device_dsts = cp.asarray([1, 3, 4, 0, 1, 3, 5, 5], dtype=np.int32)
-    #device_weights = cp.asarray([0.1, 2.1, 1.1, 5.1, 3.1, 4.1, 7.2, 3.2],
-    #                            dtype=np.float32)
+    # FIXME: this graph is just a line - consider a better graph that exercises
+    # neighborhood sampling better/differently
     device_srcs = cp.arange(1e6, dtype=np.int32)
     device_dsts = cp.arange(1, 1e6+1, dtype=np.int32)
     device_weights = cp.asarray([1.0]*int(1e6), dtype=np.float32)
 
-    #start_list = cp.asarray([2, 2], dtype=np.int32)
+    # start_list == every vertex is intentionally excessive
     start_list = device_srcs
     fanout_vals = np.asarray([1, 2], dtype=np.int32)
 
@@ -157,17 +157,47 @@ def test_neighborhood_sampling_no_copy():
                  renumber=False,
                  do_expensive_check=False)
 
-    for i in range(700):
-        result = uniform_neighbor_sample(resource_handle,
-                                     sg,
-                                     start_list,
-                                     fanout_vals,
-                                     with_replacement=True,
-                                     do_expensive_check=False)
-        print(f"{i} ", end="")
+    # Ensure the only memory used after the algo call is for the result, so
+    # take a snapshot here.
+    # Assume GPU 0 will be used and the test has
+    # exclusive access and nothing else can use its memory while the test is
+    # running.
+    gc.collect()
+    device = cp.cuda.Device(0)
+    free_memory_before = device.mem_info[0]
 
-    breakpoint()
+    result = gpubenchmark(
+        uniform_neighbor_sample,
+        resource_handle,
+        sg,
+        start_list,
+        fanout_vals,
+        with_replacement=True,
+        do_expensive_check=False)
+
     assert type(result) is tuple
+    assert isinstance(result[0], cp.ndarray)
+    assert isinstance(result[1], cp.ndarray)
+    assert isinstance(result[2], cp.ndarray)
+    # Crude check that the results are accessible
+    assert result[0][0].dtype == np.int32
+    assert result[1][0].dtype == np.int32
+    assert result[2][0].dtype == np.float32
+
+    # Cleanup the result - this should leave the memory used equal to the amount
+    # prior to running the algo.
+    free_before_cleanup = device.mem_info[0]
+    print(f"{free_before_cleanup=}")
+    result_size = (len(result[0]) + len(result[1]) + len(result[2])) * (32//8)
+    del result
+    gc.collect()
+    free_after_cleanup = device.mem_info[0]
+    print(f"{free_after_cleanup=}")
+    actual_delta = free_after_cleanup - free_before_cleanup
+    expected_delta = free_memory_before - free_before_cleanup
+    leak = expected_delta - actual_delta
+    print(f"  {result_size=} {actual_delta=} {expected_delta=} {leak=}")
+    assert free_memory_before == device.mem_info[0]
 
 
 def test_sample_result():
