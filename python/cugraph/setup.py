04ad6852c2a2975c9fc7c2628a6e08011f428c9e
ptaylor <paul.e.taylor@me.com>
2022-09-13 00:09:19 -0700

    add dask-cuda to install_requires

--- python/cugraph/setup.py
+++ python/cugraph/setup.py
@@ -99,6 +99,7 @@ setup(name='cugraph'+os.getenv("PYTHON_PACKAGE_CUDA_SUFFIX", default=""),
       ],
       install_requires=[
         "numba",
+        f"dask-cuda>=22.8",
         f"cudf{os.getenv('PYTHON_PACKAGE_CUDA_SUFFIX', default='')}",
         f"dask-cudf{os.getenv('PYTHON_PACKAGE_CUDA_SUFFIX', default='')}",
         f"pylibcugraph{os.getenv('PYTHON_PACKAGE_CUDA_SUFFIX', default='')}"
