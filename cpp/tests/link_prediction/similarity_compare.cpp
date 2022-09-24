3eb2b40a869a801450b44d00197fae248557e9e1
Chuck Hastings <45364586+ChuckHastings@users.noreply.github.com>
2022-09-24 01:47:08 -0400

    Define C and C++ APIs for Jaccard/Sorensen/Overlap (#2624)
    This PR defines C and C++ APIs for the similarity algorithms Jaccard, Sorensen and Overlap, as well as the C++ unit tests.
    
    Legacy implementation of Jaccard and Overlap are moved to a legacy directory to make the transition clear.
    
    Closes #2541 
    Closes #2542 
    Closes #2544
    Closes #2545
    
    Authors:
      - Chuck Hastings (https://github.com/ChuckHastings)
    
    Approvers:
      - Seunghwa Kang (https://github.com/seunghwak)
      - Naim (https://github.com/naimnv)
      - Joseph Nke (https://github.com/jnke2016)
      - Rick Ratzel (https://github.com/rlratzel)
    
    URL: https://github.com/rapidsai/cugraph/pull/2624

---
+++ cpp/tests/link_prediction/similarity_compare.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governin_from_mtxg permissions and
+ * limitations under the License.
+ */
+
+#include <link_prediction/similarity_compare.hpp>
+
+#include <thrust/execution_policy.h>
+#include <thrust/sort.h>
+
+// FIXME:  To support weighted variants we need to compute the minimum weight of edge edge going to
+// the intersection
+//         and the maximum weight of each edge otherwise.  Need to think about how that works.
+//         Probably need to implement a custom set intersection loop rather than using the stl
+//         version, since then we can operate on each element appropriately.  That would mean
+//         adapting this class.
+template <typename T>
+struct intersection_count_t {
+  size_t count{0};
+  using value_type = T;
+
+  void push_back(T const&) { ++count; }
+};
+
+namespace cugraph {
+namespace test {
+
+template <typename vertex_t, typename weight_t, typename test_t>
+void similarity_compare(
+  vertex_t num_vertices,
+  std::tuple<std::vector<vertex_t>, std::vector<vertex_t>, std::optional<std::vector<weight_t>>>&&
+    edge_list,
+  std::tuple<std::vector<vertex_t>, std::vector<vertex_t>>&& vertex_pairs,
+  std::vector<weight_t>&& similarity_score,
+  test_t const& test_functor)
+{
+  // TBD
+}
+
+template void similarity_compare(
+  int32_t num_vertices,
+  std::tuple<std::vector<int32_t>, std::vector<int32_t>, std::optional<std::vector<float>>>&&
+    edge_list,
+  std::tuple<std::vector<int32_t>, std::vector<int32_t>>&& vertex_pairs,
+  std::vector<float>&& result_score,
+  test_jaccard_t const& test_functor);
+
+template void similarity_compare(
+  int32_t num_vertices,
+  std::tuple<std::vector<int32_t>, std::vector<int32_t>, std::optional<std::vector<float>>>&&
+    edge_list,
+  std::tuple<std::vector<int32_t>, std::vector<int32_t>>&& vertex_pairs,
+  std::vector<float>&& result_score,
+  test_sorensen_t const& test_functor);
+
+template void similarity_compare(
+  int32_t num_vertices,
+  std::tuple<std::vector<int32_t>, std::vector<int32_t>, std::optional<std::vector<float>>>&&
+    edge_list,
+  std::tuple<std::vector<int32_t>, std::vector<int32_t>>&& vertex_pairs,
+  std::vector<float>&& result_score,
+  test_overlap_t const& test_functor);
+
+template void similarity_compare(
+  int64_t num_vertices,
+  std::tuple<std::vector<int64_t>, std::vector<int64_t>, std::optional<std::vector<float>>>&&
+    edge_list,
+  std::tuple<std::vector<int64_t>, std::vector<int64_t>>&& vertex_pairs,
+  std::vector<float>&& result_score,
+  test_jaccard_t const& test_functor);
+
+template void similarity_compare(
+  int64_t num_vertices,
+  std::tuple<std::vector<int64_t>, std::vector<int64_t>, std::optional<std::vector<float>>>&&
+    edge_list,
+  std::tuple<std::vector<int64_t>, std::vector<int64_t>>&& vertex_pairs,
+  std::vector<float>&& result_score,
+  test_sorensen_t const& test_functor);
+
+template void similarity_compare(
+  int64_t num_vertices,
+  std::tuple<std::vector<int64_t>, std::vector<int64_t>, std::optional<std::vector<float>>>&&
+    edge_list,
+  std::tuple<std::vector<int64_t>, std::vector<int64_t>>&& vertex_pairs,
+  std::vector<float>&& result_score,
+  test_overlap_t const& test_functor);
+
+}  // namespace test
+}  // namespace cugraph
