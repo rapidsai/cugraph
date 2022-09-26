f0c1e99a1613261bc80642c894e5259e393a164c
Chuck Hastings <45364586+ChuckHastings@users.noreply.github.com>
2022-09-26 18:24:55 -0400

    Define k-core API and tests (#2712)
    Define k-core C++ API and tests.
    
    Closes #2631 
    Closes #2632 
    Closes #2633 
    Closes #2635
    
    Authors:
      - Chuck Hastings (https://github.com/ChuckHastings)
    
    Approvers:
      - Naim (https://github.com/naimnv)
      - Brad Rees (https://github.com/BradReesWork)
      - Seunghwa Kang (https://github.com/seunghwak)
      - Joseph Nke (https://github.com/jnke2016)
    
    URL: https://github.com/rapidsai/cugraph/pull/2712

--- cpp/src/c_api/core_result.cpp
+++ cpp/src/c_api/core_result.cpp
@@ -18,6 +18,39 @@
 
 #include <c_api/core_result.hpp>
 
+extern "C" cugraph_error_code_t cugraph_core_result_create(
+  const cugraph_resource_handle_t* handle,
+  cugraph_type_erased_device_array_view_t* vertices,
+  cugraph_type_erased_device_array_view_t* core_numbers,
+  cugraph_core_result_t** core_result,
+  cugraph_error_t** error)
+{
+  cugraph_error_code_t error_code{CUGRAPH_SUCCESS};
+
+  cugraph::c_api::cugraph_type_erased_device_array_t* vertices_copy;
+  cugraph::c_api::cugraph_type_erased_device_array_t* core_numbers_copy;
+
+  error_code = cugraph_type_erased_device_array_create_from_view(
+    handle,
+    vertices,
+    reinterpret_cast<cugraph_type_erased_device_array_t**>(&vertices_copy),
+    error);
+  if (error_code == CUGRAPH_SUCCESS) {
+    error_code = cugraph_type_erased_device_array_create_from_view(
+      handle,
+      core_numbers,
+      reinterpret_cast<cugraph_type_erased_device_array_t**>(&core_numbers_copy),
+      error);
+
+    if (error_code == CUGRAPH_SUCCESS) {
+      auto internal_pointer =
+        new cugraph::c_api::cugraph_core_result_t{vertices_copy, core_numbers_copy};
+      *core_result = reinterpret_cast<cugraph_core_result_t*>(internal_pointer);
+    }
+  }
+  return error_code;
+}
+
 extern "C" cugraph_type_erased_device_array_view_t* cugraph_core_result_get_vertices(
   cugraph_core_result_t* result)
 {
@@ -39,5 +72,38 @@ extern "C" void cugraph_core_result_free(cugraph_core_result_t* result)
   auto internal_pointer = reinterpret_cast<cugraph::c_api::cugraph_core_result_t*>(result);
   delete internal_pointer->vertex_ids_;
   delete internal_pointer->core_numbers_;
+  delete internal_pointer;
+}
+
+cugraph_type_erased_device_array_view_t* cugraph_k_core_result_get_src_vertices(
+  cugraph_k_core_result_t* result)
+{
+  auto internal_pointer = reinterpret_cast<cugraph::c_api::cugraph_k_core_result_t*>(result);
+  return reinterpret_cast<cugraph_type_erased_device_array_view_t*>(
+    internal_pointer->src_vertices_->view());
+}
+
+cugraph_type_erased_device_array_view_t* cugraph_k_core_result_get_dst_vertices(
+  cugraph_k_core_result_t* result)
+{
+  auto internal_pointer = reinterpret_cast<cugraph::c_api::cugraph_k_core_result_t*>(result);
+  return reinterpret_cast<cugraph_type_erased_device_array_view_t*>(
+    internal_pointer->dst_vertices_->view());
+}
+
+cugraph_type_erased_device_array_view_t* cugraph_k_core_result_get_weights(
+  cugraph_k_core_result_t* result)
+{
+  auto internal_pointer = reinterpret_cast<cugraph::c_api::cugraph_k_core_result_t*>(result);
+  return reinterpret_cast<cugraph_type_erased_device_array_view_t*>(
+    internal_pointer->weights_->view());
+}
+
+void cugraph_k_core_result_free(cugraph_k_core_result_t* result)
+{
+  auto internal_pointer = reinterpret_cast<cugraph::c_api::cugraph_k_core_result_t*>(result);
+  delete internal_pointer->src_vertices_;
+  delete internal_pointer->dst_vertices_;
+  delete internal_pointer->weights_;
   delete internal_pointer;
 }
