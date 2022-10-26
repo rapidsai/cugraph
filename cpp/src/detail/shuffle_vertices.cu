96aa9356b51207f3d39624c679495cbd580f67b2
Charles Hastings <charlesh@nvidia.com>
2022-10-26 11:33:28 -0700

    separate some modules to improve compile speed

--- cpp/src/detail/shuffle_vertices.cu
+++ cpp/src/detail/shuffle_vertices.cu
@@ -13,10 +13,102 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <detail/shuffle_wrappers_impl.cuh>
+#include <detail/graph_utils.cuh>
+
+#include <cugraph/detail/shuffle_wrappers.hpp>
+//#include <cugraph/graph_functions.hpp>
+#include <cugraph/utilities/shuffle_comm.cuh>
+
+#include <thrust/tuple.h>
+
+#include <tuple>
 
 namespace cugraph {
 namespace detail {
+
+template <typename vertex_t, typename func_t>
+rmm::device_uvector<vertex_t> shuffle_vertices_by_gpu_id_impl(
+  raft::handle_t const& handle, rmm::device_uvector<vertex_t>&& d_vertices, func_t func)
+{
+  rmm::device_uvector<vertex_t> d_rx_vertices(0, handle.get_stream());
+  std::tie(d_rx_vertices, std::ignore) = cugraph::groupby_gpu_id_and_shuffle_values(
+    handle.get_comms(),
+    d_vertices.begin(),
+    d_vertices.end(),
+    [key_func = func] __device__(auto val) { return key_func(val); },
+    handle.get_stream());
+
+  return d_rx_vertices;
+}
+
+template <typename vertex_t, typename value_t, typename func_t>
+std::tuple<rmm::device_uvector<vertex_t>, rmm::device_uvector<value_t>>
+shuffle_vertices_and_values_by_gpu_id_impl(raft::handle_t const& handle,
+                                           rmm::device_uvector<vertex_t>&& d_vertices,
+                                           rmm::device_uvector<value_t>&& d_values,
+                                           func_t func)
+{
+  std::tie(d_vertices, d_values, std::ignore) = cugraph::groupby_gpu_id_and_shuffle_kv_pairs(
+    handle.get_comms(),
+    d_vertices.begin(),
+    d_vertices.end(),
+    d_values.begin(),
+    [key_func = func] __device__(auto val) { return key_func(val); },
+    handle.get_stream());
+
+  return std::make_tuple(std::move(d_vertices), std::move(d_values));
+}
+
+template <typename vertex_t>
+rmm::device_uvector<vertex_t> shuffle_ext_vertices_by_gpu_id(
+  raft::handle_t const& handle, rmm::device_uvector<vertex_t>&& d_vertices)
+{
+  auto const comm_size = handle.get_comms().get_size();
+
+  return shuffle_vertices_by_gpu_id_impl(
+    handle,
+    std::move(d_vertices),
+    cugraph::detail::compute_gpu_id_from_ext_vertex_t<vertex_t>{comm_size});
+}
+
+template <typename vertex_t>
+rmm::device_uvector<vertex_t> shuffle_int_vertices_by_gpu_id(
+  raft::handle_t const& handle,
+  rmm::device_uvector<vertex_t>&& d_vertices,
+  std::vector<vertex_t> const& vertex_partition_range_lasts)
+{
+  rmm::device_uvector<vertex_t> d_vertex_partition_range_lasts(vertex_partition_range_lasts.size(),
+                                                               handle.get_stream());
+  raft::update_device(d_vertex_partition_range_lasts.data(),
+                      vertex_partition_range_lasts.data(),
+                      vertex_partition_range_lasts.size(),
+                      handle.get_stream());
+
+  auto return_value = shuffle_vertices_by_gpu_id_impl(
+    handle,
+    std::move(d_vertices),
+    cugraph::detail::compute_gpu_id_from_int_vertex_t<vertex_t>{
+      {d_vertex_partition_range_lasts.data(), d_vertex_partition_range_lasts.size()}});
+
+  handle.sync_stream();
+
+  return return_value;
+}
+
+template <typename vertex_t, typename value_t>
+std::tuple<rmm::device_uvector<vertex_t>, rmm::device_uvector<value_t>>
+shuffle_ext_vertices_and_values_by_gpu_id(raft::handle_t const& handle,
+                                          rmm::device_uvector<vertex_t>&& d_vertices,
+                                          rmm::device_uvector<value_t>&& d_values)
+{
+  auto const comm_size = handle.get_comms().get_size();
+
+  return shuffle_vertices_and_values_by_gpu_id_impl(
+    handle,
+    std::move(d_vertices),
+    std::move(d_values),
+    cugraph::detail::compute_gpu_id_from_ext_vertex_t<vertex_t>{comm_size});
+}
 
 template rmm::device_uvector<int32_t> shuffle_int_vertices_by_gpu_id(
   raft::handle_t const& handle,
