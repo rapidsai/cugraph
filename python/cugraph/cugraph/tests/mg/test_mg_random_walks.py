9eee71c27e759c80435bb236b976c941794e1da7
jnke2016 <jnke2016@gmail.com>
2022-11-05 12:40:26 -0700

    add test for the edge weight path

--- python/cugraph/cugraph/tests/mg/test_mg_random_walks.py
+++ python/cugraph/cugraph/tests/mg/test_mg_random_walks.py
@@ -33,7 +33,7 @@ def setup_function():
     gc.collect()
 
 
-IS_DIRECTED = [True]
+IS_DIRECTED = [True, False]
 
 
 # =============================================================================
@@ -41,7 +41,7 @@ IS_DIRECTED = [True]
 # =============================================================================
 
 datasets = DATASETS_SMALL + [karate_asymmetric]
-# datasets = [karate_asymmetric]
+#datasets = [karate_asymmetric]
 
 fixture_params = utils.genFixtureParamsProduct(
     (datasets, "graph_file"),
@@ -81,6 +81,7 @@ def calc_random_walks(G):
     k = random.randint(1, 4)
     random_walks_type = "uniform"
     max_depth = random.randint(2, 4)
+
     start_vertices = G.nodes().compute().sample(k).reset_index(drop=True)
 
     vertex_paths, edge_weights, max_path_length = dcg.random_walks(
@@ -92,16 +93,20 @@ def calc_random_walks(G):
 
 def check_random_walks(G, path_data, seeds, max_depth, df_G=None):
     invalid_edge = 0
+    invalid_edge_wgt_path = 0
     invalid_seeds = 0
     offsets_idx = 0
     next_path_idx = 0
+    invalid_edge_wgt_path = 0
+    e_wgt_path_idx = 0
     v_paths = path_data[0].compute()
+    e_paths = path_data[1].compute()
 
     max_path_length = path_data[2]
     sizes = max_path_length
 
     for _ in range(len(seeds)):
-        for i in range(next_path_idx, next_path_idx + sizes - 1):
+        for i in range(next_path_idx, next_path_idx + sizes):
             src, dst = v_paths.iloc[i], v_paths.iloc[i + 1]
 
             if i == next_path_idx and src not in seeds.values:
@@ -117,8 +122,14 @@ def check_random_walks(G, path_data, seeds, max_depth, df_G=None):
                 if src != -1:
                     src_degree = G.out_degree([src])["degree"].compute()[0]
                     if dst == -1 and src_degree == 0:
-                        # No need to check the next element as 'dst' will become 'src'
-                        i += 1
+                        if e_paths.values[e_wgt_path_idx] != 0:
+                            wgt = e_paths.values[e_wgt_path_idx]
+                            print(
+                                "[ERR] Invalid edge weight path: "
+                                "Edge src {} dst {} has wgt 0 "
+                                "But got wgt {}".format(src, dst, wgt)
+                            )
+                            invalid_edge_wgt_path += 1
                     else:
                         exp_edge = df_G.loc[
                             (df_G["src"] == (src)) & (df_G["dst"] == (dst))
@@ -130,12 +141,33 @@ def check_random_walks(G, path_data, seeds, max_depth, df_G=None):
                                 "There is no edge src {} dst {}".format(src, dst)
                             )
                             invalid_edge += 1
+                        else:
+                            # This is a valid edge, check the edge_wgt_path
+                            if e_paths.values[e_wgt_path_idx] != 1:
+                                wgt = e_paths.values[e_wgt_path_idx]
+                                print(
+                                    "[ERR] Invalid edge weight path: "
+                                    "Edge src {} dst {} has wgt 1 "
+                                    "But got wgt {}".format(src, dst, wgt)
+                                )
+                                invalid_edge_wgt_path += 1
+                else:
+                    # v_path: src == -1, dst == -1 => e_wgt_path=0 otherwise ERROR
+                    if e_paths.values[e_wgt_path_idx] != 0:
+                        wgt = e_paths.values[e_wgt_path_idx]
+                        print(
+                            "[ERR] Invalid edge weight path: "
+                            "Edge src {} dst {} has wgt 0 "
+                            "But got wgt {}".format(src, dst, wgt)
+                        )
+                        invalid_edge_wgt_path += 1
 
-        offsets_idx += 1
+            e_wgt_path_idx += 1
         next_path_idx += sizes + 1
 
     assert invalid_edge == 0
     assert invalid_seeds == 0
+    assert invalid_edge_wgt_path == 0
     assert max_path_length == max_depth
 
 
