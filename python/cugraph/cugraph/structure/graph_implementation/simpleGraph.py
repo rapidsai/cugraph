1f346de0343e082fad01ef6741b3b9e1b08ee14d
Joseph Nke <76006812+jnke2016@users.noreply.github.com>
2022-11-14 13:53:22 -0600

    Update python WCC to leverage the CAPI (#2866)
    The CAPI has a `weakly connected component` version that can be leverage by the PLC and python API. This PR:
    1. Add a PLC implementation of `WCC`
    2. Update the python API to leverage the `PLC` API
    3. Remove the legacy implementation
    4. Update both the python and the `PLC` tests
    5. Add `PLC` API to to create a graph from a `CSR `representation
    6. Update docstrings 
    
    closes #2509 
    closes #2496
    
    Authors:
      - Joseph Nke (https://github.com/jnke2016)
    
    Approvers:
      - Chuck Hastings (https://github.com/ChuckHastings)
      - Rick Ratzel (https://github.com/rlratzel)
    
    URL: https://github.com/rapidsai/cugraph/pull/2866

--- python/cugraph/cugraph/structure/graph_implementation/simpleGraph.py
+++ python/cugraph/cugraph/structure/graph_implementation/simpleGraph.py
@@ -878,8 +878,8 @@ class simpleGraphImpl:
         self._plc_graph = SGGraph(
             resource_handle=ResourceHandle(),
             graph_properties=graph_props,
-            src_array=self.edgelist.edgelist_df[simpleGraphImpl.srcCol],
-            dst_array=self.edgelist.edgelist_df[simpleGraphImpl.dstCol],
+            src_or_offset_array=self.edgelist.edgelist_df[simpleGraphImpl.srcCol],
+            dst_or_index_array=self.edgelist.edgelist_df[simpleGraphImpl.dstCol],
             weight_array=weight_col,
             edge_id_array=id_col,
             edge_type_array=type_col,
