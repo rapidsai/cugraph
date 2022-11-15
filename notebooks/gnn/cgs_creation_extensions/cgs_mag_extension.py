ddfd200adba53c18937668ab1df63e07f0deecd7
Alexandria Barghi <abarghi@nvidia.com>
2022-11-15 18:08:34 +0000

    start nb tests

---
+++ notebooks/gnn/cgs_creation_extensions/cgs_mag_extension.py
@@ -0,0 +1,18 @@
+import cudf
+import dask_cudf
+
+from cugraph.experimental import MGPropertyGraph
+from cugraph.experimental import PropertyGraph
+
+
+import cudf
+from ogb.nodeproppred import NodePropPredDataset
+
+def create_mag(server):
+    
+
+    pG = PropertyGraph()
+
+   
+
+    return pG
