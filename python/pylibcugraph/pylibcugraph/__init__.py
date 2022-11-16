1d8b822ce4e4fe008a51a72f8e8f70f8b79a0829
jnke2016 <jnke2016@gmail.com>
2022-11-16 06:23:03 -0800

    add a pylibcugraph implementation of sorensen leveraging the CAPI

--- python/pylibcugraph/pylibcugraph/__init__.py
+++ python/pylibcugraph/pylibcugraph/__init__.py
@@ -45,6 +45,8 @@ from pylibcugraph.core_number import core_number
 
 from pylibcugraph.jaccard_coefficients import jaccard_coefficients
 
+from pylibcugraph.sorensen_coefficients import sorensen_coefficients
+
 from pylibcugraph.two_hop_neighbors import get_two_hop_neighbors
 
 from pylibcugraph.louvain import louvain
