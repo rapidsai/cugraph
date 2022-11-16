8b40446439bbc01d02ee79c70ab0039f2401f994
jnke2016 <jnke2016@gmail.com>
2022-11-16 13:28:42 -0800

    fix destination dtype

--- cpp/src/c_api/extract_ego.cpp
+++ cpp/src/c_api/extract_ego.cpp
@@ -128,7 +128,7 @@ struct extract_ego_functor : public cugraph::c_api::abstract_functor {
 
       result_ = new cugraph::c_api::cugraph_induced_subgraph_result_t{
         new cugraph::c_api::cugraph_type_erased_device_array_t(src, graph_->vertex_type_),
-        new cugraph::c_api::cugraph_type_erased_device_array_t(dst, graph_->edge_type_),
+        new cugraph::c_api::cugraph_type_erased_device_array_t(dst, graph_->vertex_type_),
         wgt ? new cugraph::c_api::cugraph_type_erased_device_array_t(*wgt, graph_->weight_type_)
             : NULL,
         new cugraph::c_api::cugraph_type_erased_device_array_t(edge_offsets,
