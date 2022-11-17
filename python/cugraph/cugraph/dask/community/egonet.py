167c8edcdd3b5b82559df0e397345b3a38906058
jnke2016 <jnke2016@gmail.com>
2022-11-16 16:08:17 -0800

    fix typo

--- python/cugraph/cugraph/dask/community/egonet.py
+++ python/cugraph/cugraph/dask/community/egonet.py
@@ -130,7 +130,7 @@ def ego_graph(input_graph, n, radius=1, center=True):
     # Initialize dask client
     client = input_graph._client
 
-    elif isinstance(n, (int, list)):
+    if isinstance(n, (int, list)):
         n = cudf.Series(n)
     elif not isinstance(
         n, (cudf.Series, dask_cudf.Series, cudf.DataFrame, dask_cudf.DataFrame)
