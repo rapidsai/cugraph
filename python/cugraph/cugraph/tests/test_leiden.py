83c059cc3d504f9288866836208348757f98ef25
jnke2016 <jnke2016@gmail.com>
2022-11-18 14:49:11 -0800

    ensure a weighted graph is passsed to Leiden

--- python/cugraph/cugraph/tests/test_leiden.py
+++ python/cugraph/cugraph/tests/test_leiden.py
@@ -75,17 +75,13 @@ def test_leiden(graph_file):
 
 @pytest.mark.parametrize("graph_file", DATASETS)
 def test_leiden_nx(graph_file):
-    edgevals = True
     dataset_path = graph_file.get_path()
     NM = utils.read_csv_for_nx(dataset_path)
 
-    if edgevals:
-        G = nx.from_pandas_edgelist(NM, create_using=nx.Graph(), source="0", target="1")
-    else:
-        G = nx.from_pandas_edgelist(
-            NM, create_using=nx.Graph(), source="0", target="1", edge_attr="2"
-        )
-
+    G = nx.from_pandas_edgelist(
+        NM, create_using=nx.Graph(), source="0", target="1", edge_attr="weight"
+    )
+    #print("weighted is ", G.edgelist.weights)
     leiden_parts, leiden_mod = cugraph_leiden(G)
     louvain_parts, louvain_mod = cugraph_louvain(G)
 
@@ -101,4 +97,14 @@ def test_leiden_directed_graph():
     )
 
     with pytest.raises(ValueError):
+        parts, mod = cugraph_leiden(G)
+
+def test_leiden_unweighted_graph():
+
+    edgevals = True
+    G = karate_asymmetric.get_graph(
+        create_using=cugraph.Graph(directed=True), ignore_weights=True
+    )
+
+    with pytest.raises(RuntimeError):
         parts, mod = cugraph_leiden(G)
