a58aadd1dd806a8ec039f326da42421a280b6268
Rick Ratzel <rratzel@nvidia.com>
2022-11-22 13:46:14 -0600

    Updates to run uniform_neighbor_sample for local SG.

--- python/cugraph-service/tests/bench_uniform_neighbor_sample.py
+++ python/cugraph-service/tests/bench_uniform_neighbor_sample.py
@@ -12,26 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
-uniform neighbor sample
-
-cugraph ("local")
-  SG - small graph
-    small sample, large sample
-  SNMG - medium graph
-    small sample, large sample
-  MNMG - large graph
-    small sample, large sample
-
-cugraph-service
-  SG - small graph
-    small sample, large sample
-  SNMG - medium graph
-    small sample, large sample
-  MNMG - large graph
-    small sample, large sample
-"""
-
 import pytest
 import numpy as np
 from pylibcugraph.testing.utils import gen_fixture_params
@@ -49,11 +29,17 @@ except ImportError:
 
     gpubenchmark = pytest_benchmark.plugin.benchmark
 
-from cugraph import Graph
-from cugraph.experimental import datasets
+from cugraph import (
+    Graph,
+    uniform_neighbor_sample,
+)
 from cugraph.generators import rmat
+from cugraph.experimental import datasets
+from cugraph.dask import uniform_neighbor_sample as uniform_neighbor_sample_mg
 
 from cugraph_service_client import RemoteGraph
+
+seed = 42
 
 # pytest param values ("pv" suffix) used for defining input combinations. These
 # also include markers for easily running specific combinations.
@@ -87,27 +73,27 @@ karate_pv = pytest.param(
     id="dataset=karate",
 )
 small_low_degree_rmat_pv = pytest.param(
-    {"scale": 16, "edgefactor": 4, "seed": 42},
+    {"scale": 16, "edgefactor": 4, "seed": seed},
     id="dataset=rmat_16_4",
 )
 small_high_degree_rmat_pv = pytest.param(
-    {"scale": 16, "edgefactor": 32, "seed": 42},
+    {"scale": 16, "edgefactor": 32, "seed": seed},
     id="dataset=rmat_16_32",
 )
 large_low_degree_rmat_pv = pytest.param(
-    {"scale": 24, "edgefactor": 4, "seed": 42},
+    {"scale": 24, "edgefactor": 4, "seed": seed},
     id="dataset=rmat_24_4",
 )
 large_high_degree_rmat_pv = pytest.param(
-    {"scale": 24, "edgefactor": 32, "seed": 42},
+    {"scale": 24, "edgefactor": 32, "seed": seed},
     id="dataset=rmat_24_32",
 )
 huge_low_degree_rmat_pv = pytest.param(
-    {"scale": 30, "edgefactor": 4, "seed": 42},
+    {"scale": 30, "edgefactor": 4, "seed": seed},
     id="dataset=rmat_30_4",
 )
 huge_high_degree_rmat_pv = pytest.param(
-    {"scale": 30, "edgefactor": 32, "seed": 42},
+    {"scale": 30, "edgefactor": 32, "seed": seed},
     id="dataset=rmat_30_32",
 )
 large_start_list_pv = pytest.param(
@@ -148,14 +134,25 @@ graph_obj_fixture_params = gen_fixture_params(
 
 
 def get_edgelist(graph_data, gpu_config):
+    """
+    Return an edgelist DataFrame based on the data to be loaded/generated and
+    the GPU configuration (single-GPU, multi-GPU, etc.)
+    """
     is_mg = gpu_config in ["SNMG", "MNMG"]
 
     if isinstance(graph_data, datasets.Dataset):
         edgelist_df = graph_data.get_edgelist()
+        # FIXME: edgelist_df should have column names that match the defaults
+        # for G.from_cudf_edgelist()
+        edgelist_df.rename(
+            columns={"src": "source", "dst": "destination", "wgt": "weight"},
+            inplace=True,
+        )
         if is_mg:
             return dask_cudf.from_cudf(edgelist_df)
         else:
             return edgelist_df
+
     # Assume dictionary contains RMAT params
     elif isinstance(graph_data, dict):
         scale = graph_data["scale"]
@@ -175,55 +172,142 @@ def get_edgelist(graph_data, gpu_config):
         )
         rng = np.random.default_rng(seed)
         edgelist_df["weight"] = rng.random(size=len(edgelist_df))
+        edgelist_df.rename(
+            columns={"src": "source", "dst": "destination"}, inplace=True
+        )
         return edgelist_df
+
     else:
         raise TypeError(
             "graph_data can only be Dataset or dict, " f"got {type(graph_data)}"
         )
 
 
+def get_uniform_neighbor_sample_args(G, seed, start_list_len, fanout_list_len):
+    """
+    Return a dictionary containing the args for uniform_neighbor_sample based
+    on the graph and desired args passed in. For example, if a large start list
+    and small fanout list is desired, a "large" (based on graph size) list of
+    valid vert IDs for the graph passed in and a "small" list of fanout values
+    will be returned.
+
+    The dictionary return value allows for easily supporting other args without
+    having to maintain an order of values in a return tuple, for example.
+    """
+    if start_list_len not in ["SMALL", "LARGE"]:
+        raise ValueError(f"got unexpected value {start_list_len=}")
+    if fanout_list_len not in ["SMALL", "LARGE"]:
+        raise ValueError(f"got unexpected value {fanout_list_len=}")
+
+    rng = np.random.default_rng(seed)
+    num_verts = G.number_of_vertices()
+
+    if start_list_len == "LARGE":
+        start_list = rng.choice(num_verts, min(1000, int(num_verts * 0.5)))
+    else:
+        start_list = rng.choice(num_verts, 2)
+
+    if fanout_list_len == "LARGE":
+        num_edges = G.number_of_edges()
+        avg_degree = num_edges // num_verts
+        max_fanout = min(avg_degree, 5)
+        if max_fanout == 1:
+            fanout_choices = [1]
+        else:
+            fanout_choices = np.arange(1, max_fanout)
+        fanout_list = [rng.choice(fanout_choices, 1)[0] for _ in range(5)]
+    else:
+        fanout_list = [2, 1]
+
+    return {
+        "start_list": list(start_list),
+        "fanout_vals": list(fanout_list),
+        "with_replacement": True,
+    }
+
+
 @pytest.fixture(scope="module", params=graph_obj_fixture_params)
-def graph_obj(request):
+def graph_objs(request):
+    """
+    Fixture that returns a Graph object and algo callable based on the
+    parameters. This handles instantiating the correct type (Graph or
+    RemoteGraph) and populating it with graph data. This also ensures a
+    cugraph-service server is running in the case of a RemoteGraph. The
+    callable returned will be appropriate for the graph type (ie. "local" or
+    "remote" algo API).
+    """
     (graph_type, gpu_config, graph_data) = request.param
+
+    if gpu_config not in ["SG", "SNMG", "MNMG"]:
+        raise RuntimeError(f"got unexpected gpu_config value: {gpu_config}")
+
     if graph_type is Graph:
+        # FIXME: need to consider directed/undirected?
         G = Graph()
         edgelist_df = get_edgelist(graph_data, gpu_config)
         if gpu_config == "SG":
             G.from_cudf_edgelist(edgelist_df)
-        elif gpu_config == "SNMG":
-            G.from_dask_cudf_edgelist(edgelist_df)
-        elif gpu_config == "MNMG":
-            raise NotImplementedError("config=MNMG")
+            uns_func = uniform_neighbor_sample
         else:
-            raise RuntimeError(
-                f"got unexpected gpu_config value for Graph: {gpu_config}"
-            )
+            G.from_dask_cudf_edgelist(edgelist_df)
+            uns_func = uniform_neighbor_sample_mg
 
     elif graph_type is RemoteGraph:
         raise NotImplementedError
         if gpu_config == "SG":
+            # Ensure SG server is running
             # client = create_sg_client()
             G = RemoteGraph()
+        elif gpu_config == "SNMG":
+            # Ensure SNMG server is running
+            pass
+        else:
+            # Ensure MNMG server is running
+            pass
 
     else:
-        raise RuntimeError(f"{graph_type} is invalid")
+        raise RuntimeError(f"{graph_type=} is invalid")
 
-    yield G
+    yield (G, uns_func)
+
+
+################################################################################
+# Benchmarks
+"""
+@pytest.mark.parametrize("start_list_len", [small_start_list_pv, large_start_list_pv])
+@pytest.mark.parametrize(
+    "fanout_vals_len", [small_fanout_list_pv, large_fanout_list_pv]
+)
+def bench_uniform_neighbor_sample(
+    gpubenchmark, graph_objs, start_list_len, fanout_vals_len
+):
+"""
 
 
 @pytest.mark.parametrize("start_list_len", [small_start_list_pv, large_start_list_pv])
 @pytest.mark.parametrize(
     "fanout_vals_len", [small_fanout_list_pv, large_fanout_list_pv]
 )
-def bench_uniform_neighbor_sample(
-    gpubenchmark, graph_obj, start_list_len, fanout_vals_len
-):
+def bench_uniform_neighbor_sample(graph_objs, start_list_len, fanout_vals_len):
     """ """
-    # G = graph_obj
-    # breakpoint()
-    import time
+    (G, uniform_neighbor_sample_func) = graph_objs
 
-    def f():
-        time.sleep(0.01)
-
-    gpubenchmark(f)
+    uns_args = get_uniform_neighbor_sample_args(
+        G, seed, start_list_len, fanout_vals_len
+    )
+    print(f"\n{uns_args}")
+    # FIXME: uniform_neighbor_sample cannot take a np.ndarray for start_list
+    """
+    gpubenchmark(uniform_neighbor_sample_func,
+                 G,
+                 start_list=uns_args["start_list"],
+                 fanout_vals=uns_args["fanout_vals"],
+                 with_replacement=uns_args["with_replacement"],
+                 )
+    """
+    uniform_neighbor_sample_func(
+        G,
+        start_list=uns_args["start_list"],
+        fanout_vals=uns_args["fanout_vals"],
+        with_replacement=uns_args["with_replacement"],
+    )
