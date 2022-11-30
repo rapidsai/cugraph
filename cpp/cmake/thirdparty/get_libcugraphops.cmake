f6baf316e6052a4ccac64c0f3b5e73689a8ef119
Vyas Ramasubramani <vyasr@nvidia.com>
2022-11-30 08:53:20 -0800

    Implement ALLOW_CLONE_CUGRAPH_OPS.

--- cpp/cmake/thirdparty/get_libcugraphops.cmake
+++ cpp/cmake/thirdparty/get_libcugraphops.cmake
@@ -30,15 +30,17 @@ endif()
 
 function(find_and_configure_cugraph_ops)
 
-    set(oneValueArgs VERSION REPO PINNED_TAG BUILD_STATIC EXCLUDE_FROM_ALL CLONE_ON_PIN)
+    set(oneValueArgs VERSION REPO PINNED_TAG BUILD_STATIC EXCLUDE_FROM_ALL ALLOW_CLONE_CUGRAPH_OPS)
     cmake_parse_arguments(PKG "" "${oneValueArgs}" "" ${ARGN})
 
-    if(PKG_CLONE_ON_PIN AND NOT PKG_PINNED_TAG STREQUAL "branch-${CUGRAPH_CUGRAPH_OPS_VERSION}")
-        message("Pinned tag found: ${PKG_PINNED_TAG}. Cloning cugraph-ops locally.")
-        set(CPM_DOWNLOAD_cugraph-ops ON)
-    elseif(PKG_BUILD_STATIC AND (NOT CPM_cugraph-ops_SOURCE))
-      message(STATUS "CUGRAPH: Cloning cugraph-ops locally to build static libraries.")
-      set(CPM_DOWNLOAD_cugraph-ops ON)
+    if(PKG_ALLOW_CLONE_CUGRAPH_OPS)
+        if(NOT PKG_PINNED_TAG STREQUAL "branch-${CUGRAPH_CUGRAPH_OPS_VERSION}")
+            message("Pinned tag found: ${PKG_PINNED_TAG}. Cloning cugraph-ops locally.")
+            set(CPM_DOWNLOAD_cugraph-ops ON)
+        elseif(PKG_BUILD_STATIC AND (NOT CPM_cugraph-ops_SOURCE))
+            message(STATUS "CUGRAPH: Cloning cugraph-ops locally to build static libraries.")
+            set(CPM_DOWNLOAD_cugraph-ops ON)
+        endif()
     endif()
 
     set(CUGRAPH_OPS_BUILD_SHARED_LIBS ON)
@@ -72,7 +74,10 @@ find_and_configure_cugraph_ops(VERSION      ${CUGRAPH_MIN_VERSION_cugraph_ops}
                                PINNED_TAG   ${CUGRAPH_CUGRAPH_OPS_BRANCH}
                                BUILD_STATIC     ${CUGRAPH_USE_CUGRAPH_OPS_STATIC}
                                EXCLUDE_FROM_ALL ${CUGRAPH_EXCLUDE_CUGRAPH_OPS_FROM_ALL}
-                               # When PINNED_TAG above doesn't match cugraph,
-                               # force local cugraph-ops clone in build directory
-                               # even if it's already installed.
-                               CLONE_ON_PIN ON)
+                               # Allow cloning cugraph-ops in cases when we
+                               # expect the local copy of cugraph-ops to not be
+                               # the one that we want. Cases include static
+                               # linking such that we need to compile a static
+                               # lib or during development when using a
+                               # different branch of cugraph-ops.
+                               ALLOW_CLONE_CUGRAPH_OPS ${ALLOW_CLONE_CUGRAPH_OPS})
