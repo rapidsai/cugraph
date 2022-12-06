28df7ab30d340b06f4919d84c0721e2a6c187792
Seunghwa Kang <seunghwak@nvidia.com>
2022-12-05 16:02:11 -0800

    update HighResTimer

--- cpp/src/utilities/high_res_timer.hpp
+++ cpp/src/utilities/high_res_timer.hpp
@@ -15,92 +15,69 @@
  */
 #pragma once
 
-#include <ctime>
+#include <cugraph/utilities/error.hpp>
+
+#include <chrono>
 #include <iostream>
 #include <map>
-#include <sstream>
-#include <stdexcept>
+#include <ostream>
+#include <stack>
 #include <string>
-
-//#define TIMING
+#include <tuple>
 
 class HighResTimer {
  public:
-  HighResTimer() : timers() {}
-  ~HighResTimer() {}
-
   void start(std::string label)
   {
-    auto it = timers.find(label);
-    if (it == timers.end()) {
-      it = timers.insert(std::make_pair(label, std::make_pair<int, int64_t>(int{0}, int64_t{0})))
-             .first;
-    }
-
-    timespec start_time;
-    clock_gettime(CLOCK_REALTIME, &start_time);
-    it->second.second -= start_time.tv_sec * 1000000000 + start_time.tv_nsec;
-
-    open_label = label;
+    start_stack.push(std::make_tuple(label, std::chrono::steady_clock::now()));
   }
 
   void stop()
   {
-    timespec stop_time;
-    clock_gettime(CLOCK_REALTIME, &stop_time);
+    auto stop_time           = std::chrono::steady_clock::now();
+    auto [label, start_time] = start_stack.top();
+    start_stack.pop();
 
-    auto it = timers.find(open_label);
-    it->second.first++;
-    it->second.second += stop_time.tv_sec * 1000000000 + stop_time.tv_nsec;
-  }
-
-  double get_average_runtime(std::string const& label)
-  {
-    auto it = timers.find(label);
-    if (it != timers.end()) {
-      return (static_cast<double>(it->second.second) / (1000000.0 * it->second.first));
-    } else {
-      std::stringstream ss;
-      ss << "ERROR: timing label: " << label << "not found.";
-
-      throw std::runtime_error(ss.str());
+    auto it = labeled_timers.find(label);
+    if (it == labeled_timers.end()) {
+      labeled_timers[label] = std::make_tuple(size_t{0}, double{0});
+      it                    = labeled_timers.find(label);
     }
+
+    std::chrono::duration<double> diff = stop_time - start_time;
+
+    auto& timer = it->second;
+    std::get<0>(timer) += 1;
+    std::get<1>(timer) += diff.count();
   }
 
-  //
-  //  Add display functions... specific label or entire structure
-  //
   void display(std::ostream& os)
   {
-    os << "Timer Results (in ms):" << std::endl;
-    for (auto it = timers.begin(); it != timers.end(); ++it) {
-      os << "   " << it->first << " called " << it->second.first
-         << " times, average time: " << (it->second.second / (1000000.0 * it->second.first))
-         << std::endl;
+    for (auto it = labeled_timers.begin(); it != labeled_timers.end(); ++it) {
+      auto [count, duration] = it->second;
+      os << it->first << " called " << count
+         << " times, average time: " << duration / static_cast<double>(count) << " s." << std::endl;
     }
   }
 
   void display(std::ostream& os, std::string label)
   {
-    auto it = timers.find(label);
-    os << it->first << " called " << it->second.first
-       << " times, average time: " << (it->second.second / (1000000.0 * it->second.first))
-       << std::endl;
+    auto it = labeled_timers.find(label);
+    CUGRAPH_EXPECTS(it != labeled_timers.end(), "Invalid input argument: invalid label.");
+    auto [count, duration] = it->second;
+    os << it->first << " called " << count
+       << " times, average time: " << duration / static_cast<double>(count) << " s." << std::endl;
   }
 
   void display_and_clear(std::ostream& os)
   {
-    os << "Timer Results (in ms):" << std::endl;
-    for (auto it = timers.begin(); it != timers.end(); ++it) {
-      os << "   " << it->first << " called " << it->second.first
-         << " times, average time: " << (it->second.second / (1000000.0 * it->second.first))
-         << std::endl;
-    }
-
-    timers.clear();
+    display(os);
+    labeled_timers.clear();
   }
 
  private:
-  std::map<std::string, std::pair<int, int64_t>> timers;
-  std::string open_label;  // should probably be a stack...
+  std::map<std::string, std::tuple<size_t, double> /* # calls, aggregate duration in seconds */>
+    labeled_timers{};
+  std::stack<std::tuple<std::string, std::chrono::time_point<std::chrono::steady_clock>>>
+    start_stack{};
 };
