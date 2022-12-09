c1a56e20b255131b72c2442a7be495eeb928bc36
Chuck Hastings <45364586+ChuckHastings@users.noreply.github.com>
2022-12-09 14:51:01 -0500

    Add dependencies.yaml for rapids-dependency-file-generator (#3042)
    First cut at dependencies.yaml.
    
    We could certainly decompose this more, but wanted to get something out there quickly.
    
    Authors:
       - Chuck Hastings (https://github.com/ChuckHastings)
       - AJ Schmidt (https://github.com/ajschmidt8)
    
    Approvers:
       - AJ Schmidt (https://github.com/ajschmidt8)
       - Brad Rees (https://github.com/BradReesWork)
       - Bradley Dice (https://github.com/bdice)

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -54,7 +54,7 @@ sed_runner "s/set(cugraph_version .*)/set(cugraph_version ${NEXT_FULL_TAG})/g" p
 sed_runner 's/version = .*/version = '"'${NEXT_SHORT_TAG}'"'/g' docs/cugraph/source/conf.py
 sed_runner 's/release = .*/release = '"'${NEXT_FULL_TAG}'"'/g' docs/cugraph/source/conf.py
 
-for FILE in conda/environments/*.yml; do
+for FILE in conda/environments/*.yaml dependencies.yaml; do
    sed_runner "s/libcugraphops=${CURRENT_SHORT_TAG}/libcugraphops=${NEXT_SHORT_TAG}/g" ${FILE};
    sed_runner "s/cudf=${CURRENT_SHORT_TAG}/cudf=${NEXT_SHORT_TAG}/g" ${FILE};
    sed_runner "s/rmm=${CURRENT_SHORT_TAG}/rmm=${NEXT_SHORT_TAG}/g" ${FILE};
