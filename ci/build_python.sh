76839354f4d3395950229c5f99ce359ddf53fb8a
AJ Schmidt <aschmidt@nvidia.com>
2022-12-20 11:32:17 -0500

    [skip ci] add remaining pkgs to `build_python.sh`

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -13,11 +13,37 @@ rapids-logger "Begin py build"
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
-# TODO: Remove `--no-test` flag once importing on a CPU
+# TODO: Remove `--no-test` flags once importing on a CPU
 # node works correctly
 rapids-mamba-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
+  conda/recipes/pylibcugraph
+
+rapids-mamba-retry mambabuild \
+  --no-test \
+  --channel "${CPP_CHANNEL}" \
+  --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/cugraph
+
+rapids-mamba-retry mambabuild \
+  --no-test \
+  --channel "${CPP_CHANNEL}" \
+  --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
+  conda/recipes/cugraph-service
+
+rapids-mamba-retry mambabuild \
+  --no-test \
+  --channel "${CPP_CHANNEL}" \
+  --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
+  conda/recipes/cugraph-pyg
+
+rapids-mamba-retry mambabuild \
+  --no-test \
+  --channel "${CPP_CHANNEL}" \
+  --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
+  --channel dglteam \
+  --channel pytorch \
+  conda/recipes/cugraph-dgl
 
 rapids-upload-conda-to-s3 python
