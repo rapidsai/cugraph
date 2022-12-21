1b68e2ce0861ef1f9aa1c6a32000e6c1fd95b9b4
Alexandria Barghi <abarghi@nvidia.com>
2022-12-21 17:03:42 +0000

    fix merge conflict

--- build.sh
+++ build.sh
@@ -175,11 +175,7 @@ if hasArg --cmake_default_generator; then
     CMAKE_GENERATOR_OPTION=""
 fi
 if hasArg --pydevelop; then
-<<<<<<< HEAD
-    PYTHON_ARGS_FOR_INSTALL="-m pip install --no-build-isolation -e ."
-=======
     PYTHON_ARGS_FOR_INSTALL="-m pip install --no-build-isolation --no-deps -e ."
->>>>>>> python-sampling-edge-info
 fi
 
 # If clean or uninstall targets given, run them prior to any other steps
