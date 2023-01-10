49186c32c405714065223838a0e409d3a8021005
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2023-01-10 14:32:19 -0500

    Update python/cugraph/cugraph/traversal/bfs.py
    Co-authored-by: Alex Barghi <105237337+alexbarghi-nv@users.noreply.github.com>

--- python/cugraph/cugraph/traversal/bfs.py
+++ python/cugraph/cugraph/traversal/bfs.py
@@ -93,7 +93,7 @@ def _convert_df_to_output_type(df, input_type):
     Given a cudf.DataFrame df, convert it to a new type appropriate for the
     graph algos in this module, based on input_type.
     """
-    if input_type in [Graph]:
+    if input_type is Graph:
         return df
 
     elif is_nx_graph_type(input_type):
