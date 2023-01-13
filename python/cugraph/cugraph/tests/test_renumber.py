a67b46b63872248ffffd93212fda38e888d55447
jnke2016 <jnke2016@gmail.com>
2023-01-12 18:43:07 -0800

    remove outdated tests still using cython.cu renumbering

--- python/cugraph/cugraph/tests/test_renumber.py
+++ python/cugraph/cugraph/tests/test_renumber.py
@@ -25,50 +25,6 @@ from cugraph.testing import utils
 from cugraph.experimental.datasets import DATASETS
 
 
-def test_renumber_ips():
-    source_list = [
-        "192.168.1.1",
-        "172.217.5.238",
-        "216.228.121.209",
-        "192.16.31.23",
-    ]
-    dest_list = [
-        "172.217.5.238",
-        "216.228.121.209",
-        "192.16.31.23",
-        "192.168.1.1",
-    ]
-
-    pdf = pd.DataFrame({"source_list": source_list, "dest_list": dest_list})
-
-    gdf = cudf.from_pandas(pdf)
-
-    gdf["source_as_int"] = gdf["source_list"].str.ip2int()
-    gdf["dest_as_int"] = gdf["dest_list"].str.ip2int()
-
-    renumbered_gdf, renumber_map = NumberMap.renumber(
-        gdf, "source_as_int", "dest_as_int", preserve_order=True
-    )
-
-    input_check = renumbered_gdf.merge(gdf, on=["source_list", "dest_list"])
-
-    output_check = renumber_map.from_internal_vertex_id(
-        renumbered_gdf,
-        renumber_map.renumbered_src_col_name,
-        external_column_names=["check_src"],
-    )
-    output_check = renumber_map.from_internal_vertex_id(
-        output_check,
-        renumber_map.renumbered_dst_col_name,
-        external_column_names=["check_dst"],
-    )
-
-    merged = output_check.merge(input_check, on=["source_list", "dest_list"])
-
-    assert_series_equal(merged["check_src"], merged["source_as_int"], check_names=False)
-    assert_series_equal(merged["check_dst"], merged["dest_as_int"], check_names=False)
-
-
 def test_renumber_ips_cols():
 
     source_list = [
@@ -114,39 +70,6 @@ def test_renumber_ips_cols():
     assert_series_equal(merged["check_dst"], merged["dest_as_int"], check_names=False)
 
 
-def test_renumber_negative():
-    source_list = [4, 6, 8, -20, 1]
-    dest_list = [1, 29, 35, 0, 77]
-
-    df = pd.DataFrame({"source_list": source_list, "dest_list": dest_list})
-
-    gdf = cudf.DataFrame.from_pandas(df[["source_list", "dest_list"]])
-    gdf["original_src"] = gdf["source_list"]
-    gdf["original_dst"] = gdf["dest_list"]
-
-    renumbered_gdf, renumber_map = NumberMap.renumber(
-        gdf, "source_list", "dest_list", preserve_order=True
-    )
-
-    input_check = renumbered_gdf.merge(gdf, on=["original_src", "original_dst"])
-
-    output_check = renumber_map.from_internal_vertex_id(
-        renumbered_gdf,
-        renumber_map.renumbered_src_col_name,
-        external_column_names=["check_src"],
-    )
-    output_check = renumber_map.from_internal_vertex_id(
-        output_check,
-        renumber_map.renumbered_dst_col_name,
-        external_column_names=["check_dst"],
-    )
-
-    merged = output_check.merge(input_check, on=["original_src", "original_dst"])
-
-    assert_series_equal(merged["check_src"], merged["original_src"], check_names=False)
-    assert_series_equal(merged["check_dst"], merged["original_dst"], check_names=False)
-
-
 def test_renumber_negative_col():
     source_list = [4, 6, 8, -20, 1]
     dest_list = [1, 29, 35, 0, 77]
@@ -178,46 +101,6 @@ def test_renumber_negative_col():
 
     assert_series_equal(merged["check_src"], merged["original_src"], check_names=False)
     assert_series_equal(merged["check_dst"], merged["original_dst"], check_names=False)
-
-
-@pytest.mark.parametrize("graph_file", DATASETS)
-def test_renumber_files(graph_file):
-    gc.collect()
-    dataset_path = graph_file.get_path()
-    M = utils.read_csv_for_nx(dataset_path)
-    sources = cudf.Series(M["0"])
-    destinations = cudf.Series(M["1"])
-
-    translate = 1000
-
-    df = cudf.DataFrame()
-    df["src"] = cudf.Series([x + translate for x in sources.values_host])
-    df["dst"] = cudf.Series([x + translate for x in destinations.values_host])
-
-    exp_src = cudf.Series([x + translate for x in sources.values_host])
-    exp_dst = cudf.Series([x + translate for x in destinations.values_host])
-
-    renumbered_df, renumber_map = NumberMap.renumber(
-        df, "src", "dst", preserve_order=True
-    )
-
-    unrenumbered_df = renumber_map.unrenumber(
-        renumbered_df, renumber_map.renumbered_src_col_name, preserve_order=True
-    )
-    unrenumbered_df = renumber_map.unrenumber(
-        unrenumbered_df, renumber_map.renumbered_dst_col_name, preserve_order=True
-    )
-
-    assert_series_equal(
-        exp_src,
-        unrenumbered_df[renumber_map.renumbered_src_col_name],
-        check_names=False,
-    )
-    assert_series_equal(
-        exp_dst,
-        unrenumbered_df[renumber_map.renumbered_dst_col_name],
-        check_names=False,
-    )
 
 
 @pytest.mark.parametrize("graph_file", DATASETS)
@@ -355,7 +238,7 @@ def test_renumber_unrenumber_non_default_vert_names():
         }
     )
 
-    renumbered_df, number_map = NumberMap.renumber(input_gdf, "col_a", "col_b")
+    renumbered_df, number_map = NumberMap.renumber(input_gdf, ["col_a"], ["col_b"])
 
     some_result_gdf = cudf.DataFrame({"vertex": [0, 1, 2, 3]})
     expected_values = [99, 199, 2, 32]
