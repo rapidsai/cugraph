189926bb6669554d78a81036c10ab1c32c45d661
Rick Ratzel <rratzel@nvidia.com>
2023-01-16 16:44:43 -0600

    Adds proper check for running as a script

--- ci/wheel_smoke_test_cugraph.py
+++ ci/wheel_smoke_test_cugraph.py
@@ -14,23 +14,24 @@
 import cudf
 import cugraph
 
-edgelist = cudf.DataFrame({"source": ["a", "b", "c"], "destination": ["b", "c", "d"]})
+if __name__ == "__main__":
+    edgelist = cudf.DataFrame({"source": ["a", "b", "c"], "destination": ["b", "c", "d"]})
 
-# directed graph
-G = cugraph.Graph(directed=True)
-G.from_cudf_edgelist(edgelist, store_transposed=True)
-result_df = cugraph.pagerank(G)
+    # directed graph
+    G = cugraph.Graph(directed=True)
+    G.from_cudf_edgelist(edgelist, store_transposed=True)
+    result_df = cugraph.pagerank(G)
 
-assert(result_df["pagerank"].sum() == 1.0)
-assert(result_df.sort_values(by="pagerank")["vertex"].values_host.tolist()
-       == ["a", "b", "c", "d"])
+    assert(result_df["pagerank"].sum() == 1.0)
+    assert(result_df.sort_values(by="pagerank")["vertex"].values_host.tolist()
+           == ["a", "b", "c", "d"])
 
-# undirected graph
-G = cugraph.Graph(directed=False)
-G.from_cudf_edgelist(edgelist, store_transposed=True)
-result_df = cugraph.pagerank(G)
+    # undirected graph
+    G = cugraph.Graph(directed=False)
+    G.from_cudf_edgelist(edgelist, store_transposed=True)
+    result_df = cugraph.pagerank(G)
 
-assert(result_df["pagerank"].sum() == 1.0)
-result_df.set_index("vertex", inplace=True)
-assert(result_df.loc["a", "pagerank"] == result_df.loc["d", "pagerank"])
-assert(result_df.loc["b", "pagerank"] == result_df.loc["c", "pagerank"])
+    assert(result_df["pagerank"].sum() == 1.0)
+    result_df.set_index("vertex", inplace=True)
+    assert(result_df.loc["a", "pagerank"] == result_df.loc["d", "pagerank"])
+    assert(result_df.loc["b", "pagerank"] == result_df.loc["c", "pagerank"])
--- ci/wheel_smoke_test_pylibcugraph.py
+++ ci/wheel_smoke_test_pylibcugraph.py
@@ -14,34 +14,35 @@
 import cupy
 from pylibcugraph import ResourceHandle, GraphProperties, SGGraph, pagerank
 
-src_array = cupy.asarray([100, 201, 302], dtype="int32")
-dst_array = cupy.asarray([201, 302, 403], dtype="int32")
-wgt_array = cupy.asarray([1.0, 1.0, 1.0], dtype="float32")
+if __name__ == "__main__":
+    src_array = cupy.asarray([100, 201, 302], dtype="int32")
+    dst_array = cupy.asarray([201, 302, 403], dtype="int32")
+    wgt_array = cupy.asarray([1.0, 1.0, 1.0], dtype="float32")
 
-resource_handle = ResourceHandle()
+    resource_handle = ResourceHandle()
 
-G = SGGraph(resource_handle,
-            GraphProperties(is_symmetric=False, is_multigraph=False),
-            src_array,
-            dst_array,
-            wgt_array,
-            store_transposed=True,
-            renumber=True,
-            do_expensive_check=True,
-            )
+    G = SGGraph(resource_handle,
+                GraphProperties(is_symmetric=False, is_multigraph=False),
+                src_array,
+                dst_array,
+                wgt_array,
+                store_transposed=True,
+                renumber=True,
+                do_expensive_check=True,
+                )
 
-(vertices, pageranks) = pagerank(resource_handle=resource_handle,
-                                 graph=G,
-                                 precomputed_vertex_out_weight_vertices=None,
-                                 precomputed_vertex_out_weight_sums=None,
-                                 initial_guess_vertices=None,
-                                 initial_guess_values=None,
-                                 alpha=0.85,
-                                 epsilon=1.0e-6,
-                                 max_iterations=500,
-                                 do_expensive_check=True,
-                                 )
+    (vertices, pageranks) = pagerank(resource_handle=resource_handle,
+                                     graph=G,
+                                     precomputed_vertex_out_weight_vertices=None,
+                                     precomputed_vertex_out_weight_sums=None,
+                                     initial_guess_vertices=None,
+                                     initial_guess_values=None,
+                                     alpha=0.85,
+                                     epsilon=1.0e-6,
+                                     max_iterations=500,
+                                     do_expensive_check=True,
+                                     )
 
-assert(pageranks.sum() == 1.0)
-results = dict(zip(vertices.tolist(),pageranks.tolist()))
-assert(results[100] < results[201] < results[302] < results[403])
+    assert(pageranks.sum() == 1.0)
+    results = dict(zip(vertices.tolist(),pageranks.tolist()))
+    assert(results[100] < results[201] < results[302] < results[403])
