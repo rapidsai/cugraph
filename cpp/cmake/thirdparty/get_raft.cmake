04eeafe522c56dce57613847ae043fa752469475
Mickael Ide <mide@nvidia.com>
2023-01-23 16:51:40 +0100

    Revert raft test

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -66,8 +66,8 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION    ${CUGRAPH_MIN_VERSION_raft}
-                        FORK       lowener
-                        PINNED_TAG 23.02-rm-depr
+                        FORK       rapidsai
+                        PINNED_TAG branch-${CUGRAPH_BRANCH_VERSION_raft}
 
                         # When PINNED_TAG above doesn't match cugraph,
                         # force local raft clone in build directory
