6d92f2c945f6cbd65cd3b7165626d8cf9ad35d32
Alexandria Barghi <abarghi@nvidia.com>
2023-01-26 14:49:15 +0000

    style fix

--- python/cugraph-pyg/cugraph_pyg/tests/mg/test_mg_cugraph_sampler.py
+++ python/cugraph-pyg/cugraph_pyg/tests/mg/test_mg_cugraph_sampler.py
@@ -185,7 +185,6 @@ def test_neighbor_sample_multi_vertex(multi_edge_multi_vertex_graph_1, dask_clie
 
         assert list(node_ids) == list(actual_vertex_ids)
 
-
     for edge_type, ei in G.items():
         expected_df = cudf.DataFrame(
             {
