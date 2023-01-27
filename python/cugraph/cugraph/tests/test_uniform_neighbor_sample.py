f73ab40e9bd18dce31a01874373d9c324ffd4295
Vibhu Jawa <vibhujawa@gmail.com>
2023-01-26 16:25:40 -0800

    Switch to  raise NotImplementedError

--- python/cugraph/cugraph/tests/test_uniform_neighbor_sample.py
+++ python/cugraph/cugraph/tests/test_uniform_neighbor_sample.py
@@ -449,4 +449,4 @@ def test_uniform_neighbor_sample_empty_start_list():
 def test_multi_client_sampling():
     # See gist for example test to write
     # https://gist.github.com/VibhuJawa/1b705427f7a0c5a2a4f58e0a3e71ef21
-    pass
+    raise NotImplementedError
