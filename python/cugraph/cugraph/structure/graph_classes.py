80631c8dda81bff5ca036e5549c301cc796ad25d
Vibhu Jawa <vibhujawa@gmail.com>
2023-01-27 10:12:28 -0800

    Update python/cugraph/cugraph/structure/graph_classes.py
    Co-authored-by: Rick Ratzel <3039903+rlratzel@users.noreply.github.com>

--- python/cugraph/cugraph/structure/graph_classes.py
+++ python/cugraph/cugraph/structure/graph_classes.py
@@ -81,7 +81,13 @@ class Graph:
                 )
 
     def __getattr__(self, name):
-        # accessing `_Impl` will lead to recursion
+        """
+        __getattr__() is called automatically by python when an attribute does not
+        exist. Since this class is attempting to hide the internal `_Impl` object,
+        which is intended to contain many of the attributes needed by this class,
+        __getattr__ is used to "pass through" attribute access to _Impl and make it
+        appear as if the _Impl attributes are contained in this class.
+        """
         # so avoiding that
         if name == "_Impl":
             raise AttributeError(name)
