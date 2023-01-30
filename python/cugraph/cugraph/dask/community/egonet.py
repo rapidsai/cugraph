97541575fb074de1b5acc090ba2696e6203c231b
Vibhu Jawa <vibhujawa@gmail.com>
2023-01-31 04:47:02 +0530

    Make graph objects accessible across multiple clients (#3192)
    This PR makes `cugraph.structure.graph_classes` dask serializable and de-serializable.  This means that we can cleanly use dask to transmit graph objects b/w different clients without much work and using all ready supported API. 
    
    Depends on PR:  ~https://github.com/rapidsai/cugraph/pull/3184~ (PR is IN) and closes https://github.com/rapidsai/cugraph/issues/3193
    
    See docs here: https://distributed.dask.org/en/stable/publish.html
    
    
    **On client 0**
    ```python3
    print(type(g))
    client.publish_dataset(mg_graph_for_sampling=g)
    ```
    ```
    <class 'cugraph.structure.graph_classes.MultiGraph'>
    ```
    
    **On client 1** 
    ```python3
    g = client_worker_1.get_dataset('mg_graph_for_sampling')
    type(g)
    ```
    ```
    cugraph.structure.graph_classes.MultiGraph
    ```
    
    
    CC: @rlratzel
    
    Authors:
      - Vibhu Jawa (https://github.com/VibhuJawa)
    
    Approvers:
      - Rick Ratzel (https://github.com/rlratzel)
      - Erik Welch (https://github.com/eriknw)
    
    URL: https://github.com/rapidsai/cugraph/pull/3192

--- python/cugraph/cugraph/dask/community/egonet.py
+++ python/cugraph/cugraph/dask/community/egonet.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # limitations under the License.
 #
 
-from dask.distributed import wait
+from dask.distributed import wait, default_client
 
 import cugraph.dask.comms.comms as Comms
 import dask_cudf
@@ -108,7 +108,7 @@ def ego_graph(input_graph, n, radius=1, center=True):
     """
 
     # Initialize dask client
-    client = input_graph._client
+    client = default_client()
 
     if isinstance(n, (int, list)):
         n = cudf.Series(n)
