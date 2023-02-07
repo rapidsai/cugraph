a0d964da8cad8bcef18c9f07bb128d35d877f7e7
Seunghwa Kang <45857425+seunghwak@users.noreply.github.com>
2023-02-07 12:39:57 -0800

    Fix libcugraph debug build warnings/errors (#3214)
    Fix compiler warnings and link errors when building libcugraph in the debug mode.
    
    Authors:
      - Seunghwa Kang (https://github.com/seunghwak)
    
    Approvers:
      - Naim (https://github.com/naimnv)
      - Joseph Nke (https://github.com/jnke2016)
      - Chuck Hastings (https://github.com/ChuckHastings)
      - Rick Ratzel (https://github.com/rlratzel)
    
    URL: https://github.com/rapidsai/cugraph/pull/3214

--- cpp/src/utilities/eidir_graph_utils.hpp
+++ cpp/src/utilities/eidir_graph_utils.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,13 +25,19 @@ template __device__ double parallel_prefix_sum(int64_t, int32_t const*, double c
 template __device__ float parallel_prefix_sum(int64_t, int64_t const*, float const*);
 template __device__ double parallel_prefix_sum(int64_t, int64_t const*, double const*);
 
-template void offsets_to_indices<int, int>(int const*, int, int*);
-template void offsets_to_indices<long, int>(long const*, int, int*);
-template void offsets_to_indices<long, long>(long const*, long, long*);
+template void offsets_to_indices<int32_t, int32_t>(int32_t const*, int32_t, int32_t*);
+template void offsets_to_indices<int64_t, int32_t>(int64_t const*, int32_t, int32_t*);
+template void offsets_to_indices<int64_t, int64_t>(int64_t const*, int64_t, int64_t*);
 
-template __global__ void offsets_to_indices_kernel<int, int>(int const*, int, int*);
-template __global__ void offsets_to_indices_kernel<long, int>(long const*, int, int*);
-template __global__ void offsets_to_indices_kernel<long, long>(long const*, long, long*);
+template __global__ void offsets_to_indices_kernel<int32_t, int32_t>(int32_t const*,
+                                                                     int32_t,
+                                                                     int32_t*);
+template __global__ void offsets_to_indices_kernel<int64_t, int32_t>(int64_t const*,
+                                                                     int32_t,
+                                                                     int32_t*);
+template __global__ void offsets_to_indices_kernel<int64_t, int64_t>(int64_t const*,
+                                                                     int64_t,
+                                                                     int64_t*);
 
 }  // namespace detail
 }  // namespace cugraph
