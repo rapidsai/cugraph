36da5af588697ba5023ec490aad266035aa98fd4
BradReesWork <BradReesWork@users.noreply.github.com>
2023-02-17 14:53:47 -0500

    update ci script to skip all mg_tests

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -66,7 +66,7 @@ popd
 rapids-logger "pytest cugraph"
 pushd python/cugraph/cugraph
 pytest \
-  --ignore=tests/mg \
+  -m "not mg_test" \
   --cache-clear \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cugraph.xml" \
   --cov-config=../../.coveragerc \
