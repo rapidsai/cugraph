32fd94e34adeb63fd20d87a9d7c4967e1c6bd570
wu6u3 <tinyinlai@gmail.com>
2023-02-23 14:44:11 -0500

    change

---
+++ cpp/tests/cores/trim_tests.cpp
@@ -0,0 +1,32 @@
+#include <utilities/base_fixture.hpp>
+#include <utilities/test_graphs.hpp>
+#include <utilities/test_utilities.hpp>
+#include <utilities/thrust_wrapper.hpp>
+
+#include <cugraph/algorithms.hpp>
+#include <cugraph/graph.hpp>
+#include <cugraph/graph_functions.hpp>
+#include <cugraph/graph_view.hpp>
+#include <cugraph/utilities/high_res_timer.hpp>
+
+#include <raft/core/handle.hpp>
+#include <raft/util/cudart_utils.hpp>
+#include <rmm/device_scalar.hpp>
+#include <rmm/device_uvector.hpp>
+#include <rmm/mr/device/cuda_memory_resource.hpp>
+
+#include <gtest/gtest.h>
+
+#include <numeric>
+#include <vector>
+
+template<typename vertex_t, typename edge_t>
+std::vector<edge_t> core_number_reference(edge_t const* offsets,
+                                          vertex_t const* indices,
+                                          vertex_t num_vertices){
+  raft::handle_t handle;
+  graph_view_t<vertex_t, edge_t, false, false> graph_view; 
+  auto graph_view_tuple =  trim(handle, graph_view);
+  
+} 
+
