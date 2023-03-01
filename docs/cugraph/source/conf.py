87caf30b1668e7d22bf92b87ac85f57d1f3286b7
galipremsagar <sagarprem75@gmail.com>
2023-03-01 11:03:07 -0800

    Fix docs build

--- docs/cugraph/source/conf.py
+++ docs/cugraph/source/conf.py
@@ -116,6 +116,8 @@ todo_include_todos = False
 #
 html_theme_options = {
     "external_links": [],
+    # https://github.com/pydata/pydata-sphinx-theme/issues/1220
+    "icon_links": [],
     "github_url": "https://github.com/rapidsai/cugraph",
     "twitter_url": "https://twitter.com/rapidsai",
     "show_toc_level": 1,
