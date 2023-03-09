69b445eb3ce1c7955d8bbbbf06afa4d868042082
wu6u3 <tinyinlai@gmail.com>
2023-03-09 14:53:46 -0500

    test

--- cpp/src/cores/trim.cu
+++ cpp/src/cores/trim.cu
@@ -1,6 +1,5 @@
 #pragma once
 
-
 #include <cugraph/algorithms.hpp>
 #include <cugraph/graph_functions.hpp>
 #include <cugraph/graph_view.hpp>
@@ -12,27 +11,44 @@
 namespace cugraph {
 
 template <typename vertex_t, typename edge_t, typename weight_t, bool multi_gpu>
-std::tuple<rmm::device_uvector<vertex_t>,
-           rmm::device_uvector<vertex_t>,
-           std::optional<rmm::device_uvector<weight_t>>>
+rmm::device_uvector<vertex_t> 
 trim(raft::handle_t const& handle,
-       graph_view_t<vertex_t, edge_t, false, multi_gpu> const& graph_view,
-       bool do_expensive_check)
+       graph_view_t<vertex_t, edge_t, false, multi_gpu> const& graph_view)
 {
 
-  
-  auto in_degree = graph_view.compute_in_degree();
-  auto out_degree = graph_view.compute_out_degree();
+  CUGRAPH_EXPECTS(!graph_view.is_multigraph(),
+                  "Invalid input argument: trim currently does not support multi-graphs.");
  
-  rmm::device_uvector<vertex_t> remaining_vertices(graph_view.local_vertex_partition_range_size(),  handle.get_stream());
+  auto in_degrees = graph_view.compute_in_degree();
+  auto out_degrees = graph_view.compute_out_degree();
 
+  // remove in-degree = 0 vertex
+  rmm::device_uvector<vertex_t> remaining_vertices(graph_view.local_vertex_partition_range_size(),
+                                                   handle.get_stream());
   remaining_vertices.resize(
-    thrust::distance( remaining_vertices.begin(), thrust::copy_if(
-        handle.get_thrust_policy(), thrust::make_counting_iterator(graph_view.local_vertex_partition_range_first()),
-        thrust::make_counting_iterator(graph_view.local_vertex_partition_range_last()),  remaining_vertices.begin(),
-        [ in_degree ] __device__(
-          auto in_degree) { return in_degree <= 1; })),
+    thrust::distance(
+      remaining_vertices.begin(),
+      thrust::copy_if(
+        handle.get_thrust_policy(),
+        thrust::make_counting_iterator(graph_view.local_vertex_partition_range_first()),
+        thrust::make_counting_iterator(graph_view.local_vertex_partition_range_last()),
+        remaining_vertices.begin(),
+        [in_degrees] __device__(
+          auto v) { return in_degrees[v] > edge_t{0}; })),
     handle.get_stream());
+  // remove out-degree = 0 vertex
+  remaining_vertices.resize(
+    thrust::distance(
+      remaining_vertices.begin(),
+      thrust::copy_if(
+        handle.get_thrust_policy(),
+        thrust::make_counting_iterator(graph_view.local_vertex_partition_range_first()),
+        thrust::make_counting_iterator(graph_view.local_vertex_partition_range_last()),
+        remaining_vertices.begin(),
+        [in_degrees] __device__(
+          auto v) { return out_degrees[v] > edge_t{0}; })),
+    handle.get_stream());
+
 
   return remaining_vertices;
 }
--- cpp/tests/cores/trim_tests.cpp
+++ cpp/tests/cores/trim_tests.cpp
@@ -21,12 +21,37 @@
 #include <vector>
 
 template<typename vertex_t, typename edge_t>
-std::vector<edge_t> core_number_reference(edge_t const* offsets,
-                                          vertex_t const* indices,
-                                          vertex_t num_vertices){
+std::vector<edge_t> trim_tests(edge_t const* offsets,
+                               vertex_t const* indices,
+                               vertex_t num_vertices){
+
+  std::vector<bool> edge_valids(offsets[num_vertices], true);
+
+  for (vertex_t i = 0; i < num_vertices; ++i) {
+    for (edge_t j = offsets[i]; j < offsets[i + 1]; j++) {
+      if (indices[j] == i) {
+        edge_valids[j] = false;
+      } else if ((j > offsets[i]) && (indices[j] == indices[j - 1])) {
+        edge_valids[j] = false;
+      }
+    }
+  }
+
+
   raft::handle_t handle;
   graph_view_t<vertex_t, edge_t, false, false> graph_view; 
-  auto graph_view_tuple =  trim(handle, graph_view);
-  
+  auto remain = cugraph::trim(handle, graph_view);
+
+  auto in_degrees = remain.compute_in_degree();
+  auto out_degrees = remain.compute_out_degree();
+  auto min_in_degree = thrust::reduce(in_degrees.begin(), in_degrees.end(), thrust::minimum<vertex_t>{});
+  auto min_out_degree = thrust::reduce(out_degrees.begin(), out_degrees.end(), thrust::minimum<vertex_t>{});
+
+  ASSERT_TRUE(min_in_degree>0)<< "Triming remain indegree = 0";
+
+  ASSERT_TRUE(min_out_degree>0)<< "Triming remain indegree = 0";
+
+
+
 } 
 
