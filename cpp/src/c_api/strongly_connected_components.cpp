874cc37bd0c7fde40f8a0bce461eaa5d03e37117
Charles Hastings <charlesh@nvidia.com>
2023-03-16 12:31:15 -0700

    first cut at C API for some legacy algorithms

--- cpp/src/c_api/strongly_connected_components.cpp
+++ cpp/src/c_api/strongly_connected_components.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, NVIDIA CORPORATION.
+ * Copyright (c) 2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,8 +66,34 @@ struct scc_functor : public cugraph::c_api::abstract_functor {
         if (error_code_ != CUGRAPH_SUCCESS) return;
       }
 
-      error_code_            = CUGRAPH_NOT_IMPLEMENTED;
-      error_->error_message_ = "SCC Not currently implemented";
+      if constexpr (multi_gpu) {
+        unsupported();
+      } else {
+        auto graph =
+          reinterpret_cast<cugraph::graph_t<vertex_t, edge_t, false, false>*>(graph_->graph_);
+
+        auto number_map = reinterpret_cast<rmm::device_uvector<vertex_t>*>(graph_->number_map_);
+
+        auto graph_view = graph->view();
+        auto edge_partition_view = graph_view.local_edge_partition_view();
+
+        cugraph::legacy::GraphCSRView<vertex_t, edge_t, weight_t> legacy_graph_view(edge_partition_view.offsets().data(),
+                                                                                    edge_partition_view.indices().data(),
+                                                                                    nullptr,
+                                                                                    edge_partition_view.offsets().size() - 1,
+                                                                                    edge_partition_view.indices().size());
+
+        rmm::device_uvector<vertex_t> components(graph_view->number_of_vertices(), handle.get_stream());
+
+        cugraph::connected_components(legacy_graph_view, cugraph_cc_t::CUGRAPH_STRONG, components.data());
+        rmm::device_uvector<vertex_t> vertex_ids(graph_view.local_vertex_partition_range_size(),
+                                                 handle_.get_stream());
+        raft::copy(vertex_ids.data(), number_map->data(), vertex_ids.size(), handle_.get_stream());
+
+        result_ = new cugraph::c_api::cugraph_labeling_result_t{
+          new cugraph::c_api::cugraph_type_erased_device_array_t(vertex_ids, graph_->vertex_type_),
+          new cugraph::c_api::cugraph_type_erased_device_array_t(components, graph_->vertex_type_)};
+      }
     }
   }
 };
