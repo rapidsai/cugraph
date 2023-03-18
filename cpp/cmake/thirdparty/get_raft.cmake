c3c813a1247db150d36082cf2346790be7fd998b
Corey J. Nolet <cjnolet@gmail.com>
2023-03-18 11:41:23 -0400

    Pinning raft consolidation branch

--- cpp/cmake/thirdparty/get_raft.cmake
+++ cpp/cmake/thirdparty/get_raft.cmake
@@ -64,8 +64,8 @@ endfunction()
 # To use a different RAFT locally, set the CMake variable
 # CPM_raft_SOURCE=/path/to/local/raft
 find_and_configure_raft(VERSION    ${CUGRAPH_MIN_VERSION_raft}
-                        FORK       rapidsai
-                        PINNED_TAG branch-${CUGRAPH_BRANCH_VERSION_raft}
+                        FORK       cjnolet
+                        PINNED_TAG fea-2304-remove-nn-lib
 
                         # When PINNED_TAG above doesn't match cugraph,
                         # force local raft clone in build directory
