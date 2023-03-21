5d1f34750e8d14197f5dc6f39d3e611701967b3b
Corey J. Nolet <cjnolet@gmail.com>
2023-03-20 20:26:48 -0400

    temporarily update CI scripts to use new libraft

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2023, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -11,6 +11,8 @@ rapids-print-env
 
 rapids-logger "Begin cpp build"
 
-rapids-mamba-retry mambabuild conda/recipes/libcugraph
+LIBRAFT_CHANNEL=$(rapids-get-artifact ci/raft/pull-request/1333/c60c0cba/raft_conda_cpp_cuda11_$(arch).tar.gz)
+
+rapids-mamba-retry --channel "${LIBRAFT_CHANNEL}" mambabuild conda/recipes/libcugraph
 
 rapids-upload-conda-to-s3 cpp
