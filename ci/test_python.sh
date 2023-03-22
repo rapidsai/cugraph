a458b121aa08a1f8774f4836a6ddec0c1750fabb
Alexandria Barghi <abarghi@nvidia.com>
2023-03-22 18:43:13 +0000

    update test

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -110,7 +110,17 @@ if [[ "${RAPIDS_CUDA_VERSION}" == "11.8.0" ]]; then
     set +u
     conda activate test_cugraph_pyg
     set -u
-    
+
+    rapids-mamba-retry install \
+      --channel "${CPP_CHANNEL}" \
+      --channel "${PYTHON_CHANNEL}" \
+      libcugraph \
+      pylibcugraph \
+      cugraph \
+      cugraph-pyg
+
+    rapids-print-env
+
     # Install pytorch
     rapids-mamba-retry install \
       --force-reinstall \
@@ -124,14 +134,6 @@ if [[ "${RAPIDS_CUDA_VERSION}" == "11.8.0" ]]; then
     pip install pyg-nightly
 
     rapids-print-env
-
-    rapids-mamba-retry install \
-      --channel "${CPP_CHANNEL}" \
-      --channel "${PYTHON_CHANNEL}" \
-      libcugraph \
-      pylibcugraph \
-      cugraph \
-      cugraph-pyg
 
     rapids-logger "pytest cugraph_pyg (single GPU)"
     pushd python/cugraph-pyg/cugraph_pyg
