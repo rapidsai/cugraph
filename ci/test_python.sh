2d39e58c8e50f05ef7cc85c2cedf68bd631e91a5
Alex Barghi <105237337+alexbarghi-nv@users.noreply.github.com>
2023-03-23 12:33:57 -0400

    add deactivate

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -203,6 +203,13 @@ if [[ "${RAPIDS_CUDA_VERSION}" == "11.8.0" ]]; then
       --cov-report=term \
       .
     popd
+    
+    # Reactivate the test environment back
+    set +u
+    conda deactivate
+    conda activate test
+    set -u
+    
   else
     rapids-logger "skipping cugraph_pyg pytest on ARM64"
   fi
