5b2bc92e957d758697d9ffb30e39a2aae6528f0c
jnke2016 <jnke@gmail.com>
2023-03-28 22:56:03 -0700

    add more tests

--- python/cugraph/cugraph/tests/community/test_leiden.py
+++ python/cugraph/cugraph/tests/community/test_leiden.py
@@ -18,6 +18,7 @@ import pytest
 
 import networkx as nx
 import cugraph
+import cudf
 from cugraph.testing import utils
 from cugraph.experimental.datasets import DATASETS, karate_asymmetric
 
@@ -30,6 +31,79 @@ import warnings
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+# =============================================================================
+# Test data
+# =============================================================================
+
+_test_data = {
+    "data_1":{
+        "graph": {
+            "src_or_offset_array": [0, 1, 1, 2, 2, 2, 3, 4, 1, 3, 4, 0, 1, 3, 5, 5],
+            "dst_or_index_array": [1, 3, 4, 0, 1, 3, 5, 5, 0, 1, 1, 2, 2, 2, 3, 4],
+            "weight": [0.1, 2.1, 1.1, 5.1, 3.1, 4.1, 7.2, 3.2, 0.1, 2.1, 1.1, 5.1, 3.1, 4.1, 7.2, 3.2]
+        },
+        "max_level": 10,
+        "resolution": 1.0,
+        "input_type": "COO",
+        "expected_output": {
+            "partition": [0, 1, 0, 1, 1, 1],
+            "modularity_score": 0.218166
+        }
+    }
+}
+
+
+# =============================================================================
+# Pytest fixtures
+# =============================================================================
+@pytest.fixture(
+    scope="module",
+    params=[pytest.param(value, id=key) for (key, value) in _test_data.items()],
+)
+def input_and_expected_output(request):
+    d = request.param.copy()
+    input_graph_data = d.pop("graph")
+    input_type = d.pop("input_type")
+    src_or_offset_array = input_graph_data["src_or_offset_array"]
+    dst_or_index_array = input_graph_data["dst_or_index_array"]
+    weight = input_graph_data["weight"]
+    max_level = d.pop("max_level")
+    resolution = d.pop("resolution")
+    output = d
+
+
+    G = cugraph.Graph()
+
+    # Done in the test
+    if input_type == "COO":
+        # Create graph from an edgelist
+        df = cudf.DataFrame()
+        df["src"] = cudf.Series(src_or_offset_array, dtype="int32")
+        df["dst"] = cudf.Series(dst_or_index_array, dtype="int32")
+        df["weight"] = cudf.Series(weight, dtype="float32")
+        G.from_cudf_edgelist(
+            df, source="src", destination="dst", edge_attr="weight", store_transposed=True)
+    
+    elif input_type == "CSR":
+        # Create graph from csr
+        offsets = src_or_offset_array
+        indices = dst_or_index_array 
+        G.from_cudf_adjlist(offsets, indices, weight)
+    
+    parts, mod = cugraph.leiden(G, max_level, resolution)
+
+    parts = parts.sort_values("vertex").reset_index(drop=True)
+
+
+
+    output["result_output"] = {
+        "partition": parts["partition"],
+        "modularity_score": mod}
+    
+    return output
+
 
 
 # =============================================================================
@@ -101,3 +175,16 @@ def test_leiden_directed_graph():
 
     with pytest.raises(ValueError):
         parts, mod = cugraph_leiden(G)
+
+
+@pytest.mark.sg
+@pytest.mark.skip("Debugging")
+def test_leiden_golden_results(input_and_expected_output):
+    expected_partition = cudf.Series(
+        input_and_expected_output["expected_output"]["partition"])
+    expected_mod = input_and_expected_output["expected_output"]["modularity_score"]
+
+    result_partition = input_and_expected_output["result_output"]["partition"]
+    result_mod = input_and_expected_output["result_output"]["modularity_score"]
+
+    assert expected_mod == result_mod
