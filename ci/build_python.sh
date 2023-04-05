5ed82b2e4d723d5ef2676d44b9a6b6f40b372155
Allard Hendriksen <ahendriksen@nvidia.com>
2023-04-05 11:12:12 +0200

    Test RMM PR 1241

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -11,6 +11,11 @@ rapids-print-env
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 
+PY_VER=${RAPIDS_PY_VERSION//./}
+
+LIBRMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1241/1fd0b84/rmm_conda_cpp_cuda11_$(arch).tar.gz)
+RMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1241/1fd0b84/rmm_conda_python_cuda11__${PY_VER}_$(arch).tar.gz)
+
 rapids-logger "Begin py build"
 
 # TODO: Remove `--no-test` flags once importing on a CPU
@@ -18,17 +23,23 @@ rapids-logger "Begin py build"
 rapids-mamba-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
   conda/recipes/pylibcugraph
 
 rapids-mamba-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/cugraph
 
 rapids-mamba-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
   conda/recipes/cugraph-service
 
@@ -36,6 +47,8 @@ rapids-mamba-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
+  --channel "${LIBRMM_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
   --channel pyg \
   --channel pytorch \
   --channel pytorch-nightly \
@@ -45,6 +58,8 @@ rapids-mamba-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
   --channel "${RAPIDS_CONDA_BLD_OUTPUT_DIR}" \
+  --channel "${LIBRMM_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
   --channel dglteam \
   --channel pytorch \
   --channel pytorch-nightly \
