067f95dbfdaa1ee19a22be213a66f2678dbc473c
Vyas Ramasubramani <vyasr@nvidia.com>
2023-04-06 16:38:43 -0700

    Add nvcc flags needed to work with broken nvidia packages

--- conda/recipes/libcugraph/build.sh
+++ conda/recipes/libcugraph/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright (c) 2019-2022, NVIDIA CORPORATION.
+# Copyright (c) 2019-2023, NVIDIA CORPORATION.
 
 # This assumes the script is executed from the root of the repo directory
 
@@ -8,4 +8,5 @@
 # openmpi build dependencies). The conda package does NOT include these test
 # binaries or extra dependencies, but these are built here for use in CI runs.
 
++export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
 ./build.sh libcugraph libcugraph_etl cpp-mgtests -n -v --allgpuarch
