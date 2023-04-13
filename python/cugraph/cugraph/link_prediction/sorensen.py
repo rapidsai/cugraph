fbef6f7fac5cfc7be95f41fafa27b22da5bc14de
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2023-04-13 12:03:28 -0400

    Sphinx updates (#3468)
    Spinx is very picky and looks for indentations and black lines. Also during the build, if there is an error, Spinx just ignores the function that caused the error rather to terminating the build.
    
    Some docstrings also used reserved character {* and _} which are used for formating
    
    This PR:
    
        addresses Spinx Warnings and Errors
        addresses issues where rst is md is not added to the output
        any any other issues
    
    You will see a lot of additional blank lines added and some text clean-up for readability.
    
    Seems like the PR also picked up all the the 23.06 updates - so half the files are not really for this PR
    
    Authors:
      - Brad Rees (https://github.com/BradReesWork)
    
    Approvers:
      - Don Acosta (https://github.com/acostadon)
      - AJ Schmidt (https://github.com/ajschmidt8)
      - Rick Ratzel (https://github.com/rlratzel)
    
    URL: https://github.com/rapidsai/cugraph/pull/3468

--- python/cugraph/cugraph/link_prediction/sorensen.py
+++ python/cugraph/cugraph/link_prediction/sorensen.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2022, NVIDIA CORPORATION.
+# Copyright (c) 2021-2023, NVIDIA CORPORATION.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -60,9 +60,11 @@ def sorensen(input_graph, vertex_pair=None):
 
         df['first'] : cudf.Series
             The first vertex ID of each pair (will be identical to first if specified)
+
         df['second'] : cudf.Series
             The second vertex ID of each pair (will be identical to second if
             specified)
+
         df['sorensen_coeff'] : cudf.Series
             The computed Sorensen coefficient between the source and
             destination vertices
