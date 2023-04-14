5a3abe0cd37e6fa4ca4ced2afcf723ccaf7327ca
Bradley Dice <bdice@bradleydice.com>
2023-04-14 14:13:01 -0500

    Update copyrights.

--- cpp/src/utilities/graph_utils.cuh
+++ cpp/src/utilities/graph_utils.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2022, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2018-2023, NVIDIA CORPORATION.  All rights reserved.
  *
  * NVIDIA CORPORATION and its licensors retain all intellectual property
  * and proprietary rights in and to this software, related documentation
