b402d984bb35e9f4785f909ff48f9851902257fa
wu6u3 <tinyinlai@gmail.com>
2023-04-20 14:40:16 -0400

    add core 1 and extract graph

--- cpp/src/cores/trim.cu
+++ cpp/src/cores/trim.cu
@@ -3,7 +3,7 @@
 #include <thrust/copy.h>
 #include <thrust/iterator/discard_iterator.h>
 //#include <prims/extract_transform_e.cuh>
-#include <prims/transform_reduce_dst_nbr_intersection_of_e_endpoints_by_v.cuh>
+//#include <prims/transform_reduce_dst_nbr_intersection_of_e_endpoints_by_v.cuh>
 #include <prims/update_edge_src_dst_property.cuh>
 
 #include <cugraph/algorithms.hpp>
@@ -17,71 +17,122 @@
 
 namespace cugraph {
 template <typename vertex_t>
-struct extract_zero_core_t {
+struct extract_one_core_t {
   __device__ thrust::optional<thrust::tuple<vertex_t, vertex_t>> operator()(vertex_t src,
                                                                             vertex_t dst,
-                                                                            uint8_t src_in_zero_core,
-                                                                            uint8_t dst_in_zero_core,
+                                                                            uint8_t src_one_core,
+                                                                            uint8_t dst_one_core,
                                                                             thrust::nullopt_t) const
   {
-    return (src_in_zero_core == uint8_t{0}) && (dst_in_zero_core == uint8_t{0})
+    return (src_one_core == uint8_t{1}) && (dst_one_core == uint8_t{1})
              ? thrust::optional<thrust::tuple<vertex_t, vertex_t>>{thrust::make_tuple(src, dst)}
              : thrust::nullopt;
   }
 };
 
 template <typename edge_t>
-struct is_zero_or_greater_t {
+struct is_one_or_greater_t {
   __device__ uint8_t operator()(edge_t core_number) const
   {
-    return core_number >= edge_t{0} ? uint8_t{1} : uint8_t{0};
+    return core_number > edge_t{0} ? uint8_t{1} : uint8_t{0};
   }
 };
 
 template <typename vertex_t, typename edge_t, typename weight_t, bool multi_gpu>
 rmm::device_uvector<vertex_t> 
 trim(raft::handle_t const& handle,
-       graph_view_t<vertex_t, edge_t, false, multi_gpu> const& graph_view)
+       graph_view_t<vertex_t, edge_t, false, multi_gpu> const& graph_view,
+       std::optional<edge_property_view_t<edge_t, weight_t const*>> edge_weight_view,
+       bool do_expensive_check)
 {
-//    std::optional<rmm::device_uvector<vertex_t>> renumber_map{std::nullopt};
-//    auto cur_graph_view = modified_graph_view ? *modified_graph_view : graph_view;
-//    auto vertex_partition_range_lasts =
-//      renumber_map?
-//         std::make_optional<std::vector<vertex_t>>(graph_view.vertex_partition_range_lasts())
-//        : std::nullopt;
-    std::optional<graph_t<vertex_t, edge_t, false, multi_gpu>> modified_graph{std::nullopt};
-    std::optional<graph_view_t<vertex_t, edge_t, false, multi_gpu>> modified_graph_view{std::nullopt};
-    std::optional<rmm::device_uvector<vertex_t>> renumber_map{std::nullopt};
-    auto vertex_partition_range_lasts =
-         std::make_optional<std::vector<vertex_t>>(graph_view.vertex_partition_range_lasts());
+    //std::optional<graph_t<vertex_t, edge_t, false, multi_gpu>> modified_graph{std::nullopt};
+    //std::optional<graph_view_t<vertex_t, edge_t, false, multi_gpu>> modified_graph_view{std::nullopt};
+    //std::optional<rmm::device_uvector<vertex_t>> renumber_map{std::nullopt};
+    //auto vertex_partition_range_lasts =
+    //     std::make_optional<std::vector<vertex_t>>(graph_view.vertex_partition_range_lasts());
 
     rmm::device_uvector<edge_t> core_numbers(graph_view.number_of_vertices(), handle.get_stream());
 
     core_number(
-      handle, graph_view, core_numbers.data(), k_core_degree_type_t::OUT, size_t{0}, size_t{0});
+      handle, graph_view, core_numbers.data(), k_core_degree_type_t::OUT, size_t{1}, size_t{1});
 
-    edge_src_property_t<decltype(graph_view), uint8_t> edge_src_in_zero_cores(handle,
+    edge_src_property_t<decltype(graph_view), uint8_t> edge_src_out_one_cores(handle,
                                                                                  graph_view);
-    edge_dst_property_t<decltype(graph_view), uint8_t> edge_dst_in_zero_cores(handle,
+    edge_dst_property_t<decltype(graph_view), uint8_t> edge_dst_out_one_cores(handle,
                                                                                  graph_view);
-    auto in_zero_core_first =
-      thrust::make_transform_iterator(core_numbers.begin(), is_zero_or_greater_t<edge_t>{});
-    rmm::device_uvector<uint8_t> in_zero_core_flags(core_numbers.size(), handle.get_stream());
+    auto out_one_core_first =
+      thrust::make_transform_iterator(core_numbers.begin(), is_one_or_greater_t<edge_t>{});
+
+    rmm::device_uvector<uint8_t> out_one_core_flags(core_numbers.size(), handle.get_stream());
+
     thrust::copy(handle.get_thrust_policy(),
-                 in_zero_core_first,
-                 in_zero_core_first + core_numbers.size(),
-                 in_zero_core_flags.begin());
-    update_edge_src_property(
-      handle, graph_view, in_zero_core_flags.begin(), edge_src_in_zero_cores);
-    update_edge_dst_property(
-      handle, graph_view, in_zero_core_flags.begin(), edge_dst_in_zero_cores);
-    auto [srcs, dsts] = extract_transform_e(handle,
-                                            graph_view,
-                                            edge_src_in_zero_cores.view(),
-                                            edge_dst_in_zero_cores.view(),
-                                            edge_dummy_property_t{}.view(),
-                                            extract_zero_core_t<vertex_t>{});
+                 out_one_core_first,
+                 out_one_core_first + core_numbers.size(),
+                 out_one_core_flags.begin());
 
+    update_edge_src_property(
+      handle, graph_view, out_one_core_flags.begin(), edge_src_out_one_cores);
+    update_edge_dst_property(
+      handle, graph_view, out_one_core_flags.begin(), edge_dst_out_one_cores);
+
+    rmm::device_uvector<size_t> subgraph_offsets_out(2, handle.get_stream());
+    std::vector<size_t> h_subgraph_offsets_out{{0, out_one_core_flags.size()}};
+    raft::update_device(subgraph_offsets_out.data(),
+                      h_subgraph_offsets_out.data(),
+                      h_subgraph_offsets_out.size(),
+                      handle.get_stream());
+    handle.sync_stream();
+
+    auto [src, dst, wgt, offsets] = extract_induced_subgraphs(
+    handle,
+    graph_view,
+    edge_weight_view,
+    raft::device_span<size_t const>{subgraph_offsets_out.data(), subgraph_offsets_out.size()},
+    raft::device_span<vertex_t const>{out_one_core_flags.data(), out_one_core_flags.size()},
+    do_expensive_check);
+
+    // in degree 1 core
+    core_number(
+      handle, graph_view, core_numbers.data(), k_core_degree_type_t::IN, size_t{1}, size_t{1});
+
+    edge_src_property_t<decltype(graph_view), uint8_t> edge_src_in_one_cores(handle,
+                                                                                 graph_view);
+    edge_dst_property_t<decltype(graph_view), uint8_t> edge_dst_in_one_cores(handle,
+                                                                                 graph_view);
+    auto in_one_core_first =
+      thrust::make_transform_iterator(core_numbers.begin(), is_one_or_greater_t<edge_t>{});
+    rmm::device_uvector<uint8_t> in_one_core_flags(core_numbers.size(), handle.get_stream());
+    thrust::copy(handle.get_thrust_policy(),
+                 in_one_core_first,
+                 in_one_core_first + core_numbers.size(),
+                 in_one_core_flags.begin());
+    update_edge_src_property(
+      handle, graph_view, in_one_core_flags.begin(), edge_src_in_one_cores);
+    update_edge_dst_property(
+      handle, graph_view, in_one_core_flags.begin(), edge_dst_in_one_cores);
+    //auto [srcs, dsts] = extract_transform_e(handle,
+    //                                        graph_view,
+    //                                        edge_src_in_one_cores.view(),
+    //                                        edge_dst_in_one_cores.view(),
+    //                                        edge_dummy_property_t{}.view(),
+    //                                        extract_one_core_t<vertex_t>{});
+    rmm::device_uvector<size_t> subgraph_offsets_in(2, handle.get_stream());
+    std::vector<size_t> h_subgraph_offsets_in{{0, in_one_core_flags.size()}};
+    raft::update_device(subgraph_offsets_in.data(),
+                      h_subgraph_offsets_in.data(),
+                      h_subgraph_offsets_in.size(),
+                      handle.get_stream());
+    handle.sync_stream();
+
+   
+    auto [src_, dst_, wgt_, offsets_] = extract_induced_subgraphs(
+    handle,
+    graph_view,
+    edge_weight_view,
+    raft::device_span<size_t const>{subgraph_offsets_in.data(), subgraph_offsets_in.size()},
+    raft::device_span<vertex_t const>{in_one_core_flags.data(), in_one_core_flags.size()},
+    do_expensive_check);
+/*
     if constexpr (multi_gpu) {
       std::tie(srcs, dsts, std::ignore, std::ignore, std::ignore) =
         detail::shuffle_ext_vertex_pairs_with_values_to_local_gpu_by_edge_partitioning<vertex_t,
@@ -114,6 +165,6 @@ trim(raft::handle_t const& handle,
                                                    *vertex_partition_range_lasts);
     }
     renumber_map = std::move(tmp_renumber_map);
-
+*/
 }
 }
