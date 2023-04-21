dee2fe5ffe699da6f33710a8910f126e5798a172
Tingyu Wang <tingyuw@nvidia.com>
2023-04-21 17:24:48 -0400

    add option to use edge feat in GATConv test

--- python/cugraph-pyg/cugraph_pyg/tests/nn/test_gat_conv.py
+++ python/cugraph-pyg/cugraph_pyg/tests/nn/test_gat_conv.py
@@ -27,51 +27,95 @@ torch = import_optional("torch")
 @pytest.mark.parametrize("bias", [True, False])
 @pytest.mark.parametrize("bipartite", [True, False])
 @pytest.mark.parametrize("concat", [True, False])
-@pytest.mark.parametrize("heads", [1, 2, 3])
+@pytest.mark.parametrize("heads", [1, 2, 3, 5, 10, 16])
 @pytest.mark.parametrize("max_num_neighbors", [8, None])
-def test_gat_conv_equality(bias, bipartite, concat, heads, max_num_neighbors):
-    in_channels, out_channels = (5, 2)
-    kwargs = dict(bias=bias, concat=concat)
-
-    size = (10, 8) if bipartite else (10, 10)
-    x = torch.rand(size[0], in_channels, device="cuda")
+@pytest.mark.parametrize("use_edge_attr", [True, False])
+def test_gat_conv_equality(
+    bias, bipartite, concat, heads, max_num_neighbors, use_edge_attr
+):
     edge_index = torch.tensor(
         [
             [7, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 8, 9],
             [0, 1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 7],
         ],
-        device="cuda",
-    )
+    ).cuda()
+    size = (10, 10)
+
+    if bipartite:
+        in_channels = (5, 3)
+        x = (
+            torch.rand(size[0], in_channels[0]).cuda(),
+            torch.rand(size[1], in_channels[1]).cuda(),
+        )
+    else:
+        in_channels = 5
+        x = torch.rand(size[0], in_channels).cuda()
+    out_channels = 2
+
+    if use_edge_attr:
+        edge_dim = 3
+        edge_attr = torch.rand(edge_index.size(1), edge_dim).cuda()
+        csc, edge_attr = CuGraphGATConv.to_csc(edge_index, size, edge_attr=edge_attr)
+    else:
+        edge_dim = None
+        edge_attr = None
+        csc = CuGraphGATConv.to_csc(edge_index, size)
+
+    kwargs = dict(bias=bias, concat=concat, edge_dim=edge_dim)
 
     conv1 = GATConv(
         in_channels, out_channels, heads, add_self_loops=False, **kwargs
     ).cuda()
     conv2 = CuGraphGATConv(in_channels, out_channels, heads, **kwargs).cuda()
 
+    out_dim = heads * out_channels
     with torch.no_grad():
-        conv2.lin.weight.data[:, :] = conv1.lin_src.weight.data
-        conv2.att.data[: heads * out_channels] = conv1.att_src.data.flatten()
-        conv2.att.data[heads * out_channels :] = conv1.att_dst.data.flatten()
+        if bipartite:
+            conv2.lin_src.weight.data = conv1.lin_src.weight.data.detach().clone()
+            conv2.lin_dst.weight.data = conv1.lin_dst.weight.data.detach().clone()
+        else:
+            conv2.lin.weight.data = conv1.lin_src.weight.data.detach().clone()
 
-    if bipartite:
-        out1 = conv1((x, x[: size[1]]), edge_index)
-    else:
-        out1 = conv1(x, edge_index)
+        conv2.att.data[:out_dim] = conv1.att_src.data.flatten()
+        conv2.att.data[out_dim : 2 * out_dim] = conv1.att_dst.data.flatten()
+        if use_edge_attr:
+            conv2.att.data[2 * out_dim :] = conv1.att_edge.data.flatten()
+            conv2.lin_edge.weight.data = conv1.lin_edge.weight.data.detach().clone()
 
-    csc = CuGraphGATConv.to_csc(edge_index, size)
-    out2 = conv2(x, csc, max_num_neighbors=max_num_neighbors)
+    out1 = conv1(x, edge_index, edge_attr=edge_attr)
+    out2 = conv2(x, csc, edge_attr=edge_attr, max_num_neighbors=max_num_neighbors)
     assert torch.allclose(out1, out2, atol=1e-3)
 
     grad_output = torch.rand_like(out1)
     out1.backward(grad_output)
     out2.backward(grad_output)
 
-    assert torch.allclose(conv1.lin_src.weight.grad, conv2.lin.weight.grad, atol=1e-3)
+    if bipartite:
+        assert torch.allclose(
+            conv1.lin_src.weight.grad, conv2.lin_src.weight.grad, atol=1e-3
+        )
+        assert torch.allclose(
+            conv1.lin_dst.weight.grad, conv2.lin_dst.weight.grad, atol=1e-3
+        )
+    else:
+        assert torch.allclose(
+            conv1.lin_src.weight.grad, conv2.lin.weight.grad, atol=1e-3
+        )
+
     assert torch.allclose(
-        conv1.att_src.grad.flatten(), conv2.att.grad[: heads * out_channels], atol=1e-3
+        conv1.att_src.grad.flatten(), conv2.att.grad[:out_dim], atol=1e-3
     )
     assert torch.allclose(
-        conv1.att_dst.grad.flatten(), conv2.att.grad[heads * out_channels :], atol=1e-3
+        conv1.att_dst.grad.flatten(), conv2.att.grad[out_dim : 2 * out_dim], atol=1e-3
     )
+
+    if use_edge_attr:
+        assert torch.allclose(
+            conv1.att_edge.grad.flatten(), conv2.att.grad[2 * out_dim :], atol=1e-3
+        )
+        assert torch.allclose(
+            conv1.lin_edge.weight.grad, conv2.lin_edge.weight.grad, atol=1e-3
+        )
+
     if bias:
         assert torch.allclose(conv1.bias.grad, conv2.bias.grad, atol=1e-3)
