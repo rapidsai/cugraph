936d5f9a4178d228b32f4b7f6c062bf71bf8ac8a
Charles Hastings <charlesh@nvidia.com>
2023-05-17 14:23:07 -0700

    remove legacy leiden

--- cpp/include/cugraph/algorithms.hpp
+++ cpp/include/cugraph/algorithms.hpp
@@ -678,47 +678,6 @@ void flatten_dendrogram(raft::handle_t const& handle,
                         typename graph_view_t::vertex_type* clustering);
 
 /**
- * @brief      Legacy Leiden implementation
- *
- * Compute a clustering of the graph by maximizing modularity using the Leiden improvements
- * to the Louvain method.
- *
- * Computed using the Leiden method described in:
- *
- *    Traag, V. A., Waltman, L., & van Eck, N. J. (2019). From Louvain to Leiden:
- *    guaranteeing well-connected communities. Scientific reports, 9(1), 5233.
- *    doi: 10.1038/s41598-019-41695-z
- *
- * @throws cugraph::logic_error when an error occurs.
- *
- * @tparam vertex_t                  Type of vertex identifiers.
- *                                   Supported value : int (signed, 32-bit)
- * @tparam edge_t                    Type of edge identifiers.
- *                                   Supported value : int (signed, 32-bit)
- * @tparam weight_t                  Type of edge weights. Supported values : float or double.
- *
- * @param[in]  handle                Library handle (RAFT). If a communicator is set in the handle,
- * @param[in]  graph                 input graph object (CSR)
- * @param[out] clustering            Pointer to device array where the clustering should be stored
- * @param[in]  max_level             (optional) maximum number of levels to run (default 100)
- * @param[in]  resolution            (optional) The value of the resolution parameter to use.
- *                                   Called gamma in the modularity formula, this changes the size
- *                                   of the communities.  Higher resolutions lead to more smaller
- *                                   communities, lower resolutions lead to fewer larger
- * communities. (default 1)
- *
- * @return                           a pair containing:
- *                                     1) number of levels of the returned clustering
- *                                     2) modularity of the returned clustering
- */
-template <typename vertex_t, typename edge_t, typename weight_t>
-std::pair<size_t, weight_t> leiden(raft::handle_t const& handle,
-                                   legacy::GraphCSRView<vertex_t, edge_t, weight_t> const& graph,
-                                   vertex_t* clustering,
-                                   size_t max_level    = 100,
-                                   weight_t resolution = weight_t{1});
-
-/**
  * @brief      Leiden implementation
  *
  * Compute a clustering of the graph by maximizing modularity using the Leiden improvements
