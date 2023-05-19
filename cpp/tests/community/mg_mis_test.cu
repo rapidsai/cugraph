cfca807310a4275b2f0cc59891b463e0f530e816
Md Naim <mnaim@dgx18.aselab.nvidia.com>
2023-05-18 17:23:38 -0700

    Use thrust random generator inside device code

--- cpp/tests/community/mg_mis_test.cu
+++ cpp/tests/community/mg_mis_test.cu
@@ -219,6 +219,12 @@ TEST_P(Tests_MGMaximalIndependentSet_File, CheckInt32Int32FloatFloat)
     override_File_Usecase_with_cmd_line_arguments(GetParam()));
 }
 
+TEST_P(Tests_MGMaximalIndependentSet_File, CheckInt32Int64FloatFloat)
+{
+  run_current_test<int32_t, int64_t, float, int>(
+    override_File_Usecase_with_cmd_line_arguments(GetParam()));
+}
+
 TEST_P(Tests_MGMaximalIndependentSet_File, CheckInt64Int64FloatFloat)
 {
   run_current_test<int64_t, int64_t, float, int>(
