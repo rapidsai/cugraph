68b214da2895fccfdabd03f5537c7d929d6039f6
Vyas Ramasubramani <vyasr@nvidia.com>
2023-05-31 10:30:10 -0700

    Fix typo

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -16,8 +16,8 @@ rapids-logger "Begin py build"
 PY_VER=${RAPIDS_PY_VERSION//./}
 _CUDA_MAJOR=$(nvcc --version | tail -n 2 | head -n 1 | cut -d',' -f 2 | cut -d' ' -f 3 | cut -d'.' -f 1)
 
-LIBRMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1278/4e1392d/rmm_conda_cpp_cuda${CUDA_MAJOR}_$(arch).tar.gz)
-RMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1278/4e1392d/rmm_conda_python_cuda${CUDA_MAJOR}_${PY_VER}_$(arch).tar.gz)
+LIBRMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1278/4e1392d/rmm_conda_cpp_cuda${_CUDA_MAJOR}_$(arch).tar.gz)
+RMM_CHANNEL=$(rapids-get-artifact ci/rmm/pull-request/1278/4e1392d/rmm_conda_python_cuda${_CUDA_MAJOR}_${PY_VER}_$(arch).tar.gz)
 # TODO: Remove `--no-test` flags once importing on a CPU
 # node works correctly
 rapids-mamba-retry mambabuild \
