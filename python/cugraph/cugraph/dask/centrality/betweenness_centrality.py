2ed4efa29e8099463d0ff81d15d4bcf2f22c8b5e
jnke2016 <jnke@gmail.com>
2023-06-29 11:21:16 -0700

    add mg eddge betwennness centrality

--- python/cugraph/cugraph/dask/centrality/betweenness_centrality.py
+++ python/cugraph/cugraph/dask/centrality/betweenness_centrality.py
@@ -17,6 +17,7 @@ from dask.distributed import wait, get_client
 from pylibcugraph import (
     ResourceHandle,
     betweenness_centrality as pylibcugraph_betweenness_centrality,
+    edge_betweenness_centrality as pylibcugraph_edge_betweenness_centrality,
 )
 import cugraph.dask.comms.comms as Comms
 from cugraph.dask.common.input_utils import get_distributed_data
@@ -28,14 +29,24 @@ import dask
 from typing import Union
 
 
-def convert_to_cudf(cp_arrays: cp.ndarray) -> cudf.DataFrame:
+def convert_to_cudf(cp_arrays: cp.ndarray, edge_bc: bool) -> cudf.DataFrame:
     """
     create a cudf DataFrame from cupy arrays
     """
-    cupy_vertices, cupy_values = cp_arrays
     df = cudf.DataFrame()
-    df["vertex"] = cupy_vertices
-    df["betweenness_centrality"] = cupy_values
+    if edge_bc:
+        cupy_src_vertices, cupy_dst_vertices, cupy_values, cupy_edge_ids = \
+            cp_arrays
+        df["src"] = cupy_src_vertices
+        df["dst"] = cupy_dst_vertices
+        df["betweenness_centrality"] = cupy_values
+        if cupy_edge_ids is not None:
+            df["edge_ids"] = cupy_edge_ids
+    
+    else:
+        cupy_vertices, cupy_values = cp_arrays
+        df["vertex"] = cupy_vertices
+        df["betweenness_centrality"] = cupy_values
     return df
 
 
@@ -47,18 +58,29 @@ def _call_plc_betweenness_centrality(
     normalized: bool,
     endpoints: bool,
     do_expensive_check: bool,
+    edge_bc: bool,
 ) -> cudf.DataFrame:
 
-    cp_arrays = pylibcugraph_betweenness_centrality(
+    if edge_bc:
+        cp_arrays = pylibcugraph_edge_betweenness_centrality(
         resource_handle=ResourceHandle(Comms.get_handle(sID).getHandle()),
         graph=mg_graph_x,
         k=k,
         random_state=random_state,
         normalized=normalized,
-        include_endpoints=endpoints,
         do_expensive_check=do_expensive_check,
     )
-    return convert_to_cudf(cp_arrays)
+    else:
+        cp_arrays = pylibcugraph_betweenness_centrality(
+            resource_handle=ResourceHandle(Comms.get_handle(sID).getHandle()),
+            graph=mg_graph_x,
+            k=k,
+            random_state=random_state,
+            normalized=normalized,
+            include_endpoints=endpoints,
+            do_expensive_check=do_expensive_check,
+        )
+    return convert_to_cudf(cp_arrays, edge_bc)
 
 
 def _mg_call_plc_betweenness_centrality(
@@ -68,8 +90,9 @@ def _mg_call_plc_betweenness_centrality(
     k: dict,
     random_state: int,
     normalized: bool,
-    endpoints: bool,
     do_expensive_check: bool,
+    endpoints: bool = False,
+    edge_bc: bool = False,
 ) -> dask_cudf.DataFrame:
 
     result = [
@@ -82,6 +105,7 @@ def _mg_call_plc_betweenness_centrality(
             normalized,
             endpoints,
             do_expensive_check,
+            edge_bc,
             workers=[w],
             allow_other_workers=False,
             pure=False,
@@ -114,8 +138,8 @@ def betweenness_centrality(
     To improve performance. rather than doing an all-pair shortest path,
     a sample of k starting vertices can be used.
 
-    CuGraph does not currently support the 'endpoints' and 'weight' parameters
-    as seen in the corresponding networkX call.
+    CuGraph does not currently support 'weight' parameters as seen in the
+    corresponding networkX call.
 
     Parameters
     ----------
@@ -216,14 +240,154 @@ def betweenness_centrality(
     client = get_client()
 
     ddf = _mg_call_plc_betweenness_centrality(
-        input_graph,
-        client,
-        Comms.get_session_id(),
-        k,
-        random_state,
-        normalized,
-        endpoints,
-        do_expensive_check,
+        input_graph=input_graph,
+        client=client,
+        sID=Comms.get_session_id(),
+        k=k,
+        random_state=random_state,
+        normalized=normalized,
+        endpoints=endpoints,
+        do_expensive_check=do_expensive_check,
+    )
+
+    if input_graph.renumbered:
+        return input_graph.unrenumber(ddf, "vertex")
+
+    return ddf
+
+
+def edge_betweenness_centrality(
+    input_graph,
+    k: Union[
+        int, list, cudf.Series, cudf.DataFrame, dask_cudf.Series, dask_cudf.DataFrame
+    ] = None,
+    normalized: bool = True,
+    random_state: int = None,
+) -> dask_cudf.DataFrame:
+    """
+    Compute the edge betweenness centrality for all edges of the graph G.
+    Betweenness centrality is a measure of the number of shortest paths
+    that pass over an edge.  An edge with a high betweenness centrality
+    score has more paths passing over it and is therefore believed to be
+    more important.
+
+    To improve performance. rather than doing an all-pair shortest path,
+    a sample of k starting vertices can be used.
+
+    CuGraph does not currently support 'weight' parameters as seen in the
+    corresponding networkX call.
+
+    Parameters
+    ----------
+    input_graph: cuGraph.Graph
+        The graph can be either directed (Graph(directed=True)) or undirected.
+        Weights in the graph are ignored, the current implementation uses a parallel
+        variation of the Brandes Algorithm (2001) to compute exact or approximate
+        betweenness. If weights are provided in the edgelist, they will not be
+        used.
+
+    k : int, list or (dask)cudf object or None, optional (default=None)
+        If k is not None, use k node samples to estimate betweenness.  Higher
+        values give better approximation.  If k is either a list or a (dask)cudf,
+        use its content for estimation: it contain vertex identifiers. If k is None
+        (the default), all the vertices are used to estimate betweenness.  Vertices
+        obtained through sampling or defined as a list will be used as sources for
+        traversals inside the algorithm.
+
+    normalized : bool, optional (default=True)
+        If True normalize the resulting betweenness centrality values
+
+    random_state : int, optional (default=None)
+        if k is specified and k is an integer, use random_state to initialize the
+        random number generator.
+        Using None defaults to a hash of process id, time, and hostname
+        If k is either None or list or cudf objects: random_state parameter is
+        ignored.
+
+    Returns
+    -------
+    betweenness_centrality : dask_cudf.DataFrame
+        GPU distributed data frame containing two dask_cudf.Series of size V:
+        the vertex identifiers and the corresponding betweenness centrality values.
+
+        ddf['src'] : dask_cudf.Series
+            Contains the vertex identifiers of the source of each edge
+
+        ddf['dst'] : dask_cudf.Series
+            Contains the vertex identifiers of the destination of each edge
+
+        ddf['betweenness_centrality'] : dask_cudf.Series
+            Contains the betweenness centrality of edges
+        
+        ddf["edge_id"] : dask_cudf.Series
+            Contains the edge ids of edges if there are.
+
+    Examples
+    --------
+    >>> import cugraph.dask as dcg
+    >>> import dask_cudf
+    >>> # ... Init a DASK Cluster
+    >>> #    see https://docs.rapids.ai/api/cugraph/stable/dask-cugraph.html
+    >>> # Download dataset from https://github.com/rapidsai/cugraph/datasets/..
+    >>> chunksize = dcg.get_chunksize(datasets_path / "karate.csv")
+    >>> ddf = dask_cudf.read_csv(datasets_path / "karate.csv",
+    ...                          chunksize=chunksize, delimiter=" ",
+    ...                          names=["src", "dst", "value"],
+    ...                          dtype=["int32", "int32", "float32"])
+    >>> dg = cugraph.Graph(directed=True)
+    >>> dg.from_dask_cudf_edgelist(ddf, source='src', destination='dst')
+    >>> pr = dcg.edge_betweenness_centrality(dg)
+
+    """
+
+    if input_graph.store_transposed is True:
+        warning_msg = (
+            "Betweenness centrality expects the 'store_transposed' flag "
+            "to be set to 'False' for optimal performance during "
+            "the graph creation"
+        )
+        warnings.warn(warning_msg, UserWarning)
+
+    if not isinstance(k, (dask_cudf.DataFrame, dask_cudf.Series)):
+        if isinstance(k, (cudf.DataFrame, cudf.Series, list)):
+            if isinstance(k, list):
+                k_dtype = input_graph.nodes().dtype
+                k = cudf.Series(k, dtype=k_dtype)
+
+        if isinstance(k, (cudf.Series, cudf.DataFrame)):
+            splits = cp.array_split(cp.arange(len(k)), len(Comms.get_workers()))
+            k = {w: [k.iloc[splits[i]]] for i, w in enumerate(Comms.get_workers())}
+
+    else:
+        if k is not None:
+            k = get_distributed_data(k)
+            wait(k)
+            k = k.worker_to_parts
+
+    if input_graph.renumbered:
+        if isinstance(k, dask_cudf.DataFrame):
+            tmp_col_names = k.columns
+
+        elif isinstance(k, dask_cudf.Series):
+            tmp_col_names = None
+
+        if isinstance(k, (dask_cudf.DataFrame, dask_cudf.Series)):
+            k = input_graph.lookup_internal_vertex_id(k, tmp_col_names)
+
+    # FIXME: should we add this parameter as an option?
+    do_expensive_check = False
+
+    client = get_client()
+
+    ddf = _mg_call_plc_betweenness_centrality(
+        input_graph=input_graph,
+        client=client,
+        sID=Comms.get_session_id(),
+        k=k,
+        random_state=random_state,
+        normalized=normalized,
+        do_expensive_check=do_expensive_check,
+        edge_bc=True,
     )
 
     if input_graph.renumbered:
