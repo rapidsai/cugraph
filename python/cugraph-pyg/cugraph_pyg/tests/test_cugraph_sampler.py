2995c62035b2f32c2a5ff72a022874dde695258e
Vibhu Jawa <vibhujawa@gmail.com>
2023-07-25 23:26:02 -0700

    Make tests same as upstream

--- python/cugraph-pyg/cugraph_pyg/tests/test_cugraph_sampler.py
+++ python/cugraph-pyg/cugraph_pyg/tests/test_cugraph_sampler.py
@@ -70,7 +70,7 @@ def test_neighbor_sample(basic_graph_1):
 
     # check the hop dictionaries
     assert len(out.num_sampled_nodes) == 1
-    assert out.num_sampled_nodes["vt1"].tolist() == [4, 1]
+    assert out.num_sampled_nodes["vt1"].tolist() == [4, 4]
 
     assert len(out.num_sampled_edges) == 1
     assert out.num_sampled_edges[("vt1", "pig", "vt1")].tolist() == [6]
@@ -118,8 +118,8 @@ def test_neighbor_sample_multi_vertex(multi_edge_multi_vertex_graph_1):
 
     # check the hop dictionaries
     assert len(out.num_sampled_nodes) == 2
-    assert out.num_sampled_nodes["black"].tolist() == [2, 0]
-    assert out.num_sampled_nodes["brown"].tolist() == [3, 0]
+    assert out.num_sampled_nodes["black"].tolist() == [2, 2]
+    assert out.num_sampled_nodes["brown"].tolist() == [3, 2]
 
     assert len(out.num_sampled_edges) == 5
     assert out.num_sampled_edges[("brown", "horse", "brown")].tolist() == [2]
@@ -165,9 +165,9 @@ def test_neighbor_sample_mock_sampling_results(abc_graph):
     assert out.col[("B", "ba", "A")].tolist() == [1, 1]
 
     assert len(out.num_sampled_nodes) == 3
-    assert out.num_sampled_nodes["A"].tolist() == [2, 0, 0, 0, 0]
-    assert out.num_sampled_nodes["B"].tolist() == [0, 2, 0, 0, 0]
-    assert out.num_sampled_nodes["C"].tolist() == [0, 0, 2, 0, 1]
+    assert out.num_sampled_nodes["A"].tolist() == [2, 0, 1, 0, 1]
+    assert out.num_sampled_nodes["B"].tolist() == [0, 2, 0, 1, 0]
+    assert out.num_sampled_nodes["C"].tolist() == [0, 0, 2, 0, 2]
 
     assert len(out.num_sampled_edges) == 3
     assert out.num_sampled_edges[("A", "ab", "B")].tolist() == [3, 0, 1, 0]
