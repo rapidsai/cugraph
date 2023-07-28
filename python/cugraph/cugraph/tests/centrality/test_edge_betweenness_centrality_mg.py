2f91b5f05336c2572ee18d507068166483142aa7
jnke2016 <jnke@gmail.com>
2023-07-28 10:00:58 -0700

    remove expensive test

--- python/cugraph/cugraph/tests/centrality/test_edge_betweenness_centrality_mg.py
+++ python/cugraph/cugraph/tests/centrality/test_edge_betweenness_centrality_mg.py
@@ -41,7 +41,7 @@ SUBSET_SIZE_OPTIONS = [4, None]
 
 
 # email_Eu_core is too expensive to test
-datasets = DATASETS_UNDIRECTED + [email_Eu_core]
+datasets = DATASETS_UNDIRECTED
 
 
 # =============================================================================
