e06a8108421e9ced8a40beecd6080e8eb22f86de
jnke2016 <jnke@gmail.com>
2023-08-03 12:31:05 -0700

    unrenumber before extracting the upper triangular part

--- python/cugraph/cugraph/structure/graph_implementation/simpleDistributedGraph.py
+++ python/cugraph/cugraph/structure/graph_implementation/simpleDistributedGraph.py
@@ -411,6 +411,9 @@ class simpleDistributedGraphImpl:
                 srcCol = self.renumber_map.renumbered_src_col_name
                 dstCol = self.renumber_map.renumbered_dst_col_name
                 edgelist_df = self.edgelist.edgelist_df
+                # unrenumber before extracting the upper triangular part
+                edgelist_df = self.renumber_map.unrenumber(edgelist_df, srcCol)
+                edgelist_df = self.renumber_map.unrenumber(edgelist_df, dstCol)
 
             edgelist_df[srcCol], edgelist_df[dstCol] = edgelist_df[
                 [srcCol, dstCol]
