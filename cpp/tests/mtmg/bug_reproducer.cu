7b7916d4d29d7e39e51c1819b4f2028703c6a80a
Charles Hastings <charlesh@nvidia.com>
2023-08-09 10:32:33 -0700

    smaller reproducer

--- cpp/tests/mtmg/bug_reproducer.cu
+++ cpp/tests/mtmg/bug_reproducer.cu
@@ -13,9 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <cugraph/mtmg/resource_manager.hpp>
-#include <cugraph/utilities/error.hpp>
-
+#include <raft/comms/std_comms.hpp>
+#include <raft/core/handle.hpp>
 #include <raft/util/cudart_utils.hpp>
 
 #include <rmm/device_uvector.hpp>
@@ -24,79 +23,68 @@
 
 #include <vector>
 
-#include <cugraph/detail/shuffle_wrappers.hpp>
-#include <cugraph/utilities/host_scalar_comm.hpp>
-
 int main(int argc, char** argv)
 {
-  using vertex_t = int32_t;
-  using edge_t   = int32_t;
-  using weight_t = float;
-
-  int num_gpus{2};
+  int comm_size{2};
+  int gpu_row_comm_size{1};
 
   ncclUniqueId instance_manager_id;
   ncclGetUniqueId(&instance_manager_id);
 
-  cugraph::mtmg::resource_manager_t resource_manager;
-
   std::vector<std::thread> running_threads;
 
-  resource_manager.register_local_gpu(0, rmm::cuda_device_id{0});
-  resource_manager.register_local_gpu(1, rmm::cuda_device_id{1});
+  std::vector<std::shared_ptr<ncclComm_t>> nccl_comms{};
+  std::vector<std::shared_ptr<raft::handle_t>> handles{};
 
-  std::cout << "create instance_manager" << std::endl;
+  nccl_comms.reserve(comm_size);
+  handles.reserve(comm_size);
 
-  auto instance_manager = resource_manager.create_instance_manager(
-    resource_manager.registered_ranks(), instance_manager_id);
+  cudaSetDevice(0);
+  auto resource_0 = std::make_shared<rmm::mr::cuda_memory_resource>();
+  rmm::mr::set_per_device_resource(rmm::cuda_device_id{0}, resource_0.get());
 
-  for (int i = 0; i < num_gpus; ++i) {
-    running_threads.emplace_back([&instance_manager]() {
-      auto thread_handle = instance_manager->get_handle();
+  raft::handle_t tmp_handle_0;
 
-      if (thread_handle.get_thread_rank() > 0) return;
+  nccl_comms.push_back(std::make_shared<ncclComm_t>());
+  handles.push_back(std::make_shared<raft::handle_t>(tmp_handle_0, resource_0));
 
-      auto& comm = thread_handle.raft_handle().get_comms();
+  cudaSetDevice(1);
+  auto resource_1 = std::make_shared<rmm::mr::cuda_memory_resource>();
+  rmm::mr::set_per_device_resource(rmm::cuda_device_id{1}, resource_1.get());
 
-      std::vector<vertex_t> h_sorted_local_vertices;
-      if (thread_handle.get_rank() == 0) {
-        h_sorted_local_vertices = {{32}};
-      } else {
-        h_sorted_local_vertices = {{33}};
-      }
+  raft::handle_t tmp_handle_1;
 
-      rmm::device_uvector<vertex_t> sorted_local_vertices(1, thread_handle.get_stream());
-      raft::update_device(sorted_local_vertices.data(),
-                          h_sorted_local_vertices.data(),
-                          h_sorted_local_vertices.size(),
-                          thread_handle.get_stream());
-      thread_handle.raft_handle().sync_stream();
+  nccl_comms.push_back(std::make_shared<ncclComm_t>());
+  handles.push_back(std::make_shared<raft::handle_t>(tmp_handle_1, resource_1));
 
-      std::cout << "in compute_renumber_map... step 3 shuffle, rank = " << thread_handle.get_rank()
-                << std::endl;
-      raft::print_device_vector("  sorted_local_vertices",
-                                sorted_local_vertices.data(),
-                                sorted_local_vertices.size(),
-                                std::cout);
-      sorted_local_vertices =
-        cugraph::detail::shuffle_ext_vertices_to_local_gpu_by_vertex_partitioning(
-          thread_handle.raft_handle(), std::move(sorted_local_vertices));
+  for (int i = 0; i < comm_size; ++i) {
+    running_threads.emplace_back(
+      [&instance_manager_id, rank = i, gpu_row_comm_size, comm_size, &nccl_comms, &handles]() {
+        cudaSetDevice(rank);
 
-      std::cout << "computing local min, rank = " << comm.get_rank() << std::endl;
+        std::cout << "call nccl_comms_init_rank, rank = " << rank << std::endl;
 
-      vertex_t min = 32 + comm.get_rank();
+        ncclCommInitRank(nccl_comms[rank].get(), comm_size, instance_manager_id, rank);
 
-      std::cout << "rank = " << comm.get_rank() << ", local min = " << min << std::endl;
-      min = cugraph::host_scalar_allreduce(
-        comm, min, raft::comms::op_t::MIN, thread_handle.get_stream());
+        std::cout << "call build_comms_nccl_only" << std::endl;
+        raft::comms::build_comms_nccl_only(handles[rank].get(), *nccl_comms[rank], comm_size, rank);
+        std::cout << "back from build_comms_nccl_only" << std::endl;
 
-      std::cout << "after detail::host_scalar_allreduce call, rank = " << thread_handle.get_rank()
-                << ", min = " << min << std::endl;
-    });
+        auto& comm = handles[rank]->get_comms();
+
+        int row_idx = rank / gpu_row_comm_size;
+        int col_idx = rank % gpu_row_comm_size;
+
+        handles[rank]->set_subcomm(
+          "gpu_row_comm",
+          std::make_shared<raft::comms::comms_t>(comm.comm_split(row_idx, col_idx)));
+        handles[rank]->set_subcomm(
+          "gpu_col_comm",
+          std::make_shared<raft::comms::comms_t>(comm.comm_split(col_idx, row_idx)));
+
+        std::cout << "back from init_subcomm" << std::endl;
+      });
   }
 
-  // Wait for CPU threads to complete
   std::for_each(running_threads.begin(), running_threads.end(), [](auto& t) { t.join(); });
-  running_threads.resize(0);
-  instance_manager->reset_threads();
 }
