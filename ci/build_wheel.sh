a6f55041837c4cfc41a7df074a342dd1887ed20c
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-22 12:46:08 -0700

    First pass at simplifying the build_wheel script

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -17,7 +17,7 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
 # This is the version of the suffix with a preceding hyphen. It's used
 # everywhere except in the final wheel name.
-PACKAGE_CUDA_SUFFIX="-{RAPIDS_PY_CUDA_SUFFIX}"
+PACKAGE_CUDA_SUFFIX="-${RAPIDS_PY_CUDA_SUFFIX}"
 
 # Manually install dependencies because we're building without isolation.
 rapids-dependency-file-generator \
@@ -31,29 +31,18 @@ done
 
 python -m pip install -r requirements.txt
 
-# pyproject.toml updates
-sed -i "s/^version = .*/version = \"${version_override}\"/g" \
-  python/cugraph/pyproject.toml \
-  python/cugraph-dgl/pyproject.toml \
-  python/cugraph-pyg/pyproject.toml \
-  python/cugraph-service/client/pyproject.toml \
-  python/cugraph-service/server/pyproject.toml \
-  python/pylibcugraph/pyproject.toml
+# Patch project metadata files to include the CUDA version suffix and version override.
+pyproject_file="${package_dir}/pyproject.toml"
 
-# pylibcugraph pyproject.toml cuda suffixes
-sed -i "s/name = \"pylibcugraph\"/name = \"pylibcugraph${PACKAGE_CUDA_SUFFIX}\"/g" python/pylibcugraph/pyproject.toml
-for dep in rmm pylibraft cudf; do
-  sed -i "s/${dep}/${dep}${PACKAGE_CUDA_SUFFIX}/g" python/pylibcugraph/pyproject.toml
-done
+sed -i "s/^version = .*/version = \"${version_override}\"/g" ${pyproject_file}
+sed -i "s/name = \"${package_name}\"/name = \"${package_name}${PACKAGE_CUDA_SUFFIX}\"/g" ${pyproject_file}
 
-# cugraph pyproject.toml cuda suffixes
-sed -i "s/name = \"cugraph\"/name = \"cugraph${PACKAGE_CUDA_SUFFIX}\"/g" python/cugraph/pyproject.toml
 for dep in rmm cudf raft-dask pylibcugraph pylibraft ucx-py; do
-  sed -i "s/${dep}/${dep}${PACKAGE_CUDA_SUFFIX}/g" python/cugraph/pyproject.toml
+  sed -i "s/${dep}/${dep}${PACKAGE_CUDA_SUFFIX}/g" ${pyproject_file}
 done
 
 if [[ $PACKAGE_CUDA_SUFFIX == "-cu12" ]]; then
-    sed -i "s/cupy-cuda11x/cupy-cuda12x/g" python/cugraph/pyproject.toml
+    sed -i "s/cupy-cuda11x/cupy-cuda12x/g" ${pyproject_file}
 fi
 
 cd "${package_dir}"
