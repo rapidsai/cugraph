f63a2903f1c45ebb81d98bfc6d80a08a4c9a97e2
Alexandria Barghi <abarghi@nvidia.com>
2023-08-22 08:05:48 -0700

    remove deprecated sampling options, support other sequence types

--- python/cugraph/cugraph/dask/sampling/uniform_neighbor_sample.py
+++ python/cugraph/cugraph/dask/sampling/uniform_neighbor_sample.py
@@ -287,112 +287,6 @@ def _call_plc_uniform_neighbor_sample(
     )
 
 
-def _call_plc_uniform_neighbor_sample_legacy(
-    sID,
-    mg_graph_x,
-    st_x,
-    label_list,
-    label_to_output_comm_rank,
-    fanout_vals,
-    with_replacement,
-    weight_t,
-    with_edge_properties,
-    random_state=None,
-    return_offsets=False,
-    return_hops=True,
-):
-    start_list_x = st_x[start_col_name]
-    batch_id_list_x = st_x[batch_col_name] if batch_col_name in st_x else None
-    cp_arrays = pylibcugraph_uniform_neighbor_sample(
-        resource_handle=ResourceHandle(Comms.get_handle(sID).getHandle()),
-        input_graph=mg_graph_x,
-        start_list=start_list_x,
-        label_list=label_list,
-        label_to_output_comm_rank=label_to_output_comm_rank,
-        h_fan_out=fanout_vals,
-        with_replacement=with_replacement,
-        do_expensive_check=False,
-        with_edge_properties=with_edge_properties,
-        batch_id_list=batch_id_list_x,
-        random_state=random_state,
-        return_hops=return_hops,
-    )
-
-    output = convert_to_cudf(
-        cp_arrays, weight_t, with_edge_properties, return_offsets=return_offsets
-    )
-
-    if isinstance(output, (list, tuple)) and len(output) == 1:
-        return output[0]
-    return output
-
-
-def _mg_call_plc_uniform_neighbor_sample_legacy(
-    client,
-    session_id,
-    input_graph,
-    ddf,
-    label_list,
-    label_to_output_comm_rank,
-    fanout_vals,
-    with_replacement,
-    weight_t,
-    indices_t,
-    with_edge_properties,
-    random_state,
-    return_offsets=False,
-    return_hops=True,
-):
-    result = [
-        client.submit(
-            _call_plc_uniform_neighbor_sample_legacy,
-            session_id,
-            input_graph._plc_graph[w],
-            ddf[w][0],
-            label_list,
-            label_to_output_comm_rank,
-            fanout_vals,
-            with_replacement,
-            weight_t=weight_t,
-            with_edge_properties=with_edge_properties,
-            # FIXME accept and properly transmute a numpy/cupy random state.
-            random_state=hash((random_state, i)),
-            workers=[w],
-            allow_other_workers=False,
-            pure=False,
-            return_offsets=return_offsets,
-            return_hops=return_hops,
-        )
-        for i, w in enumerate(Comms.get_workers())
-    ]
-
-    empty_df = (
-        create_empty_df_with_edge_props(
-            indices_t, weight_t, return_offsets=return_offsets
-        )
-        if with_edge_properties
-        else create_empty_df(indices_t, weight_t)
-    )
-
-    if return_offsets:
-        result = [delayed(lambda x: x, nout=2)(r) for r in result]
-        ddf = dask_cudf.from_delayed(
-            [r[0] for r in result], meta=empty_df[0], verify_meta=False
-        ).persist()
-        ddf_offsets = dask_cudf.from_delayed(
-            [r[1] for r in result], meta=empty_df[1], verify_meta=False
-        ).persist()
-        wait(ddf)
-        wait(ddf_offsets)
-        wait([r.release() for r in result])
-        return ddf, ddf_offsets
-    else:
-        ddf = dask_cudf.from_delayed(result, meta=empty_df, verify_meta=False).persist()
-        wait(ddf)
-        wait([r.release() for r in result])
-        return ddf
-
-
 def _mg_call_plc_uniform_neighbor_sample(
     client,
     session_id,
@@ -501,170 +395,12 @@ def _mg_call_plc_uniform_neighbor_sample(
         return tuple(return_dfs)
 
 
-def _uniform_neighbor_sample_legacy(
-    input_graph: Graph,
-    start_list: Sequence,
-    fanout_vals: List[int],
-    with_replacement: bool = True,
-    with_edge_properties: bool = False,
-    batch_id_list: Sequence = None,
-    label_list: Sequence = None,
-    label_to_output_comm_rank: bool = None,
-    random_state: int = None,
-    return_offsets: bool = False,
-    return_hops: bool = False,
-    _multiple_clients: bool = False,
-) -> Union[dask_cudf.DataFrame, Tuple[dask_cudf.DataFrame, dask_cudf.DataFrame]]:
-    warnings.warn(
-        "The batch_id_list, label_list, and label_to_output_comm_rank "
-        "parameters are deprecated.  Consider using with_batch_ids, "
-        "keep_batches_together, min_batch_id, and max_batch_id instead."
-    )
-
-    if isinstance(start_list, int):
-        start_list = [start_list]
-
-    if isinstance(start_list, list):
-        start_list = cudf.Series(
-            start_list,
-            dtype=input_graph.edgelist.edgelist_df[
-                input_graph.renumber_map.renumbered_src_col_name
-            ].dtype,
-        )
-
-    elif with_edge_properties and batch_id_list is None:
-        batch_id_list = cudf.Series(cp.zeros(len(start_list), dtype="int32"))
-
-    # fanout_vals must be a host array!
-    # FIXME: ensure other sequence types (eg. cudf Series) can be handled.
-    if isinstance(fanout_vals, list):
-        fanout_vals = numpy.asarray(fanout_vals, dtype="int32")
-    else:
-        raise TypeError("fanout_vals must be a list, " f"got: {type(fanout_vals)}")
-
-    if "value" in input_graph.edgelist.edgelist_df:
-        weight_t = input_graph.edgelist.edgelist_df["value"].dtype
-    else:
-        weight_t = "float32"
-
-    if "_SRC_" in input_graph.edgelist.edgelist_df:
-        indices_t = input_graph.edgelist.edgelist_df["_SRC_"].dtype
-    elif src_n in input_graph.edgelist.edgelist_df:
-        indices_t = input_graph.edgelist.edgelist_df[src_n].dtype
-    else:
-        indices_t = numpy.int32
-
-    start_list = start_list.rename(start_col_name)
-    if batch_id_list is not None:
-        batch_id_list = batch_id_list.rename(batch_col_name)
-        if hasattr(start_list, "compute"):
-            # mg input
-            start_list = start_list.to_frame()
-            batch_id_list = batch_id_list.to_frame()
-            ddf = start_list.merge(
-                batch_id_list,
-                how="left",
-                left_index=True,
-                right_index=True,
-            )
-        else:
-            # sg input
-            ddf = cudf.concat(
-                [
-                    start_list,
-                    batch_id_list,
-                ],
-                axis=1,
-            )
-    else:
-        ddf = start_list.to_frame()
-
-    if input_graph.renumbered:
-        ddf = input_graph.lookup_internal_vertex_id(ddf, column_name=start_col_name)
-
-    if hasattr(ddf, "compute"):
-        ddf = get_distributed_data(ddf)
-        wait(ddf)
-        ddf = ddf.worker_to_parts
-    else:
-        splits = cp.array_split(cp.arange(len(ddf)), len(Comms.get_workers()))
-        ddf = {w: [ddf.iloc[splits[i]]] for i, w in enumerate(Comms.get_workers())}
-
-    client = get_client()
-    session_id = Comms.get_session_id()
-    if _multiple_clients:
-        # Distributed centralized lock to allow
-        # two disconnected processes (clients) to coordinate a lock
-        # https://docs.dask.org/en/stable/futures.html?highlight=lock#distributed.Lock
-        lock = Lock("plc_graph_access")
-        if lock.acquire(timeout=100):
-            try:
-                ddf = _mg_call_plc_uniform_neighbor_sample_legacy(
-                    client=client,
-                    session_id=session_id,
-                    input_graph=input_graph,
-                    ddf=ddf,
-                    label_list=label_list,
-                    label_to_output_comm_rank=label_to_output_comm_rank,
-                    fanout_vals=fanout_vals,
-                    with_replacement=with_replacement,
-                    weight_t=weight_t,
-                    indices_t=indices_t,
-                    with_edge_properties=with_edge_properties,
-                    random_state=random_state,
-                    return_offsets=return_offsets,
-                    return_hops=return_hops,
-                )
-            finally:
-                lock.release()
-        else:
-            raise RuntimeError(
-                "Failed to acquire lock(plc_graph_access) while trying to sampling"
-            )
-    else:
-        ddf = _mg_call_plc_uniform_neighbor_sample_legacy(
-            client=client,
-            session_id=session_id,
-            input_graph=input_graph,
-            ddf=ddf,
-            label_list=label_list,
-            label_to_output_comm_rank=label_to_output_comm_rank,
-            fanout_vals=fanout_vals,
-            with_replacement=with_replacement,
-            weight_t=weight_t,
-            indices_t=indices_t,
-            with_edge_properties=with_edge_properties,
-            random_state=random_state,
-            return_offsets=return_offsets,
-            return_hops=return_hops,
-        )
-
-    if return_offsets:
-        ddf, offsets_ddf = ddf
-    if input_graph.renumbered:
-        ddf = input_graph.unrenumber(ddf, "sources", preserve_order=True)
-        ddf = input_graph.unrenumber(ddf, "destinations", preserve_order=True)
-
-    if return_offsets:
-        return ddf, offsets_ddf
-
-    return ddf
-
-
-uniform_neighbor_sample_legacy = deprecated_warning_wrapper(
-    _uniform_neighbor_sample_legacy
-)
-
-
 def uniform_neighbor_sample(
     input_graph: Graph,
     start_list: Sequence,
     fanout_vals: List[int],
     with_replacement: bool = True,
-    with_edge_properties: bool = False,
-    batch_id_list: Sequence = None,  # deprecated
-    label_list: Sequence = None,  # deprecated
-    label_to_output_comm_rank: bool = None,  # deprecated
+    with_edge_properties: bool = False, # deprecated
     with_batch_ids: bool = False,
     keep_batches_together=False,
     min_batch_id=None,
@@ -698,26 +434,9 @@ def uniform_neighbor_sample(
         Flag to specify if the random sampling is done with replacement
 
     with_edge_properties: bool, optional (default=False)
+        Deprecated.
         Flag to specify whether to return edge properties (weight, edge id,
         edge type, batch id, hop id) with the sampled edges.
-
-    batch_id_list: cudf.Series or dask_cudf.Series (int32), optional (default=None)
-        Deprecated.
-        List of batch ids that will be returned with the sampled edges if
-        with_edge_properties is set to True.
-
-    label_list: cudf.Series or dask_cudf.Series (int32), optional (default=None)
-        Deprecated.
-        List of unique batch id labels.  Used along with
-        label_to_output_comm_rank to assign batch ids to GPUs.
-
-    label_to_out_comm_rank: cudf.Series or dask_cudf.Series (int32),
-    optional (default=None)
-        Deprecated.
-        List of output GPUs (by rank) corresponding to batch
-        id labels in the label list.  Used to assign each batch
-        id to a GPU.
-        Must be in ascending order (i.e. [0, 0, 1, 2]).
 
     with_batch_ids: bool, optional (default=False)
         Flag to specify whether batch ids are present in the start_list
@@ -831,41 +550,6 @@ def uniform_neighbor_sample(
                         Contains the batch offsets for the renumber maps
     """
 
-    if (
-        batch_id_list is not None
-        or label_list is not None
-        or label_to_output_comm_rank is not None
-    ):
-        if prior_sources_behavior or deduplicate_sources:
-            raise ValueError(
-                "unique sources, carry_over_sources, and deduplicate_sources"
-                " are not supported with batch_id_list, label_list, and"
-                " label_to_output_comm_rank.  Consider using with_batch_ids"
-                " and keep_batches_together instead."
-            )
-
-        if renumber:
-            raise ValueError(
-                "renumber is not supported with batch_id_list, label_list, "
-                "and label_to_output_comm_rank.  Consider using "
-                "with_batch_ids and keep_batches_together instead."
-            )
-
-        return uniform_neighbor_sample_legacy(
-            input_graph,
-            start_list,
-            fanout_vals,
-            with_replacement=with_replacement,
-            with_edge_properties=with_edge_properties,
-            batch_id_list=batch_id_list,
-            label_list=label_list,
-            label_to_output_comm_rank=label_to_output_comm_rank,
-            random_state=random_state,
-            return_offsets=return_offsets,
-            return_hops=return_hops,
-            _multiple_clients=_multiple_clients,
-        )
-
     if isinstance(start_list, int):
         start_list = [start_list]
 
@@ -906,12 +590,17 @@ def uniform_neighbor_sample(
             "when performing renumbering."
         )
 
-    # fanout_vals must be a host array!
-    # FIXME: ensure other sequence types (eg. cudf Series) can be handled.
-    if isinstance(fanout_vals, list):
+    # fanout_vals must be passed to pylibcugraph as a host array
+    if isinstance(fanout_vals, numpy.ndarray):
+        fanout_vals = fanout_vals.astype('int32')
+    elif isinstance(fanout_vals, list):
         fanout_vals = numpy.asarray(fanout_vals, dtype="int32")
+    elif isinstance(fanout_vals, cp.ndarray):
+        fanout_vals = fanout_vals.get().astype('int32')
+    elif isinstance(fanout_vals, cudf.Series):
+        fanout_vals = fanout_vals.values_host.astype('int32')
     else:
-        raise TypeError("fanout_vals must be a list, " f"got: {type(fanout_vals)}")
+        raise TypeError("fanout_vals must be a sequence, " f"got: {type(fanout_vals)}")
 
     if "value" in input_graph.edgelist.edgelist_df:
         weight_t = input_graph.edgelist.edgelist_df["value"].dtype
