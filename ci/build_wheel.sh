7557a03072853e31e34be0b94f20564545d3acd4
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-22 12:14:24 -0700

    Move apply_wheel_modifications logic into build_wheel.sh

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -15,15 +15,9 @@ version_override="$(rapids-pip-wheel-version ${RAPIDS_DATE_STRING})"
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
-bash ci/release/apply_wheel_modifications.sh ${version_override} "-${RAPIDS_PY_CUDA_SUFFIX}"
-echo "The package name and/or version was modified in the package source. The git diff is:"
-git diff
-
-# Install CI tools using pip
-python -m pip install "rapids-dependency-file-generator==1.*"
-
-# Temporary patch for adding it to the PATH
-pyenv rehash
+# This is the version of the suffix with a preceding hyphen. It's used
+# everywhere except in the final wheel name.
+PACKAGE_CUDA_SUFFIX="-{RAPIDS_PY_CUDA_SUFFIX}"
 
 # Manually install dependencies because we're building without isolation.
 rapids-dependency-file-generator \
@@ -31,14 +25,42 @@ rapids-dependency-file-generator \
   --file_key py_build_${package_name} \
   --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch)" | tee requirements.txt
 
+# pyproject.toml updates
+sed -i "s/^version = .*/version = \"${version_override}\"/g" \
+  python/cugraph/pyproject.toml \
+  python/cugraph-dgl/pyproject.toml \
+  python/cugraph-pyg/pyproject.toml \
+  python/cugraph-service/client/pyproject.toml \
+  python/cugraph-service/server/pyproject.toml \
+  python/pylibcugraph/pyproject.toml
+
+# pylibcugraph pyproject.toml cuda suffixes
+sed -i "s/name = \"pylibcugraph\"/name = \"pylibcugraph${PACKAGE_CUDA_SUFFIX}\"/g" python/pylibcugraph/pyproject.toml
+sed -i "s/rmm/rmm${PACKAGE_CUDA_SUFFIX}/g" python/pylibcugraph/pyproject.toml
+sed -i "s/pylibraft/pylibraft${PACKAGE_CUDA_SUFFIX}/g" python/pylibcugraph/pyproject.toml
+sed -i "s/cudf/cudf${PACKAGE_CUDA_SUFFIX}/g" python/pylibcugraph/pyproject.toml
+
+# cugraph pyproject.toml cuda suffixes
+sed -i "s/name = \"cugraph\"/name = \"cugraph${PACKAGE_CUDA_SUFFIX}\"/g" python/cugraph/pyproject.toml
+sed -i "s/rmm/rmm${PACKAGE_CUDA_SUFFIX}/g" python/cugraph/pyproject.toml
+sed -i "s/cudf/cudf${PACKAGE_CUDA_SUFFIX}/g" python/cugraph/pyproject.toml
+sed -i "s/raft-dask/raft-dask${PACKAGE_CUDA_SUFFIX}/g" python/cugraph/pyproject.toml
+sed -i "s/pylibcugraph/pylibcugraph${PACKAGE_CUDA_SUFFIX}/g" python/cugraph/pyproject.toml
+sed -i "s/pylibraft/pylibraft${PACKAGE_CUDA_SUFFIX}/g" python/cugraph/pyproject.toml
+sed -i "s/ucx-py/ucx-py${PACKAGE_CUDA_SUFFIX}/g" python/cugraph/pyproject.toml
+
+if [[ $PACKAGE_CUDA_SUFFIX == "-cu12" ]]; then
+    sed -i "s/cupy-cuda11x/cupy-cuda12x/g" python/cugraph/pyproject.toml
+fi
+
 # TODO: Remove this once the dependency file generator supports matrix entries,
 # https://github.com/rapidsai/dependency-file-generator/pull/48
-sed -i "s/rmm==/rmm-${RAPIDS_PY_CUDA_SUFFIX}==/g" requirements.txt
-sed -i "s/cudf==/cudf-${RAPIDS_PY_CUDA_SUFFIX}==/g" requirements.txt
-sed -i "s/raft-dask==/raft-dask-${RAPIDS_PY_CUDA_SUFFIX}==/g" requirements.txt
-sed -i "s/pylibcugraph==/pylibcugraph-${RAPIDS_PY_CUDA_SUFFIX}==/g" requirements.txt
-sed -i "s/pylibraft==/pylibraft-${RAPIDS_PY_CUDA_SUFFIX}==/g" requirements.txt
-sed -i "s/ucx-py==/ucx-py-${RAPIDS_PY_CUDA_SUFFIX}==/g" requirements.txt
+sed -i "s/rmm==/rmm${PACKAGE_CUDA_SUFFIX}==/g" requirements.txt
+sed -i "s/cudf==/cudf${PACKAGE_CUDA_SUFFIX}==/g" requirements.txt
+sed -i "s/raft-dask==/raft-dask${PACKAGE_CUDA_SUFFIX}==/g" requirements.txt
+sed -i "s/pylibcugraph==/pylibcugraph${PACKAGE_CUDA_SUFFIX}==/g" requirements.txt
+sed -i "s/pylibraft==/pylibraft${PACKAGE_CUDA_SUFFIX}==/g" requirements.txt
+sed -i "s/ucx-py==/ucx-py${PACKAGE_CUDA_SUFFIX}==/g" requirements.txt
 
 python -m pip install -r requirements.txt
 
