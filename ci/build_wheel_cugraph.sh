079da27c48b118ebb9863c1adf59faf30db0ac64
Vyas Ramasubramani <vyasr@nvidia.com>
2023-08-25 09:00:56 -0700

    Update ci/build_wheel_cugraph.sh
    Co-authored-by: AJ Schmidt <ajschmidt8@users.noreply.github.com>

--- ci/build_wheel_cugraph.sh
+++ ci/build_wheel_cugraph.sh
@@ -10,7 +10,7 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 # must be made available to the isolated build step, and there is no way to
 # manually install it into that environment.
 RAPIDS_PY_WHEEL_NAME=pylibcugraph_${RAPIDS_PY_CUDA_SUFFIX} rapids-download-wheels-from-s3 ./local-pylibcugraph
-export PIP_FIND_LINKS=`pwd`/local-pylibcugraph
+export PIP_FIND_LINKS=$(pwd)/local-pylibcugraph
 
 export SKBUILD_CONFIGURE_OPTIONS="-DDETECT_CONDA_ENV=OFF -DCUGRAPH_BUILD_WHEELS=ON -DFIND_CUGRAPH_CPP=OFF -DCPM_cugraph-ops_SOURCE=${GITHUB_WORKSPACE}/cugraph-ops/"
 
