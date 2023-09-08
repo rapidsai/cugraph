e473c5b5ea41b9bba2f4c44a261f215534f5e77c
Rick Ratzel <rratzel@nvidia.com>
2023-09-08 14:16:53 -0500

    Adds wheel smoke test scrip for nx-cugraph.

---
+++ ci/wheel_smoke_test_nx-cugraph.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2023, NVIDIA CORPORATION.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+
+import networkx as nx
+import nx_cugraph
+
+
+if __name__ == "__main__":
+    G = nx.Graph()
+    G.add_edges_from([(0, 1), (1, 2), (2, 3)])
+    nx_result = nx.betweenness_centrality(G)
+    nxcu_result = nx_cugraph.betweenness_centrality(G)
+
+    nx_nodes = nx_result.keys()
+    nxcu_nodes = nxcu_result.keys()
+    assert nxcu_nodes == nx_nodes
+    for node_id in nx_nodes:
+        nx_bc = nx_result[node_id]
+        nxcu_bc = nxcu_result[node_id]
+        assert math.isclose(nx_bc, nxcu_bc, rel_tol=1e-7), \
+            f"bc for {node_id=} are not close enough: {nx_bc=}, {nxcu_bc=}"
