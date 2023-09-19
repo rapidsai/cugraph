b2e85bff39a411d02f5a167f7bfab376ae9ccb67
Tingyu Wang <tingyuw@nvidia.com>
2023-09-19 13:33:01 -0400

    Update `cugraph-dgl` conv layers to use improved graph class (#3849)
    This PR:
    - Removes the usage of the deprecated `StaticCSC` and `SampledCSC`
    - Support creating CSR and storing edge information in SparseGraph
    - clean up unit tests
    - Adds GATv2Conv layer
    - Adds `pylibcugraphops` as a dependency of `cugraph-dgl` conda package
    
    Authors:
      - Tingyu Wang (https://github.com/tingyu66)
    
    Approvers:
      - Jake Awe (https://github.com/AyodeAwe)
      - Vibhu Jawa (https://github.com/VibhuJawa)
      - Brad Rees (https://github.com/BradReesWork)
    
    URL: https://github.com/rapidsai/cugraph/pull/3849

--- python/cugraph-dgl/cugraph_dgl/nn/conv/gatv2conv.py
+++ python/cugraph-dgl/cugraph_dgl/nn/conv/gatv2conv.py
@@ -22,20 +22,23 @@ nn = import_optional("torch.nn")
 ops_torch = import_optional("pylibcugraphops.pytorch")
 
 
-class GATConv(BaseConv):
-    r"""Graph attention layer from `Graph Attention Network
-    <https://arxiv.org/pdf/1710.10903.pdf>`__, with the sparse aggregation
+class GATv2Conv(BaseConv):
+    r"""GATv2 from `How Attentive are Graph Attention Networks?
+    <https://arxiv.org/pdf/2105.14491.pdf>`__, with the sparse aggregation
     accelerated by cugraph-ops.
 
     Parameters
     ----------
-    in_feats : int or tuple
-        Input feature size. A pair denotes feature sizes of source and
-        destination nodes.
+    in_feats : int, or pair of ints
+        Input feature size; i.e, the number of dimensions of :math:`h_i^{(l)}`.
+        If the layer is to be applied to a unidirectional bipartite graph, `in_feats`
+        specifies the input feature size on both the source and destination nodes.
+        If a scalar is given, the source and destination node feature size
+        would take the same value.
     out_feats : int
-        Output feature size.
+        Output feature size; i.e, the number of dimensions of :math:`h_i^{(l+1)}`.
     num_heads : int
-        Number of heads in multi-head attention.
+        Number of heads in Multi-Head Attention.
     feat_drop : float, optional
         Dropout rate on feature. Defaults: ``0``.
     concat : bool, optional
@@ -55,39 +58,12 @@ class GATConv(BaseConv):
         input graph. By setting ``True``, it will suppress the check and let the
         users handle it by themselves. Defaults: ``False``.
     bias : bool, optional
-        If True, learns a bias term. Defaults: ``True``.
-
-    Examples
-    --------
-    >>> import dgl
-    >>> import torch
-    >>> from cugraph_dgl.nn import GATConv
-    ...
-    >>> device = 'cuda'
-    >>> g = dgl.graph(([0,1,2,3,2,5], [1,2,3,4,0,3])).to(device)
-    >>> g = dgl.add_self_loop(g)
-    >>> feat = torch.ones(6, 10).to(device)
-    >>> conv = GATConv(10, 2, num_heads=3).to(device)
-    >>> res = conv(g, feat)
-    >>> res
-    tensor([[[ 0.2340,  1.9226],
-            [ 1.6477, -1.9986],
-            [ 1.1138, -1.9302]],
-            [[ 0.2340,  1.9226],
-            [ 1.6477, -1.9986],
-            [ 1.1138, -1.9302]],
-            [[ 0.2340,  1.9226],
-            [ 1.6477, -1.9986],
-            [ 1.1138, -1.9302]],
-            [[ 0.2340,  1.9226],
-            [ 1.6477, -1.9986],
-            [ 1.1138, -1.9302]],
-            [[ 0.2340,  1.9226],
-            [ 1.6477, -1.9986],
-            [ 1.1138, -1.9302]],
-            [[ 0.2340,  1.9226],
-            [ 1.6477, -1.9986],
-            [ 1.1138, -1.9302]]], device='cuda:0', grad_fn=<ViewBackward0>)
+        If set to :obj:`False`, the layer will not learn
+        an additive bias. (default: :obj:`True`)
+    share_weights : bool, optional
+        If set to :obj:`True`, the same matrix for :math:`W_{left}` and
+        :math:`W_{right}` in the above equations, will be applied to the source
+        and the target node of every edge. (default: :obj:`False`)
     """
 
     def __init__(
@@ -102,6 +78,7 @@ class GATConv(BaseConv):
         residual: bool = False,
         allow_zero_in_degree: bool = False,
         bias: bool = True,
+        share_weights: bool = False,
     ):
         super().__init__()
         self.in_feats = in_feats
@@ -113,23 +90,28 @@ class GATConv(BaseConv):
         self.edge_feats = edge_feats
         self.negative_slope = negative_slope
         self.allow_zero_in_degree = allow_zero_in_degree
+        self.share_weights = share_weights
 
-        if isinstance(in_feats, int):
-            self.lin = nn.Linear(in_feats, num_heads * out_feats, bias=False)
+        self.lin_src = nn.Linear(self.in_feats_src, num_heads * out_feats, bias=bias)
+        if share_weights:
+            if self.in_feats_src != self.in_feats_dst:
+                raise ValueError(
+                    f"Input feature size of source and destination "
+                    f"nodes must be identical when share_weights is enabled, "
+                    f"but got {self.in_feats_src} and {self.in_feats_dst}."
+                )
+            self.lin_dst = self.lin_src
         else:
-            self.lin_src = nn.Linear(
-                self.in_feats_src, num_heads * out_feats, bias=False
-            )
             self.lin_dst = nn.Linear(
-                self.in_feats_dst, num_heads * out_feats, bias=False
+                self.in_feats_dst, num_heads * out_feats, bias=bias
             )
+
+        self.attn = nn.Parameter(torch.Tensor(num_heads * out_feats))
 
         if edge_feats is not None:
             self.lin_edge = nn.Linear(edge_feats, num_heads * out_feats, bias=False)
-            self.attn_weights = nn.Parameter(torch.Tensor(3 * num_heads * out_feats))
         else:
             self.register_parameter("lin_edge", None)
-            self.attn_weights = nn.Parameter(torch.Tensor(2 * num_heads * out_feats))
 
         if bias and concat:
             self.bias = nn.Parameter(torch.Tensor(num_heads, out_feats))
@@ -151,14 +133,11 @@ class GATConv(BaseConv):
     def reset_parameters(self):
         r"""Reinitialize learnable parameters."""
         gain = nn.init.calculate_gain("relu")
-        if hasattr(self, "lin"):
-            nn.init.xavier_normal_(self.lin.weight, gain=gain)
-        else:
-            nn.init.xavier_normal_(self.lin_src.weight, gain=gain)
-            nn.init.xavier_normal_(self.lin_dst.weight, gain=gain)
+        nn.init.xavier_normal_(self.lin_src.weight, gain=gain)
+        nn.init.xavier_normal_(self.lin_dst.weight, gain=gain)
 
         nn.init.xavier_normal_(
-            self.attn_weights.view(-1, self.num_heads, self.out_feats), gain=gain
+            self.attn.view(-1, self.num_heads, self.out_feats), gain=gain
         )
         if self.lin_edge is not None:
             self.lin_edge.reset_parameters()
@@ -199,6 +178,7 @@ class GATConv(BaseConv):
             :math:`H` is the number of heads, and :math:`D_{out}` is size of
             output feature.
         """
+
         if isinstance(g, dgl.DGLHeteroGraph):
             if not self.allow_zero_in_degree:
                 if (g.in_degrees() == 0).any():
@@ -214,13 +194,14 @@ class GATConv(BaseConv):
                         "suppress the check and let the code run."
                     )
 
-        bipartite = isinstance(nfeat, (list, tuple))
+        nfeat_bipartite = isinstance(nfeat, (list, tuple))
+        graph_bipartite = nfeat_bipartite or self.share_weights is False
 
         _graph = self.get_cugraph_ops_CSC(
-            g, is_bipartite=bipartite, max_in_degree=max_in_degree
+            g, is_bipartite=graph_bipartite, max_in_degree=max_in_degree
         )
 
-        if bipartite:
+        if nfeat_bipartite:
             nfeat = (self.feat_drop(nfeat[0]), self.feat_drop(nfeat[1]))
             nfeat_dst_orig = nfeat[1]
         else:
@@ -235,26 +216,16 @@ class GATConv(BaseConv):
                 )
             efeat = self.lin_edge(efeat)
 
-        if bipartite:
-            if not hasattr(self, "lin_src"):
-                raise RuntimeError(
-                    f"{self.__class__.__name__}.in_feats must be a pair of "
-                    f"integers to allow bipartite node features, but got "
-                    f"{self.in_feats}."
-                )
-            nfeat_src = self.lin_src(nfeat[0])
-            nfeat_dst = self.lin_dst(nfeat[1])
+        if nfeat_bipartite:
+            nfeat = (self.lin_src(nfeat[0]), self.lin_dst(nfeat[1]))
+        elif graph_bipartite:
+            nfeat = (self.lin_src(nfeat), self.lin_dst(nfeat[: g.num_dst_nodes()]))
         else:
-            if not hasattr(self, "lin"):
-                raise RuntimeError(
-                    f"{self.__class__.__name__}.in_feats is expected to be an "
-                    f"integer, but got {self.in_feats}."
-                )
-            nfeat = self.lin(nfeat)
+            nfeat = self.lin_src(nfeat)
 
-        out = ops_torch.operators.mha_gat_n2n(
-            (nfeat_src, nfeat_dst) if bipartite else nfeat,
-            self.attn_weights,
+        out = ops_torch.operators.mha_gat_v2_n2n(
+            nfeat,
+            self.attn,
             _graph,
             num_heads=self.num_heads,
             activation="LeakyReLU",
