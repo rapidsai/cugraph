c458918868dae9792d093cb0037a7890b58cacac
Rick Ratzel <rratzel@nvidia.com>
2023-09-27 23:34:37 -0500

    Removed unused imports, adds comment describing fixture args (ids, etc.), consolidates small/medium/large datasets and fixtures into a single set that uses markers instead.

--- benchmarks/nx-cugraph/pytest-based/bench_algos.py
+++ benchmarks/nx-cugraph/pytest-based/bench_algos.py
@@ -11,15 +11,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import time
-
 import networkx as nx
 import pandas as pd
 import pytest
 from cugraph import datasets
 
-# FIXME: promote these to cugraph.datasets
+# FIXME: promote these to cugraph.datasets so the following steps aren't
+# necessary
+#
+# These datasets can be downloaded using the script in the 'datasets' dir:
+#
+# cd <repo dir>/datasets
+# ./get_test_data.sh --benchmark
+#
+# Then set the following env var so the dataset utils can find their location:
+#
+# export RAPIDS_DATASET_ROOT_DIR=<repo dir>/datasets
+#
 from cugraph_benchmarking.params import (
     hollywood,
     europe_osm,
@@ -31,16 +39,18 @@ from cugraph_benchmarking.params import (
 # Fixtures and helpers
 backend_params = ["cugraph", None]
 
-small_dataset_params = [datasets.karate,
-                        datasets.netscience,
-                        datasets.email_Eu_core,
-                        ]
-medium_dataset_params = [cit_patents,
-                         hollywood,
-                         europe_osm,
-                         ]
-large_dataset_params = [soc_livejournal,
-                        ]
+dataset_params = [
+    pytest.param(datasets.karate, marks=[pytest.mark.small, pytest.mark.undirected]),
+    pytest.param(datasets.netscience, marks=[pytest.mark.small, pytest.mark.directed]),
+    pytest.param(
+        datasets.email_Eu_core, marks=[pytest.mark.small, pytest.mark.directed]
+    ),
+    pytest.param(cit_patents, marks=[pytest.mark.medium, pytest.mark.directed]),
+    pytest.param(hollywood, marks=[pytest.mark.medium, pytest.mark.undirected]),
+    pytest.param(europe_osm, marks=[pytest.mark.medium, pytest.mark.undirected]),
+    pytest.param(soc_livejournal, marks=[pytest.mark.large, pytest.mark.directed]),
+]
+
 
 def nx_graph_from_dataset(dataset_obj):
     """
@@ -55,36 +65,27 @@ def nx_graph_from_dataset(dataset_obj):
     else:
         header = None
 
-    pandas_edgelist = pd.read_csv(dataset_obj.get_path(),
-                                  delimiter=dataset_obj.metadata["delim"],
-                                  names=names,
-                                  dtype=dict(zip(names, dtypes)),
-                                  header=header,
-                                  )
-    G = nx.from_pandas_edgelist(pandas_edgelist,
-                                source=names[0],
-                                target=names[1],
-                                create_using=create_using)
+    pandas_edgelist = pd.read_csv(
+        dataset_obj.get_path(),
+        delimiter=dataset_obj.metadata["delim"],
+        names=names,
+        dtype=dict(zip(names, dtypes)),
+        header=header,
+    )
+    G = nx.from_pandas_edgelist(
+        pandas_edgelist, source=names[0], target=names[1], create_using=create_using
+    )
     return G
 
 
-@pytest.fixture(scope="module", params=small_dataset_params,
-                ids=lambda ds: f"ds={str(ds)}")
-def small_graph_obj(request):
-    dataset = request.param
-    return nx_graph_from_dataset(dataset)
-
-
-@pytest.fixture(scope="module", params=medium_dataset_params,
-                ids=lambda ds: f"ds={str(ds)}")
-def medium_graph_obj(request):
-    dataset = request.param
-    return nx_graph_from_dataset(dataset)
-
-
-@pytest.fixture(scope="module", params=large_dataset_params,
-                ids=lambda ds: f"ds={str(ds)}")
-def large_graph_obj(request):
+# Test IDs are generated using the lambda assigned to the ids arg to provide an
+# easier-to-read name from the Dataset obj string repr.
+# See: https://docs.pytest.org/en/stable/reference/reference.html#pytest-fixture
+@pytest.fixture(scope="module", params=dataset_params, ids=lambda ds: f"ds={str(ds)}")
+def graph_obj(request):
+    """
+    Returns a NX Graph or DiGraph obj from the dataset instance parameter.
+    """
     dataset = request.param
     return nx_graph_from_dataset(dataset)
 
@@ -94,6 +95,7 @@ def large_graph_obj(request):
 # for all NX versions
 try:
     from networkx.classes import backends  # NX <3.2
+
     _using_legacy_dispatcher = True
 except ImportError:
     backends = None
@@ -125,10 +127,13 @@ def get_legacy_backend_selector(backend_name):
     # can be reapplied multiple times. This is obviously a hack and networkx
     # >=3.2 makes this use case properly supported.
     if backend_name == "cugraph":
+
         def wrapper(*args, **kwargs):
             backends._registered_algorithms = {}
             return testing_dispatch(*args, **kwargs)
+
     else:
+
         def wrapper(*args, **kwargs):
             backends._registered_algorithms = {}
             return orig_dispatch(*args, **kwargs)
@@ -142,17 +147,20 @@ def get_backend_selector(backend_name):
     "backend" kwarg on it.
     This is only supported for NetworkX >= 3.2
     """
+
     def get_callable_for_func(func):
         def wrapper(*args, **kwargs):
             kwargs["backend"] = backend_name
             return func(*args, **kwargs)
+
         return wrapper
 
     return get_callable_for_func
 
 
-@pytest.fixture(scope="module", params=backend_params,
-                ids=lambda backend: f"backend={backend}")
+@pytest.fixture(
+    scope="module", params=backend_params, ids=lambda backend: f"backend={backend}"
+)
 def backend_selector(request):
     """
     Returns a callable that takes a function algo and wraps it in another
@@ -172,64 +180,34 @@ k_params = [10, 100, 1000]
 
 
 @pytest.mark.parametrize("normalized", normalized_params, ids=lambda norm: f"{norm=}")
-def bench_betweenness_centrality_small(benchmark,
-                                       small_graph_obj,
-                                       backend_selector,
-                                       normalized):
-    result = benchmark(backend_selector(nx.betweenness_centrality),
-                       small_graph_obj, weight=None, normalized=normalized)
+def bench_betweenness_centrality(benchmark, graph_obj, backend_selector, normalized):
+    result = benchmark(
+        backend_selector(nx.betweenness_centrality),
+        graph_obj,
+        weight=None,
+        normalized=normalized,
+    )
     assert type(result) is dict
 
 
 @pytest.mark.parametrize("normalized", normalized_params, ids=lambda norm: f"{norm=}")
-def bench_edge_betweenness_centrality_small(benchmark,
-                                            small_graph_obj,
-                                            backend_selector,
-                                            normalized):
-    result = benchmark(backend_selector(nx.edge_betweenness_centrality),
-                       small_graph_obj, weight=None, normalized=normalized)
+def bench_edge_betweenness_centrality(
+    benchmark, graph_obj, backend_selector, normalized
+):
+    result = benchmark(
+        backend_selector(nx.edge_betweenness_centrality),
+        graph_obj,
+        weight=None,
+        normalized=normalized,
+    )
     assert type(result) is dict
 
 
-@pytest.mark.parametrize("k", k_params, ids=lambda k: f"{k=}")
-def bench_betweenness_centrality_medium(benchmark,
-                                        medium_graph_obj,
-                                        backend_selector,
-                                        k):
-    result = benchmark(backend_selector(nx.betweenness_centrality),
-                       medium_graph_obj, weight=None, k=k)
-    assert type(result) is dict
-
-
-@pytest.mark.parametrize("k", k_params, ids=lambda k: f"{k=}")
-def bench_edge_betweenness_centrality_medium(benchmark,
-                                             medium_graph_obj,
-                                             backend_selector,
-                                             k):
-    result = benchmark(backend_selector(nx.edge_betweenness_centrality),
-                       medium_graph_obj, weight=None, k=k)
-    assert type(result) is dict
-
-
-def bench_louvain_communities_small(benchmark,
-                                    small_graph_obj,
-                                    backend_selector):
+def bench_louvain_communities(benchmark, graph_obj, backend_selector):
     # The cugraph backend for louvain_communities only supports undirected graphs
-    if isinstance(small_graph_obj, nx.DiGraph):
-        G = small_graph_obj.to_undirected()
+    if isinstance(graph_obj, nx.DiGraph):
+        G = graph_obj.to_undirected()
     else:
-        G = small_graph_obj
-    result = benchmark(backend_selector(nx.community.louvain_communities), G)
-    assert type(result) is list
-
-
-def bench_louvain_communities_medium(benchmark,
-                                     medium_graph_obj,
-                                     backend_selector):
-    # The cugraph backend for louvain_communities only supports undirected graphs
-    if isinstance(medium_graph_obj, nx.DiGraph):
-        G = medium_graph_obj.to_undirected()
-    else:
-        G = medium_graph_obj
+        G = graph_obj
     result = benchmark(backend_selector(nx.community.louvain_communities), G)
     assert type(result) is list
