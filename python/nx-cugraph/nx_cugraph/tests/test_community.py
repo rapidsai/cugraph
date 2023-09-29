07b90d17877aebcc09e8c0de437473596a4dbd9c
Erik Welch <erik.n.welch@gmail.com>
2023-09-29 06:35:53 -0500

    fix test by handling difference in louvain in networkx <3.2

--- python/nx-cugraph/nx_cugraph/tests/test_community.py
+++ python/nx-cugraph/nx_cugraph/tests/test_community.py
@@ -11,25 +11,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import networkx as nx
+import pytest
 
 import nx_cugraph as nxcg
 
 
 def test_louvain_isolated_nodes():
+    is_nx_30_or_31 = hasattr(nx.classes, "backends")
+
     def check(left, right):
         assert len(left) == len(right)
         assert set(map(frozenset, left)) == set(map(frozenset, right))
 
     # Empty graph (no nodes)
     G = nx.Graph()
-    nx_result = nx.community.louvain_communities(G)
-    cg_result = nxcg.community.louvain_communities(G)
-    check(nx_result, cg_result)
+    if is_nx_30_or_31:
+        with pytest.raises(ZeroDivisionError):
+            nx.community.louvain_communities(G)
+    else:
+        nx_result = nx.community.louvain_communities(G)
+        cg_result = nxcg.community.louvain_communities(G)
+        check(nx_result, cg_result)
     # Graph with no edges
     G.add_nodes_from(range(5))
-    nx_result = nx.community.louvain_communities(G)
-    cg_result = nxcg.community.louvain_communities(G)
-    check(nx_result, cg_result)
+    if is_nx_30_or_31:
+        with pytest.raises(ZeroDivisionError):
+            nx.community.louvain_communities(G)
+    else:
+        nx_result = nx.community.louvain_communities(G)
+        cg_result = nxcg.community.louvain_communities(G)
+        check(nx_result, cg_result)
     # Graph with isolated nodes
     G.add_edge(1, 2)
     nx_result = nx.community.louvain_communities(G)
