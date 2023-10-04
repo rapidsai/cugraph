26af14ebad6a6b1f115779d90d3c0a68f0d380ee
Alex Barghi <105237337+alexbarghi-nv@users.noreply.github.com>
2023-10-04 11:44:01 -0400

    cuGraph-PyG MFG Creation and Conversion (#3873)
    Integrates the new CSR bulk sampler output, allowing reading of batches without having to call CSC conversion or count the numbers of vertices and edges in each batch.  Should result in major performance improvements, especially for small batches.
    
    Authors:
      - Alex Barghi (https://github.com/alexbarghi-nv)
      - Seunghwa Kang (https://github.com/seunghwak)
      - Brad Rees (https://github.com/BradReesWork)
    
    Approvers:
      - Brad Rees (https://github.com/BradReesWork)
      - Ray Douglass (https://github.com/raydouglass)
      - Tingyu Wang (https://github.com/tingyu66)
    
    URL: https://github.com/rapidsai/cugraph/pull/3873

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -200,8 +200,11 @@ if [[ "${RAPIDS_CUDA_VERSION}" == "11.8.0" ]]; then
       --channel pytorch \
       --channel nvidia \
       'pyg=2.3' \
-      'pytorch>=2.0' \
-      'pytorch-cuda>=11.8'
+      'pytorch=2.0.0' \
+      'pytorch-cuda=11.8'
+    
+    # Install pyg dependencies (which requires pip)
+    pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cu118.html
 
     rapids-mamba-retry install \
       --channel "${CPP_CHANNEL}" \
