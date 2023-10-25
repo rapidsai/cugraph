b586d9514106034303b90877cdc67744389d0172
divyegala <divyegala@gmail.com>
2023-10-25 15:29:31 -0700

    import missing package

--- python/nx-cugraph/setup.py
+++ python/nx-cugraph/setup.py
@@ -10,7 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from setuptools import setup
+from setuptools import find_packages, setup
 
 packages = find_packages(include=["nx_cugraph*"])
 setup(
