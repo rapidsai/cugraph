dfaf556142b4467dd52918f7238bfca9dc951d21
divyegala <divyegala@gmail.com>
2023-11-01 12:54:50 -0700

    clarify comments more

--- python/cugraph-pyg/cugraph_pyg/_version.py
+++ python/cugraph-pyg/cugraph_pyg/_version.py
@@ -17,7 +17,9 @@ import importlib.resources
 
 # Read VERSION file from the module that is symlinked to VERSION file
 # in the root of the repo at build time or copied to the moudle at
-# installation
+# installation. VERSION is a separate file that allows CI build-time scripts
+# to update version info (including commit hashes) without modifying 
+# source files.
 __version__ = (
     importlib.resources.files("cugraph_pyg").joinpath("VERSION").read_text().strip()
 )
