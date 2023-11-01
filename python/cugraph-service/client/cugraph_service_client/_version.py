1ad1ef2dc6b32a761bb71c067320019d17eba66f
divyegala <divyegala@gmail.com>
2023-11-01 13:02:36 -0700

    style fix

--- python/cugraph-service/client/cugraph_service_client/_version.py
+++ python/cugraph-service/client/cugraph_service_client/_version.py
@@ -18,7 +18,7 @@ import importlib.resources
 # Read VERSION file from the module that is symlinked to VERSION file
 # in the root of the repo at build time or copied to the moudle at
 # installation. VERSION is a separate file that allows CI build-time scripts
-# to update version info (including commit hashes) without modifying 
+# to update version info (including commit hashes) without modifying
 # source files.
 __version__ = (
     importlib.resources.files("cugraph_service_client")
