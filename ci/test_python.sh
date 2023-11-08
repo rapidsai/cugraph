0f4693cd793131ea101930b5ec2063815e191828
Alexandria Barghi <abarghi@nvidia.com>
2023-11-08 10:25:34 -0800

    attempt to fix package resolution issue

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -200,15 +200,21 @@ if [[ "${RAPIDS_CUDA_VERSION}" == "11.8.0" ]]; then
     # Install pytorch
     rapids-mamba-retry install \
       --force-reinstall \
-      --channel pyg \
       --channel pytorch \
       --channel nvidia \
-      'pyg=2.3' \
-      'pytorch=2.0.0' \
+      --channel conda-forge \
+      'pytorch=2.1.0' \
       'pytorch-cuda=11.8'
     
     # Install pyg dependencies (which requires pip)
-    pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cu118.html
+    pip install \
+        torch_geometric==2.4.0
+        pyg_lib \
+        torch_scatter \
+        torch_sparse \
+        torch_cluster \
+        torch_spline_conv \
+      -f https://data.pyg.org/whl/torch-2.1.0+cu118.html
 
     rapids-mamba-retry install \
       --channel "${CPP_CHANNEL}" \
