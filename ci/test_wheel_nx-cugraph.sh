e7159ff90d1bdfae0e185ef5516b3e426baaa4f1
Rick Ratzel <rratzel@nvidia.com>
2023-11-17 11:28:23 -0600

    Downloads pylibcugraph artifact from S3 and installs it.

--- ci/test_wheel_nx-cugraph.sh
+++ ci/test_wheel_nx-cugraph.sh
@@ -3,4 +3,9 @@
 
 set -eoxu pipefail
 
+# Download the pylibcugraph built in the previous step
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+RAPIDS_PY_WHEEL_NAME="pylibcugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-pylibcugraph-dep
+python -m pip install --no-deps ./local-pylibcugraph-dep/pylibcugraph*.whl
+
 ./ci/test_wheel.sh nx-cugraph python/nx-cugraph
