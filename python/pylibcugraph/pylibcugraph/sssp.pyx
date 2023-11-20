0ceeb4f2b735b0d8ca2bc916efdc1682b58b51da
jnke2016 <jnke2016@gmail.com>
2023-11-20 05:16:27 -0800

    update doctests

--- python/pylibcugraph/pylibcugraph/sssp.pyx
+++ python/pylibcugraph/pylibcugraph/sssp.pyx
@@ -109,7 +109,7 @@ def sssp(ResourceHandle resource_handle,
     >>> graph_props = pylibcugraph.GraphProperties(
     ...     is_symmetric=False, is_multigraph=False)
     >>> G = pylibcugraph.SGGraph(
-    ...     resource_handle, graph_props, srcs, dsts, weights,
+    ...     resource_handle, graph_props, srcs, dsts, weight_array=weights,
     ...     store_transposed=False, renumber=False, do_expensive_check=False)
     >>> (vertices, distances, predecessors) = pylibcugraph.sssp(
     ...     resource_handle, G, source=1, cutoff=999,
