3f1c7b5ef8cd296aa5ad012cf855abdd2dfeb84b
Chuck Hastings <45364586+ChuckHastings@users.noreply.github.com>
2023-11-20 21:17:24 -0500

    Update C API graph creation function signatures (#3982)
    Updating the C API graph creation functions to support the following:
    * Add support for isolated vertices
    * Add MG optimization to support multiple device arrays per rank as input and concatenate them internally
    * Add MG optimization to internally compute the number of edges via allreduce rather than requiring it as an input parameter (this can be expensive to compute in python)
    
    This PR implements these features.  Some simple tests exist to check for isolate vertices (by running pagerank which generates a different result if the graph has isolated vertices).  A simple test for multiple input arrays exists for the MG case.
    
    Closes #3947 
    Closes #3974
    
    Authors:
      - Chuck Hastings (https://github.com/ChuckHastings)
      - Naim (https://github.com/naimnv)
    
    Approvers:
      - Naim (https://github.com/naimnv)
      - Joseph Nke (https://github.com/jnke2016)
      - Seunghwa Kang (https://github.com/seunghwak)
    
    URL: https://github.com/rapidsai/cugraph/pull/3982

--- cpp/src/structure/detail/structure_utils.cuh
+++ cpp/src/structure/detail/structure_utils.cuh
@@ -21,6 +21,7 @@
 #include <cugraph/utilities/device_functors.cuh>
 #include <cugraph/utilities/error.hpp>
 #include <cugraph/utilities/misc_utils.cuh>
+#include <cugraph/utilities/packed_bool_utils.hpp>
 
 #include <raft/core/handle.hpp>
 #include <raft/util/device_atomics.cuh>
@@ -33,6 +34,7 @@
 #include <thrust/for_each.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
+#include <thrust/iterator/transform_output_iterator.h>
 #include <thrust/iterator/zip_iterator.h>
 #include <thrust/remove.h>
 #include <thrust/scan.h>
@@ -496,6 +498,63 @@ void sort_adjacency_list(raft::handle_t const& handle,
   }
 }
 
-}  // namespace detail
+template <typename comparison_t>
+std::tuple<size_t, rmm::device_uvector<uint32_t>> mark_entries(raft::handle_t const& handle,
+                                                               size_t num_entries,
+                                                               comparison_t comparison)
+{
+  rmm::device_uvector<uint32_t> marked_entries(cugraph::packed_bool_size(num_entries),
+                                               handle.get_stream());
 
+  thrust::tabulate(handle.get_thrust_policy(),
+                   marked_entries.begin(),
+                   marked_entries.end(),
+                   [comparison, num_entries] __device__(size_t idx) {
+                     auto word          = cugraph::packed_bool_empty_mask();
+                     size_t start_index = idx * cugraph::packed_bools_per_word();
+                     size_t bits_in_this_word =
+                       (start_index + cugraph::packed_bools_per_word() < num_entries)
+                         ? cugraph::packed_bools_per_word()
+                         : (num_entries - start_index);
+
+                     for (size_t bit = 0; bit < bits_in_this_word; ++bit) {
+                       if (comparison(start_index + bit)) word |= cugraph::packed_bool_mask(bit);
+                     }
+
+                     return word;
+                   });
+
+  size_t bit_count = thrust::transform_reduce(
+    handle.get_thrust_policy(),
+    marked_entries.begin(),
+    marked_entries.end(),
+    [] __device__(auto word) { return __popc(word); },
+    size_t{0},
+    thrust::plus<size_t>());
+
+  return std::make_tuple(bit_count, std::move(marked_entries));
+}
+
+template <typename T>
+rmm::device_uvector<T> remove_flagged_elements(raft::handle_t const& handle,
+                                               rmm::device_uvector<T>&& vector,
+                                               raft::device_span<uint32_t const> remove_flags,
+                                               size_t remove_count)
+{
+  rmm::device_uvector<T> result(vector.size() - remove_count, handle.get_stream());
+
+  thrust::copy_if(
+    handle.get_thrust_policy(),
+    thrust::make_counting_iterator(size_t{0}),
+    thrust::make_counting_iterator(vector.size()),
+    thrust::make_transform_output_iterator(result.begin(),
+                                           indirection_t<size_t, T*>{vector.data()}),
+    [remove_flags] __device__(size_t i) {
+      return !(remove_flags[cugraph::packed_bool_offset(i)] & cugraph::packed_bool_mask(i));
+    });
+
+  return result;
+}
+
+}  // namespace detail
 }  // namespace cugraph
