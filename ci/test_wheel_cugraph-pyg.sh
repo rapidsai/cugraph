7d784c851d378a2c6d6a670b962a3bde9014969a
Tingyu Wang <tingyuw@nvidia.com>
2024-01-08 17:25:25 -0500

    set dataset dir env variable

--- ci/test_wheel_cugraph-pyg.sh
+++ ci/test_wheel_cugraph-pyg.sh
@@ -16,6 +16,8 @@ RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-
 # pip creates wheels using python package names
 python -m pip install $(ls ./dist/${python_package_name}*.whl)[test]
 
+# RAPIDS_DATASET_ROOT_DIR is used by test scripts
+export RAPIDS_DATASET_ROOT_DIR="$(realpath datasets)"
 
 if [[ "${CUDA_VERSION}" == "11.8.0" ]]; then
   rapids-logger "Installing PyTorch and PyG dependencies"
@@ -23,19 +25,19 @@ if [[ "${CUDA_VERSION}" == "11.8.0" ]]; then
   rapids-retry python -m pip install torch==2.1.0 --index-url ${PYTORCH_URL}
   rapids-retry python -m pip install torch-geometric==2.4.0
   rapids-retry python -m pip install \
-        pyg_lib \
-        torch_scatter \
-        torch_sparse \
-        torch_cluster \
-        torch_spline_conv \
-      -f https://data.pyg.org/whl/torch-2.1.0+cu118.html
+    pyg_lib \
+    torch_scatter \
+    torch_sparse \
+    torch_cluster \
+    torch_spline_conv \
+    -f https://data.pyg.org/whl/torch-2.1.0+cu118.html
 
   rapids-logger "pytest cugraph-pyg (single GPU)"
   python -m pytest \
-      --cache-clear \
-      --ignore=tests/int \
-      --ignore=tests/mg \
-       python/cugraph-pyg/cugraph_pyg/tests
+    --cache-clear \
+    --ignore=tests/int \
+    --ignore=tests/mg \
+    python/cugraph-pyg/cugraph_pyg/tests
 else
   rapids-logger "skipping cugraph-pyg wheel test on CUDA!=11.8"
 fi
