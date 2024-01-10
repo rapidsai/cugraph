1f4077adafd1fbb7e842ec103730953b2051dad9
Tingyu Wang <tingyuw@nvidia.com>
2024-01-10 17:07:09 -0500

    year

--- conda/recipes/cugraph-equivariant/build.sh
+++ conda/recipes/cugraph-equivariant/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2024, NVIDIA CORPORATION.
 
 # This assumes the script is executed from the root of the repo directory
 
