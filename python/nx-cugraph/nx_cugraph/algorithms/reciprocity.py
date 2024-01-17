c7de9aba6810136811955d1db2abf3e6bb882496
Rick Ratzel <rratzel@nvidia.com>
2024-01-17 11:29:13 -0600

    Adds comments.

--- python/nx-cugraph/nx_cugraph/algorithms/reciprocity.py
+++ python/nx-cugraph/nx_cugraph/algorithms/reciprocity.py
@@ -21,12 +21,13 @@ __all__ = ["reciprocity", "overall_reciprocity"]
 
 
 @not_implemented_for("undirected", "multigraph")
-@networkx_algorithm
+@networkx_algorithm(version_added="24.02")
 def reciprocity(G, nodes=None):
     if nodes is None:
         return overall_reciprocity(G)
     G = _to_directed_graph(G)
     N = G._N
+    # 'nodes' can also be a single node identifier
     if nodes in G:
         index = nodes if G.key_to_id is None else G.key_to_id[nodes]
         mask = (G.src_indices == index) | (G.dst_indices == index)
@@ -34,25 +35,31 @@ def reciprocity(G, nodes=None):
         if src_indices.size == 0:
             raise nx.NetworkXError("Not defined for isolated nodes.")
         dst_indices = G.dst_indices[mask]
-        # Upcast to int64 so indices don't overflow
-        flat_indices = N * src_indices.astype(np.int64) + dst_indices
-        recip_indices = cp.intersect1d(
-            flat_indices,
-            src_indices + N * dst_indices.astype(np.int64),
+        # Create two lists of edge identifiers, one for each direction.
+        # Edge identifiers can be created from a pair of node
+        # identifiers. Simply adding src IDs to dst IDs is not adequate, so
+        # make one set of values (either src or dst depending on direction)
+        # unique by multiplying values by N.
+        # Upcast to int64 so indices don't overflow.
+        edges_a_b = N * src_indices.astype(np.int64) + dst_indices
+        edges_b_a = src_indices + N * dst_indices.astype(np.int64)
+        # Find the matching edge identifiers in each list. The edge identifier
+        # generation ensures the ID for A->B == the ID for B->A
+        recip_indices = cp.intersect1d(edges_a_b, edges_b_a,
             # assume_unique=True,  # cupy <= 12.2.0 also assumes sorted
         )
         num_selfloops = (src_indices == dst_indices).sum().tolist()
-        return (recip_indices.size - num_selfloops) / flat_indices.size
+        return (recip_indices.size - num_selfloops) / edges_a_b.size
 
     # Don't include self-loops
     mask = G.src_indices != G.dst_indices
     src_indices = G.src_indices[mask]
     dst_indices = G.dst_indices[mask]
-    # Upcast to int64 so indices don't overflow
-    flat_indices = N * src_indices.astype(np.int64) + dst_indices
-    recip_indices = cp.intersect1d(
-        flat_indices,
-        src_indices + N * dst_indices.astype(np.int64),
+    # Create two lists of edges, one for each direction, and find the matching
+    # IDs in each list (see description above).
+    edges_a_b = N * src_indices.astype(np.int64) + dst_indices
+    edges_b_a = src_indices + N * dst_indices.astype(np.int64)
+    recip_indices = cp.intersect1d(edges_a_b, edges_b_a,
         # assume_unique=True,  # cupy <= 12.2.0 also assumes sorted
     )
     numer = cp.bincount(recip_indices // N, minlength=N)
@@ -69,12 +76,12 @@ def overall_reciprocity(G):
     G = _to_directed_graph(G)
     if G.number_of_edges() == 0:
         raise nx.NetworkXError("Not defined for empty graphs")
-    # Upcast to int64 so indices don't overflow
-    flat_indices = G._N * G.src_indices.astype(np.int64) + G.dst_indices
-    recip_indices = cp.intersect1d(
-        flat_indices,
-        G.src_indices + G._N * G.dst_indices.astype(np.int64),
+    # Create two lists of edges, one for each direction, and find the matching
+    # IDs in each list (see description in reciprocity()).
+    edges_a_b = G._N * G.src_indices.astype(np.int64) + G.dst_indices
+    edges_b_a = G.src_indices + G._N * G.dst_indices.astype(np.int64)
+    recip_indices = cp.intersect1d(edges_a_b, edges_b_a,
         # assume_unique=True,  # cupy <= 12.2.0 also assumes sorted
     )
     num_selfloops = (G.src_indices == G.dst_indices).sum().tolist()
-    return (recip_indices.size - num_selfloops) / flat_indices.size
+    return (recip_indices.size - num_selfloops) / edges_a_b.size
