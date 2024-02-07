217ef47eded505b9c11c6a446d855ebe70e9e0f8
Seunghwa Kang <seunghwak@nvidia.com>
2024-02-07 15:18:06 -0800

    update extract_transform_v_frontier_outgoing_e to support edge masking

--- cpp/tests/prims/mg_extract_transform_v_frontier_outgoing_e.cu
+++ cpp/tests/prims/mg_extract_transform_v_frontier_outgoing_e.cu
@@ -115,6 +115,7 @@ struct e_op_t {
 };
 
 struct Prims_Usecase {
+  bool edge_masking{false};
   bool check_correctness{true};
 };
 
@@ -179,6 +180,13 @@ class Tests_MGExtractTransformVFrontierOutgoingE
     }
 
     auto mg_graph_view = mg_graph.view();
+
+    std::optional<cugraph::edge_property_t<decltype(mg_graph_view), bool>> edge_mask{std::nullopt};
+    if (prims_usecase.edge_masking) {
+      edge_mask =
+        cugraph::test::generate<vertex_t, bool>::edge_property(*handle_, mg_graph_view, 2);
+      mg_graph_view.attach_edge_mask((*edge_mask).view());
+    }
 
     // 2. run MG extract_transform_v_frontier_outgoing_e
 
@@ -458,7 +466,7 @@ INSTANTIATE_TEST_SUITE_P(
   file_test,
   Tests_MGExtractTransformVFrontierOutgoingE_File,
   ::testing::Combine(
-    ::testing::Values(Prims_Usecase{true}),
+    ::testing::Values(Prims_Usecase{false, true}, Prims_Usecase{true, true}),
     ::testing::Values(cugraph::test::File_Usecase("test/datasets/karate.mtx"),
                       cugraph::test::File_Usecase("test/datasets/web-Google.mtx"),
                       cugraph::test::File_Usecase("test/datasets/ljournal-2008.mtx"),
@@ -466,7 +474,8 @@ INSTANTIATE_TEST_SUITE_P(
 
 INSTANTIATE_TEST_SUITE_P(rmat_small_test,
                          Tests_MGExtractTransformVFrontierOutgoingE_Rmat,
-                         ::testing::Combine(::testing::Values(Prims_Usecase{true}),
+                         ::testing::Combine(::testing::Values(Prims_Usecase{false, true},
+                                                              Prims_Usecase{true, true}),
                                             ::testing::Values(cugraph::test::Rmat_Usecase(
                                               10, 16, 0.57, 0.19, 0.19, 0, false, false))));
 
@@ -478,7 +487,7 @@ INSTANTIATE_TEST_SUITE_P(
                           factor (to avoid running same benchmarks more than once) */
   Tests_MGExtractTransformVFrontierOutgoingE_Rmat,
   ::testing::Combine(
-    ::testing::Values(Prims_Usecase{false}),
+    ::testing::Values(Prims_Usecase{false, false}, Prims_Usecase{true, false}),
     ::testing::Values(cugraph::test::Rmat_Usecase(20, 32, 0.57, 0.19, 0.19, 0, false, false))));
 
 CUGRAPH_MG_TEST_PROGRAM_MAIN()
