fc3b380acebb09936dada59ef96bd4f7cd67a5b4
Seunghwa Kang <seunghwak@nvidia.com>
2024-02-07 15:16:43 -0800

    add missing include

--- cpp/include/cugraph/utilities/mask_utils.cuh
+++ cpp/include/cugraph/utilities/mask_utils.cuh
@@ -25,6 +25,7 @@
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/iterator/transform_iterator.h>
 #include <thrust/transform.h>
+#include <thrust/transform_reduce.h>
 
 namespace cugraph {
 
