6b486d4fb85b0c10caacc5c1ba42305036abb1c6
Seunghwa Kang <seunghwak@nvidia.com>
2024-04-01 11:25:19 -0700

    add test debug utilities

---
+++ cpp/tests/utilities/debug_utilities.hpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <cugraph/edge_property.hpp>
+#include <cugraph/graph_view.hpp>
+
+#include <raft/core/handle.hpp>
+#include <raft/core/span.hpp>
+
+#include <string>
+
+namespace cugraph {
+namespace test {
+
+template <typename vertex_t, typename edge_t, typename weight_t, bool store_transposed, bool multi_gpu>
+void print_edges(
+  raft::handle_t const& handle,
+  cugraph::graph_view_t<vertex_t, edge_t, store_transposed, multi_gpu> const& graph_view,
+  std::optional<cugraph::edge_property_view_t<edge_t, weight_t const*>> edge_weight_view,
+  std::optional<raft::device_span<vertex_t const>> renumber_map);
+
+}  // namespace test
+}  // namespace cugraph
