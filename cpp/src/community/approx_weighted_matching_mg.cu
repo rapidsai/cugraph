49fd768d3141c4fb27c35d4829fe1dbf181604d3
Naim <naim@uib.no>
2024-04-15 10:28:56 +0200

    Rename files

--- cpp/src/community/approx_weighted_matching_mg.cu
+++ cpp/src/community/approx_weighted_matching_mg.cu
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "matching_impl.cuh"
+#include "approx_weighted_matching_impl.cuh"
 
 namespace cugraph {
 
