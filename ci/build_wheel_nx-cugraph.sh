2baffb710723bfb0bb00ddd57c84be0310158b5f
Vyas Ramasubramani <vyasr@nvidia.com>
2024-04-16 23:54:05 +0000

    Delete now superfluous build scripts

--- ci/build_wheel_nx-cugraph.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
-
-set -euo pipefail
-
-./ci/build_wheel.sh nx-cugraph python/nx-cugraph
--- ci/build_wheel_pylibcugraph.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
-
-set -euo pipefail
-
-export SKBUILD_CMAKE_ARGS="-DDETECT_CONDA_ENV=OFF;-DFIND_CUGRAPH_CPP=OFF;-DCPM_cugraph-ops_SOURCE=${GITHUB_WORKSPACE}/cugraph-ops/"
-
-./ci/build_wheel.sh pylibcugraph python/pylibcugraph
