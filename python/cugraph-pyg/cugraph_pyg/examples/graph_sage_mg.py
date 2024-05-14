2a672e665f769c5cebedb7ca2d356ac59ed8c512
Alexandria Barghi <abarghi@nvidia.com>
2024-05-14 12:05:09 -0700

    move instructions to python file

--- python/cugraph-pyg/cugraph_pyg/examples/graph_sage_mg.py
+++ python/cugraph-pyg/cugraph_pyg/examples/graph_sage_mg.py
@@ -11,6 +11,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# For this script, dask must be started first in a separate process.
+# To do this, the `start_dask.sh` script has been provided.  This scripts starts
+# a dask scheduler and dask workers.  To select the GPUs and amount of memory
+# allocated to dask per GPU, the `CUDA_VISIBLE_DEVICES` and `WORKER_RMM_POOL_SIZE`
+# arguments in that script can be modified.
+# To connect to dask, the scheduler JSON file must be provided.  This can be done
+# using the `--dask_scheduler_file` argument in the mg python script being run.
+
+
 
 from ogb.nodeproppred import NodePropPredDataset
 
