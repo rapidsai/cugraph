d50a2a5ecd4620a2a73437478b797f25c00b62d0
Kyle Edwards <kyedwards@nvidia.com>
2024-05-14 13:19:54 -0400

    Fix dependencies and _version

--- python/cugraph/cugraph/_version.py
+++ python/cugraph/cugraph/_version.py
@@ -15,17 +15,17 @@
 
 import importlib.resources
 
-# Read VERSION file from the module that is symlinked to VERSION file
-# in the root of the repo at build time or copied to the moudle at
-# installation. VERSION is a separate file that allows CI build-time scripts
-# to update version info (including commit hashes) without modifying
-# source files.
 __version__ = (
-    importlib.resources.files("cugraph").joinpath("VERSION").read_text().strip()
+    importlib.resources.files(__package__).joinpath("VERSION").read_text().strip()
 )
 try:
     __git_commit__ = (
-        importlib.resources.files("cugraph").joinpath("GIT_COMMIT").read_text().strip()
+        importlib.resources.files(__package__)
+        .joinpath("GIT_COMMIT")
+        .read_text()
+        .strip()
     )
 except FileNotFoundError:
     __git_commit__ = ""
+
+__all__ = ["__version__", "__git_commit__"]
