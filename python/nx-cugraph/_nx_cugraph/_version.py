09190df33b4b4c3e1532e4f30870449825929f4e
Kyle Edwards <kyedwards@nvidia.com>
2024-05-14 13:22:26 -0400

    _version.py

--- python/nx-cugraph/_nx_cugraph/_version.py
+++ python/nx-cugraph/_nx_cugraph/_version.py
@@ -11,22 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
 import importlib.resources
 
-# Read VERSION file from the module that is symlinked to VERSION file
-# in the root of the repo at build time or copied to the module at
-# installation. VERSION is a separate file that allows CI build-time scripts
-# to update version info (including commit hashes) without modifying
-# source files.
 __version__ = (
-    importlib.resources.files("_nx_cugraph").joinpath("VERSION").read_text().strip()
+    importlib.resources.files(__package__).joinpath("VERSION").read_text().strip()
 )
 try:
     __git_commit__ = (
-        importlib.resources.files("_nx_cugraph")
+        importlib.resources.files(__package__)
         .joinpath("GIT_COMMIT")
         .read_text()
         .strip()
     )
 except FileNotFoundError:
     __git_commit__ = ""
+
+__all__ = ["__version__", "__git_commit__"]
