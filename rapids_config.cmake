5edef0cd2d81e648cb810add7e31a7a6bac24ca7
ptaylor <paul.e.taylor@me.com>
2024-05-14 21:53:41 +0000

    test rapids-cmake with CCCL 2.5

--- rapids_config.cmake
+++ rapids_config.cmake
@@ -25,6 +25,9 @@ else()
       "Could not determine RAPIDS version. Contents of VERSION file:\n${_rapids_version_formatted}")
 endif()
 
+set(rapids-cmake-repo trxcllnt/rapids-cmake)
+set(rapids-cmake-branch fea/cccl-2.5)
+
 if(NOT EXISTS "${CMAKE_CURRENT_BINARY_DIR}/CUGRAPH_RAPIDS-${RAPIDS_VERSION_MAJOR_MINOR}.cmake")
   file(
     DOWNLOAD
