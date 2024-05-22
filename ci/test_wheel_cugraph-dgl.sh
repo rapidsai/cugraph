585fcc0ce4e3629b808fd967372794e6a096792c
Ralph Liu <ralphl@nvidia.com>
2024-05-22 07:23:45 -0700

    Add pip commands to fix error

--- ci/test_wheel_cugraph-dgl.sh
+++ ci/test_wheel_cugraph-dgl.sh
@@ -32,8 +32,14 @@ fi
 PYTORCH_URL="https://download.pytorch.org/whl/cu${PYTORCH_CUDA_VER}"
 DGL_URL="https://data.dgl.ai/wheels/cu${PYTORCH_CUDA_VER}/repo.html"
 
+# Scripts to resolve import error
 rapids-logger "Installing PyTorch and DGL"
 rapids-retry python -m pip install torch --index-url ${PYTORCH_URL}
 rapids-retry python -m pip install dgl==2.0.0 --find-links ${DGL_URL}
 
-python -m pytest python/cugraph-dgl/tests
+pip3 uninstall -y torch torchvision torchaudio
+pip3 cache purge
+
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu118
+
+# python -m pytest python/cugraph-dgl/tests
