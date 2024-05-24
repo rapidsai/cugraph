96633db82b9a70facbdc0120260e79f283d24a3b
ptaylor <paul.e.taylor@me.com>
2024-05-24 16:50:57 +0000

    limit CI parallelism to n_cpus - 1

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -56,6 +56,7 @@ fi
 
 cd "${package_dir}"
 
+CMAKE_BUILD_PARALLEL_LEVEL="${PARALLEL_LEVEL:-$(nproc --all --ignore=1)}" \
 python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
 
 # pure-python packages should be marked as pure, and not have auditwheel run on them.
