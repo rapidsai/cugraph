566eb2277aad5230bfc463ea34a9968478f53dfa
jnke2016 <jnke2016@gmail.com>
2024-05-29 16:31:01 -0700

    fix style

--- cpp/src/community/k_truss_impl.cuh
+++ cpp/src/community/k_truss_impl.cuh
@@ -963,9 +963,9 @@ k_truss(raft::handle_t const& handle,
                             edge_dst_dummy_property_t{}.view(),
                             e_property_triangle_count.view(),
                             extract_weak_edges<vertex_t, edge_t>{k});
-
+      auto done = 1
       if constexpr (multi_gpu) {
-        if (num_invalid_edges == 0) { done = 0; }
+        if (weak_edgelist_srcs.size() == 0) { done = 0; }
         done = host_scalar_allreduce(
           handle.get_comms(), done, raft::comms::op_t::MAX, handle.get_stream());
 
