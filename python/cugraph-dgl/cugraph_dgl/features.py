265f5467d935b499d1fdae8e771ccea8871adfe0
Alexandria Barghi <abarghi@nvidia.com>
2024-06-07 14:37:08 -0700

    basic graph/fs

--- python/cugraph-dgl/cugraph_dgl/features.py
+++ python/cugraph-dgl/cugraph_dgl/features.py
@@ -11,14 +11,111 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-dgl = import_optional('dgl')
+import warnings
 
-class FeatureStore(dgl.FeatureStorage):
+from cugraph.utilities.utils import import_optional, MissingModule
+
+torch = import_optional("torch")
+dgl = import_optional("dgl")
+wgth = import_optional("pylibwholegraph.torch")
+
+
+class WholeFeatureStore(
+    object if isinstance(dgl, MissingModule) else dgl.storages.base.FeatureStorage
+):
     """
     Interface for feature storage.
     """
 
-    def requires_ddp(self) -> bool:
-        return False
+    def __init__(
+        self,
+        tensor: "torch.Tensor",
+        memory_type: str = "distributed",
+        location: str = "cpu",
+    ):
+        """
+        Constructs a new WholeFeatureStore object that wraps a WholeGraph wholememory
+        distributed tensor.
 
-    def fetch
+        Parameters
+        ----------
+        t: torch.Tensor
+            The local slice of the tensor being distributed.  These should be in order
+            by rank (i.e. rank 0 contains elements 0-9, rank 1 contains elements 10-19,
+            rank 3 contains elements 20-29, etc.)  The sizes do not need to be equal.
+        memory_type: str (optional, default='distributed')
+            The memory type of this store.  Options are
+            'distributed', 'chunked', and 'continuous'.
+            For more information consult the WholeGraph
+            documentation.
+        location: str(optional, default='cpu')
+            The location ('cpu' or 'cuda') where data is stored.
+        """
+        self.__wg_comm = wgth.get_local_node_communicator()
+
+        if len(tensor.shape) > 2:
+            raise ValueError("Only 1-D or 2-D tensors are supported by WholeGraph.")
+
+        rank = torch.distributed.get_rank()
+        world_size = torch.distributed.get_world_size()
+
+        ld = torch.tensor(tensor.shape[0], device="cuda", dtype=torch.int64)
+        sizes = torch.empty((world_size,), device="cuda", dtype=torch.int64)
+        torch.distributed.all_gather_into_tensor(sizes, ld)
+
+        sizes = sizes.cpu()
+        ld = sizes.sum()
+
+        self.__td = -1 if len(tensor.shape) == 1 else tensor.shape[1]
+        global_shape = [
+            int(ld),
+            self.__td if self.__td > 0 else 1,
+        ]
+
+        if self.__td < 0:
+            tensor = tensor.reshape((tensor.shape[0], 1))
+
+        wg_tensor = wgth.create_wholememory_tensor(
+            self.__wg_commm,
+            memory_type,
+            location,
+            global_shape,
+            tensor.dtype,
+            [global_shape[1], 1],
+        )
+
+        offset = sizes[:rank].sum() if rank > 0 else 0
+
+        wg_tensor.scatter(
+            tensor.clone(memory_format=torch.contiguous_format).cuda(),
+            torch.arange(
+                offset, offset + tensor.shape[0], dtype=torch.int64, device="cuda"
+            ).contiguous(),
+        )
+
+        self.__wg_comm.barrier()
+
+        self.__wg_tensor = wg_tensor
+
+    def requires_ddp(self) -> bool:
+        return True
+
+    def fetch(
+        self,
+        indices: torch.Tensor,
+        device: torch.cuda.Device,
+        pin_memory=False,
+        **kwargs,
+    ):
+        if pin_memory:
+            warnings.warn("pin_memory has no effect for WholeFeatureStorage.")
+
+        t = self.__wg_tensor.gather(
+            indices.cuda(),
+            force_dtype=self.__wg_tensor.dtype,
+        )
+
+        if self.__td < 0:
+            t = t.reshape((t.shape[0],))
+
+        return t
--- python/cugraph-dgl/cugraph_dgl/graph.py
+++ python/cugraph-dgl/cugraph_dgl/graph.py
@@ -11,10 +11,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import cupy
+from cugraph.utilities.utils import import_optional
 
 from cugraph_dgl.typing import TensorType
 from cugraph_dgl.utils.cugraph_conversion_utils import _cast_to_torch_tensor
+from cugraph_dgl.features import WholeFeatureStore
 
 from typing import Union, Optional, Dict, Tuple
 
@@ -24,7 +25,9 @@ dgl = import_optional("dgl")
 torch = import_optional("torch")
 tensordict = import_optional("tensordict")
 
-HOMOGENEOUS_EDGE_TYPE = ('n','e','n')
+HOMOGENEOUS_NODE_TYPE = "n"
+HOMOGENEOUS_EDGE_TYPE = (HOMOGENEOUS_NODE_TYPE, "e", HOMOGENEOUS_NODE_TYPE)
+
 
 class Graph:
     """
@@ -40,48 +43,208 @@ class Graph:
     call put_edge_index with its slice.
     """
 
-    def __init__(self, is_multi_gpu: bool=False):
+    def __init__(
+        self,
+        is_multi_gpu: bool = False,
+        ndata_storage="torch",
+        edata_storage="torch",
+        **kwargs,
+    ):
         """
         Parameters
         ----------
         is_multi_gpu: bool (optional, default=False)
             Specifies whether this graph is distributed across GPUs.
+        ndata_storage: str (optional, default='torch')
+            Specifies where node data should be stored
+            (options are 'torch' and 'wholegraph').
+            If using PyTorch tensors for storage ('torch')
+            then data will be replicated across workers and data
+            for all nodes should be provided when calling add_nodes.
+            If using WholeGraph wholememory tensors for storage,
+            then data will be distributed across workers and only
+            the local slice of the data should be provided when
+            calling add_nodes.
+        edata_storage: str (optional, default='torch')
+            If using PyTorch tensors for storage ('torch')
+            then data will be replicated across workers and data
+            for all nodes should be provided when calling add_edge.
+            If using WholeGraph wholememory tensors for storage,
+            then data will be distributed across workers and only
+            the local slice of the data should be provided when
+            calling add_edges.
+        kwargs:
+            Optional kwargs for WholeGraph feature storage.
         """
 
+        if ndata_storage not in ("torch", "wholegraph"):
+            raise ValueError(
+                "Invalid node storage type (valid types are 'torch' and 'wholegraph')"
+            )
+        if edata_storage not in ("torch", "wholegraph"):
+            raise ValueError(
+                "Invalid edge storage type (valid types are 'torch' and 'wholegraph')"
+            )
+
+        self.__num_nodes_dict = {}
         self.__edge_indices = tensordict.TensorDict({}, batch_size=(2,))
+
         self.__sizes = {}
         self.__graph = None
         self.__vertex_offsets = None
         self.__handle = None
         self.__is_multi_gpu = is_multi_gpu
 
-    def to_canonical_etype(self, etype: Union[str, Tuple[str, str, str]]) -> Tuple[str, str, str]:
+        self.__ndata_storage_type = (
+            WholeFeatureStore
+            if ndata_storage == "wholegraph"
+            else dgl.storages.pytorch_tensor.PyTorchTensorStorage
+        )
+        self.__edata_storage_type = (
+            WholeFeatureStore
+            if edata_storage == "wholegraph"
+            else dgl.storages.pytorch_tensor.PyTorchTensorStorage
+        )
+        self.__ndata_storage = {}
+        self.__edata_storage = {}
+        self.__wg_kwargs = kwargs
+
+    @property
+    def is_multi_gpu(self):
+        return self.__is_multi_gpu
+
+    def to_canonical_etype(
+        self, etype: Union[str, Tuple[str, str, str]]
+    ) -> Tuple[str, str, str]:
         if etype is None:
-            if len(self.__edge_indices.keys(leaves_only=True,include_nested=True)) > 1:
+            if len(self.__edge_indices.keys(leaves_only=True, include_nested=True)) > 1:
                 raise ValueError("Edge type is required for heterogeneous graphs.")
             return HOMOGENEOUS_EDGE_TYPE
 
         if isinstance(etype, Tuple[str, str, str]):
             return etype
-    
-        for src_type, rel_type, dst_type in self.__edge_indices.keys(leaves_only=True,include_nested=True):
+
+        for src_type, rel_type, dst_type in self.__edge_indices.keys(
+            leaves_only=True, include_nested=True
+        ):
             if etype == rel_type:
                 return (src_type, rel_type, dst_type)
-    
-        raise ValueError(
-            "Unknown relation type " + etype
-        )
-    
-    def add_edges(self, u: TensorType, v: TensorType, data:Optional[Dict[str, TensorType]]=None, etype:Optional[Union[str, Tuple[str, str, str]]]=None) -> None:
+
+        raise ValueError("Unknown relation type " + etype)
+
+    def add_nodes(
+        self,
+        global_num_nodes: int,
+        data: Optional[Dict[str, TensorType]] = None,
+        ntype: Optional[str] = None,
+    ):
         """
-        Adds edges to this graph.
+        Adds the given number of nodes to this graph.  Can only be called once
+        per node type. The number of nodes specified here refers to the total
+        number of nodes across workers. If the backing feature store is
+        distributed (i.e. wholegraph), then only local features should
+        be passed to the data argument.  If the backing feature store is
+        replicated, then features for all nodes should be passed to the
+        data argument, including those for nodes not on the local worker.
+
+        Parameters
+        ----------
+        global_num_nodes: int
+            The total number of nodes of the given type in this graph.
+            The same number should be passed to every worker.
+        data: Dict[str, TensorType] (optional, default=None)
+            Node feature tensors.
+        ntype: str (optional, default=None)
+            The node type being modified.  Required for heterogeneous graphs.
+        """
+        if ntype is None:
+            if len(self.__num_nodes_dict.keys()) > 1:
+                raise ValueError("Node type is required for heterogeneous graphs.")
+            ntype = HOMOGENEOUS_NODE_TYPE
+
+        if ntype in self.__num_nodes_dict:
+            raise ValueError(
+                "Calling add_nodes multiple types for the same "
+                "node type is not allowed in cuGraph-DGL"
+            )
+
+        if self.is_multi_gpu:
+            # Ensure all nodes got the same number of nodes passed
+            world_size = torch.distributed.get_world_size()
+            local_size = torch.tensor(
+                [global_num_nodes], device="cuda", dtype=torch.int64
+            )
+            ns = torch.empty((world_size,), device="cuda", dtype=torch.int64)
+            torch.distributed.all_gather_into_tensor(ns, local_size)
+            if not (ns == global_num_nodes).all():
+                raise ValueError("The global number of nodes must match on all workers")
+
+            # Ensure the sum of the feature shapes equals the global number of nodes.
+            for feature_name, feature_tensor in data.items():
+                features_size = torch.tensor(
+                    [int(feature_tensor.shape[0])], device="cuda", dtype=torch.int64
+                )
+                torch.distributed.all_reduce(
+                    features_size, op=torch.distributed.ReduceOp.SUM
+                )
+                if features_size != global_num_nodes:
+                    raise ValueError(
+                        "The total length of the feature vector across workers must"
+                        " match the global number of nodes but it does not match for "
+                        f"{feature_name}."
+                    )
+
+        self.__num_nodes_dict[ntype] = global_num_nodes
+
+        for feature_name, feature_tensor in data.items():
+            self.__ndata_storage[ntype, feature_name] = self.__ndata_storage_type(
+                feature_tensor, **self.__wg_kwargs
+            )
+
+    def __check_node_ids(self, ntype: str, ids: TensorType):
+        """
+        Ensures all node ids in the provided id tensor are valid.
+        Raises a ValueError if any are invalid.
+
+        Parameters
+        ----------
+        ntype: str
+            The node type being validated against.
+        ids:
+            The tensor of ids being validated.
+        """
+        if ntype in self.__num_nodes_dict:
+            if ids.max() + 1 > self.__num_nodes(ntype):
+                raise ValueError(
+                    f"input tensor contains invalid node ids for type {ntype}"
+                )
+        else:
+            raise ValueError(
+                f"add_nodes() must be called for type {ntype} before calling num_edges."
+            )
+
+    def add_edges(
+        self,
+        u: TensorType,
+        v: TensorType,
+        data: Optional[Dict[str, TensorType]] = None,
+        etype: Optional[Union[str, Tuple[str, str, str]]] = None,
+    ) -> None:
+        """
+        Adds edges to this graph.  Must be called after add_nodes
+        is called for the src/dst node type. If the backing feature
+        store is distributed (i.e. wholegraph), then only local
+        features should be passed to the data argument.  If the
+        backing feature store is replicated, then features for
+        all edges should be passed to the data argument,
+        including those for edges not on the local worker.
 
         Parameters
         ----------
         u: TensorType
-            1d tensor of source vertex ids.
+            1d tensor of source node ids (local slice of the distributed edgelist).
         v: TensorType
-            1d tensor of destination vertex ids.
+            1d tensor of destination node ids (local slice of the distributed edgelist).
         data: Dict[str, TensorType] (optional, default=None)
             Dictionary containing edge features for the new edges.
         etype: Union[str, Tuple[str, str, str]]
@@ -89,20 +252,43 @@ class Graph:
             for homogeneous graphs, which have only one edge type.
         """
 
+        # Validate all inputs before proceeding
+        # The number of nodes for the src/dst type needs to be known and there cannot
+        # be any edges of this type in the graph.
         dgl_can_edge_type = self.to_canonical_etype(etype)
+        src_type, _, dst_type = dgl_can_edge_type
+        if dgl_can_edge_type in self.__edge_indices.keys(
+            leaves_only=True, include_nested=True
+        ):
+            raise ValueError(
+                "This cuGraph-DGL graph already contains edges of type"
+                f" {dgl_can_edge_type}. Calling add_edges multiple times"
+                " for the same edge type is not supported."
+            )
+        self.__check_node_ids(src_type, u)
+        self.__check_node_ids(dst_type, v)
 
-        new_edges = torch.stack([
-            _cast_to_torch_tensor(u),
-            _cast_to_torch_tensor(v),
-        ])
+        self.__edge_indices[dgl_can_edge_type] = torch.stack(
+            [
+                _cast_to_torch_tensor(u),
+                _cast_to_torch_tensor(v),
+            ]
+        )
 
-        if dgl_can_edge_type in self.__edge_indices.keys(leaves_only=True, include_nested=True):
-            self.__edge_indices[dgl_can_edge_type] = torch.concat([
-                self.__edge_indices[dgl_can_edge_type],
-                new_edges,
-            ], dim=1)
-        else:
-            self.__edge_indices[dgl_can_edge_type] = new_edges
-        
         if data is not None:
-            
+            for attr_name, attr_tensor in data.items():
+                self.__edata_storage[
+                    dgl_can_edge_type, attr_name
+                ] = self.__edata_storage_type(attr_tensor, **self.__wg_kwargs)
+
+    def num_nodes(self, ntype: str = None):
+        """
+        Returns the number of nodes of ntype, or if ntype is not provided,
+        the total number of nodes in the graph.
+        """
+        if ntype is None:
+            if len(self.__num_nodes_dict.keys()) > 1:
+                raise ValueError("ntype is required for heterogeneous graphs")
+            return self.__num_nodes_dict[HOMOGENEOUS_NODE_TYPE]
+
+        return self.__num_nodes_dict[ntype]
