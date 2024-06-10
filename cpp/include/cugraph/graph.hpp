a3f10d8407fbeab366edadab4b424fe83bed81f7
Seunghwa Kang <seunghwak@nvidia.com>
2024-06-10 11:06:52 -0700

    add invalid_(vertex|edge|component)_id_v

--- cpp/include/cugraph/graph.hpp
+++ cpp/include/cugraph/graph.hpp
@@ -319,11 +319,20 @@ struct invalid_idx<
 template <typename vertex_t>
 struct invalid_vertex_id : invalid_idx<vertex_t> {};
 
+template <typename vertex_t>
+using invalid_vertex_id_v = invalid_vertex_id<vertex_t>::value;
+
 template <typename edge_t>
 struct invalid_edge_id : invalid_idx<edge_t> {};
 
-template <typename vertex_t>
-struct invalid_component_id : invalid_idx<vertex_t> {};
+template <typename edge_t>
+using invalid_edge_id_v = invalid_edge_id<edge_t>::value;
+
+template <typename component_t>
+struct invalid_component_id : invalid_idx<component_t> {};
+
+template <typename component_t>
+using invalid_component_id_v = invalid_component_id<component_t>::value;
 
 template <typename vertex_t>
 __host__ __device__ std::enable_if_t<std::is_signed<vertex_t>::value, bool> is_valid_vertex(
