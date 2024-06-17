3a1ec06a4359b4a8dea02476a95ecaf0474b2c4e
Bradley Dice <bdice@bradleydice.com>
2024-06-17 13:34:34 -0700

    Don't define PARALLEL_LEVEL (get from gha-tools rapids-configure-sccache).

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -57,9 +57,6 @@ fi
 cd "${package_dir}"
 
 
-PARALLEL_LEVEL=$(python -c \
-  "from math import ceil; from multiprocessing import cpu_count; print(ceil(cpu_count()/2))")
-
 SKBUILD_BUILD_OPTIONS="-j${PARALLEL_LEVEL}" \
 python -m pip wheel \
     -w dist \
