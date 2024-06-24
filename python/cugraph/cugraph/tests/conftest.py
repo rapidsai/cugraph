0b1b96fc630edfd74f3c7dcabfae9b4e021b1383
BradReesWork <BradReesWork@users.noreply.github.com>
2024-06-24 12:52:43 -0400

    blank line added

--- python/cugraph/cugraph/tests/conftest.py
+++ python/cugraph/cugraph/tests/conftest.py
@@ -24,6 +24,7 @@ import tempfile
 from dask_cuda.utils_test import IncreasedCloseTimeoutNanny
 
 import warnings
+
 warnings.filterwarnings("ignore", category=DeprecationWarning, module="cugraph")
 
 
