99b7deea070c6956fae49480a242a75afd4de53f
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2024-06-26 09:07:26 -0400

    Update python/cugraph/cugraph/traversal/sssp.py
    Co-authored-by: Don Acosta <97529984+acostadon@users.noreply.github.com>

--- python/cugraph/cugraph/traversal/sssp.py
+++ python/cugraph/cugraph/traversal/sssp.py
@@ -160,7 +160,7 @@ def sssp(
         .. deprecated:: 24.08
 
            Accepting ``networkx.Graph`` is deprecated and will be removed in a
-           future version.  For ``networkx.Graph`` use netwotkx directly with
+           future version.  For ``networkx.Graph`` use networkx directly with
            the ``nx-cugraph`` plug-in.
 
     source : int
