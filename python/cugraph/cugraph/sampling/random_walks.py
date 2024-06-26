58d37242a7de2740a7b12df911e9b6fa5e25c7c0
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2024-06-26 09:06:40 -0400

    Update python/cugraph/cugraph/sampling/random_walks.py
    Co-authored-by: Don Acosta <97529984+acostadon@users.noreply.github.com>

--- python/cugraph/cugraph/sampling/random_walks.py
+++ python/cugraph/cugraph/sampling/random_walks.py
@@ -75,7 +75,7 @@ def random_walks(
         .. deprecated:: 24.08
 
            Accepting ``networkx.Graph`` is deprecated and will be removed in a
-           future version.  For ``networkx.Graph`` use netwotkx directly with
+           future version.  For ``networkx.Graph`` use networkx directly with
            the ``nx-cugraph`` plug-in.
 
     random_walks_type : str, optional (default='uniform')
