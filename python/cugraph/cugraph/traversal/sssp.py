5d254c6005920b4bcf0433805bd5af1b8512841f
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2024-06-26 09:07:35 -0400

    Update python/cugraph/cugraph/traversal/sssp.py
    Co-authored-by: Don Acosta <97529984+acostadon@users.noreply.github.com>

--- python/cugraph/cugraph/traversal/sssp.py
+++ python/cugraph/cugraph/traversal/sssp.py
@@ -331,7 +331,7 @@ def shortest_path_length(G, source, target=None):
         .. deprecated:: 24.08
 
            Accepting ``networkx.Graph`` is deprecated and will be removed in a
-           future version.  For ``networkx.Graph`` use netwotkx directly with
+           future version.  For ``networkx.Graph`` use networkx directly with
            the ``nx-cugraph`` plug-in.
 
     source : Dependant on graph type. Index of the source vertex.
