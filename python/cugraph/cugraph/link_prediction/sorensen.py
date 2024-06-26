8d2d08725e6f6d698d432f814bab1ae3c2ac7f20
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2024-06-26 09:06:17 -0400

    Update python/cugraph/cugraph/link_prediction/sorensen.py
    Co-authored-by: Don Acosta <97529984+acostadon@users.noreply.github.com>

--- python/cugraph/cugraph/link_prediction/sorensen.py
+++ python/cugraph/cugraph/link_prediction/sorensen.py
@@ -207,7 +207,7 @@ def sorensen_coefficient(
         .. deprecated:: 24.08
 
            Accepting ``networkx.Graph`` is deprecated and will be removed in a
-           future version.  For ``networkx.Graph`` use netwotkx directly with
+           future version.  For ``networkx.Graph`` use networkx directly with
            the ``nx-cugraph`` plug-in.
 
     ebunch : cudf.DataFrame or iterable of node pairs, optional (default=None)
