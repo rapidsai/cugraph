62fb74c90ecd86caf4b413815f083d0a92dc5b4e
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2024-06-26 08:58:59 -0400

    Update python/cugraph/cugraph/community/ktruss_subgraph.py
    Co-authored-by: Don Acosta <97529984+acostadon@users.noreply.github.com>

--- python/cugraph/cugraph/community/ktruss_subgraph.py
+++ python/cugraph/cugraph/community/ktruss_subgraph.py
@@ -76,7 +76,7 @@ def k_truss(
         .. deprecated:: 24.08
 
            Accepting ``networkx.Graph`` is deprecated and will be removed in a
-           future version.  For ``networkx.Graph`` use netwotkx directly with
+           future version.  For ``networkx.Graph`` use networkx directly with
            the ``nx-cugraph`` plug-in.
 
     k : int
