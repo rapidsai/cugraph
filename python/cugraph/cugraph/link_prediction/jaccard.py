b91b0496165045818b0a92a65c523759b16fb956
Brad Rees <34135411+BradReesWork@users.noreply.github.com>
2024-06-26 09:05:01 -0400

    Update python/cugraph/cugraph/link_prediction/jaccard.py
    Co-authored-by: Don Acosta <97529984+acostadon@users.noreply.github.com>

--- python/cugraph/cugraph/link_prediction/jaccard.py
+++ python/cugraph/cugraph/link_prediction/jaccard.py
@@ -192,7 +192,7 @@ def jaccard_coefficient(
         .. deprecated:: 24.08
 
            Accepting ``networkx.Graph`` is deprecated and will be removed in a
-           future version.  For ``networkx.Graph`` use netwotkx directly with
+           future version.  For ``networkx.Graph`` use networkx directly with
            the ``nx-cugraph`` plug-in.
 
     ebunch : cudf.DataFrame or iterable of node pairs, optional (default=None)
