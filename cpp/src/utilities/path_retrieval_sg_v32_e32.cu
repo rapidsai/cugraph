fd90b984f25b58f1af81619dac50aec9a53a8613
Naim <110031745+naimnv@users.noreply.github.com>
2024-06-29 00:08:37 +0200

    Refactor code base to reduce memory requirement for building libcugraph (#4506)
    Refactor code base to reduce memory requirement for building libcugraph.
    
    - split all sg.cu and mg.cu files into three files for three combination of (vertex_t, edge_t ), i.e, (32 bit, 32bit), (32 bit, 64bit), (64 bit, 64bit)
    
    Authors:
      - Naim (https://github.com/naimnv)
    
    Approvers:
      - Seunghwa Kang (https://github.com/seunghwak)
      - Chuck Hastings (https://github.com/ChuckHastings)
      - Joseph Nke (https://github.com/jnke2016)
    
    URL: https://github.com/rapidsai/cugraph/pull/4506

---
+++ cpp/src/utilities/path_retrieval_sg_v32_e32.cu
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2021-2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "path_retrieval.cuh"
+
+#include <cugraph/utilities/error.hpp>
+#include <cugraph/utilities/path_retrieval.hpp>
+
+#include <raft/core/handle.hpp>
+
+#include <rmm/device_uvector.hpp>
+#include <rmm/exec_policy.hpp>
+
+#include <thrust/sequence.h>
+#include <thrust/sort.h>
+
+namespace cugraph {
+
+template void get_traversed_cost<int32_t, float>(raft::handle_t const& handle,
+                                                 int32_t const* vertices,
+                                                 int32_t const* preds,
+                                                 float const* info_weights,
+                                                 float* out,
+                                                 int32_t stop_vertex,
+                                                 int32_t num_vertices);
+
+template void get_traversed_cost<int32_t, double>(raft::handle_t const& handle,
+                                                  int32_t const* vertices,
+                                                  int32_t const* preds,
+                                                  double const* info_weights,
+                                                  double* out,
+                                                  int32_t stop_vertex,
+                                                  int32_t num_vertices);
+
+}  // namespace cugraph
