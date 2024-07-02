24135b89c89629a84ca25eb5ba2a9a65509368aa
Garrett <gcornett@nvidia.com>
2024-07-02 14:47:56 -0700

    Update new files to take rng_state

--- cpp/src/sampling/random_walks_sg_v64_e64.cu
+++ cpp/src/sampling/random_walks_sg_v64_e64.cu
@@ -26,7 +26,7 @@ uniform_random_walks(raft::handle_t const& handle,
                      std::optional<edge_property_view_t<int64_t, float const*>> edge_weight_view,
                      raft::device_span<int64_t const> start_vertices,
                      size_t max_length,
-                     uint64_t seed);
+                     raft::random::RngState& rng_state);
 
 template std::tuple<rmm::device_uvector<int64_t>, std::optional<rmm::device_uvector<double>>>
 uniform_random_walks(raft::handle_t const& handle,
@@ -34,7 +34,7 @@ uniform_random_walks(raft::handle_t const& handle,
                      std::optional<edge_property_view_t<int64_t, double const*>> edge_weight_view,
                      raft::device_span<int64_t const> start_vertices,
                      size_t max_length,
-                     uint64_t seed);
+                     raft::random::RngState& rng_state);
 
 template std::tuple<rmm::device_uvector<int64_t>, std::optional<rmm::device_uvector<float>>>
 biased_random_walks(raft::handle_t const& handle,
@@ -42,7 +42,7 @@ biased_random_walks(raft::handle_t const& handle,
                     edge_property_view_t<int64_t, float const*> edge_weight_view,
                     raft::device_span<int64_t const> start_vertices,
                     size_t max_length,
-                    uint64_t seed);
+                    raft::random::RngState& rng_state);
 
 template std::tuple<rmm::device_uvector<int64_t>, std::optional<rmm::device_uvector<double>>>
 biased_random_walks(raft::handle_t const& handle,
@@ -50,7 +50,7 @@ biased_random_walks(raft::handle_t const& handle,
                     edge_property_view_t<int64_t, double const*> edge_weight_view,
                     raft::device_span<int64_t const> start_vertices,
                     size_t max_length,
-                    uint64_t seed);
+                    raft::random::RngState& rng_state);
 
 template std::tuple<rmm::device_uvector<int64_t>, std::optional<rmm::device_uvector<float>>>
 node2vec_random_walks(raft::handle_t const& handle,
@@ -60,7 +60,7 @@ node2vec_random_walks(raft::handle_t const& handle,
                       size_t max_length,
                       float p,
                       float q,
-                      uint64_t seed);
+                      raft::random::RngState& rng_state);
 
 template std::tuple<rmm::device_uvector<int64_t>, std::optional<rmm::device_uvector<double>>>
 node2vec_random_walks(raft::handle_t const& handle,
@@ -70,6 +70,6 @@ node2vec_random_walks(raft::handle_t const& handle,
                       size_t max_length,
                       double p,
                       double q,
-                      uint64_t seed);
+                      raft::random::RngState& rng_state);
 
 }  // namespace cugraph
