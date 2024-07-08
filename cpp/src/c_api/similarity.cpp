3fcee12867040f59094d0937ebbf9f4d66e1e5f4
jnke2016 <jnke2016@gmail.com>
2024-07-08 02:49:36 -0700

    add CAPI for cosine similarity

--- cpp/src/c_api/similarity.cpp
+++ cpp/src/c_api/similarity.cpp
@@ -288,6 +288,32 @@ struct sorensen_functor {
   }
 };
 
+struct cosine_functor {
+  template <typename vertex_t, typename edge_t, typename weight_t, bool multi_gpu>
+  rmm::device_uvector<weight_t> operator()(
+    raft::handle_t const& handle,
+    cugraph::graph_view_t<vertex_t, edge_t, false, multi_gpu> const& graph_view,
+    std::optional<cugraph::edge_property_view_t<edge_t, weight_t const*>> edge_weight_view,
+    std::tuple<raft::device_span<vertex_t const>, raft::device_span<vertex_t const>> vertex_pairs)
+  {
+    return cugraph::cosine_similarity_coefficients(handle, graph_view, edge_weight_view, vertex_pairs);
+  }
+
+  template <typename vertex_t, typename edge_t, typename weight_t, bool multi_gpu>
+  std::tuple<rmm::device_uvector<vertex_t>,
+             rmm::device_uvector<vertex_t>,
+             rmm::device_uvector<weight_t>>
+  operator()(raft::handle_t const& handle,
+             cugraph::graph_view_t<vertex_t, edge_t, false, multi_gpu> const& graph_view,
+             std::optional<cugraph::edge_property_view_t<edge_t, weight_t const*>> edge_weight_view,
+             std::optional<raft::device_span<vertex_t const>> vertices,
+             std::optional<size_t> topk)
+  {
+    return cugraph::cosine_similarity_all_pairs_coefficients(
+      handle, graph_view, edge_weight_view, vertices, topk);
+  }
+};
+
 struct overlap_functor {
   template <typename vertex_t, typename edge_t, typename weight_t, bool multi_gpu>
   rmm::device_uvector<weight_t> operator()(
