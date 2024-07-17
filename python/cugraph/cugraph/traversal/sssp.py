1e81cd3b48526b8a71d7134c7ca2a8d25dd8feaf
Ralph Liu <ralphl@nvidia.com>
2024-07-17 08:16:15 -0700

    Add additional check for Source argument dtype

--- python/cugraph/cugraph/traversal/sssp.py
+++ python/cugraph/cugraph/traversal/sssp.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2023, NVIDIA CORPORATION.
+# Copyright (c) 2019-2024, NVIDIA CORPORATION.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -222,6 +222,12 @@ def sssp(
             "'BFS' should be used instead of 'SSSP' for unweighted graphs."
         )
         raise RuntimeError(err_msg)
+
+    if not isinstance(source, int):
+        raise ValueError(
+            f"Invalid source type. 'Source' must be an interger "
+            f"but was given a {type(source)}"
+        )
 
     if not G.has_node(source):
         raise ValueError("Graph does not contain source vertex")
