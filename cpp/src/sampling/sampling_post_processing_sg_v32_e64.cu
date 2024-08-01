909f5755e56b885a35439f5cb779b606ed412037
Seunghwa Kang <seunghwak@nvidia.com>
2024-08-01 01:36:42 -0700

    implemeent heterogeneous renumbeering

--- cpp/src/sampling/sampling_post_processing_sg_v32_e64.cu
+++ cpp/src/sampling/sampling_post_processing_sg_v32_e64.cu
@@ -126,6 +126,75 @@ template std::tuple<rmm::device_uvector<int32_t>,
                     rmm::device_uvector<int32_t>,
                     std::optional<rmm::device_uvector<float>>,
                     std::optional<rmm::device_uvector<int64_t>>,
+                    std::optional<rmm::device_uvector<size_t>>,
+                    rmm::device_uvector<int32_t>,
+                    rmm::device_uvector<size_t>,
+                    std::optional<rmm::device_uvector<int64_t>>,
+                    std::optional<rmm::device_uvector<size_t>>>
+heterogeneous_renumber_and_sort_sampled_edgelist(
+  raft::handle_t const& handle,
+  rmm::device_uvector<int32_t>&& edgelist_srcs,
+  rmm::device_uvector<int32_t>&& edgelist_dsts,
+  std::optional<rmm::device_uvector<float>>&& edgelist_weights,
+  std::optional<rmm::device_uvector<int64_t>>&& edgelist_edge_ids,
+  std::optional<rmm::device_uvector<int32_t>>&& edgelist_edge_types,
+  std::optional<rmm::device_uvector<int32_t>>&& edgelist_hops,
+  std::optional<raft::device_span<int32_t const>> seed_vertices,
+  std::optional<raft::device_span<size_t const>> seed_vertex_label_offsets,
+  std::optional<raft::device_span<size_t const>> edgelist_label_offsets,
+  raft::device_span<int32_t const> vertex_type_offsets,
+  size_t num_labels,
+  size_t num_hops,
+  size_t num_vertex_types,
+  size_t num_edge_types,
+  bool src_is_major,
+  bool do_expensive_check);
+
+template std::tuple<rmm::device_uvector<int32_t>,
+                    rmm::device_uvector<int32_t>,
+                    std::optional<rmm::device_uvector<double>>,
+                    std::optional<rmm::device_uvector<int64_t>>,
+                    std::optional<rmm::device_uvector<size_t>>,
+                    rmm::device_uvector<int32_t>,
+                    rmm::device_uvector<size_t>,
+                    std::optional<rmm::device_uvector<int64_t>>,
+                    std::optional<rmm::device_uvector<size_t>>>
+heterogeneous_renumber_and_sort_sampled_edgelist(
+  raft::handle_t const& handle,
+  rmm::device_uvector<int32_t>&& edgelist_srcs,
+  rmm::device_uvector<int32_t>&& edgelist_dsts,
+  std::optional<rmm::device_uvector<double>>&& edgelist_weights,
+  std::optional<rmm::device_uvector<int64_t>>&& edgelist_edge_ids,
+  std::optional<rmm::device_uvector<int32_t>>&& edgelist_edge_types,
+  std::optional<rmm::device_uvector<int32_t>>&& edgelist_hops,
+  std::optional<raft::device_span<int32_t const>> seed_vertices,
+  std::optional<raft::device_span<size_t const>> seed_vertex_label_offsets,
+  std::optional<raft::device_span<size_t const>> edgelist_label_offsets,
+  raft::device_span<int32_t const> vertex_type_offsets,
+  size_t num_labels,
+  size_t num_hops,
+  size_t num_vertex_types,
+  size_t num_edge_types,
+  bool src_is_major,
+  bool do_expensive_check);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+template std::tuple<rmm::device_uvector<int32_t>,
+                    rmm::device_uvector<int32_t>,
+                    std::optional<rmm::device_uvector<float>>,
+                    std::optional<rmm::device_uvector<int64_t>>,
                     std::optional<rmm::device_uvector<int32_t>>,
                     std::optional<rmm::device_uvector<size_t>>>
 sort_sampled_edgelist(raft::handle_t const& handle,
