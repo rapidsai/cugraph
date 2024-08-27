445a4da221476e5d01bb2a4aa764ce2a8fb02d62
Erik Welch <erik.n.welch@gmail.com>
2024-08-27 18:21:42 +0200

    Try to get CI to pass by setting NX_CUGRAPH_ZERO=False

--- ci/run_nx_cugraph_pytests.sh
+++ ci/run_nx_cugraph_pytests.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 # Support invoking run_nx_cugraph_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/nx-cugraph/nx_cugraph
 
-pytest --capture=no --cache-clear --benchmark-disable "$@" tests
+NX_CUGRAPH_ZERO=False pytest --capture=no --cache-clear --benchmark-disable "$@" tests
