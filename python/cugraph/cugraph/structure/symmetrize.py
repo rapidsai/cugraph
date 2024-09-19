c01456adece2272e8716f29be40c7acc1f626bae
jnke2016 <jnke2016@gmail.com>
2024-09-19 07:06:26 -0700

    deprecated the python symmetrize function

--- python/cugraph/cugraph/structure/symmetrize.py
+++ python/cugraph/cugraph/structure/symmetrize.py
@@ -184,12 +184,6 @@ def symmetrize_ddf(
         result = result.reset_index(drop=True).repartition(npartitions=len(workers) * 2)
         return result
     else:
-        warnings.warn(
-            "Multi is deprecated and the removal of multi edges will no longer be "
-            "supported from 'symmetrize'. Multi edges will be removed upon creation "
-            "of graph instance.",
-            FutureWarning,
-        )
         vertex_col_name = src_name + dst_name
         result = _memory_efficient_drop_duplicates(
             result, vertex_col_name, len(workers)
@@ -257,6 +251,12 @@ def symmetrize(
     >>> df['values'] = cudf.Series(M['2'])
     >>> src, dst, val = symmetrize(df, 'sources', 'destinations', 'values', multi=True)
     """
+
+    warnings.warn(
+        "This method is deprecated and will no longer be supported. The symmetrization "
+        "of the edges are only supported by creating an undirected graph",
+        FutureWarning,
+    )
 
     # FIXME: Redundant check that should be done at the graph creation
     if "edge_id" in input_df.columns and symmetrize:
