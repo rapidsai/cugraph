ba6e5dc9a0391b971e8094d653027b36b9f34916
Charles Hastings <charlesh@nvidia.com>
2024-10-15 12:32:04 -0700

    some functions were in the wrong file and were deleted

--- cpp/src/detail/collect_local_vertex_values_mg_v32_e32.cu
+++ cpp/src/detail/collect_local_vertex_values_mg_v32_e32.cu
@@ -47,5 +47,16 @@ collect_local_vertex_values_from_ext_vertex_value_pairs<int32_t, int32_t, true>(
   int32_t default_value,
   bool do_expensive_check);
 
+template rmm::device_uvector<double>
+collect_local_vertex_values_from_ext_vertex_value_pairs<int32_t, double, true>(
+  raft::handle_t const& handle,
+  rmm::device_uvector<int32_t>&& d_vertices,
+  rmm::device_uvector<double>&& d_values,
+  rmm::device_uvector<int32_t> const& number_map,
+  int32_t local_vertex_first,
+  int32_t local_vertex_last,
+  double default_value,
+  bool do_expensive_check);
+
 }  // namespace detail
 }  // namespace cugraph
