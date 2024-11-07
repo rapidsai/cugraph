f7724672a414dc874efc56a29e62dbe77927cb9b
Ralph Liu <ralphl@nvidia.com>
2024-11-07 13:21:22 -0800

    Remove NX_CUGRAPH_USE_COMPAT_GRAPHS

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -32,7 +32,6 @@ else
     DASK_DISTRIBUTED__SCHEDULER__WORKER_TTL="1000s" \
     DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT="1000s" \
     DASK_CUDA_WAIT_WORKERS_MIN_TIMEOUT="1000s" \
-    NX_CUGRAPH_USE_COMPAT_GRAPHS=False \
     python -m pytest \
        -v \
        --import-mode=append \
