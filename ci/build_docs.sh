f4a2474a6b5a818c59491f8a7106613369234dd4
Jake Awe <jawe@nvidia.com>
2024-12-10 12:44:54 -0600

    re-add dgl package

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -23,16 +23,24 @@ rapids-print-env
 rapids-logger "Downloading artifacts from previous jobs"
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
 PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
-CONDA_CUDA_VERSION="12.1"
+if [[ "${RAPIDS_CUDA_VERSION}" == "11.8.0" ]]; then
+  CONDA_CUDA_VERSION="11.8"
+  DGL_CHANNEL="dglteam/label/th23_cu118"
+else
+  CONDA_CUDA_VERSION="12.1"
+  DGL_CHANNEL="dglteam/label/th23_cu121"
+fi
 
 rapids-mamba-retry install \
   --channel "${CPP_CHANNEL}" \
   --channel "${PYTHON_CHANNEL}" \
   --channel conda-forge \
   --channel nvidia \
+  --channel "${DGL_CHANNEL}" \
   "libcugraph=${RAPIDS_VERSION_MAJOR_MINOR}.*" \
   "pylibcugraph=${RAPIDS_VERSION_MAJOR_MINOR}.*" \
   "cugraph=${RAPIDS_VERSION_MAJOR_MINOR}.*" \
+  "cugraph-dgl=${RAPIDS_VERSION_MAJOR_MINOR}.*" \
   "cugraph-service-server=${RAPIDS_VERSION_MAJOR_MINOR}.*" \
   "cugraph-service-client=${RAPIDS_VERSION_MAJOR_MINOR}.*" \
   "libcugraph_etl=${RAPIDS_VERSION_MAJOR_MINOR}.*" \
