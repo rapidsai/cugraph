a6eea2f7cc40165e584e459351d1a92406de7415
James Lamb <jaylamb20@gmail.com>
2025-01-09 13:16:43 -0600

    move wheel installs into per-project test scripts, other packaging changes (#4847)
    Proposes some changes to small things I noticed while working on #4804.
    
    * CMake option cleanup:
       - adds `BUILD_PRIMS_BENCH OFF`, removes `BUILD_BENCH OFF` in `get_raft.cmake` (matching changes to RAFT from 23.04: https://github.com/rapidsai/raft/pull/1304)
      - adds `BUILD_BENCHMARKS OFF` in `get_cudf.cmake` ([this is the default](https://github.com/rapidsai/cudf/blob/b81d9e17fbffbb912e0128148f556bf7af41b6ab/cpp/CMakeLists.txt#L51), but better to be explicit)
    * consolidates some `.gitignore` rules, adds wheels and conda packages there
    * moves responsibility for installing CI artifacts into `ci/test_wheel_{package}.sh` and out of `ci/test_wheel.sh`
    * splits up Cython and `scikit-build-core` in `dependencies.yaml`
      - *every Python package here using Cython also uses `scikit-build-core`, but the reverse won't be true as of #4804 ... making that change here is harmless and reduces the size of the diff in #4804 a bit*
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cugraph/pull/4847

--- ci/test_wheel_cugraph.sh
+++ ci/test_wheel_cugraph.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-# Copyright (c) 2023-2024, NVIDIA CORPORATION.
+# Copyright (c) 2023-2025, NVIDIA CORPORATION.
 
 set -eoxu pipefail
 
-# Download the pylibcugraph built in the previous step
+# Download the packages built in the previous step
+mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="pylibcugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./local-pylibcugraph-dep
-python -m pip install --no-deps ./local-pylibcugraph-dep/pylibcugraph*.whl
+RAPIDS_PY_WHEEL_NAME="cugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python ./dist
+RAPIDS_PY_WHEEL_NAME="pylibcugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python ./local-pylibcugraph-dep
+
+# echo to expand wildcard before adding `[extra]` requires for pip
+python -m pip install \
+    "$(echo ./dist/cugraph*.whl)[test]" \
+    ./local-pylibcugraph-dep/pylibcugraph*.whl
 
 ./ci/test_wheel.sh cugraph
