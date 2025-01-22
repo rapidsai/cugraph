ea8d98cb718ed0fe12a04296dd67d5a011257473
James Lamb <jlamb@nvidia.com>
2025-01-21 16:53:54 -0800

    use wildcards in auditwheel exclusions

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -40,12 +40,12 @@ EXCLUDE_ARGS=(
 # 'libraft' wheels are responsible for carrying a runtime dependency on
 # these based on RAFT's needs.
 EXCLUDE_ARGS+=(
-  --exclude "libcublas.so.12"
-  --exclude "libcublasLt.so.12"
-  --exclude "libcurand.so.10"
-  --exclude "libcusolver.so.11"
-  --exclude "libcusparse.so.12"
-  --exclude "libnvJitLink.so.12"
+  --exclude "libcublas.so.*"
+  --exclude "libcublasLt.so.*"
+  --exclude "libcurand.so.*"
+  --exclude "libcusolver.so.*"
+  --exclude "libcusparse.so.*"
+  --exclude "libnvJitLink.so.*"
 )
 
 if [[ "${package_dir}" != "python/libcugraph" ]]; then
