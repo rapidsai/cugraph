88ecdb517f83eff09bc44227a9ec6eceac6852cb
Alexandria Barghi <abarghi@nvidia.com>
2025-01-28 14:18:57 -0800

    only print if not none

--- python/cugraph/cugraph/gnn/data_loading/dist_sampler.py
+++ python/cugraph/cugraph/gnn/data_loading/dist_sampler.py
@@ -577,7 +577,8 @@ class DistSampler:
         else:
             edges_call_groups, index_call_groups, label_call_groups = groups
 
-        print(input_label.sum(), [c.sum() for c in label_call_groups], flush=True)
+        if input_label is not None:
+            print(input_label.sum(), [c.sum() for c in label_call_groups], flush=True)
 
         sample_args = [
             batch_id_start,
