aad2917fe6f45f9873f4da2677017f4e64450586
Mathis HAMMEL <mathis.hammel@gmail.com>
2025-04-14 19:51:11 +0200

    Fix ForceAtlas2 early stop in repulsion kernel

--- cpp/src/layout/legacy/exact_repulsion.cuh
+++ cpp/src/layout/legacy/exact_repulsion.cuh
@@ -30,11 +30,10 @@ __global__ static void repulsion_kernel(const float* restrict x_pos,
                                         const float scaling_ratio,
                                         const vertex_t n)
 {
-  int j = (blockIdx.x * blockDim.x) + threadIdx.x;  // for every item in row
   int i = (blockIdx.y * blockDim.y) + threadIdx.y;  // for every row
   for (; i < n; i += gridDim.y * blockDim.y) {
-    for (; j < n; j += gridDim.x * blockDim.x) {
-      if (j >= i) return;
+    int j = (blockIdx.x * blockDim.x) + threadIdx.x;  // for every item in row
+    for (; j < i; j += gridDim.x * blockDim.x) {
       float x_dist   = x_pos[i] - x_pos[j];
       float y_dist   = y_pos[i] - y_pos[j];
       float distance = x_dist * x_dist + y_dist * y_dist;
