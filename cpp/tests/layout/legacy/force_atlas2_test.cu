126effef51f4f3ed484fd046f4bc8c521a1201cd
jnke2016 <jnke2016@gmail.com>
2025-04-30 08:52:44 -0700

    fix copyright

--- cpp/tests/layout/legacy/force_atlas2_test.cu
+++ cpp/tests/layout/legacy/force_atlas2_test.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2020-2025, NVIDIA CORPORATION.  All rights reserved.
  *
  * NVIDIA CORPORATION and its licensors retain all intellectual property
  * and proprietary rights in and to this software, related documentation
