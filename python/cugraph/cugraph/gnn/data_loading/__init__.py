fc11da2bfa8c9be2f172203ae9e811f06cc9ea93
Alexandria Barghi <abarghi@nvidia.com>
2025-05-08 23:39:03 -0700

    remove bulk sampler import

--- python/cugraph/cugraph/gnn/data_loading/__init__.py
+++ python/cugraph/cugraph/gnn/data_loading/__init__.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from cugraph.gnn.data_loading.bulk_sampler import BulkSampler
 from cugraph.gnn.data_loading.dist_sampler import (
     NeighborSampler,
 )
