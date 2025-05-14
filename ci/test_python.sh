e7274f464271cc191702325c1b01afc470596703
Bradley Dice <bdice@bradleydice.com>
2025-05-14 09:10:39 -0500

    Use pytest-xdist for parallel testing (#5027)
    We use `pytest-xdist` in almost every RAPIDS repository but haven't enabled it for cuGraph yet. Test times are long, so we should try to parallelize Python tests.
    
    The [last successful CI run](https://github.com/rapidsai/cugraph/actions/runs/14391805813/job/40361124281#step:9:8233) took a long time:
    ```
    3427 passed, 905 skipped, 51 deselected, 833 warnings in 5767.53s (1:36:07)
    ```
    
    I successfully ran with `--numprocesses=8 --dist=worksteal` on my local machine:
    ```
    3456 passed, 909 skipped, 1186 warnings in 881.19s (0:14:41)
    ```
    and [CI got similar results](https://github.com/rapidsai/cugraph/actions/runs/14409723669/job/40415873062?pr=5027#step:9:13339):
    ```
    3427 passed, 905 skipped, 887 warnings in 901.83s (0:15:01)
    ```
    
    This suggests that there isn't too much time lost in the overhead of parallelizing the test jobs -- it's about 6.4x faster overall.
    
    Our lowest-memory CI node has 24 GB of GPU memory. There are some tests that appear to spawn multiple processes (probably dask or MG tests) and the highest overall GPU memory usage I saw was ~18 GB -- still safely below the cap of 24 GB.
    
    The maximum memory usage I saw was ~1830 MB for any given worker. I saw 3 workers at ~1830 MB and 5 workers at ~450-600 MB for most of the test time. The memory usage seemed to be monotonically increasing for each worker through the test run, which makes me think that resources (perhaps large graphs from datasets?) are not being cleaned up after they are used for a test.
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Rick Ratzel (https://github.com/rlratzel)
      - https://github.com/jakirkham
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/cugraph/pull/5027

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -42,6 +42,9 @@ export LD_PRELOAD="${CONDA_PREFIX}/lib/libgomp.so.1"
 # RAPIDS_DATASET_ROOT_DIR is used by test scripts
 RAPIDS_DATASET_ROOT_DIR="$(realpath datasets)"
 export RAPIDS_DATASET_ROOT_DIR
+pushd "${RAPIDS_DATASET_ROOT_DIR}"
+./get_test_data.sh --subset
+popd
 
 EXITCODE=0
 trap "EXITCODE=1" ERR
@@ -51,6 +54,8 @@ rapids-logger "pytest pylibcugraph"
 ./ci/run_pylibcugraph_pytests.sh \
   --verbose \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-pylibcugraph.xml" \
+  --numprocesses=8 \
+  --dist=worksteal \
   --cov-config=../../.coveragerc \
   --cov=pylibcugraph \
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/pylibcugraph-coverage.xml" \
@@ -67,10 +72,39 @@ rapids-logger "pytest pylibcugraph"
 #
 # FIXME: TEMPORARILY disable MG PropertyGraph tests (experimental) tests and
 # bulk sampler IO tests (hangs in CI)
-rapids-logger "pytest cugraph"
+rapids-logger "pytest cugraph (not mg, with xdist)"
 ./ci/run_cugraph_pytests.sh \
   --verbose \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cugraph.xml" \
+  --numprocesses=8 \
+  --dist=worksteal \
+  -m "not mg" \
+  -k "not test_dataset and not test_bulk_sampler and not test_create_undirected_graph_from_asymmetric_adj_list and not test_uniform_neighbor_sample" \
+  --cov-config=../../.coveragerc \
+  --cov=cugraph \
+  --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cugraph-coverage.xml" \
+  --cov-report=term
+
+# Some tests fail with pytest-xdist enabled.
+# See https://github.com/rapidsai/cugraph/issues/5048
+rapids-logger "pytest cugraph (not mg, without xdist)"
+./ci/run_cugraph_pytests.sh \
+  --verbose \
+  --junitxml="${RAPIDS_TESTS_DIR}/junit-cugraph.xml" \
+  --numprocesses=1 \
+  --dist=worksteal \
+  -m "not mg" \
+  -k "not test_dataset and (test_bulk_sampler or test_create_undirected_graph_from_asymmetric_adj_list or test_uniform_neighbor_sample)" \
+  --cov-config=../../.coveragerc \
+  --cov=cugraph \
+  --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cugraph-coverage.xml" \
+  --cov-report=term
+
+rapids-logger "pytest cugraph (mg)"
+./ci/run_cugraph_pytests.sh \
+  --verbose \
+  --junitxml="${RAPIDS_TESTS_DIR}/junit-cugraph.xml" \
+  -m "mg" \
   --cov-config=../../.coveragerc \
   --cov=cugraph \
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cugraph-coverage.xml" \
