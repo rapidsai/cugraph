34089b0fd6366b5ac7884a3559f9a831127ed122
jnke2016 <jnke2016@gmail.com>
2025-06-13 11:36:47 -0700

    remove deprecation warning

--- python/cugraph/cugraph/structure/symmetrize.py
+++ python/cugraph/cugraph/structure/symmetrize.py
@@ -15,7 +15,6 @@ from cugraph.structure import graph_classes as csg
 import cudf
 import dask_cudf
 from dask.distributed import default_client
-import warnings
 
 
 def symmetrize_df(
@@ -55,13 +54,7 @@ def symmetrize_df(
         Name of the column in the data frame containing the weight ids
 
     multi : bool, optional (default=False)
-        [Deprecated,  Multi will be removed in future version,  and the removal
-        of multi edges will no longer be supported from 'symmetrize'.
-        Multi edges will be removed upon  creation of graph instance directly
-        based on if the graph is `curgaph.MultiGraph` or `cugraph.Graph`.]
-
-        Set to True if graph is a Multi(Di)Graph. This allows multiple
-        edges instead of dropping them.
+        Multi edges will be dropped if set to True.
 
     symmetrize : bool, optional (default=True)
         Default is True to perform symmetrization. If False only duplicate
@@ -133,13 +126,7 @@ def symmetrize_ddf(
         Name of the column in the data frame containing the weight ids
 
     multi : bool, optional (default=False)
-        [Deprecated,  Multi will be removed in future version,  and the removal
-        of multi edges will no longer be supported from 'symmetrize'.
-        Multi edges will be removed upon  creation of graph instance directly
-        based on if the graph is `curgaph.MultiGraph` or `cugraph.Graph`.]
-
-        Set to True if graph is a Multi(Di)Graph. This allows multiple
-        edges instead of dropping them.
+        Multi edges will be dropped if set to True.
 
     symmetrize : bool, optional (default=True)
         Default is True to perform symmetrization. If False only duplicate
@@ -178,12 +165,6 @@ def symmetrize_ddf(
         result = result.reset_index(drop=True).repartition(npartitions=len(workers) * 2)
         return result
     else:
-        warnings.warn(
-            "Multi is deprecated and the removal of multi edges will no longer be "
-            "supported from 'symmetrize'. Multi edges will be removed upon creation "
-            "of graph instance.",
-            FutureWarning,
-        )
         vertex_col_name = src_name + dst_name
         result = _memory_efficient_drop_duplicates(
             result, vertex_col_name, len(workers)
@@ -226,13 +207,7 @@ def symmetrize(
         weights column name.
 
     multi : bool, optional (default=False)
-        [Deprecated,  Multi will be removed in future version,  and the removal
-        of multi edges will no longer be supported from 'symmetrize'.
-        Multi edges will be removed upon  creation of graph instance directly
-        based on if the graph is `curgaph.MultiGraph` or `cugraph.Graph`.]
-
-        Set to True if graph is a Multi(Di)Graph. This allows multiple
-        edges instead of dropping them.
+        Multi edges will be dropped if set to True.
 
     symmetrize : bool, optional
         Default is True to perform symmetrization. If False only duplicate
