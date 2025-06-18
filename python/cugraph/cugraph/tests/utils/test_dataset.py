63cca2cae1951adebc6621d4664bf71d242e1150
Charles Hastings <charlesh@nvidia.com>
2025-06-18 14:19:33 -0700

    remove some changes that were in Ralph's debugging but not in his final PR

--- python/cugraph/cugraph/tests/utils/test_dataset.py
+++ python/cugraph/cugraph/tests/utils/test_dataset.py
@@ -11,9 +11,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import gc
 import importlib
-import os
 from pathlib import Path
 
 import pandas
