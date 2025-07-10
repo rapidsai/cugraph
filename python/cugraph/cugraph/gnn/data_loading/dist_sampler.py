0dbb12c8e4e69399a238958a26919ec5e2fc938d
Alexandria Barghi <abarghi@nvidia.com>
2025-07-10 15:41:59 -0700

    switch k to v :

--- python/cugraph/cugraph/gnn/data_loading/dist_sampler.py
+++ python/cugraph/cugraph/gnn/data_loading/dist_sampler.py
@@ -203,7 +203,7 @@ class DistSampler:
             minibatch_dict["map"] = minibatch_dict["renumber_map"]
             del minibatch_dict["renumber_map"]
             minibatch_dict = {
-                k: k if isinstance(v, str) else torch.as_tensor(v, device="cuda")
+                k: v if isinstance(v, str) else torch.as_tensor(v, device="cuda")
                 for k, v in minibatch_dict.items()
                 if v is not None
             }
@@ -499,7 +499,7 @@ class DistSampler:
             minibatch_dict["map"] = minibatch_dict["renumber_map"]
             del minibatch_dict["renumber_map"]
             minibatch_dict = {
-                k: k if isinstance(v, str) else torch.as_tensor(v, device="cuda")
+                k: v if isinstance(v, str) else torch.as_tensor(v, device="cuda")
                 for k, v in minibatch_dict.items()
                 if v is not None
             }
