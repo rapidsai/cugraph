011ab985dd1441085cd0458e0a9cd1850d983158
Alexandria Barghi <abarghi@nvidia.com>
2025-07-21 12:57:39 -0700

    properly instantiate missing function, handle ModuleNotFoundError

--- python/pylibcugraph/pylibcugraph/comms/cugraph_nccl_comms.py
+++ python/pylibcugraph/pylibcugraph/comms/cugraph_nccl_comms.py
@@ -21,20 +21,16 @@ from pylibcugraph.comms import init_subcomms
 try:
     from raft_dask.common.nccl import nccl
     from raft_dask.common.comms_utils import inject_comms_on_handle_coll_only
-except ImportError as err:
-    # Generalize since err.name is arr when
-    # libnuma.so.1 is not available
-    if err.name == "ucp" or err.name == "arr":
+except (ImportError, ModuleNotFoundError):
 
-        class MissingUCXPy:
-            def __call__(self, *args, **kwargs):
-                raise ModuleNotFoundError(
-                    "ucx-py could not be imported but is required for MG operations"
-                )
+    class MissingUCXPy:
+        def __call__(self, *args, **kwargs):
+            raise ModuleNotFoundError(
+                "ucx-py could not be imported but is required for MG operations"
+            )
 
-        raise MissingUCXPy()
-    else:
-        raise err
+    nccl = MissingUCXPy()
+    inject_comms_on_handle_coll_only = MissingUCXPy()
 
 __nccl_comms = None
 __raft_handle = None
