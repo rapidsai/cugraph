0b28bd4b730961add1c87ffea310171e23ce7bab
Seunghwa Kang <seunghwak@nvidia.com>
2025-08-15 00:17:16 -0700

    remove unused functions (to cut binary size)

--- cpp/src/converters/legacy/COOtoCSR.cu
+++ cpp/src/converters/legacy/COOtoCSR.cu
@@ -50,36 +50,4 @@ template std::unique_ptr<legacy::GraphCSR<int64_t, int64_t, double>>
 coo_to_csr<int64_t, int64_t, double>(legacy::GraphCOOView<int64_t, int64_t, double> const& graph,
                                      rmm::device_async_resource_ref);
 
-// in-place versions:
-//
-// Explicit instantiation for uint32_t + float
-template void coo_to_csr_inplace<uint32_t, uint32_t, float>(
-  legacy::GraphCOOView<uint32_t, uint32_t, float>& graph,
-  legacy::GraphCSRView<uint32_t, uint32_t, float>& result);
-
-// Explicit instantiation for uint32_t + double
-template void coo_to_csr_inplace<uint32_t, uint32_t, double>(
-  legacy::GraphCOOView<uint32_t, uint32_t, double>& graph,
-  legacy::GraphCSRView<uint32_t, uint32_t, double>& result);
-
-// Explicit instantiation for int + float
-template void coo_to_csr_inplace<int32_t, int32_t, float>(
-  legacy::GraphCOOView<int32_t, int32_t, float>& graph,
-  legacy::GraphCSRView<int32_t, int32_t, float>& result);
-
-// Explicit instantiation for int + double
-template void coo_to_csr_inplace<int32_t, int32_t, double>(
-  legacy::GraphCOOView<int32_t, int32_t, double>& graph,
-  legacy::GraphCSRView<int32_t, int32_t, double>& result);
-
-// Explicit instantiation for int64_t + float
-template void coo_to_csr_inplace<int64_t, int64_t, float>(
-  legacy::GraphCOOView<int64_t, int64_t, float>& graph,
-  legacy::GraphCSRView<int64_t, int64_t, float>& result);
-
-// Explicit instantiation for int64_t + double
-template void coo_to_csr_inplace<int64_t, int64_t, double>(
-  legacy::GraphCOOView<int64_t, int64_t, double>& graph,
-  legacy::GraphCSRView<int64_t, int64_t, double>& result);
-
 }  // namespace cugraph
