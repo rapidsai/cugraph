3bde56e23589c074341e064613f4ab41f4daf12d
Ralph Liu <137829296+nv-rliu@users.noreply.github.com>
2025-09-22 16:15:02 -0400

    Skip failing cugraph-service tests for 25.10 CI (#5265)
    This PR temporarily skips the failing `cugraph-service` tests blocking other parts of CI while it is being resolved
    
    Authors:
      - Ralph Liu (https://github.com/nv-rliu)
    
    Approvers:
      - Brad Rees (https://github.com/BradReesWork)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cugraph/pull/5265

--- ci/run_cugraph_service_pytests.sh
+++ ci/run_cugraph_service_pytests.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
 set -euo pipefail
 
 # Support invoking run_cugraph_service_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/cugraph-service
 
-pytest --capture=no --cache-clear --benchmark-disable -k "not mg" "$@" tests
+pytest --capture=no --cache-clear --benchmark-disable \
+    -k "not mg and not test_renumber_vertices_by_type and not test_renumber_edges_by_type" \
+    "$@" tests
