08a2d7139341df059657dcf2748434001906bcc4
Kyle Edwards <kyedwards@nvidia.com>
2025-10-21 14:10:02 -0400

    Use SPDX for all copyright headers
    Issue: https://github.com/rapidsai/build-infra/issues/297

--- cpp/examples/build.sh
+++ cpp/examples/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# SPDX-License-Identifier: Apache-2.0
 
 # script for building libcugraph examples
 
