41cb4ab43378d462fc005480d04326570af05fa0
Alexandria Barghi <abarghi@nvidia.com>
2025-11-06 09:51:25 -0800

    add deprecation warnings

--- python/cugraph/cugraph/gnn/data_loading/__init__.py
+++ python/cugraph/cugraph/gnn/data_loading/__init__.py
@@ -1,15 +1,34 @@
 # SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
+import warnings
+from warnings import FutureWarning
+
 from cugraph.gnn.data_loading.dist_sampler import (
-    NeighborSampler,
-    DistSampler,
+    DEPRECATED__NeighborSampler,
+    DEPRECATED__DistSampler,
 )
 from cugraph.gnn.data_loading.dist_io import (
     DistSampleWriter,
     DistSampleReader,
     BufferedSampleReader,
 )
+
+
+def DistSampler(*args, **kwargs):
+    warnings.warn(
+        FutureWarning,
+        "DistSampler is deprecated and will be removed in a future release.  Please migrate to the distributed sampling API in cuGraph-PyG.",
+    )
+    return DEPRECATED__DistSampler(*args, **kwargs)
+
+
+def NeighborSampler(*args, **kwargs):
+    warnings.warn(
+        FutureWarning,
+        "NeighborSampler is deprecated and will be removed in a future release.  Please migrate to the distributed sampling API in cuGraph-PyG.",
+    )
+    return DEPRECATED__NeighborSampler(*args, **kwargs)
 
 
 def UniformNeighborSampler(*args, **kwargs):
--- python/cugraph/cugraph/gnn/data_loading/dist_io/__init__.py
+++ python/cugraph/cugraph/gnn/data_loading/dist_io/__init__.py
@@ -1,6 +1,32 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2024-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
+import warnings
+from warnings import FutureWarning
 
-from .reader import BufferedSampleReader, DistSampleReader
-from .writer import DistSampleWriter
+from .reader import DEPRECATED__BufferedSampleReader, DEPRECATED__DistSampleReader
+from .writer import DEPRECATED__DistSampleWriter
+
+
+def BufferedSampleReader(*args, **kwargs):
+    warnings.warn(
+        FutureWarning,
+        "BufferedSampleReader is deprecated and will be removed in a future release.  Please migrate to the distributed sampling API in cuGraph-PyG.",
+    )
+    return DEPRECATED__BufferedSampleReader(*args, **kwargs)
+
+
+def DistSampleReader(*args, **kwargs):
+    warnings.warn(
+        FutureWarning,
+        "DistSampleReader is deprecated and will be removed in a future release.  Please migrate to the distributed sampling API in cuGraph-PyG.",
+    )
+    return DEPRECATED__DistSampleReader(*args, **kwargs)
+
+
+def DistSampleWriter(*args, **kwargs):
+    warnings.warn(
+        FutureWarning,
+        "DistSampleWriter is deprecated and will be removed in a future release.  Please migrate to the distributed sampling API in cuGraph-PyG.",
+    )
+    return DEPRECATED__DistSampleWriter(*args, **kwargs)
