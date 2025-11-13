ecd873efd4f5987f8eb8a3cdaf5898218b7b7839
jnke2016 <jnke2016@gmail.com>
2025-11-13 12:27:40 -0800

    Deprecate balanced cut clustering API and consolidate tests

--- python/cugraph/cugraph/tests/community/test_balanced_cut.py
+++ python/cugraph/cugraph/tests/community/test_balanced_cut.py
@@ -1,107 +1,20 @@
 # SPDX-FileCopyrightText: Copyright (c) 2019-2025, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
-import gc
-
-import random
 import pytest
 
-import cudf
 import cugraph
-from cugraph.testing import DEFAULT_DATASETS
-from cugraph.datasets import karate_asymmetric, karate
-
-
-def cugraph_call(G, partitions):
-    df = cugraph.spectralBalancedCutClustering(
-        G, partitions, num_eigen_vects=partitions
-    )
-
-    score = cugraph.analyzeClustering_edge_cut(G, partitions, df, "vertex", "cluster")
-    return set(df["vertex"].to_numpy()), score
-
-
-def random_call(G, partitions):
-    random.seed(0)
-    num_verts = G.number_of_vertices()
-
-    score = 0.0
-    for repeat in range(20):
-        assignment = []
-        for i in range(num_verts):
-            assignment.append(random.randint(0, partitions - 1))
-
-        assign_cu = cudf.DataFrame(assignment, columns=["cluster"])
-        assign_cu["vertex"] = assign_cu.index
-        assign_cu = assign_cu.astype("int32")
-
-        score += cugraph.analyzeClustering_edge_cut(G, partitions, assign_cu)
-
-    return set(range(num_verts)), (score / 10.0)
-
-
-PARTITIONS = [2, 4, 8]
-
-
-# Test all combinations of default/managed and pooled/non-pooled allocation
-
-
-@pytest.mark.sg
-@pytest.mark.parametrize("graph_file", DEFAULT_DATASETS)
-@pytest.mark.parametrize("partitions", PARTITIONS)
-def test_edge_cut_clustering(graph_file, partitions):
-    gc.collect()
-
-    # read_weights_in_sp=True => value column dtype is float32
-    G_edge = graph_file.get_graph(ignore_weights=True)
-
-    # Get the edge_cut score for partitioning versus random assignment
-    cu_vid, cu_score = cugraph_call(G_edge, partitions)
-    rand_vid, rand_score = random_call(G_edge, partitions)
-
-    # Assert that the partitioning has better edge_cut than the random
-    # assignment
-    dataset_name = graph_file.metadata["name"]
-    print("graph_file = ", dataset_name, ", partitions = ", partitions)
-    print(cu_score, rand_score)
-    assert cu_score < rand_score
-
-
-@pytest.mark.sg
-@pytest.mark.parametrize("graph_file", DEFAULT_DATASETS)
-@pytest.mark.parametrize("partitions", PARTITIONS)
-def test_edge_cut_clustering_with_edgevals(graph_file, partitions):
-    gc.collect()
-
-    G_edge = graph_file.get_graph()
-
-    # read_weights_in_sp=False => value column dtype is float64
-    G_edge.edgelist.edgelist_df["weights"] = G_edge.edgelist.edgelist_df[
-        "weights"
-    ].astype("float64")
-
-    # Get the edge_cut score for partitioning versus random assignment
-    cu_vid, cu_score = cugraph_call(G_edge, partitions)
-    rand_vid, rand_score = random_call(G_edge, partitions)
-
-    # Assert that the partitioning has better edge_cut than the random
-    # assignment
-    print(cu_score, rand_score)
-    assert cu_score < rand_score
-
-
-@pytest.mark.sg
-@pytest.mark.parametrize("partitions", PARTITIONS)
-def test_edge_cut_clustering_directed_graph(partitions):
-    G_edge = karate_asymmetric.get_graph(
-        create_using=cugraph.Graph(directed=True), ignore_weights=True
-    )
-    with pytest.raises(ValueError):
-        cugraph_call(G_edge, partitions)
+from cugraph.datasets import karate
 
 
 @pytest.mark.sg
 def test_spectral_balanced_cut_clustering_deprecation_warning():
+    """Test that spectralBalancedCutClustering emits a deprecation warning.
+    
+    Note: spectralBalancedCutClustering is deprecated in favor of
+    spectralModularityMaximizationClustering. Functional tests for spectral
+    clustering (including edge cut validation) are in test_modularity.py.
+    """
     G = karate.get_graph(
         create_using=cugraph.Graph(directed=False), ignore_weights=True
     )
