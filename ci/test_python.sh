e8e5906911d320397d7e83885cbf824ad5d88d50
Rick Ratzel <3039903+rlratzel@users.noreply.github.com>
2025-12-17 17:25:54 -0600

    Removes experimental `PropertyGraph` (SG and MG) and all references (#5374)
    Removes experimental `PropertyGraph` (SG and MG) and all references (docs, tests, notebooks, etc.).
    
    Note: we are treating this as a *non-breaking* change because of the experimental status of `PropertyGraph`.
    
    Authors:
      - Rick Ratzel (https://github.com/rlratzel)
    
    Approvers:
      - Don Acosta (https://github.com/acostadon)
      - Bradley Dice (https://github.com/bdice)
      - Alex Barghi (https://github.com/alexbarghi-nv)
    
    URL: https://github.com/rapidsai/cugraph/pull/5374

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -71,8 +71,7 @@ rapids-logger "pytest pylibcugraph"
 # the install location, and in most cases, the source tree does not have
 # extensions built in-place and will result in ImportErrors.
 #
-# FIXME: TEMPORARILY disable MG PropertyGraph tests (experimental) tests and
-# bulk sampler IO tests (hangs in CI)
+# FIXME: TEMPORARILY disable bulk sampler IO tests (hangs in CI)
 rapids-logger "pytest cugraph (not mg, with xdist)"
 ./ci/run_cugraph_pytests.sh \
   --verbose \
@@ -80,7 +79,7 @@ rapids-logger "pytest cugraph (not mg, with xdist)"
   --numprocesses=8 \
   --dist=worksteal \
   -m "not mg" \
-  -k "not test_bulk_sampler and not test_create_undirected_graph_from_asymmetric_adj_list and not test_node2vec and not(test_property_graph.py and cyber)" \
+  -k "not test_bulk_sampler and not test_create_undirected_graph_from_asymmetric_adj_list and not test_node2vec" \
   --cov-config=../../.coveragerc \
   --cov=cugraph \
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cugraph-coverage.xml" \
@@ -94,7 +93,7 @@ rapids-logger "pytest cugraph (mg, with xdist)"
   --numprocesses=8 \
   --dist=worksteal \
   -m "mg" \
-  -k "not test_property_graph_mg and not test_dist_sampler_mg" \
+  -k "not test_dist_sampler_mg" \
   --cov-config=../../.coveragerc \
   --cov=cugraph \
   --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cugraph-coverage.xml" \
