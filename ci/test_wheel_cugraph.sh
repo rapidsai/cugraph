9bf6c129439d7be540f724703494fea532d90ae7
James Lamb <jlamb@nvidia.com>
2026-03-05 16:45:46 -0600

    generate constraints

--- ci/test_wheel_cugraph.sh
+++ ci/test_wheel_cugraph.sh
@@ -13,11 +13,13 @@ PYLIBCUGRAPH_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name "wh
 CUGRAPH_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name "wheel_python" cugraph --stable --cuda "$RAPIDS_CUDA_VERSION")")
 LIBCUGRAPH_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libcugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
 
-PIP_INSTALL_ARGS=()
+# generate constraints (possibly pinning to oldest support versions of dependencies)
+rapids-generate-pip-constraints test_python "${PIP_CONSTRAINT}"
 
 # Update this when 'torch' publishes CUDA wheels supporting newer CTKs.
 #
 # See notes in 'dependencies.yaml' for details on supported versions.
+PIP_INSTALL_ARGS=()
 if \
     { [ "${CUDA_MAJOR}" -eq 12 ] && [ "${CUDA_MINOR}" -ge 9 ]; } \
     || { [ "${CUDA_MAJOR}" -eq 13 ] && [ "${CUDA_MINOR}" -le 0 ]; }; \
@@ -29,7 +31,12 @@ then
     PIP_INSTALL_ARGS+=("${TORCH_WHEEL_DIR}"/torch*.whl)
 fi
 
-# echo to expand wildcard before adding `[extra]` requires for pip
+# notes:
+#
+#   * echo to expand wildcard before adding `[test]` requires for pip
+#   * just providing --constraint="${PIP_CONSTRAINT}" to be explicit, and because
+#     that environment variable is ignored if any other --constraint are passed via the CLI
+#
 rapids-pip-retry install \
     --prefer-binary \
     "$(echo "${CUGRAPH_WHEELHOUSE}"/cugraph*.whl)[test]" \
