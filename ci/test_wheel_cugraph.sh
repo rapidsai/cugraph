61c2b73514681e66b324a44baaa63250052a6a40
James Lamb <jaylamb20@gmail.com>
2026-03-13 16:35:16 -0500

    ensure 'torch' CUDA wheels are installed in CI, remove unused dependencies (#5453)
    Contributes to https://github.com/rapidsai/build-planning/issues/257
    
    * ensures that when `torch` is installed in wheels CI, it's always a CUDA variant
    * removes unused test dependencies `ogb`, `pydantic`, `torchdata`, and `torchmetrics`
      - *I suspect these are left over from before GNN packages moved out to https://github.com/rapidsai/cugraph-gnn*
    
    ## Notes for Reviewers
    
    ### How does this help with testing against a mix of CTKs?
    
    `torch` CUDA wheels tightly pin to `nvidia-{thing}` wheels and soon, `cuda-toolkit` (see https://github.com/rapidsai/build-planning/issues/255).
    
    Forcing the use of CUDA `torch` wheels ensures we'll catch dependency conflicts in CI. Without this, `pip` could silently fall back to CPU-only `torch` from pypi.org.
    
    ### How I tested this
    
    This PR uses some patterns I've tested elsewhere too:
    
    * https://github.com/rapidsai/rmm/pull/2270#discussion_r2886508712
    * https://github.com/rapidsai/cugraph-gnn/pull/425
    
    I also tested a commit here with both the PR and nightly test matrices, to be sure we covered everything: https://github.com/rapidsai/cugraph/actions/runs/23062364778/job/66996357196?pr=5453
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Alex Barghi (https://github.com/alexbarghi-nv)
      - Gil Forsyth (https://github.com/gforsyth)
    
    URL: https://github.com/rapidsai/cugraph/pull/5453

--- ci/test_wheel_cugraph.sh
+++ ci/test_wheel_cugraph.sh
@@ -13,10 +13,54 @@ PYLIBCUGRAPH_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name "wh
 CUGRAPH_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name "wheel_python" cugraph --stable --cuda "$RAPIDS_CUDA_VERSION")")
 LIBCUGRAPH_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libcugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp)
 
-# echo to expand wildcard before adding `[extra]` requires for pip
+# generate constraints (possibly pinning to oldest support versions of dependencies)
+rapids-generate-pip-constraints test_python "${PIP_CONSTRAINT}"
+
+# Update this when 'torch' publishes CUDA wheels supporting newer CTKs.
+#
+# See notes in 'dependencies.yaml' for details on supported versions.
+CUDA_MAJOR=$(echo "${RAPIDS_CUDA_VERSION}" | cut -d'.' -f1)
+CUDA_MINOR=$(echo "${RAPIDS_CUDA_VERSION}" | cut -d'.' -f2)
+PIP_INSTALL_ARGS=()
+torch_downloaded=false
+if \
+    { [ "${CUDA_MAJOR}" -eq 12 ] && [ "${CUDA_MINOR}" -eq 9 ]; } \
+    || { [ "${CUDA_MAJOR}" -eq 13 ] && [ "${CUDA_MINOR}" -eq 0 ]; }; \
+then
+    # ensure a CUDA variant of 'torch' is used
+    rapids-logger "Downloading PyTorch CUDA wheels"
+    TORCH_WHEEL_DIR="$(mktemp -d)"
+    ./ci/download-torch-wheels.sh "${TORCH_WHEEL_DIR}"
+    PIP_INSTALL_ARGS+=("${TORCH_WHEEL_DIR}"/torch*.whl)
+    torch_downloaded=true
+fi
+
+# notes:
+#
+#   * echo to expand wildcard before adding `[test]` requires for pip
+#   * just providing --constraint="${PIP_CONSTRAINT}" to be explicit, and because
+#     that environment variable is ignored if any other --constraint are passed via the CLI
+#
 rapids-pip-retry install \
+    --prefer-binary \
+    --constraint "${PIP_CONSTRAINT}" \
     "$(echo "${CUGRAPH_WHEELHOUSE}"/cugraph*.whl)[test]" \
     "${PYLIBCUGRAPH_WHEELHOUSE}"/pylibcugraph*.whl \
-    "${LIBCUGRAPH_WHEELHOUSE}"/libcugraph*.whl
+    "${LIBCUGRAPH_WHEELHOUSE}"/libcugraph*.whl \
+    "${PIP_INSTALL_ARGS[@]}"
+
+# TODO: remove this when RAPIDS wheels and 'torch' CUDA wheels have compatible package requirements
+#
+#    * https://github.com/rapidsai/cugraph/issues/5443
+#    * https://github.com/rapidsai/build-planning/issues/257
+#    * https://github.com/rapidsai/build-planning/issues/255
+#
+CUDA_MAJOR="${RAPIDS_CUDA_VERSION%%.*}"
+CUDA_MINOR=$(echo "${RAPIDS_CUDA_VERSION}" | cut -d'.' -f2)
+if [[ "${CUDA_MAJOR}" == "13" ]] && [[ "${torch_downloaded}" == "true" ]]; then
+    pip install \
+        --upgrade \
+        "nvidia-nvjitlink>=${CUDA_MAJOR}.${CUDA_MINOR}"
+fi
 
 ./ci/test_wheel.sh cugraph
