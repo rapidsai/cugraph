69c1223db1d04705ef3fd3b83a0da6744f5965ff
Seunghwa Kang <45857425+seunghwak@users.noreply.github.com>
2026-03-16 13:17:35 -0700

    Strongly Connected Components Part 1/2 (#5442)
    SCC implementation Part 1.
    
    Part 2 (trimming & set intersection) is provided in https://github.com/rapidsai/cugraph/pull/5448
    
    Authors:
      - Seunghwa Kang (https://github.com/seunghwak)
    
    Approvers:
      - Chuck Hastings (https://github.com/ChuckHastings)
    
    URL: https://github.com/rapidsai/cugraph/pull/5442

---
+++ cpp/src/components/strongly_connected_components_mg_v32_e32.cu
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "components/strongly_connected_components_impl.cuh"
+
+namespace cugraph {
+
+// MG instantiations
+
+template rmm::device_uvector<int32_t> strongly_connected_components(
+  raft::handle_t const& handle,
+  graph_view_t<int32_t, int32_t, false, true> const& graph_view,
+  bool do_expensive_check);
+
+}  // namespace cugraph
