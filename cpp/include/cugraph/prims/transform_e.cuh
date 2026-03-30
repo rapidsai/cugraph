c0332b5b7737264f4c7fd9960ed761b2463f67e8
Seunghwa Kang <seunghwak@nvidia.com>
2026-03-30 14:40:59 -0700

    update include statements

--- cpp/include/cugraph/prims/transform_e.cuh
+++ cpp/include/cugraph/prims/transform_e.cuh
@@ -4,13 +4,12 @@
  */
 #pragma once
 
-#include "detail/prim_utils.cuh"
-
 #include <cugraph/edge_partition_device_view.cuh>
 #include <cugraph/edge_partition_edge_property_device_view.cuh>
 #include <cugraph/edge_partition_endpoint_property_device_view.cuh>
 #include <cugraph/edge_src_dst_property.hpp>
 #include <cugraph/graph_view.hpp>
+#include <cugraph/prims/detail/prim_utils.cuh>
 #include <cugraph/utilities/error.hpp>
 #include <cugraph/utilities/mask_utils.cuh>
 #include <cugraph/utilities/packed_bool_utils.hpp>
