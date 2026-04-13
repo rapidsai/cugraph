0b1a7d721b6a63fd280e83268eb72116bb7442ea
BradReesWork <brees@nvidia.com>
2026-04-13 15:20:50 -0400

    Issues 5397 - drooped batched_ego_graph

--- python/cugraph/cugraph/tests/sampling/test_egonet_mg.py
+++ python/cugraph/cugraph/tests/sampling/test_egonet_mg.py
@@ -5,6 +5,7 @@ import gc
 
 import pytest
 
+import cudf
 import cugraph
 import dask_cudf
 import cugraph.dask as dcg
@@ -12,7 +13,8 @@ from cugraph.testing import utils
 from cugraph.dask.common.mg_utils import is_single_gpu
 from pylibcugraph.testing import gen_fixture_params_product
 from cudf.testing.testing import assert_frame_equal, assert_series_equal
-
+from pylibcugraph import ego_graph as pylibcugraph_ego_graph
+from pylibcugraph import ResourceHandle
 
 # =============================================================================
 # Pytest Setup / Teardown - called for each test function
@@ -70,13 +72,52 @@ def input_expected_output(input_combo):
         input_data_path, directed=directed, edgevals=True
     )
 
-    sg_cugraph_ego_graphs = cugraph.batched_ego_graphs(G, seeds=seeds, radius=radius)
+    if isinstance(seeds, (int, list)):
+        seeds = cudf.Series(seeds)
+    if isinstance(seeds, cudf.Series):
+        if G.renumbered is True:
+            seeds = G.lookup_internal_vertex_id(seeds)
+    elif isinstance(seeds, cudf.DataFrame):
+        if G.renumbered is True:
+            seeds = G.lookup_internal_vertex_id(seeds, seeds.columns)
+
+
+    # Match the seed to the vertex dtype
+    n_type = G.edgelist.edgelist_df["src"].dtype
+    n = seeds.astype(n_type)
+    do_expensive_check = False
+
+    source, destination, weight, offset = pylibcugraph_ego_graph(
+        resource_handle=ResourceHandle(),
+        graph=G._plc_graph,
+        source_vertices=n,
+        radius=radius,
+        do_expensive_check=do_expensive_check,
+    )
+
+    df = cudf.DataFrame()
+    df["src"] = source
+    df["dst"] = destination
+    if weight is not None:
+        df["weight"] = weight
+
+    if G.renumbered:
+        df, src_names = G.unrenumber(df, "src", get_column_names=True)
+        df, dst_names = G.unrenumber(df, "dst", get_column_names=True)
+    else:
+        # FIXME: The original 'src' and 'dst' are not stored in 'simpleGraph'
+        src_names = "src"
+        dst_names = "dst"
+
+    offset = cudf.Series(offset)
+    sg_cugraph_ego_graphs = (df, offset)
 
     # Save the results back to the input_combo dictionary to prevent redundant
     # cuGraph runs. Other tests using the input_combo fixture will look for
     # them, and if not present they will have to re-run the same cuGraph call.
-
     input_combo["sg_cugraph_results"] = sg_cugraph_ego_graphs
+
+
     chunksize = dcg.get_chunksize(input_data_path)
     ddf = dask_cudf.read_csv(
         input_data_path,
