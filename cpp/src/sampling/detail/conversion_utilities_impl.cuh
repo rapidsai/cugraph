073ae8728d36fe4325a071a7aeab193ce4b571e8
Bradley Dice <bdice@bradleydice.com>
2026-04-29 16:07:28 -0500

    Split conversion utility instantiation from implementation
    Move the conversion utility template implementation into an impl header and
    leave conversion_utilities.cu as the explicit-instantiation owner.

---
+++ cpp/src/sampling/detail/conversion_utilities_impl.cuh
@@ -0,0 +1,48 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "sampling/detail/sampling_utils.hpp"
+
+#include <cugraph/utilities/misc_utils.cuh>
+
+#include <cuda/functional>
+
+namespace cugraph {
+namespace detail {
+
+rmm::device_uvector<int32_t> convert_starting_vertex_label_offsets_to_labels(
+  raft::handle_t const& handle, raft::device_span<size_t const> starting_vertex_label_offsets)
+{
+  return expand_sparse_offsets(starting_vertex_label_offsets, int32_t{0}, handle.get_stream());
+}
+
+template <typename label_t>
+rmm::device_uvector<int32_t> flatten_label_map(
+  raft::handle_t const& handle,
+  std::tuple<raft::device_span<label_t const>, raft::device_span<int32_t const>>
+    label_to_output_comm_rank)
+{
+  label_t max_label = thrust::reduce(handle.get_thrust_policy(),
+                                     std::get<0>(label_to_output_comm_rank).begin(),
+                                     std::get<0>(label_to_output_comm_rank).end(),
+                                     label_t{0},
+                                     cuda::maximum<label_t>());
+
+  rmm::device_uvector<int32_t> label_map(max_label + 1, handle.get_stream());
+
+  thrust::fill(handle.get_thrust_policy(), label_map.begin(), label_map.end(), int32_t{0});
+  thrust::scatter(handle.get_thrust_policy(),
+                  std::get<1>(label_to_output_comm_rank).begin(),
+                  std::get<1>(label_to_output_comm_rank).end(),
+                  std::get<0>(label_to_output_comm_rank).begin(),
+                  label_map.begin());
+
+  return label_map;
+}
+
+}  // namespace detail
+}  // namespace cugraph
