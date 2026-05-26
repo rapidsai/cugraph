c617517c394b860f941bb71e2d9046a9c8cded04
Charles Hastings <charlesh@nvidia.com>
2026-05-25 20:10:45 -0700

    Missed a few CUGRAPH_EXPORT uses

--- cpp/src/structure/edge_partition_device_view_mg_v32_e32.cu
+++ cpp/src/structure/edge_partition_device_view_mg_v32_e32.cu
@@ -32,7 +32,7 @@ template CUGRAPH_EXPORT __host__ rmm::device_uvector<edge_t> compute_local_degre
   raft::device_span<edge_t const> offsets,
   rmm::cuda_stream_view stream);
 
-template __host__ void compute_number_of_edges_with_mask_async_mg<vertex_t, edge_t>(
+template CUGRAPH_EXPORT __host__ void compute_number_of_edges_with_mask_async_mg<vertex_t, edge_t>(
   cuda::std::optional<uint32_t const*> edge_mask,
   raft::device_span<vertex_t const> majors,
   raft::device_span<size_t> count,
@@ -42,7 +42,7 @@ template __host__ void compute_number_of_edges_with_mask_async_mg<vertex_t, edge
   raft::device_span<edge_t const> offsets,
   rmm::cuda_stream_view stream);
 
-template __host__ void compute_number_of_edges_with_mask_async_mg<vertex_t, edge_t>(
+template CUGRAPH_EXPORT __host__ void compute_number_of_edges_with_mask_async_mg<vertex_t, edge_t>(
   cuda::std::optional<uint32_t const*> edge_mask,
   std::tuple<vertex_t, vertex_t> local_vertex_partition_range,
   raft::device_span<size_t> count,
@@ -52,7 +52,7 @@ template __host__ void compute_number_of_edges_with_mask_async_mg<vertex_t, edge
   raft::device_span<edge_t const> offsets,
   rmm::cuda_stream_view stream);
 
-template __host__ void
+template CUGRAPH_EXPORT __host__ void
 compute_number_of_edges_with_mask_async_mg_with_local_major_offsets<vertex_t, edge_t>(
   cuda::std::optional<uint32_t const*> edge_mask,
   raft::device_span<uint32_t const> local_major_offsets,
