b4c30d8e2bd91ab3d947581a22795501aa1663d4
James Lamb <jlamb@nvidia.com>
2026-06-04 22:16:25 -0500

    comment everything else out

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -27,90 +27,88 @@ conda install \
   'dask-cudf=26.6.*'
 echo "---"
 
-exit 1
+# rapids-print-env
 
-rapids-print-env
+# conda install -c rapidsai -c conda-forge 'dask-cudf=26.6.*'
 
-conda install -c rapidsai -c conda-forge 'dask-cudf=26.6.*'
+# #CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
+# CUGRAPH_COMMIT=bbe88597298aba868fe7d2afc96c5b367590ab4a
+# CPP_CHANNEL=$(
+#     RAPIDS_BUILD_WORKFLOW_NAME=build.yaml \
+#     rapids-get-pr-artifact cugraph 5542 cpp conda "${CUGRAPH_COMMIT}"
+# )
 
-#CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
-CUGRAPH_COMMIT=bbe88597298aba868fe7d2afc96c5b367590ab4a
-CPP_CHANNEL=$(
-    RAPIDS_BUILD_WORKFLOW_NAME=build.yaml \
-    rapids-get-pr-artifact cugraph 5542 cpp conda "${CUGRAPH_COMMIT}"
-)
+# echo "--- CPP channel (${CPP_CHANNEL}) contents ---"
+# find "${CPP_CHANNEL}" -type f -name '*'
+# echo ""
 
-echo "--- CPP channel (${CPP_CHANNEL}) contents ---"
-find "${CPP_CHANNEL}" -type f -name '*'
-echo ""
+# RAPIDS_PACKAGE_VERSION="26.06.00"
+# export RAPIDS_PACKAGE_VERSION
 
-RAPIDS_PACKAGE_VERSION="26.06.00"
-export RAPIDS_PACKAGE_VERSION
+# # populates `RATTLER_CHANNELS` array and `RATTLER_ARGS` array
+# source rapids-rattler-channel-string
 
-# populates `RATTLER_CHANNELS` array and `RATTLER_ARGS` array
-source rapids-rattler-channel-string
+# # override RATTLER_ARGS (use flexible channel priority)
+# RATTLER_ARGS=(
+#     "--experimental"
+#     "--no-build-id"
+#     "--channel-priority" "disabled"
+#     "--output-dir" "$RAPIDS_CONDA_BLD_OUTPUT_DIR"
+# )
 
-# override RATTLER_ARGS (use flexible channel priority)
-RATTLER_ARGS=(
-    "--experimental"
-    "--no-build-id"
-    "--channel-priority" "disabled"
-    "--output-dir" "$RAPIDS_CONDA_BLD_OUTPUT_DIR"
-)
+# rapids-logger "Prepending channel ${CPP_CHANNEL} to RATTLER_CHANNELS"
 
-rapids-logger "Prepending channel ${CPP_CHANNEL} to RATTLER_CHANNELS"
+# RATTLER_CHANNELS=("--channel" "${CPP_CHANNEL}" "${RATTLER_CHANNELS[@]}")
 
-RATTLER_CHANNELS=("--channel" "${CPP_CHANNEL}" "${RATTLER_CHANNELS[@]}")
+# sccache --stop-server 2>/dev/null || true
 
-sccache --stop-server 2>/dev/null || true
+# rapids-logger "Building pylibcugraph"
 
-rapids-logger "Building pylibcugraph"
+# # --no-build-id allows for caching with `sccache`
+# # more info is available at
+# # https://rattler.build/latest/tips_and_tricks/#using-sccache-or-ccache-with-rattler-build
+# rattler-build build -vvv --recipe conda/recipes/pylibcugraph \
+#                     "${RATTLER_ARGS[@]}" \
+#                     "${RATTLER_CHANNELS[@]}"
 
-# --no-build-id allows for caching with `sccache`
-# more info is available at
-# https://rattler.build/latest/tips_and_tricks/#using-sccache-or-ccache-with-rattler-build
-rattler-build build -vvv --recipe conda/recipes/pylibcugraph \
-                    "${RATTLER_ARGS[@]}" \
-                    "${RATTLER_CHANNELS[@]}"
+# sccache --show-adv-stats
+# sccache --stop-server >/dev/null 2>&1 || true
 
-sccache --show-adv-stats
-sccache --stop-server >/dev/null 2>&1 || true
+# rapids-logger "Building cugraph"
 
-rapids-logger "Building cugraph"
+# echo "--- rattler cache contents ---"
+# find ${HOME}/.cache/rattler -name '*'
+# echo "---"
 
-echo "--- rattler cache contents ---"
-find ${HOME}/.cache/rattler -name '*'
-echo "---"
+# echo "--- (before build) is there a local dask-cudf somewhere? ---"
+# find / -name '*dask-cudf*' || true
+# echo "---"
 
-echo "--- (before build) is there a local dask-cudf somewhere? ---"
-find / -name '*dask-cudf*' || true
-echo "---"
+# rattler-build build -vvv --recipe conda/recipes/cugraph \
+#                     "${RATTLER_ARGS[@]}" \
+#                     "${RATTLER_CHANNELS[@]}" || true
 
-rattler-build build -vvv --recipe conda/recipes/cugraph \
-                    "${RATTLER_ARGS[@]}" \
-                    "${RATTLER_CHANNELS[@]}" || true
+# echo "--- (after build) is there a local dask-cudf somewhere? ---"
+# find / -name '*dask-cudf*' || true
+# echo ""
 
-echo "--- (after build) is there a local dask-cudf somewhere? ---"
-find / -name '*dask-cudf*' || true
-echo ""
+# echo "--- trying to install dask-cudf directly ---"
+# conda install \
+#   -vvv \
+#   --yes \
+#   -c rapidsai \
+#   -c conda-forge \
+#   'dask-cudf=26.6.*'
+# echo "---"
 
-echo "--- trying to install dask-cudf directly ---"
-conda install \
-  -vvv \
-  --yes \
-  -c rapidsai \
-  -c conda-forge \
-  'dask-cudf=26.6.*'
-echo "---"
+# exit 1
 
-exit 1
+# sccache --show-adv-stats
+# sccache --stop-server >/dev/null 2>&1 || true
 
-sccache --show-adv-stats
-sccache --stop-server >/dev/null 2>&1 || true
+# # remove build_cache directory to avoid uploading the entire source tree
+# # tracked in https://github.com/prefix-dev/rattler-build/issues/1424
+# rm -rf "$RAPIDS_CONDA_BLD_OUTPUT_DIR"/build_cache
 
-# remove build_cache directory to avoid uploading the entire source tree
-# tracked in https://github.com/prefix-dev/rattler-build/issues/1424
-rm -rf "$RAPIDS_CONDA_BLD_OUTPUT_DIR"/build_cache
-
-RAPIDS_PACKAGE_NAME="$(rapids-package-name conda_python cugraph --stable --cuda)"
-export RAPIDS_PACKAGE_NAME
+# RAPIDS_PACKAGE_NAME="$(rapids-package-name conda_python cugraph --stable --cuda)"
+# export RAPIDS_PACKAGE_NAME
