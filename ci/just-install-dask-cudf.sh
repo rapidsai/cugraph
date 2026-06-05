c5eb38dd5cee3de202920f4cec0d5143f3380012
James Lamb <jlamb@nvidia.com>
2026-06-04 23:28:05 -0500

    print env, even more verbose

--- ci/just-install-dask-cudf.sh
+++ ci/just-install-dask-cudf.sh
@@ -8,16 +8,20 @@ set -euo pipefail
 unset CONDA_CHANNEL_ALIAS
 
 # try to match 'conda info' from a passing (local) run to see if that matters
-export CONDA_OVERRIDE_ARCHSPEC=aarch64
-export CONDA_OVERRIDE_LINUX="6.5.0=0"
+# export CONDA_OVERRIDE_ARCHSPEC=aarch64
+# export CONDA_OVERRIDE_LINUX="6.5.0=0"
 
 # clear the cache
 conda clean --yes --index-cache
 conda clean --yes --all
 
-echo "--- looking for ANY local repodata ---"
-find / -name '*repodata*' || true
+echo "--- env ---"
+env
 echo "---"
+
+# echo "--- looking for ANY local repodata ---"
+# find / -name '*repodata*' || true
+# echo "---"
 
 # check the volumes GitHub mounts in
 # -v "/home/runner/_work":"/__w"
@@ -28,15 +32,15 @@ echo "---"
 # -v "/home/runner/_work/_temp/_github_home":"/github/home"
 # -v "/home/runner/_work/_temp/_github_workflow":"/github/workflow"
 
-echo "--- checking HOME (${HOME}) ---"
-find "${HOME}" -name '*' || true
-echo ""
+# echo "--- checking HOME (${HOME}) ---"
+# find "${HOME}" -name '*' || true
+# echo ""
 
 echo "--- trying to install 'dask-cudf' ---"
 conda config --system --remove channels rapidsai-nightly
 conda info
 conda install \
-  -vv \
+  -vvv \
   --yes \
   -c rapidsai \
   -c conda-forge \
