fd36bdaf0e47ca72bf2901118ac939fda6f453d3
Chuck Hastings <45364586+ChuckHastings@users.noreply.github.com>
2026-07-13 16:30:16 -0400

    Apply suggestion from @ChuckHastings

--- python/utils/analyse_mtx_sparsity.py
+++ python/utils/analyse_mtx_sparsity.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2018-2022, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2018-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # Input:    <matrix.mtx>
