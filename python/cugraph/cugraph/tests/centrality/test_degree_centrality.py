b95b17b14534a749d7b1da8ba7c8f45db16cebf4
BradReesWork <brees@nvidia.com>
2026-09-15 13:14:56 -0400

    simple code tweak for testing

--- python/cugraph/cugraph/tests/centrality/test_degree_centrality.py
+++ python/cugraph/cugraph/tests/centrality/test_degree_centrality.py
@@ -29,8 +29,8 @@ def test_degree_centrality_multi_column(graph_file):
     dataset_path = graph_file.get_path()
     cu_M = utils.read_csv_file(dataset_path)
     cu_M.rename(columns={"0": "src_0", "1": "dst_0"}, inplace=True)
-    cu_M["src_1"] = cu_M["src_0"] + 1000
-    cu_M["dst_1"] = cu_M["dst_0"] + 1000
+    cu_M["src_1"] = cu_M["src_0"] + 100000
+    cu_M["dst_1"] = cu_M["dst_0"] + 100000
 
     G1 = cugraph.Graph(directed=True)
     G1.from_cudf_edgelist(
