c9767f2323ae033e3efd181bdd3a64cbfd41a51f
Charles Hastings <charlesh@nvidia.com>
2026-09-17 14:20:24 -0700

    add header so to deprecate the public header change rather than just changing it

---
+++ cpp/include/cugraph_c/labeling_algorithms.h
@@ -0,0 +1,15 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+/**
+ * @file labeling_algorithms.h
+ * @deprecated Include cugraph_c/components_algorithms.h instead. This header remains
+ *             for compatibility with releases that documented weakly/strongly connected
+ *             components under the labeling_algorithms name.
+ */
+
+#include <cugraph_c/components_algorithms.h>
